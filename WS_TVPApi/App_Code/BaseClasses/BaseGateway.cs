a811c00eacf77c1bd856c0770b56fe8fe7d5ce68
Oded Shafran <Oded@tvinci.com>
2011-09-06 11:05:12 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/Projects/TVPProAPIs@8993 542a500b-ef30-3f42-a8c6-a0c997676187

--- WS_TVPApi/App_Code/BaseClasses/BaseGateway.cs
+++ WS_TVPApi/App_Code/BaseClasses/BaseGateway.cs
@@ -105,7 +105,7 @@ public abstract class BaseGateway : System.Web.UI.Page
         string mac = Request.QueryString["identity"];
         if (groupId == 125)
         {
-            if (mac == "00043053C7C7")
+            if (mac != "00043050E79A")
                 return m_SiteService.SignIn(GetInitObj(), "adina@tvinci.com", "eliron27").SiteGuid;
             else
                 return m_SiteService.SignIn(GetInitObj(), "yonatan@tvinci.com", "eliron27").SiteGuid;
