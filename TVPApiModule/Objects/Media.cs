3e69b9a43bfcb62ad316bb0a8129af23d0830dca
Ohad Malinovsky <OhadM@tvinci.com>
2012-04-19 07:31:47 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/Projects/TVPProAPIs@9930 542a500b-ef30-3f42-a8c6-a0c997676187

--- TVPApiModule/Objects/Media.cs
+++ TVPApiModule/Objects/Media.cs
@@ -48,7 +48,7 @@ namespace TVPApi
         public string SubURL;
         public string GeoBlock;
         public long TotalItems;
-        public int like_counter;
+        public int? like_counter;
 
         public struct File
         {
@@ -204,10 +204,10 @@ namespace TVPApi
             if (!row.IsGeoBlockNull())
             {
                 GeoBlock = row.GeoBlock;
-            }
-            if (!String.IsNullOrEmpty(row.like_counter))
+            }            
+            if (!String.IsNullOrEmpty(row.Likes))
             {
-                like_counter = Convert.ToInt32(row.like_counter);
+                like_counter = Convert.ToInt32(row.Likes);
             }
 
             MediaWebLink = GetMediaWebLink(groupID, initObj.Platform);
