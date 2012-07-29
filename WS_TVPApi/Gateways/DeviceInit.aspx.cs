e464439fd5671dab3ce2a66721359dd108e64325
Elram Damti <Elram@tvinci.com>
2012-07-29 08:49:38 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/Projects/TVPProAPIs@10846 542a500b-ef30-3f42-a8c6-a0c997676187

--- WS_TVPApi/Gateways/DeviceInit.aspx.cs
+++ WS_TVPApi/Gateways/DeviceInit.aspx.cs
@@ -17,15 +17,38 @@ public partial class Gateways_DeviceInit : System.Web.UI.Page
         // Get querystring values for device bundle ID and device UDID
         string sBundleID = string.Empty;
         string sUDID = string.Empty;
-        if (!Request.QueryString.AllKeys.Contains("bid") || !Request.QueryString.AllKeys.Contains("udid"))
         {
-            Response.Clear();
-            Response.Write(@"{""Error"" : ""Missing 'bid' or 'udid' parameters""}");
-        }
-        else
-        {
-            sUDID = Request.QueryString["udid"].ToString();
-            sBundleID = Request.QueryString["bid"].ToString();
+            try
+            {
+                sUDID = Request.QueryString["udid"].ToString();
+                sBundleID = Request.QueryString["bid"].ToString();
+            }
+            catch (Exception ex) { }
+            
+            // change to check if method is post or get
+            if (string.IsNullOrEmpty(sUDID) && string.IsNullOrEmpty(sBundleID))
+            {
+                Response.ContentType = "application/json; charset=utf-8";
+
+                System.IO.Stream body = Request.InputStream;
+                System.Text.Encoding encoding = Request.ContentEncoding;
+                System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+
+                string sJsonRequest = reader.ReadToEnd();
+
+                if (!string.IsNullOrEmpty(sJsonRequest))
+                {
+                    Newtonsoft.Json.Linq.JObject param = Newtonsoft.Json.Linq.JObject.Parse(sJsonRequest);
+                    sBundleID = param["bid"].ToString();
+                    sUDID = param["udid"].ToString();
+                }
+            }
+
+            if (string.IsNullOrEmpty(sUDID) && string.IsNullOrEmpty(sBundleID))
+            {
+                Response.Clear();
+                Response.Write(@"{""Error"" : ""Missing 'bid' or 'udid' parameters""}");
+            }
 
             // Get device init object
             DeviceInit deviceInit = GetInit(sUDID, sBundleID);
@@ -69,7 +92,7 @@ public partial class Gateways_DeviceInit : System.Web.UI.Page
 
             retDeviceInit.GatewayURL = "http://173.231.146.34:9003/tvpapi/gateways/jsonpostgw.aspx";
             retDeviceInit.MainMenuID = "43";
-            retDeviceInit.AllowBrowseMode = "false";
+            retDeviceInit.AllowBrowseMode = "true";
         }
         else if (sBundleID.ToLower().Equals("com.tvinci.development.toggle"))
         {
@@ -85,9 +108,47 @@ public partial class Gateways_DeviceInit : System.Web.UI.Page
             retDeviceInit.initObj.Locale.LocaleDevice = string.Empty;
             retDeviceInit.initObj.Locale.LocaleLanguage = string.Empty;
 
+            retDeviceInit.FacebookURL = "http://173.231.146.5/social/facebook_api.aspx?action=getdata&groupId=147&platform=1&domain=1";
+
             retDeviceInit.GatewayURL = "http://173.231.146.34:9003/tvpapi/gateways/jsonpostgw.aspx";
-            retDeviceInit.MainMenuID = "43";
+            retDeviceInit.MainMenuID = "45";
+            retDeviceInit.AllowBrowseMode = "false";
+
+            retDeviceInit.FilesFormat = new DeviceInit.oFilesFormat() { Main = "iOS Clear", Trailer = "Trailer" };
+        }
+        else if (sBundleID.ToLower().Equals("com.tvinci2.activities") || sBundleID.ToLower().Equals("com.tvinci.development.ipadtvincidemo"))
+        {
+            retDeviceInit.initObj = new TVPApi.InitializationObject();
+            retDeviceInit.initObj.ApiUser = "tvpapi_125";
+            retDeviceInit.initObj.ApiPass = "11111";
+            retDeviceInit.initObj.SiteGuid = string.Empty;
+            retDeviceInit.initObj.UDID = sUDID;
+            retDeviceInit.initObj.Platform = TVPApi.PlatformType.iPad;
+            retDeviceInit.initObj.Locale = new TVPApi.Locale();
+            retDeviceInit.initObj.Locale.LocaleUserState = TVPApi.LocaleUserState.Unknown;
+            retDeviceInit.initObj.Locale.LocaleCountry = string.Empty;
+            retDeviceInit.initObj.Locale.LocaleDevice = string.Empty;
+            retDeviceInit.initObj.Locale.LocaleLanguage = string.Empty;
+
+            retDeviceInit.FacebookURL = "http://173.231.146.5/social/facebook_api.aspx?action=getdata&groupId=125&platform=3&domain=1";
+
+            retDeviceInit.GatewayURL = "http://173.231.146.34:9003/tvpapi/gateways/jsonpostgw.aspx";
+            retDeviceInit.MainMenuID = "26";
             retDeviceInit.AllowBrowseMode = "true";
+
+            if (sBundleID.ToLower().Equals("com.tvinci2.activities"))
+                retDeviceInit.FilesFormat = new DeviceInit.oFilesFormat() { Main = "Android Tablet Main", Trailer = "Trailer" };
+            else
+                retDeviceInit.FilesFormat = new DeviceInit.oFilesFormat() { Main = "IPad Main", Trailer = "Trailer" };
+
+            retDeviceInit.MediaTypes = new List<KeyValuePair<int, string>>();
+            retDeviceInit.MediaTypes.Add(new KeyValuePair<int, string>(272, "Movie")); 
+            retDeviceInit.MediaTypes.Add(new KeyValuePair<int, string>(273, "Episode"));
+            retDeviceInit.MediaTypes.Add(new KeyValuePair<int, string>(289, "Live"));
+            retDeviceInit.MediaTypes.Add(new KeyValuePair<int, string>(277, "Series"));
+            retDeviceInit.MediaTypes.Add(new KeyValuePair<int, string>(278, "Person"));
+            retDeviceInit.MediaTypes.Add(new KeyValuePair<int, string>(279, "Package"));
+            retDeviceInit.MediaTypes.Add(new KeyValuePair<int, string>(215, "Article"));
         }
 
         return retDeviceInit;
@@ -101,10 +162,7 @@ public class DeviceInit
     public TVPApi.InitializationObject initObj { get; set; }
     public string GatewayURL { get; set; }
     public string LogoURL { get; set; }
-    public string SpotlightCatID { get; set; }
-    public string HomeLeftCatID { get; set; }
-    public string HomeRightCatID { get; set; }
-    public string RootCatID { get; set; }
+    public string FacebookURL { get; set; }
     public string SmallPicSize { get; set; }
     public string MediumPicSize { get; set; }
     public string LargePicSize { get; set; }
@@ -112,6 +170,14 @@ public class DeviceInit
     public string SD { get; set; }
     public string MainMenuID { get; set; }
     public string AllowBrowseMode { get; set; }
+    public oFilesFormat FilesFormat { get; set; }
+    public List<KeyValuePair<int, string>> MediaTypes { get; set; }
+
+    public struct oFilesFormat
+    {
+        public string Trailer;
+        public string Main;
+    }
 }
 
 public class MyStringEnumConverter : Newtonsoft.Json.Converters.StringEnumConverter
