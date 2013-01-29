42af3ba4f6858ddfcef7298a90838b14b5be90be
Amishai  Guilburd <Amishai.G@tvinci.com>
2013-01-29 16:06:01 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/Projects/TVPProAPIs@13874 542a500b-ef30-3f42-a8c6-a0c997676187

--- WS_TVPApi/App_Code/WS_Code/Interfaces/ISocialService.cs
+++ WS_TVPApi/App_Code/WS_Code/Interfaces/ISocialService.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ServiceModel;
+using TVPApi;
+using TVPPro.SiteManager.TvinciPlatform.Social;
+using TVPPro.SiteManager.TvinciPlatform.Users;
 
 namespace TVPApiServices
 {
     [ServiceContract]
     public interface ISocialService
     {
-     
+        [OperationContract]
+        FriendWatchedObject[] GetAllFriendsWatched(InitializationObject initObj, int maxResult);
+
+        [OperationContract]
+        FriendWatchedObject[] GetFriendsWatchedByMedia(InitializationObject initObj, int mediaId);
+
+        [OperationContract]
+        User[] GetUsersLikedMedia(InitializationObject initObj, int mediaID, bool onlyFriends, int startIndex, int pageSize);
     }
 }
--- WS_TVPApi/App_Code/WS_Code/SocialService.cs
+++ WS_TVPApi/App_Code/WS_Code/SocialService.cs
@@ -11,6 +11,7 @@ using log4net;
 using TVPPro.SiteManager.TvinciPlatform.Users;
 using TVPPro.SiteManager.TvinciPlatform.Social;
 using System.Configuration;
+using TVPApiModule.Services;
 
 
 namespace TVPApiServices
@@ -21,8 +22,81 @@ namespace TVPApiServices
     [System.Web.Script.Services.ScriptService]
     public class SocialService : System.Web.Services.WebService, ISocialService
     {
-        private readonly ILog logger = LogManager.GetLogger(typeof(SiteService));
-      
+        private readonly ILog logger = LogManager.GetLogger(typeof(SocialService));
 
+        [WebMethod(EnableSession = true, Description = "Get all medias that user's friends watched")]
+        public FriendWatchedObject[] GetAllFriendsWatched(InitializationObject initObj, int maxResult)
+        {
+            int groupId = ConnectionHelper.GetGroupID("tvpapi", "GetAllFriendsWatched", initObj.ApiUser, initObj.ApiPass, SiteHelper.GetClientIP());
+            logger.InfoFormat("GetAllFriendsWatched-> [{0}, {1}], Params:[user: {2}]", groupId, initObj.Platform, initObj.SiteGuid);
+
+            if (groupId > 0)
+            {
+                try
+                {
+                    int siteGuid = 0;
+                    if (Int32.TryParse(initObj.SiteGuid, out siteGuid))
+                        return new TVPPro.SiteManager.Services.SocialService().GetAllFriendsWatched(siteGuid, maxResult);
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("GetAllFriendsWatched->", ex);
+                }
+            }
+            else
+                logger.ErrorFormat("GetAllFriendsWatched-> 'Unknown group' Username: {0}, Password: {1}", initObj.ApiUser, initObj.ApiPass);
+
+            return null;
+        }
+
+        [WebMethod(EnableSession = true, Description = "Get all friends that watched the specified media")]
+        public FriendWatchedObject[] GetFriendsWatchedByMedia(InitializationObject initObj, int mediaId)
+        {
+            int groupId = ConnectionHelper.GetGroupID("tvpapi", "GetFriendsWatchedByMedia", initObj.ApiUser, initObj.ApiPass, SiteHelper.GetClientIP());
+            logger.InfoFormat("GetFriendsWatchedByMedia-> [{0}, {1}], Params:[user: {2}]", groupId, initObj.Platform, initObj.SiteGuid);
+
+            if (groupId > 0)
+            {
+                try
+                {
+                    int siteGuid = 0;
+                    if (Int32.TryParse(initObj.SiteGuid, out siteGuid))
+                        return new TVPPro.SiteManager.Services.SocialService().GetFriendsWatchedByMedia(siteGuid, mediaId);
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("GetFriendsWatchedByMedia->", ex);
+                }
+            }
+            else
+                logger.ErrorFormat("GetFriendsWatchedByMedia-> 'Unknown group' Username: {0}, Password: {1}", initObj.ApiUser, initObj.ApiPass);
+
+            return null;
+        }
+
+        [WebMethod(EnableSession = true, Description = "Get all friends that liked a media")]
+        public User[] GetUsersLikedMedia(InitializationObject initObj, int mediaID, bool onlyFriends, int startIndex, int pageSize)
+        {
+            int groupId = ConnectionHelper.GetGroupID("tvpapi", "GetUsersLikedMedia", initObj.ApiUser, initObj.ApiPass, SiteHelper.GetClientIP());
+            logger.InfoFormat("GetUsersLikedMedia-> [{0}, {1}], Params:[user: {2}]", groupId, initObj.Platform, initObj.SiteGuid);
+
+            if (groupId > 0)
+            {
+                try
+                {
+                    int siteGuid = 0;
+                    if (Int32.TryParse(initObj.SiteGuid, out siteGuid))
+                        return TVPPro.SiteManager.Services.UsersService.Instance.GetUsersLikedMedia(siteGuid, mediaID, (int)initObj.Platform, onlyFriends, startIndex, pageSize);
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("GetUsersLikedMedia->", ex);
+                }
+            }
+            else
+                logger.ErrorFormat("GetUsersLikedMedia-> 'Unknown group' Username: {0}, Password: {1}", initObj.ApiUser, initObj.ApiPass);
+
+            return null;
+        }
     }
 }
