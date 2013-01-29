b7c17d947b0069c7581acbffcb54dac1cee016ee
Amishai  Guilburd <Amishai.G@tvinci.com>
2013-01-29 13:39:01 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/Projects/TVPProAPIs@13868 542a500b-ef30-3f42-a8c6-a0c997676187

---
+++ WS_TVPApi/App_Code/WS_Code/Interfaces/ISocialService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+
+namespace TVPApiServices
+{
+    [ServiceContract]
+    public interface ISocialService
+    {
+     
+    }
+}
---
+++ WS_TVPApi/App_Code/WS_Code/SocialService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TVPApi;
+using TVPPro.SiteManager.Helper;
+using System.Web.Services;
+using log4net;
+using TVPPro.SiteManager.TvinciPlatform.Users;
+using TVPPro.SiteManager.TvinciPlatform.Social;
+using System.Configuration;
+
+
+namespace TVPApiServices
+{
+    [WebService(Namespace = "http://platform-us.tvinci.com/tvpapi/ws")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class SocialService : System.Web.Services.WebService, ISocialService
+    {
+        private readonly ILog logger = LogManager.GetLogger(typeof(SiteService));
+      
+
+    }
+}
