702ac37ad4fd6ca97a718bafa7f7b34db24a88ff
Amishai  Guilburd <Amishai.G@tvinci.com>
2013-05-01 12:39:15 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/Projects/TVPProAPIs@14761 542a500b-ef30-3f42-a8c6-a0c997676187

--- WS_TVPApi/Gateways/JsonPostGW.aspx.cs
+++ WS_TVPApi/Gateways/JsonPostGW.aspx.cs
@@ -5,12 +5,15 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using TVPApi;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 public partial class Gateways_JsonPostGW : BaseGateway
 {
     protected override void OnLoad(EventArgs e)
     {
-
         base.OnLoad(e);
     }
 
@@ -19,40 +22,43 @@ public partial class Gateways_JsonPostGW : BaseGateway
         Response.ContentType = "application/json; charset=utf-8";
         Response.AppendHeader("Access-Control-Allow-Origin", "*");
 
-        System.IO.Stream body = Request.InputStream;
-        System.Text.Encoding encoding = Request.ContentEncoding;
-        System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
-
-        //List<TVPApi.TagMetaPair> tagPairs = new List<TagMetaPair>();
-        //tagPairs.Add(new TagMetaPair("Genre", "Cas"));
-        //tagPairs.Add(new TagMetaPair("Genre", "Cas"));
-
-        //Object o = Newtonsoft.Json.Linq.JArray.FromObject(tagPairs);
+        Stream body = Request.InputStream;
+        Encoding encoding = Request.ContentEncoding;
+        StreamReader reader = new System.IO.StreamReader(body, encoding);
 
         string sJsonRequest = reader.ReadToEnd();
 
         if (!string.IsNullOrEmpty(sJsonRequest))
         {
-            Newtonsoft.Json.Linq.JObject json = Newtonsoft.Json.Linq.JObject.Parse(sJsonRequest);
+            JObject json = JObject.Parse(sJsonRequest);
 
-            foreach (KeyValuePair<string, Newtonsoft.Json.Linq.JToken> pair in json)
+            foreach (KeyValuePair<string, JToken> pair in json)
             {
                 string sValue = string.Empty;
 
-                if (pair.Value.GetType() == typeof(Newtonsoft.Json.Linq.JArray))
-                {
-                    sValue = pair.Value.ToString(Newtonsoft.Json.Formatting.None);
-                }
+                if (pair.Value.GetType() == typeof(JArray))
+                    sValue = pair.Value.ToString(Formatting.None);
                 else
-                {
-                    sValue = (!pair.Key.Equals("initObj") && !pair.Key.Equals("tagPairs") && !pair.Key.Equals("metaPairs") && !pair.Key.Equals("userBasicData") && !pair.Key.Equals("userDynamicData")) ? pair.Value.ToString(Newtonsoft.Json.Formatting.None).Replace("\"", @"") : pair.Value.ToString(Newtonsoft.Json.Formatting.None);
-                }
+                    sValue = (!pair.Key.Equals("initObj") &&
+                              !pair.Key.Equals("tagPairs") &&
+                              !pair.Key.Equals("metaPairs") &&
+                              !pair.Key.Equals("userBasicData") &&
+                              !pair.Key.Equals("userDynamicData")) ? pair.Value.ToString(Formatting.None).Replace("\"", @"") : pair.Value.ToString(Formatting.None);
 
                 HttpContext.Current.Items.Add(pair.Key, sValue);
             }
         }
 
-        MethodFinder queryServices = new MethodFinder(m_MediaService, m_SiteService, m_PricingService, m_DomainService, m_BillingService, m_ConditionalAccessService, m_SocialService, m_UsersService, m_NotificationService);
+        // add web service
+        MethodFinder queryServices = new MethodFinder(m_MediaService,
+                                                      m_SiteService,
+                                                      m_PricingService,
+                                                      m_DomainService,
+                                                      m_BillingService,
+                                                      m_ConditionalAccessService,
+                                                      m_SocialService,
+                                                      m_UsersService,
+                                                      m_NotificationService);
 
         queryServices.ProcessRequest();
     }
