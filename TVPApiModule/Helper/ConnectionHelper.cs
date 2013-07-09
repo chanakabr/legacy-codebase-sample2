b41141cf09e9e501bafaa207b8dd5b840338cd22
Idan Langer <Idan.l@tvinci.com>
2013-07-09 15:08:06 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/Projects/TVPProAPIs@15838 542a500b-ef30-3f42-a8c6-a0c997676187

--- TVPApiModule/Helper/ConnectionHelper.cs
+++ TVPApiModule/Helper/ConnectionHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Collections;
 using log4net;
@@ -16,53 +18,103 @@ namespace TVPApi
     public class ConnectionHelper
     {
         private static readonly ILog logger = LogManager.GetLogger(typeof(ConnectionHelper));
-
-        public ConnectionHelper()
-        {
-
-        }
+        private static ReaderWriterLockSlim _locker = new ReaderWriterLockSlim();
+        private static Dictionary<string, Dictionary<string, int>> _groupsModulesIPs = new Dictionary<string, Dictionary<string, int>>();
 
         //Get group ID according to ws user name
         static public Int32 GetGroupID(string sWSName, string sModuleName, string sUN, string sPass, string sIP)
         {
-            try
+            if (_groupsModulesIPs.ContainsKey(sUN + ":" + sPass))
             {
-                Int32 nGroupID = 0;
-                ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery(GetTvinciConnectionString());
-                
-                selectQuery += "select group_id from groups_modules_ips where is_active=1 and status=1 and ";
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("USERNAME", "=", sUN);
-                selectQuery += "and";
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("PASSWORD", "=", sPass);
-                selectQuery += "and (";
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("MODULE_NAME", "=", sModuleName);
-                selectQuery += " or ";
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("MODULE_NAME", "=", "00000");
-                selectQuery += ") and (";
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("IP", "=", sIP);
-                selectQuery += " or ";
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("IP", "=", "00000");
-                selectQuery += ") and";
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("WS_NAME", "=", sWSName);
-                selectQuery += "order by MODULE_NAME desc";
-                if (selectQuery.Execute("query", true) != null)
+                return GetValueFromDict(sWSName, sModuleName, sUN, sPass, sIP);
+            }
+            else
+            {
+                try
                 {
-                    Int32 nCount = selectQuery.Table("query").DefaultView.Count;
-                    if (nCount > 0)
+                    _locker.EnterWriteLock();
+                    
+                    //Second check to make sure another thread didn't add the value already
+                    if (!_groupsModulesIPs.ContainsKey(sUN + ":" + sPass))
                     {
-                        nGroupID = int.Parse(selectQuery.Table("query").DefaultView[0].Row["group_id"].ToString());
+                        Dictionary<string, int> tmpDict = GetGroupModulesIpsFromDb(sUN, sPass);
+                        if (tmpDict != null)
+                        {
+                            _groupsModulesIPs.Add(sUN + ":" + sPass, tmpDict);
+                            return GetValueFromDict(sWSName, sModuleName, sUN, sPass, sIP);
+                        }
+                        else return 0;
+                    }
+                    else return GetValueFromDict(sWSName, sModuleName, sUN, sPass, sIP);
+                }
+
+                catch (Exception)
+                {
+                    return 0;
+                }
+
+                finally
+                {
+                    _locker.ExitWriteLock();
+                }
+
+            }
+        }
+
+        private static int GetValueFromDict(string sWSName, string sModuleName, string sUN, string sPass, string sIP)
+        {
+            if (_groupsModulesIPs[sUN + ":" + sPass].ContainsKey("00000" + ":" + sWSName + ":" + "00000"))
+            {
+                return _groupsModulesIPs[sUN + ":" + sPass]["00000" + ":" + sWSName + ":" + "00000"];
+            }
+            else if (_groupsModulesIPs[sUN + ":" + sPass].ContainsKey("00000" + ":" + sWSName + ":" + sIP))
+            {
+                return _groupsModulesIPs[sUN + ":" + sPass]["00000" + ":" + sWSName + ":" + sIP];
+            }
+            else if (_groupsModulesIPs[sUN + ":" + sPass].ContainsKey(sModuleName + ":" + sWSName + ":" + "00000"))
+            {
+                return _groupsModulesIPs[sUN + ":" + sPass][sModuleName + ":" + sWSName + ":" + "00000"];
+            }
+            else if (_groupsModulesIPs[sUN + ":" + sPass].ContainsKey(sModuleName + ":" + sWSName + ":" + sIP))
+            {
+                return _groupsModulesIPs[sUN + ":" + sPass][sModuleName + ":" + sWSName + ":" + sIP];
+            }
+            else return 0;
+        }
+
+        private static Dictionary<string, int> GetGroupModulesIpsFromDb(string sUN, string sPass)
+        {
+            Dictionary<string, int> retVal = null;
+
+            ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery(GetTvinciConnectionString());
+
+            selectQuery += "select * from groups_modules_ips where is_active=1 and status=1 and ";
+            selectQuery += ODBCWrapper.Parameter.NEW_PARAM("USERNAME", "=", sUN);
+            selectQuery += "and";
+            selectQuery += ODBCWrapper.Parameter.NEW_PARAM("PASSWORD", "=", sPass);
+
+            DataTable dt = selectQuery.Execute("query", true);
+            if (dt != null)
+            {
+                Int32 nCount = dt.DefaultView.Count;
+                if (nCount > 0)
+                {
+                    retVal = new Dictionary<string, int>();
+                    foreach (DataRow item in dt.Rows)
+                    {
+
+                        retVal[item["MODULE_NAME"] + ":" + item["WS_NAME"] + ":" + item["IP"]] =
+                            int.Parse(item["GROUP_ID"].ToString());
                     }
                 }
-                selectQuery.Finish();
-                selectQuery = null;
-                return nGroupID;
             }
-            catch (Exception ex)
-            {
-                logger.Error(string.Format("GetGroupID-> Params:[sWSName: {0}, sModuleName: {1}, sUN: {2}, sPass: {3}, sIP: {4}]", sWSName, sModuleName, sUN, sPass, sIP), ex);
-            }
-            return 0;
+
+
+            selectQuery.Finish();
+            selectQuery = null;
+            return retVal;
         }
+
 
 
         //Init delegates
@@ -90,12 +142,12 @@ namespace TVPApi
             //Tvinci.Data.TVMDataLoader.Protocols.Protocol.GetTVMConfigurationMethod = delegate() { return ConfigManager.GetInstance().GetConfig(groupID, platform).TechnichalConfiguration.TVMConfiguration; };
             //TVPPro.SiteManager.Manager.TechnicalManager.GetTVMConfiguration;
             Tvinci.Data.TVMDataLoader.Protocols.Protocol.GetRequestLanguageMethod = GetFlashVarsLangVal;
-            Tvinci.Data.TVMDataLoader.TVMProvider.GetTVMUrlMethod = delegate(bool b) { return ConfigurationManager.AppSettings["TVM_API_URL"]; }; 
+            Tvinci.Data.TVMDataLoader.TVMProvider.GetTVMUrlMethod = delegate(bool b) { return ConfigurationManager.AppSettings["TVM_API_URL"]; };
             //TVPPro.SiteManager.Manager.TextLocalization.Instance.Dispose();
             //TVPPro.SiteManager.Manager.TextLocalization.Instance.TranslationCulture = HttpContext.Current.Items["GroupID"].ToString();
-           // TVPPro.SiteManager.Manager.TextLocalization.Instance.Sync(null);
+            // TVPPro.SiteManager.Manager.TextLocalization.Instance.Sync(null);
 
-          //  TVPPro.SiteManager.Manager.TextLocalization.Instance.RegisterInstance();
+            //  TVPPro.SiteManager.Manager.TextLocalization.Instance.RegisterInstance();
         }
 
         public static string GetFlashVarsLangVal()
