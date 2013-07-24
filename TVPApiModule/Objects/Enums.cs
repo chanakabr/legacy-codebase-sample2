8541f65e1da9651a5ebf56657d5a18be2928b6ee
Amishai  Guilburd <Amishai.G@tvinci.com>
2013-07-24 13:16:27 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/Projects/TVPProAPIs@16042 542a500b-ef30-3f42-a8c6-a0c997676187

---
+++ TVPApiModule/Objects/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TVPApiModule.Objects
+{
+    public class Enums
+    {
+        public enum eCutWith
+        {
+            OR = 0,
+            AND = 1
+        }
+    }
+}
