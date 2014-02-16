445058b61323ecbe22599669aae3dc1f3b9831cd
Lior Tamam <lior.t@tvinci.com>
2014-02-16 14:36:35 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/TvmCore@19998 542a500b-ef30-3f42-a8c6-a0c997676187

---
+++ CouchbaseManager/CouchbaseManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Configuration;
+using Couchbase;
+using Couchbase.Configuration;
+
+namespace CouchbaseManager
+{
+    public enum eCouchbaseBucket { UNKNOWN = 0, NOTIFICATION = 1, SOCIALHUB = 2, SOCIALFRIENDS = 3, EPG = 4 }
+
+    public class CouchbaseManager
+    {
+        private static volatile Dictionary<string, CouchbaseClient> m_CouchbaseInstances = new Dictionary<string, CouchbaseClient>();
+        private static object syncObj = new object();
+        private static ReaderWriterLockSlim m_oSyncLock = new ReaderWriterLockSlim();
+
+        public static CouchbaseClient GetInstance(eCouchbaseBucket eBucket)
+        {
+            CouchbaseClient tempClient = null;
+
+            if (m_CouchbaseInstances == null)
+            {
+                m_CouchbaseInstances = new Dictionary<string, CouchbaseClient>();
+            }
+
+            if (m_oSyncLock.TryEnterWriteLock(1000))
+            {
+                try
+                {
+                    if (!m_CouchbaseInstances.ContainsKey(eBucket.ToString()))
+                    {
+                        CouchbaseClient client = createNewInstance(eBucket);
+
+                        if (client != null)
+                        {
+                            m_CouchbaseInstances.Add(eBucket.ToString(), client);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+                finally
+                {
+                    m_oSyncLock.ExitWriteLock();
+                }
+            }
+
+            // If item already exist
+            if (m_oSyncLock.TryEnterReadLock(1000))
+            {
+                try
+                {
+                    m_CouchbaseInstances.TryGetValue(eBucket.ToString(), out tempClient);
+                }
+                catch (Exception ex)
+                {
+                }
+                finally
+                {
+                    m_oSyncLock.ExitReadLock();
+                }
+            }
+
+            return tempClient;
+        }
+
+        private static CouchbaseClient createNewInstance(eCouchbaseBucket eBucket)
+        {
+            CouchbaseClient oRes = null;
+            switch (eBucket)
+            {
+                case eCouchbaseBucket.SOCIALHUB:
+                case eCouchbaseBucket.SOCIALFRIENDS:
+                case eCouchbaseBucket.EPG:
+                    var socialBucketSection = (CouchbaseClientSection)ConfigurationManager.GetSection(string.Format("couchbase/{0}", eBucket.ToString().ToLower()));
+                    oRes = new CouchbaseClient(socialBucketSection);
+                    break;
+                case eCouchbaseBucket.NOTIFICATION:
+                    break;
+            }
+
+            return oRes;
+        }
+    }
+}
