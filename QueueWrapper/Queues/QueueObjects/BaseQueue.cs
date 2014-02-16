b312a00a3a0d96efa6989f6267d4d62d89b4634a
Lior Tamam <lior.t@tvinci.com>
2014-02-16 13:41:19 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/TvmCore@19984 542a500b-ef30-3f42-a8c6-a0c997676187

---
+++ QueueWrapper/Queues/QueueObjects/BaseQueue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QueueWrapper
+{
+    public abstract class BaseQueue : IQueueable
+    {
+        #region Private Members
+
+        private IQueueImpl m_QueueImpl;
+
+        #endregion
+
+        #region CTOR
+
+        public BaseQueue()
+        { }
+
+        #endregion
+
+        #region IQueuable
+
+        //public abstract bool Enqueue(ApiObjects.MediaIndexingObjects.QueueObject record, int nGroupId);
+        public virtual bool Enqueue(ApiObjects.MediaIndexingObjects.QueueObject record, string sRouteKey)
+        {
+            bool bIsEnqueueSucceeded = false;
+            string sMessage = string.Empty;
+
+            if (record != null)
+            {
+                sMessage = record.ToString();
+                if (this.Implementation != null)
+                {
+                    bIsEnqueueSucceeded = this.Implementation.Enqueue(sMessage, sRouteKey);
+                }
+            }
+
+            return bIsEnqueueSucceeded;
+        }
+
+        //public abstract T Dequeue<T>(string sQueueName, out string sAckId);
+        public virtual T Dequeue<T>(string sQueueName, out string sAckId)
+        {
+            sAckId = string.Empty;
+
+            T objectReturned = default(T);
+            if (!string.IsNullOrEmpty(sQueueName))
+            {
+                objectReturned = this.Implementation.Dequeue<T>(sQueueName, out sAckId);
+            }
+
+            return objectReturned;
+        }
+
+        #endregion
+
+        #region Getters
+
+        internal IQueueImpl Implementation
+        {
+            get { return this.m_QueueImpl; }
+            set { this.m_QueueImpl = value; }
+        }
+
+        #endregion
+
+    }
+}
