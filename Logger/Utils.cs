1f094cd6c49bd75c816b268115b1d4ae31c2e168
Lior Tamam <lior.t@tvinci.com>
2014-02-16 14:19:40 +0000

    git-svn-id: https://svn-srv.tvinci.local/svn/Web/trunk/TvmCore@19991 542a500b-ef30-3f42-a8c6-a0c997676187

---
+++ Logger/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logger
+{
+    public class Utils
+    {
+        public static string GetTcmConfigValue(string sKey)
+        {           
+            string result = string.Empty;
+            try
+            {
+                result = TCMClient.Settings.Instance.GetValue<string>(sKey);
+            }
+            catch (Exception ex)
+            {
+                result = string.Empty;
+                Logger.Log("Logger", "Key=" + sKey + "," + ex.Message, "Tcm");
+            }
+            return result;
+        }
+    }
+}
