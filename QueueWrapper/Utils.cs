a72c855c9b50aedb74eb946c5975c9ee747cb6a5
Gilad Matkovitch <gilad.m@tvinci.com>
2014-02-17 10:55:27 +0200

    Added: 1. Logger project - AMQPAppender and Log4Net objects (Log4Net.config, Log4NetManager.cs, Logger4Net.cs, ILogger4Net.cs and Model objects 'BaseLog') to support log writing to RabbitMQ 2. QueueWrapper project - Used in Logger to call RabbitQueue

---
+++ QueueWrapper/Utils.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace QueueWrapper
+{
+    public static class Utils
+    {
+        public static string GetConfigValue(string sKey)
+        {
+            string sValue = string.Empty;
+
+            try
+            {
+                sValue = GetValue(sKey);
+            }
+            catch (Exception ex)
+            {
+                //Logger.Logger.Log("Catalog Url", "Cannot read catalog url", "Catalog Url");
+            }
+
+            return sValue;
+        }
+
+        private static string GetValue(string sKey)
+        {
+            string configuration = ConfigurationManager.AppSettings[sKey];
+
+            if (configuration != null && !configuration.ToString().Equals(string.Empty))
+                return configuration.ToString();
+
+            return string.Empty;
+        }
+
+        internal static MutexSecurity CreateMutex()
+        {
+            var sid = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+            MutexSecurity mutexSecurity = new MutexSecurity();
+            mutexSecurity.AddAccessRule(new MutexAccessRule(sid, MutexRights.FullControl, AccessControlType.Allow));
+            mutexSecurity.AddAccessRule(new MutexAccessRule(sid, MutexRights.ChangePermissions, AccessControlType.Deny));
+            mutexSecurity.AddAccessRule(new MutexAccessRule(sid, MutexRights.Delete, AccessControlType.Deny));
+
+            return mutexSecurity;
+        }
+
+        internal static T JsonToObject<T>(string json)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            T obj = js.Deserialize<T>(json);
+            return (T)obj;  
+        }
+    }
+}
