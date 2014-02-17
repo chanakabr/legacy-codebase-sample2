62a397ace607de741d856d1edb24d84149db31b8
Gilad Matkovitch <gilad.m@tvinci.com>
2014-02-17 16:55:20 +0200

    Added: ApiOrcaReommendationsConfiguration RecommendationsHelper LiveRecommendation VideoRecommendation

---
+++ TVPApiModule/Objects/ORCARecommendations/LiveRecommendation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TVPApiModule.Objects.ORCARecommendations
+{
+    public class LiveRecommendation
+    {
+        public string ProgramID { get; set; }
+    }
+}
