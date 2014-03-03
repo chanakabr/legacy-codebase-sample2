3dd7d503855e8ad1ac75a5f52ecfe07c56412f97
Avi Gabay <avi.g@tvinci.com>
2014-03-03 14:18:15 +0200

    Added ESBulkRequesObj to commit

---
+++ ElasticSearch/Common/ESBulkRequestObj.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ElasticSearch.Common
+{
+    public class ESBulkRequestObj<T>
+    {
+        public string index { get; set; }
+        public string type { get; set; }
+        public T docID { get; set; }
+        public string document { get; set; }
+        public string routing { get; set; }
+
+    }
+}
