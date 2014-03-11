d316b6e6c99a452389796c64b5f4fcfd3c5d4f10
Ira Yanai <iray@tvinci.com>
2014-03-11 09:43:23 +0200

    ES - Fix

--- ElasticSearch/Common/ESSerializer.cs
+++ ElasticSearch/Common/ESSerializer.cs
@@ -346,7 +346,7 @@ namespace ElasticSearch.Common
                     {
 
 
-                        List<string> lMetaValues = oEpg.Tags[sMetaName];
+                        List<string> lMetaValues = oEpg.Metas[sMetaName];
                         if (lMetaValues != null && lMetaValues.Count > 0)
                         {
                             for (int i = 0; i < lMetaValues.Count; i++)
