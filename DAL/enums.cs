33824e5aa20aefd848d2e4e063631e896cf45fc8
Michael Mars <michael@tvinci.com>
2014-03-24 12:29:01 +0200

    UserActivationState

---
+++ DAL/enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public enum UserActivationState
+    {
+        Error = -2,
+        UserDoesNotExist = -1,
+        Activated = 0,
+        NotActivated = 1,
+        NotActivatedByMaster = 2,
+        UserNotInDomain = 3         // Or removed 
+    }
+    
+}
