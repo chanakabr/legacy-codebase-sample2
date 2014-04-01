2a546847113144619d7d1179db2b3a35f3b0b78a
Liat Regev <Liat.R@tvinci.com>
2014-04-01 10:58:23 +0300

    statistics bl

---
+++ StaticticsBL/TvinciStaticticsBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ApiObjects.Statistics;
+using DalCB;
+
+namespace StatisticsBL
+{
+    public class TvinciStaticticsBL: BaseStaticticsBL
+    {
+        protected StatisicsDal_CouchBase m_oMediaCouchbase;
+
+        public TvinciStaticticsBL(int nGroupID)
+        {
+            this.m_nGroupID = nGroupID;
+            m_oMediaCouchbase = new DalCB.StatisicsDal_CouchBase(m_nGroupID);
+        }
+
+        public override BuzzWeightedAverScore GetBuzzAverScore(string sKey)
+        {
+            string sFullKey = string.Concat("was_", sKey);
+            BuzzWeightedAverScore oRes = m_oMediaCouchbase.GetBuzzAverScore(sFullKey);
+            oRes = (oRes != null) ? oRes : null;
+            return oRes;
+        }
+    }
+}
