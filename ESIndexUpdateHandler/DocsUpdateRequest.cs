a61f2858a1861999b7b9b882c610a7f4b41daa98
Avi Gabay <avi.g@tvinci.com>
2014-04-30 11:04:21 +0300

    Add - elasticsearch indexing and and rebuilding support

---
+++ ESIndexUpdateHandler/DocsUpdateRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ESIndexUpdateHandler
+{
+    [Serializable]
+    public class DocsUpdateRequest
+    {
+        [JsonProperty("group_id")]
+        public int GroupID { get; set; }
+
+        [JsonProperty("ids")]
+        public List<int> DocIDs { get; set; }
+
+        [JsonProperty("action", Required = Required.Always)]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ApiObjects.eAction Action { get; set; }
+
+        [JsonProperty("type", Required = Required.Always)]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ApiObjects.eObjectType Type { get; set; }
+
+    }
+}
---
+++ ESIndexUpdateHandler/Updaters/ChannelUpdater.cs
@@ -0,0 +1,137 @@
+﻿using ApiObjects.SearchObjects;
+using Catalog;
+using ElasticSearch.Searcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ESIndexUpdateHandler.Updaters
+{
+    public class ChannelUpdater : IUpdateable
+    {
+        public static readonly string PERCOLATOR = "_percolator";
+
+        private int m_nGroupID;
+        private ElasticSearch.Common.ESSerializer m_oESSerializer;
+        private ElasticSearch.Common.ElasticSearchApi m_oESApi;
+
+        public List<int> IDs { get; set; }
+        public ApiObjects.eAction Action { get; set; }
+
+        public ChannelUpdater(int nGroupID)
+        {
+            m_nGroupID = nGroupID;
+            m_oESSerializer = new ElasticSearch.Common.ESSerializer();
+            m_oESApi = new ElasticSearch.Common.ElasticSearchApi();
+        }
+
+        public bool Start()
+        {
+            bool result = false;
+            Logger.Logger.Log("Info", "Start Channel update", "ESUpdateHandler");
+            if (IDs == null || IDs.Count == 0)
+            {
+                Logger.Logger.Log("Info", "Channel Id list empty", "ESUpdateHandler");
+                result = true;
+
+                return result;
+            }
+
+            if (!m_oESApi.IndexExists(ElasticsearchTasksCommon.Utils.GetMediaGroupAliasStr(m_nGroupID)))
+            {
+                Logger.Logger.Log("Error", string.Format("Index of type media for group {0} does not exist", m_nGroupID), "ESUpdateHandler");
+                return result;
+            }
+
+            switch (Action)
+            {
+                case ApiObjects.eAction.Off:
+                case ApiObjects.eAction.Delete:
+                    result = DeleteChannel(IDs);
+                    break;
+                case ApiObjects.eAction.On:
+                case ApiObjects.eAction.Update:
+                    result = UpdateChannel(IDs);
+                    break;
+                default:
+                    result = true;
+                    break;
+            }
+
+            return result;
+        }
+
+        private bool DeleteChannel(List<int> lIDs)
+        {
+            bool bRes = false;
+            string sIndex = m_nGroupID.ToString();
+
+            List<string> aliases = m_oESApi.GetAliases(sIndex);
+            if (aliases != null && aliases.Count > 0)
+            {
+                bRes = true;
+
+                bool bTemp;
+                foreach (int nChannelID in lIDs)
+                {
+                    foreach (string index in aliases)
+                    {
+                        bTemp = m_oESApi.DeleteDoc(PERCOLATOR, index, nChannelID.ToString());
+                        bRes &= bTemp;
+
+                        if (!bTemp)
+                        {
+                            Logger.Logger.Log("Error", string.Concat("Could not delete channel from elasticsearch. ID=", nChannelID), "ESUpdateHandler");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Logger.Logger.Log("Error", string.Concat("Could not find indices for alias ", sIndex), "ESUpdateHandler");
+            }
+
+            return bRes;
+        }
+
+        private bool UpdateChannel(List<int> lChannelIds)
+        {
+            bool bRes = false;
+            Group oGroup = GroupsCache.Instance.GetGroup(m_nGroupID);
+            if (oGroup == null || oGroup.m_oGroupChannels == null)
+                return bRes;
+
+            List<string> aliases = m_oESApi.GetAliases(m_nGroupID.ToString());
+
+            Channel oChannel;
+            MediaSearchObj oSearchObj;
+            ESMediaQueryBuilder oQueryParser;
+            string sQueryStr;
+
+            if (aliases != null && aliases.Count > 0)
+            {
+                foreach (int nChannelID in lChannelIds)
+                {
+                    oChannel = ChannelRepository.GetChannel(nChannelID, oGroup);
+                    if (oChannel != null && oChannel.m_nIsActive == 1)
+                    {
+                        oQueryParser = new ESMediaQueryBuilder() { QueryType = eQueryType.EXACT, m_nGroupID = oChannel.m_nGroupID };
+                        oSearchObj = ElasticsearchTasksCommon.Utils.BuildBaseChannelSearchObject(oChannel);
+                        oQueryParser.oSearchObject = oSearchObj;
+                        sQueryStr = oQueryParser.BuildSearchQueryString(false);
+
+                        foreach (string sIndex in aliases)
+                        {
+                            bRes = m_oESApi.AddQueryToPercolator(sIndex, oChannel.m_nChannelID.ToString(), ref sQueryStr);
+                        }
+                    }
+                }
+            }
+
+            return bRes;
+        }
+
+        
+    }
+}
