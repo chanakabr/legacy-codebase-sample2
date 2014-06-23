da1d98bc95368ab8b154abccbf5819e334d9303e
Avi Gabay <avi.g@tvinci.com>
2014-06-23 12:58:14 +0300

    elektra_ESDeleteResults

---
+++ ElasticSearch/Common/DeleteResults/ESDeleteResult.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ElasticSearch.Common.DeleteResults
+{
+    [Serializable]
+    public class ESDeleteResult
+    {
+        [JsonProperty("ok")]
+        public bool Ok { get; set; }
+        [JsonProperty("found")]
+        public bool Found { get; set; }
+        [JsonProperty("_index")]
+        public string Index { get; set; }
+        [JsonProperty("_type")]
+        public string Type { get; set; }
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+
+        public static ESDeleteResult GetDeleteResult(string response)
+        {
+            ESDeleteResult result = null;
+
+            try
+            {
+                result = JsonConvert.DeserializeObject<ESDeleteResult>(response);
+            }
+            catch (Exception ex)
+            {
+                result = new ESDeleteResult();
+                Logger.Logger.Log("Error", string.Format("Could not convert ES delete response json. input={0}; ex={1}; stack={2}", response, ex.Message, ex.StackTrace), "Elasticsearch");
+            }
+
+            return result;
+        }
+
+    }
+}
