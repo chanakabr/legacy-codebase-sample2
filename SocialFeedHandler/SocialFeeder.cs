8b98418d85d345fae161a4829ffd0a1229e22393
Avi Gabay <avi.g@tvinci.com>
2014-07-07 16:26:07 +0300

    added new remote task handler for social feeder

---
+++ SocialFeedHandler/SocialFeeder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Social;
+using SocialBL;
+
+namespace SocialFeedHandler
+{
+    public class SocialFeeder
+    {
+        public int m_nGroupID { get; set; }
+        public string m_sSiteGuid { get; set; }
+        public string m_sDbActionID { get; set; }
+
+        public SocialFeeder(int nGroupId, string sSiteGuid, string sDbActionID)
+        {
+            m_nGroupID = nGroupId;
+            m_sSiteGuid = sSiteGuid;
+            m_sDbActionID = sDbActionID;
+        }
+
+        public bool UpdateFriendsFeed()
+        {
+            bool bResult = false;
+
+            if (m_nGroupID == 0 || string.IsNullOrEmpty(m_sSiteGuid) || string.IsNullOrEmpty(m_sDbActionID))
+            {
+                throw new ArgumentNullException("Must provide a valud for group ID, site guid, and db action id");
+            }
+
+            FacebookWrapper oFBWrapper = new FacebookWrapper(m_nGroupID);
+            BaseSocialBL oSocialBL = BaseSocialBL.GetBaseSocialImpl(m_nGroupID) as BaseSocialBL;
+            
+
+            List<string> lFriendsGuid = null;
+
+            int nSiteGuid;
+            if (int.TryParse(m_sSiteGuid, out nSiteGuid))
+            {
+                int nNumOfFriends = oFBWrapper.GetUserFriendsGuid(nSiteGuid, out lFriendsGuid);
+
+                if (nNumOfFriends > 0)
+                {
+                    bResult = oSocialBL.AddActivityToUserFeed(lFriendsGuid, m_sDbActionID);
+                }
+                else
+                {
+                    Logger.Logger.Log("Info", "user with site guid {0} has no friends. action with id {1} not published to feed", "SocialFeedHandler");
+                }
+            }
+            return bResult;
+        }
+    }
+}
---
+++ SocialFeedHandler/TaskHandler.cs
@@ -0,0 +1,39 @@
+﻿using ApiObjects.MediaIndexingObjects;
+using RemoteTasksCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialFeedHandler
+{
+    public class TaskHandler : ITaskHandler
+    {
+        public string HandleTask(string data)
+        {
+            string res = "fail";
+
+            try
+            {
+                Logger.Logger.Log("Info", string.Concat("starting social feeder request. data=", data), "ESUpdateHandler");
+
+                SocialData request = Newtonsoft.Json.JsonConvert.DeserializeObject<SocialData>(data);
+
+                SocialFeeder feeder = new SocialFeeder(request.GroupId, request.ActorSiteGuid, request.DbActionId);
+                bool bResult = feeder.UpdateFriendsFeed();
+
+                if (bResult)
+                {
+                    res = "success";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return res;
+        }
+    }
+}
