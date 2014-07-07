4d7e7299e429554d4d2104f5b6037f092948a2e9
Idan Langer <idan.l@tvinci.com>
2014-07-07 12:29:02 +0300

    Added init with TCM configuration + bugfixes

---
+++ Catalog/ChannelObjRequest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Catalog
+{
+    [DataContract]
+    public class ChannelObjRequest : BaseRequest, IRequestImp
+    {
+        [DataMember]
+        public int ChannelId { get; set; }
+
+        public ChannelObjRequest(Int32 nChannelID, Int32 nGroupID, Int32 nPageSize, Int32 nPageIndex, string sUserIP, Filter oFilter, string sSignature, string sSignString, ApiObjects.SearchObjects.OrderObj oOrderObj)
+            : base(nPageSize, nPageIndex, sUserIP, nGroupID, oFilter, sSignature, sSignString)
+        {
+            ChannelId = nChannelID;
+        }
+
+                public ChannelObjRequest(ChannelObjRequest c)
+            : base(c.m_nPageSize, c.m_nPageIndex, c.m_sUserIP, c.m_nGroupID, c.m_oFilter, c.m_sSignature, c.m_sSignString)
+        {
+            ChannelId = c.ChannelId;
+        }
+
+        public BaseResponse GetResponse(BaseRequest oBaseRequest)
+        {
+            ChannelObjRequest request = (ChannelObjRequest)oBaseRequest;
+            ChannelObjResponse response = new ChannelObjResponse();
+            Group group = null;
+            Channel channel = null;
+            GroupsCache.Instance.GetGroupAndChannel(request.ChannelId, request.m_nGroupID, ref group, ref channel);
+            if (channel != null)
+            {
+                response = new ChannelObjResponse
+                {
+                    ChannelObj = channel,
+                };
+            }
+            return (BaseResponse)response;
+        }
+    }
+}
---
+++ Catalog/Crowdsourcing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ApiObjects.CrowdsourceItems.Base;
+using DAL;
+using Tvinci.Core.DAL;
+
+namespace Catalog
+{
+    public static class Crowdsourcing
+    {
+        public static List<BaseCrowdsourceItem> GetCroudsourceItems(int groupId, int languageId)
+        {
+             CatalogDAL.GetGroupLanguages(groupId);
+            return DAL.CrowdsourceDAL.GetCsList(groupId, languageId);
+        }
+    }
+}
