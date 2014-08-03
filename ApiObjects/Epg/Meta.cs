4a97c8567322f93c04b78d1d27464f4f5d7ef1cf
Liat Regev <Liat.R@tvinci.com>
2014-08-03 13:38:37 +0300

    add new files to branch

---
+++ ApiObjects/Epg/Meta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ApiObjects.Epg
+{
+    [JsonObject(Title = "Mate")]
+    public class Meta
+    {
+        [JsonProperty("MetaType")]
+        public string MetaType { get; set; }
+        
+        [JsonProperty("MetaValues")]
+        public List<MetaValues> MetaValues { get; set; } //KeyValuePair<Language, tagValue>
+    }
+
+
+    [JsonObject(Title = "MetaValues")]
+    public class MetaValues
+    {
+        [JsonProperty("MetaTranslate")]
+        public List<KeyValuePair<string, string>> MetaTranslate { get; set; } //KeyValuePair<Language, tagValue>
+    }
+}
