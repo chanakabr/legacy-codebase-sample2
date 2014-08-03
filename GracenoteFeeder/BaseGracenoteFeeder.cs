8bb15c55c3e547341854697626cb57a90adfce2f
Liat Regev <Liat.R@tvinci.com>
2014-08-03 12:29:32 +0300

    add GraceNoteFeeder changes file

---
+++ GracenoteFeeder/BaseGracenoteFeeder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace GracenoteFeeder
+{
+    public abstract class BaseGracenoteFeeder
+    {
+
+        abstract public void SaveChannel(XmlDocument xmlDoc);
+    }
+}    
