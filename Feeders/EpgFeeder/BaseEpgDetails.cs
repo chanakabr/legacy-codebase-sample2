e6e8eaaf4bd616c6920bfad11631652a7e6f93fc
Liat Regev <Liat.R@tvinci.com>
2014-08-03 14:08:25 +0300

    few more fiules

---
+++ Feeders/EpgFeeder/BaseEpgDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace EpgFeeder
+{
+     [JsonObject(Title = "BaseEpgDetails")]
+    public class BaseEpgDetails
+    {
+        [JsonProperty("ChannelId")]
+        public string ChannelId { get; set; }
+        [JsonProperty("Identifier")]
+        public string Identifier { get; set; }
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+        [JsonProperty("StartDate")]
+        public string StartDate { get; set; }
+        [JsonProperty("EndDate")]
+        public string EndDate { get; set; }
+        [JsonProperty("Pic")]
+        public string Pic { get; set; }
+        [JsonProperty("Language")]
+        public string Language { get; set; }
+    }
+}
---
+++ Feeders/EpgFeeder/Mate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace EpgFeeder
+{
+ /*   [JsonObject(Title = "Mate")]
+    public class Meta
+    {
+        [JsonProperty("MetaType")]
+        public string MetaType { get; set; }
+
+
+        [JsonProperty("MetaValues")]
+        public List<MetaValues> MetaValues { get; set; } //KeyValuePair<Language, tagValue>
+    }
+
+
+    [JsonObject(Title = "MetaValues")]
+    public class MetaValues
+    {
+        [JsonProperty("MetaTranslate")]
+        public List<KeyValuePair<string, string>> MetaTranslate { get; set; } //KeyValuePair<Language, tagValue>
+
+    }
+    */
+
+
+}
