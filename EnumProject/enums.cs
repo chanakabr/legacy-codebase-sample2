54302e7ab5ea45d404ba5387f2bbc65f7a0e3ba3
Liat Regev <Liat.R@tvinci.com>
2014-08-05 12:52:23 +0300

    insert  enum Language into the class

--- EnumProject/enums.cs
+++ EnumProject/enums.cs
@@ -5,16 +5,16 @@ using System.Text;
 
 namespace EnumProject
 {
-    public enum Language
-    {
-        English,
-        Hebrew,
-        Russian,
-        Arabic
-    }
-
     public class enums
     {
+        public enum Language
+        {
+            English,
+            Hebrew,
+            Russian,
+            Arabic
+        }
+
         public enum FieldTypes
         {
             Unknown,
