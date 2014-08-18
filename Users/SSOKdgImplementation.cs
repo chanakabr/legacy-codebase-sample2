6be2ab490459061906ac55a1354dec6adc3fa17a
Idan Langer <idan.l@tvinci.com>
2014-08-18 16:50:37 +0300

    Revert "Added application/json header to KDG-SSO"
    This reverts commit 658fa8d11d5045e4da631ae53949ed567a81af90.

--- Users/SSOKdgImplementation.cs
+++ Users/SSOKdgImplementation.cs
@@ -179,7 +179,6 @@ namespace Users
                     KdgLoginReq loginReq = new KdgLoginReq(username, password, clientIp);
                     using (WebClient client = new WebClient())
                     {
-                        client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
                         string reqStr = JsonConvert.SerializeObject(loginReq);
                         string kdgRespStr = client.UploadString(kdgUrl, reqStr);
                         kdgLoginRespObj = JsonConvert.DeserializeObject<KdgLoginResp>(kdgRespStr);
