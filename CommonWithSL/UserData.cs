fdf33721ab4729b4a0531361a22d8b1fc335d9f1
Sajed Owesat <sajed.owesat@galilsoftware.com>
2014-08-19 13:47:14 +0300

    added UserData Model

---
+++ CommonWithSL/UserData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonWithSL
+{
+    public class UserData
+    {
+        public string UserName { get; set; }
+        public string ParentalControl { get; set; }
+        public string SiteGuid { get; set; }
+    }
+}
