0da051f9ec4969c58fc02bcb356f4d2aae2c7c2c
Ofir Sudai <ofir.s@tvinci.com>
2014-09-04 09:51:41 +0300

    added logs

--- RemoteTasksService/Service.svc.cs
+++ RemoteTasksService/Service.svc.cs
@@ -23,15 +23,21 @@ namespace RemoteTasksService
 
             try
             {
+                Logger.Logger.Log("Info", string.Concat("Add Task Request Started: ", request.task), "RemoteTasksService");
+
                 string taskHandlerName = TCMClient.Settings.Instance.GetValue<string>(string.Format("CELERY_ROUTING.{0}", request.task));
 
                 ITaskHandler taskHandler = (ITaskHandler)Activator.CreateInstance(Type.GetType(string.Format("{0}.TaskHandler, {0}", taskHandlerName)));
 
                 response.retval = taskHandler.HandleTask(request.data);
                 response.status = "success";
+
+                Logger.Logger.Log("Info", string.Concat("Add Task Request Success: ", request.task), "RemoteTasksService");
             }
             catch (Exception ex)
             {
+                Logger.Logger.Log("Error", string.Format("Add Task Request Fail: {0}, Error: {1}, Stacktrace: {2}", request.task, ex.Message, ex.StackTrace), "RemoteTasksService");
+
                 response.status = "failure";
                 response.reason = ex.Message;
             }
