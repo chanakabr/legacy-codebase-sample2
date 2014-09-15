99732b2da5583e202ef7ac30f2e7842b86e008cd
Liat Regev <Liat.R@tvinci.com>
2014-09-15 11:43:06 +0300

    more changes for excel user list

--- Web Sites/TVM/adm_users_list_excel.aspx.cs
+++ Web Sites/TVM/adm_users_list_excel.aspx.cs
@@ -38,14 +38,59 @@ public partial class adm_users_list_excel : System.Web.UI.Page
 
     public string GetTableCSV()
     {
-
-        Int32 nGroupID = LoginManager.GetLoginGroupID();
-
+ 
         DataTable dtUsers = new DataTable();
         Dictionary<int, string> dUserDD = new Dictionary<int, string>();
+        
+        Int32 nGroupID = LoginManager.GetLoginGroupID();
+        string sFreeText = string.Empty;
+        int gap_between_user_ids = WS_Utils.GetTcmIntValue("gap_between_user_ids");
+        int page = 0;
+        if (Request.Form["0_val"] != null && !string.IsNullOrEmpty(Request.Form["0_val"].ToString())) // bulk for ids 
+        {
+            page = int.Parse(Request.Form["0_val"].ToString());
+        }
+        if (Request.Form["1_val"] != null && !string.IsNullOrEmpty(Request.Form["1_val"].ToString())) // Free text for username 
+        {
+            sFreeText = Request.Form["1_val"].ToString();
+        }
 
+        DataSet ds = DAL.UsersDal.Get_UsersListByBulk(nGroupID, sFreeText, gap_between_user_ids, page);
+
+        if (ds != null && ds.Tables != null && ds.Tables.Count >= 1)
+        {
+            dtUsers = ds.Tables[0];
+
+            if (ds.Tables.Count == 2)
+            {
+                foreach (DataRow dr in ds.Tables[1].Rows)
+                {
+                    int nUserID = ODBCWrapper.Utils.GetIntSafeVal(dr, "user_id");
+                    string sKey = ODBCWrapper.Utils.GetSafeStr(dr, "DATA_TYPE");
+                    string sVal = ODBCWrapper.Utils.GetSafeStr(dr, "DATA_VALUE");
+                    if (!dUserDD.ContainsKey(nUserID))
+                    {
+                        dUserDD.Add(nUserID, string.Empty);
+                    }
+                    dUserDD[nUserID] += string.Format("<{0}:{1}> ", sKey, sVal);                    
+                }
+
+                for (int i = 0; i < dtUsers.Rows.Count; i++)
+                {
+                    int nUserID = int.Parse(dtUsers.Rows[i]["id"].ToString());
+                    if (dUserDD.ContainsKey(nUserID))
+                    {
+                        dtUsers.Rows[i]["Dynamic_Data"] = dUserDD[nUserID];
+                    }
+                }
+            }
+
+
+        }
+
+        #region OLD CODE
+        /*
         ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
-
         selectQuery += "select ";
         selectQuery += "u.is_active,u.id as id,u.id as 'User ID',u.status,u.fail_count,u.Password,u.USERNAME as 'Username',u.FIRST_NAME as 'First Name',u.LAST_NAME as 'Last Name',";
         selectQuery += "u.EMAIL_ADD as 'Email Address',u.REG_AFF as 'Affiliate',u.HANDLING_STATUS as 'Open Ticket',lcs.description as 'State',ut.ID as 'User Type ID',ut.description as 'User Type'";
@@ -60,12 +105,12 @@ public partial class adm_users_list_excel : System.Web.UI.Page
         string sFreeText = "";
         if (Request.Form["1_val"] != null && !string.IsNullOrEmpty(Request.Form["1_val"].ToString())) // Free text for username 
         {
-            sFreeText = Request.Form["1_val"].ToString();               
+            sFreeText = Request.Form["1_val"].ToString();
             string sLike = "like(N'%" + sFreeText + "%')";
             selectQuery += " and (u.USERNAME " + sLike + " or u.FIRST_NAME " + sLike + " or u.LAST_NAME " + sLike + " or u.EMAIL_ADD " + sLike + ")";
         }
 
-        int gap_between_user_ids = WS_Utils.GetTcmIntValue("gap_between_user_ids");
+       
         int fromID = 0;
         int toID = 0;
         if (Request.Form["0_val"] != null && !string.IsNullOrEmpty(Request.Form["0_val"].ToString())) // bulk for ids 
@@ -76,7 +121,7 @@ public partial class adm_users_list_excel : System.Web.UI.Page
             fromID = minUserID + int.Parse(Request.Form["0_val"].ToString()) * gap_between_user_ids;
             toID = fromID + gap_between_user_ids;
 
-            selectQuery += " and ";        
+            selectQuery += " and ";
             selectQuery += ODBCWrapper.Parameter.NEW_PARAM("u.id", ">=", fromID);
             selectQuery += " and ";
             selectQuery += ODBCWrapper.Parameter.NEW_PARAM("u.id", "<=", toID);
@@ -97,7 +142,7 @@ public partial class adm_users_list_excel : System.Web.UI.Page
         }
         selectQuery.Finish();
         selectQuery = null;
-
+        
         if (dUserDD.Keys.Count == 0)
         {
             return "";
@@ -134,7 +179,8 @@ public partial class adm_users_list_excel : System.Web.UI.Page
             dtUsers.Rows[i]["Dynamic_Data"] = dUserDD[nUserID];
 
         }
-
+        */
+        #endregion
         GridView gv = new GridView();
 
         gv.DataSource = dtUsers;
@@ -167,13 +213,7 @@ public partial class adm_users_list_excel : System.Web.UI.Page
         theRecord.SetConnectionKey("users_connection");
              
         // get the default gap from confuguration
-        int nBulk =  WS_Utils.GetTcmIntValue("gap_between_user_ids");
-        int minUserID = 0;
-        int maxUserID = 0;
-        DataTable dt = DAL.UsersDal.getUserMinMaxID(LoginManager.GetLoginGroupID(), nBulk, ref minUserID, ref maxUserID);
-        Session["minUserID"] = minUserID;
-        Session["maxUserID"] = maxUserID;
-
+        DataTable dt = BuildUserGapTable();        
         DataRecordDropDownField dr_use_list = new DataRecordDropDownField("", "txt", "ID", string.Empty, string.Empty, 60, true);
         dr_use_list.SetSelectsDT(dt);
         dr_use_list.Initialize("user gap list", "adm_table_header_nbg", "FormInput", "gapList", false);
@@ -191,7 +231,52 @@ public partial class adm_users_list_excel : System.Web.UI.Page
         string sTable = theRecord.GetTableHTML("adm_users_list_excel.aspx?submited=1");
 
         return sTable;
-    } 
+    }
+
+    private DataTable BuildUserGapTable()
+    {
+        try
+        {
+            int nBulk = WS_Utils.GetTcmIntValue("gap_between_user_ids");
+            int numOfIDs = 0;
+            int numOfBulks = 0;
+            ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
+            selectQuery.SetConnectionKey("USERS_CONNECTION_STRING");
+            selectQuery.SetCachedSec(0);
+
+            selectQuery += "SELECT  count (id) as numOfIDs  from  users u (nolock) WHERE IS_ACTIVE=1 AND STATUS=1 AND ";
+            selectQuery += ODBCWrapper.Parameter.NEW_PARAM("GROUP_ID", "=", LoginManager.GetLoginGroupID());
+
+            if (selectQuery.Execute("query", true) != null)
+            {
+                Int32 nCount = selectQuery.Table("query").DefaultView.Count;
+                if (nCount > 0)
+                {
+                    numOfIDs = int.Parse(selectQuery.Table("query").DefaultView[0].Row["numOfIDs"].ToString());
+                }
+            }
+            selectQuery.Finish();
+            selectQuery = null;
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id", typeof(int));
+            dt.Columns.Add("txt", typeof(string));
+
+            int div = (numOfIDs) / nBulk;
+            int mod = (numOfIDs) % nBulk > 0 ? 1 : 0;
+            numOfBulks = div + mod;
+
+            for (int i = 0; i < numOfBulks; i++)
+            {
+                dt.Rows.Add(i+1, (i * nBulk).ToString() + " - " + ((i + 1) * nBulk).ToString());
+            }
+            return dt;
+        }
+        catch (Exception ex)
+        {
+            return new DataTable();
+        }
+    }
 
 
 }
