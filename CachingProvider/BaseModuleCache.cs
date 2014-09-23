06d0ec11053f1a59a2c6bb882706857fe1614296
Liat Regev <Liat.R@tvinci.com>
2014-09-23 15:11:10 +0300

    no message

---
+++ CachingProvider/BaseModuleCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CachingProvider
+{
+    public class BaseModuleCache
+    {
+        public object result {get;set;}
+        public BaseModuleCache()
+        {
+        }
+    }
+}
---
+++ CachingProvider/VersionModuleCache.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CachingProvider
+{
+    public class VersionModuleCache : BaseModuleCache
+    {
+        public string version { get; set; }
+
+        public VersionModuleCache()
+            : base()
+        {
+            version = string.Empty;
+        }
+    }
+}
