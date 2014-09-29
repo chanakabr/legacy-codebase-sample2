723364ccb2d863679a00170cdd700c8c9730ea8b
Ira Yanai <iray@tvinci.com>
2014-09-29 10:01:22 +0300

    UserAndPass -> UserCredentials

---
+++ ApiObjects/UserCredentials.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApiObjects
+{
+    public class UserCredentials
+    {
+        public string m_sUsername;
+        public string m_sPassword;
+
+        public UserCredentials()
+        {
+            this.m_sUsername = string.Empty;
+            this.m_sPassword = string.Empty;
+        }
+    }
+}
