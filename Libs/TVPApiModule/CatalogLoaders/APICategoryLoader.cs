a718728198a80e31b560dd8aebbc1066b3c2c79c
Irena Leibman <irena.leibman@tvinci.com>
2014-11-02 11:12:50 +0200

    Addition APICategoryLoader

---
+++ Libs/TVPApiModule/CatalogLoaders/APICategoryLoader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tvinci.Data.Loaders;
+using Tvinci.Data.Loaders.TvinciPlatform.Catalog;
+using TVPApi;
+using TVPApiModule.Manager;
+using TVPPro.SiteManager.CatalogLoaders;
+
+namespace TVPApiModule.CatalogLoaders
+{
+    public class APICategoryLoader : CategoryLoader
+    {
+        private string m_sCulture;
+
+        public string Culture
+        {
+            get { return m_sCulture; }
+            set
+            {
+                m_sCulture = value;
+                Language = TextLocalizationManager.Instance.GetTextLocalization(GroupID, (PlatformType)Enum.Parse(typeof(PlatformType), Platform)).GetLanguageDBID(value);
+            }
+        }
+
+        public  object Execute()
+        {
+            CategoryResponse retVal = null;
+            BuildRequest();
+            Log("TryExecuteGetBaseResponse:", m_oRequest);
+            if (m_oProvider.TryExecuteGetBaseResponse(m_oRequest, out m_oResponse) == eProviderResult.Success)
+            {
+                Log("Got:", m_oResponse);
+                retVal = m_oResponse as CategoryResponse;
+            }
+            return retVal;
+        }
+
+        public APICategoryLoader(int groupID, string platform, string userIP, int categoryID, string language) :
+            base(groupID, userIP, categoryID)
+        {
+            Platform = platform;
+            Culture = language;
+        }
+    }
+}
