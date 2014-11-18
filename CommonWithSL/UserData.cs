9751022b2d4f3e9983e1176186c0448e2505744b
Sajed Owesat <sajed.owesat@galilsoftware.com>
2014-11-18 10:25:32 +0200

    no message

--- CommonWithSL/UserData.cs
+++ CommonWithSL/UserData.cs
@@ -8,7 +8,21 @@ namespace CommonWithSL
     public class UserData
     {
         public string UserName { get; set; }
-        public string ParentalControl { get; set; }
+        private string _parentalControl = "18";
+        public string ParentalControl
+        {
+            get
+            {
+                return _parentalControl;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _parentalControl = value;
+                }
+            }
+        }
         public string SiteGuid { get; set; }
     }
 }
