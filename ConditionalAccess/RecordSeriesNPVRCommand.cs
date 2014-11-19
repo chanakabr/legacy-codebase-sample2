4f469d6073b462776791f12e2a1dde1f6e61a040
Gad Meyer <gad.m@tvinci.com>
2014-11-19 18:11:17 +0200

    added retrieve quota npvr command

---
+++ ConditionalAccess/RecordSeriesNPVRCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConditionalAccess
+{
+    public class RecordSeriesNPVRCommand : BaseNPVRCommand
+    {
+        protected override NPVRResponse ExecuteFlow(BaseConditionalAccess cas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
