009d50846a5bbf2eb10c9c82cb5333331b04e52c
Gad Meyer <gad.m@tvinci.com>
2014-11-20 15:30:15 +0200

    almost finished create account and delete account

--- NPVR/AlcatelLucentNPVR.cs
+++ NPVR/AlcatelLucentNPVR.cs
@@ -14,16 +14,21 @@ namespace NPVR
     {
         private static readonly string ALU_LOG_FILE = "AlcatelLucent";
         private static readonly string LOG_HEADER_EXCEPTION = "Exception";
+        private static readonly string LOG_HEADER_ERROR = "Error";
 
-        private static readonly string ALU_RESPONSE_FORM = "json";
-
-        private static readonly string ALU_ENDPOINT_RECORD = "Record";
-        private static readonly string ALU_ENDPOINT_SEASON = "Season"; // Series
-        private static readonly string ALU_ENDPOINT_USER = "User";
+        private static readonly string ALU_GENERIC_BODY = "scheduler/web/";
+        private static readonly string ALU_ENDPOINT_RECORD = "Record/";
+        private static readonly string ALU_ENDPOINT_SEASON = "Season/"; // Series
+        private static readonly string ALU_ENDPOINT_USER = "User/";
 
         private static readonly string ALU_CREATE_ACCOUNT_COMMAND = "addById";
         private static readonly string ALU_DELETE_ACCOUNT_COMMAND = "delete";
         private static readonly string ALU_GET_QUOTA_COMMAND = "getProfile";
+
+        private static readonly string ALU_FORM_URL_PARAM = "form";
+        private static readonly string ALU_QUOTA_URL_PARAM = "quota";
+        private static readonly string ALU_SCHEMA_URL_PARAM = "schema";
+        private static readonly string ALU_USER_ID_URL_PARAM = "userId";
 
         private int groupID;
 
@@ -37,9 +42,17 @@ namespace NPVR
             return args != null && args.Quota > 0 && !string.IsNullOrEmpty(args.EntityID);
         }
 
-        private string BuildUserEndpointRestCommand()
+        private string GetLogFilename()
         {
-            throw new NotImplementedException();
+            return String.Concat(ALU_LOG_FILE, "_", groupID);
+        }
+
+        private string BuildUserEndpointRestCommand(string method, string endPoint, List<KeyValuePair<string, string>> urlParams)
+        {
+            string baseUrl = TVinciShared.WS_Utils.GetTcmGenericValue<string>(String.Concat("ALU_BASE_URL_", groupID));
+            bool isAddSlash = baseUrl.EndsWith("/");
+            return String.Concat(baseUrl, isAddSlash ? "/" : string.Empty, ALU_GENERIC_BODY, endPoint, method, "?", 
+                TVinciShared.WS_Utils.BuildDelimiterSeperatedString(urlParams, "&", false, false));
         }
 
         public NPVRUserActionResponse CreateAccount(NPVRParamsObj args)
@@ -49,7 +62,29 @@ namespace NPVR
             {
                 if (IsCreateInputValid(args))
                 {
-                    Logger.Logger.Log("Create", string.Format("Create request has been issued. G ID: {0} , Params Obj: {1}", groupID, args.ToString()), ALU_LOG_FILE);
+                    Logger.Logger.Log("CreateAccount", string.Format("CreateAccount request has been issued. G ID: {0} , Params Obj: {1}", groupID, args.ToString()), GetLogFilename());
+                    List<KeyValuePair<string, string>> urlParams = new List<KeyValuePair<string, string>>(3);
+                    urlParams.Add(new KeyValuePair<string, string>(ALU_QUOTA_URL_PARAM, args.Quota.ToString()));
+                    urlParams.Add(new KeyValuePair<string,string>(ALU_SCHEMA_URL_PARAM, "1.0"));
+                    urlParams.Add(new KeyValuePair<string,string>(ALU_USER_ID_URL_PARAM, args.EntityID));
+
+                    string url = BuildUserEndpointRestCommand(ALU_CREATE_ACCOUNT_COMMAND, ALU_ENDPOINT_USER, urlParams);
+                    int httpStatusCode = 0;
+                    string responseJson = string.Empty;
+                    string errorMsg = string.Empty;
+                    if (TVinciShared.WS_Utils.TrySendHttpGetRequest(url, Encoding.UTF8, ref httpStatusCode, ref responseJson, ref errorMsg))
+                    {
+                        // parse here the json, understand the response and fill the response object
+                    }
+                    else
+                    {
+                        // log here the error. 
+                        Logger.Logger.Log(LOG_HEADER_ERROR, string.Format("CreateAccount. An error occurred while trying to contact ALU REST interface. G ID: {0} , Params Obj: {1} , HTTP Status Code: {2} , Info: {3}", groupID, args.ToString(), httpStatusCode, errorMsg), GetLogFilename());
+                        res.isOK = false;
+                        res.msg = "An error occurred. Refer to server log files.";
+                        res.quota = 0;
+                        res.entityID = string.Empty;
+                    }
 
                 }
                 else
@@ -59,7 +94,7 @@ namespace NPVR
             }
             catch (Exception ex)
             {
-                Logger.Logger.Log(LOG_HEADER_EXCEPTION, GetLogMsg("Exception at Create.", args, ex), ALU_LOG_FILE);
+                Logger.Logger.Log(LOG_HEADER_EXCEPTION, GetLogMsg("Exception at CreateAccount.", args, ex), GetLogFilename());
                 throw;
             }
 
@@ -78,7 +113,30 @@ namespace NPVR
             {
                 if (IsDeleteInputValid(args))
                 {
-                    Logger.Logger.Log("Delete", string.Format("Delete request has been issued. G ID: {0} , Params Obj: {1}", groupID, args.ToString()), ALU_LOG_FILE);
+                    Logger.Logger.Log("DeleteAccount", string.Format("DeleteAccount request has been issued. G ID: {0} , Params Obj: {1}", groupID, args.ToString()), GetLogFilename());
+                    List<KeyValuePair<string, string>> urlParams = new List<KeyValuePair<string, string>>(3);
+                    urlParams.Add(new KeyValuePair<string, string>(ALU_SCHEMA_URL_PARAM, "1.0"));
+                    urlParams.Add(new KeyValuePair<string, string>(ALU_FORM_URL_PARAM, "json"));
+                    urlParams.Add(new KeyValuePair<string, string>(ALU_USER_ID_URL_PARAM, args.EntityID));
+
+                    string url = BuildUserEndpointRestCommand(ALU_DELETE_ACCOUNT_COMMAND, ALU_ENDPOINT_USER, urlParams);
+                    int httpStatusCode = 0;
+                    string responseJson = string.Empty;
+                    string errorMsg = string.Empty;
+
+                    if (TVinciShared.WS_Utils.TrySendHttpGetRequest(url, Encoding.UTF8, ref httpStatusCode, ref responseJson, ref errorMsg))
+                    {
+                        // parse here the json, understand the response and fill the result obj.
+                    }
+                    else
+                    {
+                        // log here the error
+                        Logger.Logger.Log(LOG_HEADER_ERROR, string.Format("DeleteAccount. An error occurred while trying to contact ALU REST interface. G ID: {0} , Params Obj: {1} , HTTP Status Code: {2} , Info: {3}", groupID, args.ToString(), httpStatusCode, errorMsg), GetLogFilename());
+                        res.isOK = false;
+                        res.msg = "An error occurred. Refer to server log files.";
+                        res.quota = 0;
+                        res.entityID = args.EntityID;
+                    }
                 }
                 else
                 {
@@ -87,7 +145,7 @@ namespace NPVR
             }
             catch (Exception ex)
             {
-                Logger.Logger.Log(LOG_HEADER_EXCEPTION, GetLogMsg("Exception at Delete.", args, ex), ALU_LOG_FILE);
+                Logger.Logger.Log(LOG_HEADER_EXCEPTION, GetLogMsg("Exception at DeleteAccount.", args, ex), GetLogFilename());
                 throw;
             }
 
