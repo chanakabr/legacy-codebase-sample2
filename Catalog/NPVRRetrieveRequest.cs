83769a7b8dc6d0bfce73c3130b83d528c0f08d5d
Gad Meyer <gad.m@tvinci.com>
2014-12-01 18:03:42 +0200

    added npvr retrieval request to catalog

---
+++ Catalog/NPVRRetrieveRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Catalog
+{
+    [DataContract]
+    public class NPVRRetrieveRequest : BaseRequest, IRequestImp
+    {
+        public BaseResponse GetResponse(BaseRequest oBaseRequest)
+        {
+            NPVRRetrieveResponse res = new NPVRRetrieveResponse();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Logger.Log("Exception", string.Format("Exception at GetResponse. Req: {0} , Ex Msg: {1} , Ex Type: {2} , ST: {3}", ToString(), ex.Message, ex.GetType().Name, ex.StackTrace), "NPVRRetrieveRequest");
+                throw ex;
+            }
+
+            return res;
+        }
+    }
+}
