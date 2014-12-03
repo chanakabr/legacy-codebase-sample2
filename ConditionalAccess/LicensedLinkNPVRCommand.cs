dc7c91b8fc50e3d67cc1e19ca3adeb63f7c504b0
Gad Meyer <gad.m@tvinci.com>
2014-12-03 10:59:35 +0200

    npvr licensed link

--- ConditionalAccess/LicensedLinkNPVRCommand.cs
+++ ConditionalAccess/LicensedLinkNPVRCommand.cs
@@ -7,11 +7,29 @@ namespace ConditionalAccess
 {
     public class LicensedLinkNPVRCommand : BaseNPVRCommand
     {
+        public DateTime startTime;
+        public int format;
+        public int mediaFileID;
+        public string basicLink;
+        public string userIP;
+        public string referrer;
+        public string countryCd;
+        public string langCd;
+        public string couponCode;
 
         protected override NPVRResponse ExecuteFlow(BaseConditionalAccess cas)
         {
             LicensedLinkNPVRResponse res = new LicensedLinkNPVRResponse();
-            throw new NotImplementedException("Add here the correct params.");
+            res.mainUrl = cas.GetEPGLink(assetID, startTime, format, siteGuid, mediaFileID, basicLink, userIP, referrer, countryCd, langCd,
+                udid, couponCode);
+            if (!string.IsNullOrEmpty(res.mainUrl))
+            {
+                res.status = NPVRStatus.OK.ToString();
+            }
+            else
+            {
+                res.status = NPVRStatus.Error.ToString();
+            }
 
             return res;
         }
