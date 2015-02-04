5431e559ba46c6a70ef1a8079b25f9665a6c40e7
Oded Shafran <oded@shafranet.com>
2015-02-04 11:16:12 +0200

    Doc Generator

---
+++ WS_TVPApi/App_Code/WS_Code/RequestDescription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[System.AttributeUsage(System.AttributeTargets.Method, AllowMultiple = true)]
+public class RequestDescription : System.Attribute
+{
+    public string paramName, paramDesc;
+     
+    public RequestDescription(string paramName, string paramDesc)
+    {
+        this.paramName = paramName;
+        this.paramDesc = paramDesc;
+    }
+}
