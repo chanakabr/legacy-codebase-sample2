ec79cc2a01c190efaf841c0848d597611c635a71
Amishai Guilburd <amishai.guilburd@kaltura.com>
2015-02-04 11:47:53 +0200

    updated Status object

--- Libs/TVPApiModule/Objects/Status.cs
+++ Libs/TVPApiModule/Objects/Status.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace TVPApiModule.Objects
 {
     public class Status
     {
+        [JsonProperty(PropertyName = "code")]
         public int Code { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
     }
 }
