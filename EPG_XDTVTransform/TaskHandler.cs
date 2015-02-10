fa8dc30230f9dbc6d195673e990429fdafd2b54e
Ira Yanai <iray@tvinci.com>
2015-02-10 17:05:25 +0200

    GetAllEpgChannelsDic

--- EPG_XDTVTransform/TaskHandler.cs
+++ EPG_XDTVTransform/TaskHandler.cs
@@ -9,6 +9,7 @@ using System.Xml;
 using System.IO;
 using Tvinci.Core.DAL;
 using System.IO.Compression;
+using ApiObjects.Epg;
 
 
 namespace EPG_XDTVTransform
@@ -28,19 +29,19 @@ namespace EPG_XDTVTransform
 
             xmlDoc.LoadXml(sXml);
 
-            Dictionary<int, int> channelID_DB_ALU = getALUIDs();    
-            Dictionary<string, KeyValuePair<int, string>> channelDic = EpgDal.GetAllEpgChannelsDic(request.nGroupID);   
+            Dictionary<int, int> channelID_DB_ALU = getALUIDs();
+            Dictionary<string, EpgChannelObj> channelDic = EpgDal.GetAllEpgChannelsDic(request.nGroupID);
        
             XmlNodeList xmlChannel = xmlDoc.GetElementsByTagName("TVGRIDBATCH");
             string sChannelID = Utils.GetSingleNodeValue(xmlChannel[0], "GN_ID");        
 
             if (channelDic.ContainsKey(sChannelID))
             {
-                int nChannelIDDB = channelDic[sChannelID].Key;
+                int nChannelIDDB = channelDic[sChannelID].ChannelId;
                 if (channelID_DB_ALU.ContainsKey(nChannelIDDB))
                 {
                     int nChannelIDALU = channelID_DB_ALU[nChannelIDDB];
-                    string sChannelName = channelDic[sChannelID].Value;
+                    string sChannelName = channelDic[sChannelID].ChannelName;
 
                     //tranform to xdtv and get response to send to ALU
                     res = TransformAndResponseALU(xmlDoc, nChannelIDALU, sChannelName);
@@ -50,6 +51,7 @@ namespace EPG_XDTVTransform
                     Logger.Logger.Log("InsertProgramsPerChannel", string.Format("no ALU channel ID found for channel {0}. channel cannot be sent to ALU", nChannelIDDB), "EPG_XDTVTransform");
                 }
             }
+
             return res;
         }
 
