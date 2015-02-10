07112a598f8da393b97cd1c3b991a92348c5abd3
Ira Yanai <iray@tvinci.com>
2015-02-10 15:07:21 +0000

    no message

--- ConditionalAccess/Response/BillingResponse.cs
+++ ConditionalAccess/Response/BillingResponse.cs
@@ -9,20 +9,20 @@ namespace ConditionalAccess
     {
         public BillingResponse()
         {
-            m_oStatus = BillingResponseStatus.UnKnown;
+            m_oStatus = TvinciBilling.BillingResponseStatus.UnKnown;
             m_sRecieptCode = "";
             m_sStatusDescription = "";
         }
 
         public BillingResponse(TvinciBilling.BillingResponse toCopy)
         {
-            m_oStatus = (BillingResponseStatus)(toCopy.m_oStatus);
+            m_oStatus = (TvinciBilling.BillingResponseStatus)(toCopy.m_oStatus);
             m_sRecieptCode = toCopy.m_sRecieptCode;
             m_sStatusDescription = toCopy.m_sStatusDescription;
             m_sExternalReceiptCode = toCopy.m_sExternalReceiptCode;
         }
 
-        public BillingResponseStatus m_oStatus;
+        public TvinciBilling.BillingResponseStatus m_oStatus;
         public string m_sRecieptCode;
         public string m_sStatusDescription;
         public string m_sExternalReceiptCode;
