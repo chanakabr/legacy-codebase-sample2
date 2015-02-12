8a2e863d6f0ba1723abdc7821c0f34248a4d12bd
Amishai Guilburd <amishai.guilburd@kaltura.com>
2015-02-12 15:36:22 +0200

    New Response Object

---
+++ ApiObjects/Response/StatusWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ApiObjects.Response
+{
+    public class StatusWrapper
+    {
+        public StatusWrapper()
+        {
+            Status = new Status();
+        }
+
+        [JsonProperty(PropertyName = "status")]
+        public Status Status { get; set; }
+    }
+}
