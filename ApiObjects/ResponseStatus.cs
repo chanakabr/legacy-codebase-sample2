0917572d4df23ab146560070a6bb52e1ee0f84c9
Amishai Guilburd <amishai.guilburd@kaltura.com>
2015-02-12 15:16:03 +0200

    New Response Object

---
+++ ApiObjects/ResponseStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApiObjects
+{
+    public enum eResponseStatus
+    {
+        OK = 0,
+        Error = 1,
+
+        // Domain Section 1000 - 1999
+
+        DomainAlreadyExists = 1000,
+        ExceededLimit = 1001,
+        DeviceTypeNotAllowed = 1002,
+        DeviceNotInDomin = 1003,
+        MasterEmailAlreadyExists = 1004,
+        UserNotInDomain = 1005,
+        DomainNotExists = 1006,
+        HouseholdUserFailed = 1007,
+        DomainCreatedWithoutNPVRAccount = 1008,
+        DomainSuspended = 1009,
+        DlmNotExist = 1010,
+        WrongPasswordOrUserName = 1011,
+        NoDlmId = 1012,
+
+        // User Section 2000 - 2999
+
+    }
+}
---
+++ ApiObjects/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ApiObjects
+{
+    public class Status
+    {
+        [JsonProperty(PropertyName = "code")]
+        public int Code { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+    }
+}
