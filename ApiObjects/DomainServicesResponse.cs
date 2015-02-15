8032f3724d5cb4c137d1219386077dd6ed29db96
Irena Leibman <irena.leibman@kaltura.com>
2015-02-15 19:03:59 +0200

    DomainServicesResponse addition

---
+++ ApiObjects/DomainServicesResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApiObjects
+{
+    public class DomainServicesResponse
+    {
+        public StatusObject Status { get; set; }
+
+        public List<ServiceObject> Services { get; set; }
+
+        public DomainServicesResponse(StatusObject status, List<ServiceObject> services)
+        {
+            Status = status;
+            Services = services;
+        }
+
+        public DomainServicesResponse(int code, List<ServiceObject> services)
+        {
+            Status = new StatusObject(code);
+            Services = services;
+        }
+
+        public DomainServicesResponse()
+        {
+            Status = new StatusObject();
+        }
+    }
+}
