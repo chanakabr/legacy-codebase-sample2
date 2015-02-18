73fff676e4611251acc36c6a7447226810ca21c9
Gilad Matkovitch <gilad.m@tvinci.com>
2015-02-18 18:23:29 +0200

    New objects for social operations

---
+++ TVPApiModule/Objects/Responses/ActionProperties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TVPApiModule.Objects.Responses
+{
+    public class ActionProperties
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+}
