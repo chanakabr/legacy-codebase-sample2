65add740a5c4703dc252eae2f8d423d411540fc7
Irena Leibman <irena.leibman@kaltura.com>
2015-03-05 16:24:21 +0200

    Addition of DataContract and DataMember

--- ApiObjects/Response/Status.cs
+++ ApiObjects/Response/Status.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
+using System.Runtime.Serialization;
 
 
 namespace ApiObjects.Response
 {
     [Serializable]
+    [DataContract]
     public class Status
     {
+        [DataMember]
         public int Code { get; set; }
 
+        [DataMember]
         public string Message { get; set; }
 
         public Status(int code = 0, string message = "")
