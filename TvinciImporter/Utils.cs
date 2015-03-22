99679e8ff34edf4232eb9b098e2a965ec835f602
Amishai Guilburd <amishai.guilburd@kaltura.com>
2015-03-22 10:59:09 +0200

    policy fix

---
+++ TvinciImporter/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TvinciImporter
+{
+    public class Utils
+    {
+        public static byte[] Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                return sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+            }
+        }
+    }
+}
