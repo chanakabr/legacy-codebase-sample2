c35564bd7cb6b8064bb05f1303e2f805c7e2c899
Irena Leibman <irena.leibman@kaltura.com>
2015-03-30 19:33:47 +0300

    Addition of new component for channel ordering in new/edit region page (adm_group_regions_new.aspx)

---
+++ Web Sites/TVM/components/duallist/js/number.js
@@ -0,0 +1,142 @@
+var Number = function (params, attachToElement) {
+    var channelNumber, channelNumberNotEmpty;
+    if (params.ChannelNumber) {
+        channelNumber = params.ChannelNumber || "No number";
+        channelNumberNotEmpty = (params.ChannelNumber) ? true : false;
+    } else {
+        channelNumber = "";
+        channelNumberNotEmpty = false;
+    }
+
+    var numberComponent;
+
+    var init = function () {
+        createNumberComponent();
+        bindEvents();
+    };
+
+    var createNumberComponent = function () {
+        /// create icon + channelNumber        
+        //var protoTypeDOM = document.createElement('div');
+        //numberComponent = protoTypeDOM.cloneNode(false);
+        //$(numberComponent).addClass('calendar-comp');
+
+        $(attachToElement).append($('<input class="channel-number" />').val(channelNumber));
+
+        //debugger;
+        //var numberComponent = document.createElement('div');//protoTypeDOM.cloneNode(false);
+        //$(numberComponent).addClass('date');
+        //$(numberComponent).addClass('start-date');
+
+        //var numberInput = document.createElement('input');        
+        //numberInput.setAttribute('data-date-verify', 'true');
+        //numberInput.setAttribute('placeholder', '');
+        ////$(numberInput).addClass('has-placeholder');
+        //numberInput.value = channelNumber;
+        //var minusSpan = document.createElement('span');
+        //minusSpan.innerHTML = '-';
+
+        //var calendarIcon = document.createElement('a');
+        //calendarIcon.setAttribute('href', 'javascript:;');
+        //$(calendarIcon).addClass('calendar-icon');
+
+        //startDateTooltip.appendChild(tooltipTriangle.cloneNode(true));
+        //startDivInput.appendChild(startInput);
+        //startDivInput.appendChild(startDateTooltip);
+
+        //numberComponent.appendChild(numberInput);
+        //numberComponent.appendChild(minusSpan);
+        //numberComponent.appendChild(endDivInput);
+        //if (startDateNotEmpty || endDateNotEmpty) {
+        //    $(attachToElement).addClass('has-date');
+        //    $(calendarComponent).find('input').attr('disabled','disabled');
+        //} else {
+        //    $(calendarComponent).addClass('hidden');
+        //}        
+        //attachToElement.appendChild(calendarIcon);
+    };
+
+    var bindEvents = function () {
+        var $numberComponent = $(attachToElement);
+        //$calendarComponent.on('click', '.calendar-icon', calendarIconClickedHandler);
+        $numberComponent.find('input[class="channel-number"]').on('blur', inputBlurHandler);
+        //$numberComponent.find('input[data-date-verify]').on('focus', inputFocusHandler);
+    };
+
+    // Handlers
+    //var calendarIconClickedHandler = function (event) {
+    //    var $target = $(event.target);
+    //    $target.toggleClass('active');
+    //    var $listItem = $target.parents('li');
+    //    if ($listItem.hasClass('has-date')) {
+    //        $listItem.removeClass('has-date');
+    //        $listItem.find('input').removeAttr('disabled');
+    //        $listItem.find('.start-date input').val(params.StartDate);
+    //        $listItem.find('.end-date input').val(params.EndDate);
+    //    }
+    //    var $calendarComp = $listItem.find('.calendar-comp');
+    //    if ($target.hasClass('active')) {
+    //        $calendarComp.removeClass('hidden');
+    //    } else {
+    //        $calendarComp.addClass('hidden');
+    //    }
+    //};
+
+
+    var inputBlurHandler = function () {
+        var isTruthy = true;//verifyDate(this);
+        var $this = $(this);
+        var $numberComp = $(numberComponent);
+        var $inputContainer = $this.parent('.date');
+        var channelNumber = $(this).val();
+        if (!isTruthy) {
+            $inputContainer.find('.tooltip').removeClass('hidden');
+        } else {
+            var itemId = this.parentElement.getAttribute('data-id');
+            changeItemNumber(itemId, channelNumber);
+        }
+
+        if ($inputContainer.hasClass('channel-number')) {
+            params.ChannelNumber = channelNumber || "No number";
+        };
+
+        //var inputFocusHandler = function () {
+        //    var $this = $(this);
+        //    var $inputContainer = $this.parent('.date');
+        //    $inputContainer.find('.tooltip').addClass('hidden');
+        //};
+
+
+        //var verifyDate = function (dateInput) {
+        //    var value = $(dateInput).val();
+        //    var dateArray = value.split('/');
+        //    if(!value){
+        //        return true;
+        //    }
+        //    if (dateArray.length != 3) {
+        //        return false;
+        //    }
+
+        //    var day = parseInt(dateArray[0]);
+        //    var month = parseInt(dateArray[1]);
+        //    var year = parseInt(dateArray[2]);
+
+        //    if (day && day < 1 && day > 31) {
+        //        return false;
+        //    }
+
+        //    if (month && month < 1 && month > 12) {
+        //        return false;
+        //    }
+
+        //    if (year && year < 2000 && year > 2200) {
+        //        return false;
+        //    }
+        //    return true;
+        //};
+
+        
+    }
+
+    init();
+}
