878121347ece6206a18223d3d4dc331b3b7841dd
Liat Regev <liatr@tvinci.com>
2015-04-01 08:20:27 +0300

    lookup tables

---
+++ Web Sites/TVM/adm_generic_lookup_table.aspx.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TVinciShared;
+
+public partial class adm_generic_lookup_table : System.Web.UI.Page
+{  
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        if (LoginManager.IsPagePermitted() == false)
+            LoginManager.LogoutFromSite("login.html");
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        if (!IsPostBack)
+        {
+            Int32 nMenuID = 0;
+            m_sMenu = TVinciShared.Menu.GetMainMenu(5, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 1, false);
+            if (Request.QueryString["search_save"] != null)
+                Session["search_save"] = "1";
+            else
+                Session["search_save"] = null;
+        }
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetTableCSV()
+    {
+        string sOldOrderBy = "";
+        if (Session["order_by"] != null)
+            sOldOrderBy = Session["order_by"].ToString();
+        DBTableWebEditor theTable = new DBTableWebEditor(true, true, false, "", "adm_table_header", "adm_table_cell", "adm_table_alt_cell", "adm_table_link", "adm_table_pager", "adm_table", sOldOrderBy, 50);
+        FillTheTableEditor(ref theTable, sOldOrderBy);
+
+        string sCSVFile = theTable.OpenCSV();
+        theTable.Finish();
+        theTable = null;
+        return sCSVFile;
+    }
+
+    protected void FillTheTableEditor(ref DBTableWebEditor theTable, string sOrderBy)
+    {
+        Int32 nGroupID = LoginManager.GetLoginGroupID();
+
+        theTable += " SELECT id ,  Name , status, is_active FROM lu_lookup where status = 1 and is_active = 1";
+        theTable.AddHiddenField("ID");
+        theTable.AddHiddenField("status");
+        theTable.AddHiddenField("is_active");
+
+
+        DataTableLinkColumn linkColumnTable = new DataTableLinkColumn("adm_generic_lookup_table_values.aspx", "Upload Table", "");
+        linkColumnTable.AddQueryStringValue("lookup_id", "field=id");
+        theTable.AddLinkColumn(linkColumnTable);
+
+        if (LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.PUBLISH) &&
+            LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.EDIT))
+        {
+            theTable.AddActivationField("lu_lookup");
+        }
+
+        if (LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.EDIT))
+        {
+            DataTableLinkColumn linkColumn1 = new DataTableLinkColumn("adm_generic_lookup_table_new.aspx", "Edit", "");
+            linkColumn1.AddQueryStringValue("lookup_id", "field=id");
+            theTable.AddLinkColumn(linkColumn1);
+        }
+
+        if (LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.REMOVE))
+        {
+            DataTableLinkColumn linkColumn = new DataTableLinkColumn("adm_generic_remove.aspx", "Delete", "STATUS=1;STATUS=3");
+            linkColumn.AddQueryStringValue("id", "field=id");
+            linkColumn.AddQueryStringValue("table", "lu_lookup");
+            linkColumn.AddQueryStringValue("confirm", "true");
+            linkColumn.AddQueryStringValue("main_menu", "5");
+            linkColumn.AddQueryStringValue("sub_menu", "1");
+            linkColumn.AddQueryStringValue("rep_field", "username");
+            linkColumn.AddQueryStringValue("rep_name", "Username");
+            theTable.AddLinkColumn(linkColumn);
+        }
+
+        if (LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.PUBLISH))
+        {
+            DataTableLinkColumn linkColumn = new DataTableLinkColumn("adm_generic_confirm.aspx", "Confirm", "STATUS=3;STATUS=4");
+            linkColumn.AddQueryStringValue("id", "field=id");
+            linkColumn.AddQueryStringValue("table", "lu_lookup");
+            linkColumn.AddQueryStringValue("confirm", "true");
+            linkColumn.AddQueryStringValue("main_menu", "5");
+            linkColumn.AddQueryStringValue("sub_menu", "1");
+            linkColumn.AddQueryStringValue("rep_field", "username");
+            linkColumn.AddQueryStringValue("rep_name", "Username");
+            theTable.AddLinkColumn(linkColumn);
+        }
+
+        if (LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.REMOVE))
+        {
+            DataTableLinkColumn linkColumn = new DataTableLinkColumn("adm_generic_confirm.aspx", "Cancel", "STATUS=3;STATUS=4");
+            linkColumn.AddQueryStringValue("id", "field=id");
+            linkColumn.AddQueryStringValue("table", "lu_lookup");
+            linkColumn.AddQueryStringValue("confirm", "false");
+            linkColumn.AddQueryStringValue("main_menu", "5");
+            linkColumn.AddQueryStringValue("sub_menu", "1");
+            linkColumn.AddQueryStringValue("rep_field", "username");
+            linkColumn.AddQueryStringValue("rep_name", "Username");
+            theTable.AddLinkColumn(linkColumn);
+        }
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        string sOldOrderBy = "";
+        if (Session["order_by"] != null)
+            sOldOrderBy = Session["order_by"].ToString();
+        DBTableWebEditor theTable = new DBTableWebEditor(true, true, true, "", "adm_table_header", "adm_table_cell", "adm_table_alt_cell", "adm_table_link", "adm_table_pager", "adm_table", sOldOrderBy, 50);
+        FillTheTableEditor(ref theTable, sOrderBy);
+
+        string sTable = theTable.GetPageHTML(int.Parse(sPageNum), sOrderBy);
+
+        theTable.Finish();
+        theTable = null;
+        return sTable;
+    }
+
+    public void GetHeader()
+    {
+        Response.Write(PageUtils.GetPreHeader() + ": Lookup Table Types");
+    }
+}
---
+++ Web Sites/TVM/adm_generic_lookup_table_new.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TVinciShared;
+
+public partial class adm_generic_lookup_table_new : System.Web.UI.Page
+{ 
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        Int32 nMenuID = 0;
+        if (!IsPostBack)
+        {
+            if (Request.QueryString["submited"] != null && Request.QueryString["submited"].ToString() == "1")
+            {
+                DBManipulator.DoTheWork();
+                return;
+            }
+            m_sMenu = TVinciShared.Menu.GetMainMenu(5, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 1, true);
+            if (Request.QueryString["lookup_id"] != null && Request.QueryString["lookup_id"].ToString() != "")
+            {
+                Session["lookup_id"] = int.Parse(Request.QueryString["lookup_id"].ToString());
+               
+            }
+            else
+                Session["lookup_id"] = 0;
+        }
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    public void GetHeader()
+    {
+        string sRet = PageUtils.GetPreHeader() + ": Lookup Type";
+        if (Session["lookup_id"] != null && Session["lookup_id"].ToString() != "" && Session["lookup_id"].ToString() != "0")
+            sRet += " - Edit";
+        else
+            sRet += " - New";
+        Response.Write(sRet);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        if (Session["error_msg"] != null && Session["error_msg"].ToString() != "")
+        {
+            Session["error_msg"] = "";
+            return Session["last_page_html"].ToString();
+        }
+        object t = null; ;
+        if (Session["lookup_id"] != null && Session["lookup_id"].ToString() != "" && int.Parse(Session["lookup_id"].ToString()) != 0)
+            t = Session["lookup_id"];
+        string sBack = "adm_generic_lookup_table.aspx?search_save=1";
+
+        DBRecordWebEditor theRecord = new DBRecordWebEditor("lu_lookup", "adm_table_pager", sBack, "", "ID", t, sBack, "");
+
+        DataRecordShortTextField dr_Name = new DataRecordShortTextField("ltr", true, 60, 128);
+        dr_Name.Initialize("Lookup name", "adm_table_header_nbg", "FormInput", "Name", true);
+        theRecord.AddRecord(dr_Name);
+
+        string sTable = theRecord.GetTableHTML("adm_generic_lookup_table_new.aspx?submited=1");
+
+        return sTable;
+    }
+}
