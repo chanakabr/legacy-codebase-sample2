eb11d6f8700167a698c24993035e5149e62d4a85
Sunny Simantov <sunny.simantov@kaltura.com>
2015-04-21 18:10:39 +0300

    Do not check if regionalization is enabled, always serialize regions of media

--- Feeders/ElasticSearchFeeder/IndexBuilders/MediaIndexBuilder.cs
+++ Feeders/ElasticSearchFeeder/IndexBuilders/MediaIndexBuilder.cs
@@ -367,32 +367,28 @@ namespace ElasticSearchFeeder.IndexBuilders
 
                         #region - get regions of media
 
-                        // Only if regionalization is enabled
-                        if (oGroup.isRegionalizationEnabled)
+                        // Regions table should be 6h on stored procedure
+                        if (ds.Tables.Count > 5 && ds.Tables[5].Columns != null && ds.Tables[5].Rows != null)
                         {
-                            // Regions table should be 6h on stored procedure
-                            if (ds.Tables.Count > 5 && ds.Tables[5].Columns != null && ds.Tables[5].Rows != null)
+                            foreach (DataRow mediaRegionRow in ds.Tables[5].Rows)
                             {
-                                foreach (DataRow mediaRegionRow in ds.Tables[5].Rows)
-                                {
-                                    int mediaId = ODBCWrapper.Utils.ExtractInteger(mediaRegionRow, "MEDIA_ID");
-                                    int regionId = ODBCWrapper.Utils.ExtractInteger(mediaRegionRow, "REGION_ID");
+                                int mediaId = ODBCWrapper.Utils.ExtractInteger(mediaRegionRow, "MEDIA_ID");
+                                int regionId = ODBCWrapper.Utils.ExtractInteger(mediaRegionRow, "REGION_ID");
 
-                                    // Accumulate region ids in list
-                                    medias[mediaId].regions.Add(regionId);
-                                }
-                            }
-
-                            // If no regions were found for this media - use 0, that indicates that the media is region-less
-                            foreach (Media media in medias.Values)
-                            {
-                                if (media.regions.Count == 0)
-                                {
-                                    media.regions.Add(0);
-                                }
+                                // Accumulate region ids in list
+                                medias[mediaId].regions.Add(regionId);
                             }
                         }
-                    
+
+                        // If no regions were found for this media - use 0, that indicates that the media is region-less
+                        foreach (Media media in medias.Values)
+                        {
+                            if (media.regions.Count == 0)
+                            {
+                                media.regions.Add(0);
+                            }
+                        }
+
 
                         #endregion
 
@@ -547,7 +543,6 @@ namespace ElasticSearchFeeder.IndexBuilders
                     }
 
                 }
-
             }
             catch (Exception ex)
             {
