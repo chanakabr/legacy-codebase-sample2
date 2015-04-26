694a300902975694a243b6ad4069b880f545baab
Sunny Simantov <sunny.simantov@kaltura.com>
2015-04-26 10:13:21 +0300

    Add new response and Slim asset info.

---
+++ Libs/TVPApiModule/Objects/Responses/AutocompleteResponse.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TVPApiModule.Objects.Responses
+{
+    public class AutocompleteResponse
+    {
+        [JsonProperty(PropertyName = "assets")]
+        public List<SlimAssetInfo> Assets
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty(PropertyName = "total_items")]
+        public int TotalItems
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty(PropertyName = "status")]
+        public Status Status
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Libs/TVPApiModule/Objects/Responses/SlimAssetInfo.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TVPApiModule.Objects.Responses
+{
+    public class SlimAssetInfo
+    {
+        [JsonProperty(PropertyName = "id")]
+        public long Id
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty(PropertyName = "type")]
+        public int Type
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+    }
+}
