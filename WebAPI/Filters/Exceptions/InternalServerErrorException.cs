5fcf716bfa9916748f8203130c5fe34b4701fad6
Oded Shafran <oded@shafranet.com>
2015-04-26 10:54:18 +0300

    gitignore

---
+++ WebAPI/Filters/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using WebAPI.Filters.Exceptions;
+using WebAPI.Models;
+
+namespace WebAPI.Filters
+{
+    public class InternalServerErrorException : ApiException
+    {
+        public InternalServerErrorException() : base(HttpStatusCode.InternalServerError, Models.StatusCode.Error, "error") { }
+
+        public InternalServerErrorException(StatusCode code, string msg) : base(HttpStatusCode.InternalServerError, code, msg) { }
+    }
+}
