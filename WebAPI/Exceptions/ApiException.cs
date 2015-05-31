0c46ef43c1f7473dfdbc42a049d005c080c46e4a
Oded Shafran <oded@shafranet.com>
2015-05-31 14:29:50 +0300

    Fixing Exception handling

--- WebAPI/Exceptions/ApiException.cs
+++ WebAPI/Exceptions/ApiException.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using WebAPI.Models.General;
@@ -14,7 +15,13 @@ namespace WebAPI.Exceptions
         public StatusCode Code { get; set; }
 
         protected ApiException(HttpStatusCode httpCode, int code, string msg)
-            : base(new HttpResponseMessage() { ReasonPhrase = msg, StatusCode = httpCode, Content = new StringContent(code.ToString()) })
+            : base(new HttpResponseMessage()
+            {
+                ReasonPhrase = msg,
+                StatusCode = httpCode,
+                Content = new ObjectContent(typeof(HttpError), new HttpError(new Exception(msg), true),
+            new JsonMediaTypeFormatter())
+            })
         {
 
         }
