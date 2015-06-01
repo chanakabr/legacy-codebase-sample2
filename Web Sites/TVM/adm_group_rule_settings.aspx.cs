99b14512fc54a9aabb8c6afabbad33bb7ef5cef3
Sunny Simantov <sunny.simantov@kaltura.com>
2015-06-01 15:07:33 +0300

    Add new pages for parental rules

---
+++ Web Sites/TVM/adm_group_rule_settings.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TVinciShared;
+
+public partial class adm_group_rule_settings : System.Web.UI.Page
+{
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+        {
+            Response.Redirect("login.html");
+        }
+        else if (LoginManager.IsPagePermitted("adm_my_group.aspx") == false)
+        {
+            LoginManager.LogoutFromSite("login.html");
+        }
+
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+        {
+            return;
+        }
+
+        Int32 nMenuID = 0;
+
+        if (!IsPostBack)
+        {
+            m_sMenu = TVinciShared.Menu.GetMainMenu(2, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 2, true);
+            if (Request.QueryString["submited"] != null && Request.QueryString["submited"].ToString() == "1")
+            {
+                DBManipulator.DoTheWork();
+                Int32 nGroupID = LoginManager.GetLoginGroupID();
+            }
+        }
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    //protected string GetWhereAmIStr()
+    //{
+    //    string sGroupName = LoginManager.GetLoginGroupName();
+    //    Int32 nGroupID = LoginManager.GetLoginGroupID();
+    //    if (Session["parent_group_id"] != null && Session["parent_group_id"].ToString() != "" && Session["parent_group_id"].ToString() != "0")
+    //        nGroupID = int.Parse(Session["parent_group_id"].ToString());
+
+    //    string sRet = "";
+    //    bool bFirst = true;
+    //    Int32 nLast = 0;
+    //    nLast = int.Parse(PageUtils.GetTableSingleVal("groups", "parent_group_id", LoginManager.GetLoginGroupID()).ToString());
+    //    while (nGroupID != nLast)
+    //    {
+    //        Int32 nParentID = int.Parse(PageUtils.GetTableSingleVal("groups", "PARENT_GROUP_ID", nGroupID).ToString());
+    //        string sHeader = PageUtils.GetTableSingleVal("groups", "group_name", nGroupID).ToString();
+    //        if (bFirst == false)
+    //            sRet = "<span style=\"cursor:pointer;\" onclick=\"document.location.href='adm_groups.aspx?parent_group_id=" + nParentID.ToString() + "';\">" + sHeader + " </span><span class=\"arrow\">&raquo; </span>" + sRet;
+    //        else
+    //            sRet = sHeader;
+    //        bFirst = false;
+    //        nGroupID = nParentID;
+    //    }
+    //    sRet = "Groups: " + sRet;
+    //    return sRet;
+    //}
+
+    public void GetHeader()
+    {
+        Response.Write(PageUtils.GetPreHeader() + ": Group Rules Settings");
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        string parentalRulesQuery = "select name as txt, id as id from parental_rules where status=1 and group_id= " + LoginManager.GetLoginGroupID().ToString();
+        string purchaseSettingsQuery = "SELECT 0 as id, 'Block' as txt UNION ALL SELECT 1 as id, 'Ask' as txt UNION ALL SELECT 2 as id, 'Allow' as txt";
+
+        if (Session["error_msg"] != null && Session["error_msg"].ToString() != string.Empty)
+        {
+            Session["error_msg"] = string.Empty;
+
+            return Session["last_page_html"].ToString();
+        }
+
+        object groupId = LoginManager.GetLoginGroupID();
+
+        string backPage = "adm_group_rule_settings.aspx?search_save=1";
+        DBRecordWebEditor theRecord = new DBRecordWebEditor("group_rule_settings", "adm_table_pager", backPage, "", "ID", groupId, backPage, "");
+
+        DataRecordDropDownField dr_default_parental_rule_movies = new DataRecordDropDownField("parental_rules", "NAME", "id", "", null, 60, true);
+        dr_default_parental_rule_movies.SetSelectsQuery(parentalRulesQuery);
+        dr_default_parental_rule_movies.Initialize("Force default parental rules (movies)", "adm_table_header_nbg", "FormInput", "DEFAULT_MOVIES_PARENTAL_RULE", false);
+        theRecord.AddRecord(dr_default_parental_rule_movies);
+
+        DataRecordDropDownField dr_default_parental_rule_tv_series = new DataRecordDropDownField("parental_rules", "NAME", "id", "", null, 60, true);
+        dr_default_parental_rule_tv_series.SetSelectsQuery(parentalRulesQuery);
+        dr_default_parental_rule_tv_series.Initialize("Force default parental rules (TV series)", "adm_table_header_nbg", "FormInput", "DEFAULT_TV_SERIES_PARENTAL_RULE", false);
+        theRecord.AddRecord(dr_default_parental_rule_tv_series);
+
+        DataRecordShortTextField dr_default_pin = new DataRecordShortTextField("ltr", true, 60, 128);
+        dr_default_pin.Initialize("Default parental rule PIN", "adm_table_header_nbg", "FormInput", "DEFAULT_PARENTAL_PIN", true);
+        theRecord.AddRecord(dr_default_pin);
+
+        DataRecordDropDownField dr_default_purchase_settings = new DataRecordDropDownField("parental_rules", "NAME", "id", "", null, 60, true);
+        dr_default_purchase_settings.SetSelectsQuery(purchaseSettingsQuery);
+        dr_default_purchase_settings.Initialize("Default purchase rules", "adm_table_header_nbg", "FormInput", "DEFAULT_PURCHASE_SETTINGS", false);
+        theRecord.AddRecord(dr_default_purchase_settings);
+
+        string sTable = theRecord.GetTableHTML("adm_group_rule_settings.aspx?submited=1");
+
+        return sTable;
+    }
+}
---
+++ Web Sites/TVM/adm_parental_rules_values.aspx.cs
@@ -0,0 +1,169 @@
+﻿using ODBCWrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TVinciShared;
+
+public partial class adm_parental_rules_values : System.Web.UI.Page
+{
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        if (LoginManager.IsPagePermitted("adm_parental_rules.aspx") == false)
+            LoginManager.LogoutFromSite("login.html");
+        if (LoginManager.IsActionPermittedOnPage("adm_parental_rules.aspx", LoginManager.PAGE_PERMISION_TYPE.EDIT) == false)
+            LoginManager.LogoutFromSite("login.html");
+
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        if (!IsPostBack)
+        {
+            if (Request.QueryString["submited"] != null && Request.QueryString["submited"].ToString().Trim() == "1")
+            {
+                int id = DBManipulator.DoTheWork();
+
+                if (id > 0)
+                {
+                    UpdateQuery updateQuery = new UpdateQuery("parental_rule_tag_values");
+
+                    updateQuery += ODBCWrapper.Parameter.NEW_PARAM("ASSET_TYPE", "=", (int)Session["asset_type"]);
+                    updateQuery += " WHERE ";
+                    updateQuery += ODBCWrapper.Parameter.NEW_PARAM("RULE_ID", "=", (int)Session["rule_id"]);
+                    updateQuery += " AND ";
+                    updateQuery += ODBCWrapper.Parameter.NEW_PARAM("GROUP_ID", "=", LoginManager.GetLoginGroupID());
+                    updateQuery.Execute();
+                    updateQuery.Finish();
+                }
+
+                return;
+            }
+
+            Int32 nMenuID = 0;
+
+            m_sMenu = TVinciShared.Menu.GetMainMenu(7, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 1, true);
+            if (Request.QueryString["rule_id"] != null &&
+                Request.QueryString["rule_id"].ToString() != "")
+            {
+                Session["rule_id"] = int.Parse(Request.QueryString["rule_id"].ToString());
+            }
+            else
+                Session["rule_id"] = 0;
+
+            if (Request.QueryString["asset_type"] != null &&
+                Request.QueryString["asset_type"].ToString() != "")
+            {
+                Session["asset_type"] = int.Parse(Request.QueryString["asset_type"].ToString());
+            }
+            else
+                Session["asset_type"] = 0;
+        }
+    }
+
+    public void GetHeader()
+    {
+        Response.Write(PageUtils.GetPreHeader() + ": Parental rule");
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetPageContent(string orderBy, string pageNum)
+    {
+        if (Session["error_msg"] != null && Session["error_msg"].ToString() != "")
+        {
+            Session["error_msg"] = "";
+            return Session["last_page_html"].ToString();
+        }
+        object ruleId = null;
+        
+        if (Session["rule_id"] != null && Session["rule_id"].ToString() != "" && int.Parse(Session["rule_id"].ToString()) != 0)
+            ruleId = Session["rule_id"];
+
+        int assetTypeId = -1;
+
+        if (Session["asset_type"] != null && Session["asset_type"] != DBNull.Value)
+        {
+            assetTypeId = Convert.ToInt32(Session["asset_type"]);
+        }
+
+        string backPage = "adm_parental_rules.aspx?search_save=1";
+
+        DBRecordWebEditor theRecord = new DBRecordWebEditor("parental_rules", "adm_table_pager", backPage, "", "ID", ruleId, backPage, "");
+
+        string tagTypeName = string.Empty;
+        Int32 tagTypeID = 0;
+
+        ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
+
+        string tagsTypesTable = string.Empty;
+        string tagsTable = string.Empty;
+        string tagTypeIdColumn = string.Empty;
+        string assetName = string.Empty;
+
+        if (assetTypeId == 2)
+        {
+            tagsTypesTable = "media_tags_types";
+            tagsTable = "tags";
+            tagTypeIdColumn = "tag_type_id";
+            assetName = "media";
+        }
+        else if (assetTypeId == 0)
+        {
+            tagsTypesTable = "epg_tags_types";
+            tagsTable = "epg_tags";
+            tagTypeIdColumn = "epg_tag_type_id";
+            assetName = "epg";
+        }
+
+        selectQuery += string.Format("select tt.id, tt.name from parental_rules pr, {0} tt where", tagsTypesTable);
+        selectQuery += ODBCWrapper.Parameter.NEW_PARAM("pr.id", "=", ruleId.ToString());
+        selectQuery += string.Format("and pr.{0}_tag_type_id=tt.id", assetName);
+        
+        if (selectQuery.Execute("query", true) != null)
+        {
+            Int32 nCount = selectQuery.Table("query").DefaultView.Count;
+
+            for (int i = 0; i < nCount; i++)
+            {
+                tagTypeName = selectQuery.Table("query").DefaultView[i].Row["NAME"].ToString();
+                tagTypeID = int.Parse(selectQuery.Table("query").DefaultView[i].Row["ID"].ToString());
+
+                DataRecordMultiField dr_tags = new DataRecordMultiField(tagsTable, "id", "id", "parental_rule_tag_values", "rule_id", "TAG_ID", true, "ltr", 60, "tags");
+                dr_tags.SetCollectionLength(20);
+                dr_tags.SetExtraWhere(tagTypeIdColumn + "=" + tagTypeID);
+                dr_tags.SetCollectionQuery(string.Format("SELECT TOP 20 value as txt, ID as val from {0} where {1} =", tagsTable, tagTypeIdColumn) + tagTypeID);
+                
+                dr_tags.Initialize(tagTypeName, "adm_table_header_nbg", "FormInput", "VALUE", false);
+
+                theRecord.AddRecord(dr_tags);
+            }
+        }
+
+        selectQuery.Finish();
+        selectQuery = null;
+
+        DataRecordShortIntField dr_groups = new DataRecordShortIntField(false, 9, 9);
+        dr_groups.Initialize("Group", "adm_table_header_nbg", "FormInput", "GROUP_ID", false);
+        dr_groups.SetValue(LoginManager.GetLoginGroupID().ToString());
+        theRecord.AddRecord(dr_groups);
+
+        string sTable = theRecord.GetTableHTML("adm_parental_rules_values.aspx?submited=1");
+
+        return sTable;
+    }
+}
