cc847723e5b9a2009fbb51b1fced9fe685e1796c
Sunny Simantov <sunny.simantov@kaltura.com>
2015-06-07 11:44:52 +0300

    Add new ParentalRule response object

--- WebAPI/ObjectsConvertor/Mapping/ApiMappings.cs
+++ WebAPI/ObjectsConvertor/Mapping/ApiMappings.cs
@@ -40,6 +40,21 @@ namespace WebAPI.Mapping.ObjectsConvertor
                 }
                 return result;
             });
+
+            // ParentalRule
+            Mapper.CreateMap<WebAPI.Api.ParentalRule, WebAPI.Models.General.ParentalRule>()
+                .ForMember(dest => dest.blockAnonymousAccess, opt => opt.MapFrom(src => src.blockAnonymousAccess))
+                .ForMember(dest => dest.description, opt => opt.MapFrom(src => src.description))
+                .ForMember(dest => dest.epgTagTypeId, opt => opt.MapFrom(src => src.epgTagTypeId))
+                .ForMember(dest => dest.epgTagValues, opt => opt.MapFrom(src => src.epgTagValues))
+                .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.id))
+                .ForMember(dest => dest.isDefault, opt => opt.MapFrom(src => src.isDefault))
+                .ForMember(dest => dest.mediaTagTypeId, opt => opt.MapFrom(src => src.mediaTagTypeId))
+                .ForMember(dest => dest.mediaTagValues, opt => opt.MapFrom(src => src.mediaTagValues))
+                .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.name))
+                .ForMember(dest => dest.order, opt => opt.MapFrom(src => src.order))
+                .ForMember(dest => dest.origin, opt => opt.MapFrom(src => src.level))
+                .ForMember(dest => dest.ruleType, opt => opt.MapFrom(src => src.ruleType));
         }
     }
 }
