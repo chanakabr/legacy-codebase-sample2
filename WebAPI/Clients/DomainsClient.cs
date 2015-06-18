0ccfddafe129811fd949aa03eebd3172a4345893
Irena Leibman <irena.leibman@kaltura.com>
2015-06-18 17:15:30 +0300

    AddUserToDomain RemoveDeviceFromDomain, RegisterDeviceByPin

--- WebAPI/Clients/DomainsClient.cs
+++ WebAPI/Clients/DomainsClient.cs
@@ -184,5 +184,81 @@ namespace WebAPI.Clients
 
             return true;
         }
+
+        internal bool RemoveDeviceFromDomain(int groupId, int domainId, string udid)
+        {
+            Group group = GroupsManager.GetGroup(groupId);
+
+            WebAPI.Domains.DomainStatusResponse response = null;
+            try
+            {
+                using (KMonitor km = new KMonitor(Events.eEvent.EVENT_WS))
+                {
+                    response = Domains.RemoveDeviceFromDomain(group.DomainsCredentials.Username, group.DomainsCredentials.Password, domainId, udid);
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Exception received while calling users service. ws address: {0}, exception: {1}", Domains.Url, ex);
+                ErrorUtils.HandleWSException(ex);
+            }
+
+            if (response == null || response.Status == null)
+            {
+                throw new ClientException((int)StatusCode.Error, StatusCode.Error.ToString());
+            }
+
+            if (response.Status.Code != (int)StatusCode.OK)
+            {
+                throw new ClientException(response.Status.Code, response.Status.Message);
+            }
+
+            if (response.DomainResponse == null || response.DomainResponse.m_oDomain == null)
+            {
+                throw new ClientException((int)StatusCode.Error, StatusCode.Error.ToString());
+            }
+
+            return true;
+        }
+
+        internal Device RegisterDeviceByPin(int groupId, int domainId, string deviceName, string pin)
+        {
+            Device result = null;
+            WebAPI.Domains.DeviceResponse response = null;
+
+            Group group = GroupsManager.GetGroup(groupId);
+
+            try
+            {
+                using (KMonitor km = new KMonitor(Events.eEvent.EVENT_WS))
+                {
+                    response = Domains.RegisterDeviceToDomainWithPIN(group.DomainsCredentials.Username, group.DomainsCredentials.Password, pin, domainId, deviceName);
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Exception received while calling users service. ws address: {0}, exception: {1}", Domains.Url, ex);
+                ErrorUtils.HandleWSException(ex);
+            }
+
+            if (response == null || response.Status == null)
+            {
+                throw new ClientException((int)StatusCode.Error, StatusCode.Error.ToString());
+            }
+
+            if (response.Status.Code != (int)StatusCode.OK)
+            {
+                throw new ClientException(response.Status.Code, response.Status.Message);
+            }
+
+            if (response.Device == null || response.Device.m_oDevice == null)
+            {
+                throw new ClientException((int)StatusCode.Error, StatusCode.Error.ToString());
+            }
+
+            result = Mapper.Map<Device>(response.Device.m_oDevice);
+
+            return result;
+        }
     }
 }
