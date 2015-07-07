8e7b2b4e12fe80086f347b20c1d635814b2454da
Liat Regev <liatr@tvinci.com>
2015-07-07 15:58:19 +0300

    new pages for payment gateway

---
+++ Web Sites/TVM/adm_billing_settings.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TVinciShared;
+
+public partial class adm_billing_settings : System.Web.UI.Page
+{
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        else if (LoginManager.IsPagePermitted("adm_billing_settings.aspx") == false)
+            LoginManager.LogoutFromSite("login.html");
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        Int32 nMenuID = 0;
+        if (!IsPostBack)
+        {
+            m_sMenu = TVinciShared.Menu.GetMainMenu(7, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 1, true);
+            if (Request.QueryString["submited"] != null && Request.QueryString["submited"].ToString() == "1")
+            {
+                DBManipulator.DoTheWork("billing_connection");
+            }
+        }
+    }
+
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    public void GetHeader()
+    {
+        Response.Write(PageUtils.GetPreHeader() + ": Billing Settings");
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        object t = null;
+
+        if (Session["error_msg"] != null && Session["error_msg"].ToString() != "")
+        {
+            Session["error_msg"] = "";
+            return Session["last_page_html"].ToString();
+        }
+
+        object groupId = LoginManager.GetLoginGroupID();
+
+        // check ig groupid is parent , if so show page with all filed else (not parent show a message)
+        int tableID = GetBillingSettingsID(ODBCWrapper.Utils.GetIntSafeVal(groupId));
+        bool isParentGroup = IsParentGroup(ODBCWrapper.Utils.GetIntSafeVal(groupId));
+
+        string sTable = string.Empty;
+        if (!isParentGroup)
+        {
+            sTable = (PageUtils.GetPreHeader() + ": Module is not implemented");
+        }
+        else
+        {
+            if (tableID > 0) // a new record
+            {
+                t = tableID;
+            }
+            string sBack = "adm_billing_settings.aspx?search_save=1";
+            DBRecordWebEditor theRecord = new DBRecordWebEditor("groups_parameters", "adm_table_pager", sBack, "", "ID", t, sBack, "");
+            theRecord.SetConnectionKey("billing_connection");
+
+            DataRecordDropDownField dr_paymentGW = new DataRecordDropDownField("payment_gateway", "name", "id", "group_id", groupId, 60, true);
+            dr_paymentGW.Initialize("Default Payment GateWay", "adm_table_header_nbg", "FormInput", "DEFAULT_PAYMENT_GATEWAY", false);
+            theRecord.AddRecord(dr_paymentGW);
+
+            sTable = theRecord.GetTableHTML("adm_billing_settings.aspx?submited=1");
+        }
+        return sTable;
+    }
+
+    private bool IsParentGroup(int groupID)
+    {
+        bool res = false;
+        try
+        {
+            ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
+            selectQuery.SetConnectionKey("MAIN_CONNECTION_STRING");
+            selectQuery += "select * from F_Get_GroupsParent(" + groupID.ToString() + ")";
+            if (selectQuery.Execute("query", true) != null)
+            {
+                Int32 nCount = selectQuery.Table("query").DefaultView.Count;
+                if (nCount > 0)
+                {
+                    int parentGroupID = ODBCWrapper.Utils.GetIntSafeVal(selectQuery.Table("query").DefaultView[0].Row, "PARENT_GROUP_ID");
+                    if (parentGroupID == 1)
+                    {
+                        res = true;
+                    }
+                }
+            }
+            selectQuery.Finish();
+            selectQuery = null;
+        }
+        catch (Exception ex)
+        {
+            res = false;
+        }
+        return res;
+    }
+
+    private int GetBillingSettingsID(int groupID)
+    {
+        int billingSettingId = 0;
+        try
+        {
+            ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
+            selectQuery.SetConnectionKey("billing_connection");
+            selectQuery += "select ID from groups_parameters where status=1 and ";
+            selectQuery += ODBCWrapper.Parameter.NEW_PARAM("GROUP_ID", "=", groupID);
+            if (selectQuery.Execute("query", true) != null)
+            {
+                Int32 nCount = selectQuery.Table("query").DefaultView.Count;
+                if (nCount > 0)
+                {
+                    billingSettingId = ODBCWrapper.Utils.GetIntSafeVal(selectQuery.Table("query").DefaultView[0].Row, "ID");
+                }
+            }
+            selectQuery.Finish();
+            selectQuery = null;
+        }
+        catch (Exception ex)
+        {
+            billingSettingId = 0;
+        }
+        return billingSettingId;
+    }
+}
---
+++ Web Sites/TVM/adm_payment_gateway_new.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TVinciShared;
+
+public partial class adm_payment_gateway_new : System.Web.UI.Page
+{
+    protected string m_sMenu;
+    protected string m_sSubMenu;    
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        if (LoginManager.IsPagePermitted("adm_payment_gateway.aspx") == false)
+            LoginManager.LogoutFromSite("login.html");
+        if (LoginManager.IsActionPermittedOnPage("adm_payment_gateway.aspx", LoginManager.PAGE_PERMISION_TYPE.EDIT) == false)
+            LoginManager.LogoutFromSite("login.html");
+
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        if (!IsPostBack)
+        {
+            if (Request.QueryString["submited"] != null && Request.QueryString["submited"].ToString().Trim() == "1")
+            {
+                Int32 nID = DBManipulator.DoTheWork("billing_connection");                
+                return;
+            }
+            
+            Int32 nMenuID = 0;
+            m_sMenu = TVinciShared.Menu.GetMainMenu(7, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 1, true);
+
+            if (Request.QueryString["paymentGW_id"] != null && Request.QueryString["paymentGW_id"].ToString() != "")
+            {
+                Session["paymentGW_id"] = int.Parse(Request.QueryString["paymentGW_id"].ToString());
+            }
+            else
+                Session["paymentGW_id"] = 0;
+        }
+    }
+
+    public void GetHeader()
+    {
+        Response.Write(PageUtils.GetPreHeader() + ": Payment Gateway");
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        if (Session["error_msg"] != null && Session["error_msg"].ToString() != "")
+        {
+            Session["error_msg"] = "";
+            return Session["last_page_html"].ToString();
+        }
+        object t = null; ;
+        if (Session["paymentGW_id"] != null && Session["paymentGW_id"].ToString() != "" && int.Parse(Session["paymentGW_id"].ToString()) != 0)
+            t = Session["paymentGW_id"];
+        string sBack = "adm_payment_gateway.aspx?search_save=1";
+
+        DBRecordWebEditor theRecord = new DBRecordWebEditor("payment_gateway", "adm_table_pager", sBack, "", "ID", t, sBack, "");
+        theRecord.SetConnectionKey("billing_connection");
+        
+        DataRecordShortTextField dr_name = new DataRecordShortTextField("ltr", true, 60, 128);
+        dr_name.Initialize("Name", "adm_table_header_nbg", "FormInput", "NAME", true);
+        theRecord.AddRecord(dr_name);
+
+        DataRecordLongTextField dr_url = new DataRecordLongTextField("ltr", true, 60, 4);
+        dr_url.Initialize("URL", "adm_table_header_nbg", "FormInput", "url", false);
+        theRecord.AddRecord(dr_url);
+        
+        string sTable = theRecord.GetTableHTML("adm_payment_gateway_new.aspx?submited=1");
+
+        return sTable;
+    }
+}
