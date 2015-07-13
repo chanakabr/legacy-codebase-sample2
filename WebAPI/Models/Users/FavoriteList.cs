ee4ff4898a5e8488b900f4656713cee66bac51b7
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2015-07-13 13:37:16 +0300

    New Files for User favorite Imp

---
+++ WebAPI/Models/Users/FavoriteList.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace WebAPI.Models.Users
+{
+
+    [DataContract(Name = "Users", Namespace = "")]
+    [XmlRoot("Users")]
+    public class FavoriteList
+    {
+        [DataMember(Name = "favorites")]
+        [JsonProperty("favorites")]
+        public List<Favorite> Favorites;
+    }
+}
