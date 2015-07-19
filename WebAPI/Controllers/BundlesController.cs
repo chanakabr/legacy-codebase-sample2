f9c766ecf5982c4fa358c62b7246951d3706edab
Oded Shafran <oded@shafranet.com>
2015-07-19 11:51:09 +0300

    fixing commented comments

--- WebAPI/Controllers/BundlesController.cs
+++ WebAPI/Controllers/BundlesController.cs
@@ -24,19 +24,19 @@ namespace WebAPI.Controllers
         /// <summary>
         /// Returns a price and a purchase status for each subscription, for a given user (if passed) and with the consideration of a coupon code (if passed). 
         /// </summary>
-        ///// <param name="partner_id">Partner identifier</param>
-        ///// <param name="subscriptions_ids">Subscription identifiers (separated by ',')</param>
-        ///// <param name="user_id">User identifier</param>
-        ///// <param name="coupon_code">Discount coupon code</param>
-        ///// <param name="udid">Device UDID</param>
-        ///// <param name="language">Language code</param>
-        ///// <param name="should_get_only_lowest">A flag that indicates if only the lowest price of a subscription should return</param>
-        ///// <remarks>Possible status codes: Bad credentials = 500000, Internal connection = 500001, Timeout = 500002, Bad request = 500003, Forbidden = 500004, Unauthorized = 500005, Configuration error = 500006, Not found = 500007, Partner is invalid = 500008 </remarks>
-        ///// <response code="200">OK</response>
-        ///// <response code="400">Bad request</response>
-        ///// <response code="403">Forbidden</response>
-        ///// <response code="500">Internal Server Error</response>
-        ///// <response code="504">Gateway Timeout</response>
+        /// <param name="partner_id">Partner identifier</param>
+        /// <param name="subscriptions_ids">Subscription identifiers (separated by ',')</param>
+        /// <param name="user_id">User identifier</param>
+        /// <param name="coupon_code">Discount coupon code</param>
+        /// <param name="udid">Device UDID</param>
+        /// <param name="language">Language code</param>
+        /// <param name="should_get_only_lowest">A flag that indicates if only the lowest price of a subscription should return</param>
+        /// <remarks>Possible status codes: Bad credentials = 500000, Internal connection = 500001, Timeout = 500002, Bad request = 500003, Forbidden = 500004, Unauthorized = 500005, Configuration error = 500006, Not found = 500007, Partner is invalid = 500008 </remarks>
+        /// <response code="200">OK</response>
+        /// <response code="400">Bad request</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="500">Internal Server Error</response>
+        /// <response code="504">Gateway Timeout</response>
         [Route("subscriptions/{subscriptions_ids}/prices"), HttpGet]
         public SubscriptionsPricesList GetSubscriptionsPrices([FromUri] string partner_id, [FromUri] string subscriptions_ids, [FromUri] string user_id = null, 
             [FromUri] string coupon_code = null, [FromUri] string udid = null , [FromUri] string language = null, [FromUri] bool should_get_only_lowest = false)
@@ -68,16 +68,16 @@ namespace WebAPI.Controllers
         /// <summary>
         /// Returns a list of subscriptions data.
         /// </summary>
-        ///// <param name="partner_id">Partner identifier</param>
-        ///// <param name="subscriptions_ids">Subscription identifiers (separated by ,)</param>
-        ///// <param name="udid">Device UDID</param>
-        ///// <param name="language">Language code</param>
-        ///// <remarks>Possible status codes: Bad credentials = 500000, Internal connection = 500001, Timeout = 500002, Bad request = 500003, Forbidden = 500004, Unauthorized = 500005, Configuration error = 500006, Not found = 500007, Partner is invalid = 500008 </remarks>
-        ///// <response code="200">OK</response>
-        ///// <response code="400">Bad request</response>
-        ///// <response code="403">Forbidden</response>
-        ///// <response code="500">Internal Server Error</response>
-        ///// <response code="504">Gateway Timeout</response>
+        /// <param name="partner_id">Partner identifier</param>
+        /// <param name="subscriptions_ids">Subscription identifiers (separated by ,)</param>
+        /// <param name="udid">Device UDID</param>
+        /// <param name="language">Language code</param>
+        /// <remarks>Possible status codes: Bad credentials = 500000, Internal connection = 500001, Timeout = 500002, Bad request = 500003, Forbidden = 500004, Unauthorized = 500005, Configuration error = 500006, Not found = 500007, Partner is invalid = 500008 </remarks>
+        /// <response code="200">OK</response>
+        /// <response code="400">Bad request</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="500">Internal Server Error</response>
+        /// <response code="504">Gateway Timeout</response>
         [Route("subscriptions/{subscriptions_ids}"), HttpGet]
         public SubscriptionsList GetSubscriptionsData([FromUri] string partner_id, string subscriptions_ids, [FromUri] string udid = null, [FromUri] string language = null)
         {
