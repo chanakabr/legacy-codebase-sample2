fc407d2f9f7399601af5bc8b8de486959820c34b
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2015-08-04 15:51:09 +0300

    MHUB-472 Add GetSelectedHouseholdPaymentGateway

---
+++ WebAPI/Models/Billing/KalturaHouseholdPaymentGatewaySelectedBy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Billing
+{
+    /// <summary>
+    /// distinction payment gateway selected by account or household
+    /// </summary>
+    public enum KalturaHouseholdPaymentGatewaySelectedBy
+    {
+        account,        
+        household
+    }
+}
--- WebAPI/ObjectsConvertor/Mapping/BillingMappings.cs
+++ WebAPI/ObjectsConvertor/Mapping/BillingMappings.cs
@@ -2,8 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using WebAPI.Billing;
+using WebAPI.Exceptions;
+using WebAPI.Managers.Models;
 
 namespace WebAPI.ObjectsConvertor.Mapping
 {
@@ -50,6 +51,27 @@ namespace WebAPI.ObjectsConvertor.Mapping
                 .ForMember(dest => dest.PendingRetries, opt => opt.MapFrom(src => src.PendingRetries))
                 .ForMember(dest => dest.SharedSecret, opt => opt.MapFrom(src => src.SharedSecret))
                 .ForMember(dest => dest.Settings, opt => opt.MapFrom(src => ConvertPaymentGatewaySettings(src.Settings)));
+
+
+            //from local object to WS object            
+            Mapper.CreateMap<WebAPI.Models.Billing.KalturaPaymentGatewayData, PaymentGateway>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.AdapterUrl, opt => opt.MapFrom(src => src.AdapterUrl))
+                .ForMember(dest => dest.TransactUrl, opt => opt.MapFrom(src => src.TransactUrl))
+                .ForMember(dest => dest.StatusUrl, opt => opt.MapFrom(src => src.StatusUrl))
+                .ForMember(dest => dest.RenewUrl, opt => opt.MapFrom(src => src.RenewUrl))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => Convert.ToInt32(src.IsActive)))
+                .ForMember(dest => dest.IsDefault, opt => opt.MapFrom(src => Convert.ToInt32(src.IsDefault)))
+                .ForMember(dest => dest.ExternalIdentifier, opt => opt.MapFrom(src => src.ExternalIdentifier))
+                .ForMember(dest => dest.PendingInterval, opt => opt.MapFrom(src => src.PendingInterval))
+                .ForMember(dest => dest.PendingRetries, opt => opt.MapFrom(src => src.PendingRetries))
+                .ForMember(dest => dest.SharedSecret, opt => opt.MapFrom(src => src.SharedSecret))
+                .ForMember(dest => dest.Settings, opt => opt.MapFrom(src => ConvertPaymentGatewaySettings(src.Settings)));
+
+            //from local object to WS object            
+            Mapper.CreateMap<HouseholdPaymentGatewayResponse, WebAPI.Models.Billing.KalturaHouseholdPaymentGatewayResponse>()
+                .ForMember(dest => dest.paymentGateway, opt => opt.MapFrom(src => src.PaymentGateway))
+                .ForMember(dest => dest.selectedBy, opt => opt.MapFrom(src => ConvertHouseholdPaymentGatewaySelectedBy(src.SelectedBy)));
 
         }
 
@@ -98,6 +120,26 @@ namespace WebAPI.ObjectsConvertor.Mapping
                     }
                 }
             }
+            return result;
+        }
+
+        private static Models.Billing.KalturaHouseholdPaymentGatewaySelectedBy ConvertHouseholdPaymentGatewaySelectedBy(WebAPI.Billing.eHouseholdPaymentGatewaySelectedBy? type)
+        {
+            WebAPI.Models.Billing.KalturaHouseholdPaymentGatewaySelectedBy result;
+
+            switch (type)
+            {
+                case WebAPI.Billing.eHouseholdPaymentGatewaySelectedBy.Account:
+                    result = Models.Billing.KalturaHouseholdPaymentGatewaySelectedBy.account;
+                    break;
+                case WebAPI.Billing.eHouseholdPaymentGatewaySelectedBy.Household:
+                    result = Models.Billing.KalturaHouseholdPaymentGatewaySelectedBy.household;
+                    break;
+                default:
+                    throw new ClientException((int)StatusCode.Error, "unknown household payment gateway selected by");
+
+            }
+
             return result;
         }
     }
