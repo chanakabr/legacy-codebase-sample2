8248f6a18bc06c2680f483536d6f2fd4357915ea
Sunny Simantov <sunny.simantov@kaltura.com>
2015-08-04 17:05:10 +0300

    New pending charge handler - new project

---
+++ PendingChargeHandler/PendingChargeRequest.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PendingChargeHandler
+{
+    [Serializable]
+    public class PendingChargeRequest
+    {
+        [JsonProperty("group_id")]
+        public int GroupID
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("id")]
+        public long ID
+        {
+            get;
+            set;
+        }
+    }
+}
