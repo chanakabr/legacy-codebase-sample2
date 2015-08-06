74b21698e4e3f11e2694764337290c5fd1324f18
Sunny Simantov <sunny.simantov@kaltura.com>
2015-08-06 16:08:56 +0300

    Add payment details and method fields to transaction responses

--- ApiObjects/Billing/TransactResult.cs
+++ ApiObjects/Billing/TransactResult.cs
@@ -9,19 +9,45 @@ namespace ApiObjects.Billing
     {
         public ApiObjects.Response.Status Status { get; set; }
 
-        //Kaltura unique ID representing the transaction
+        /// <summary>
+        /// Kaltura unique ID representing the transaction
+        /// </summary>
         public long TransactionID { get; set; }
 
-        //Transaction reference ID that were returned from the payment gateway. Returned only if the payment gateway provides this information
+        /// <summary>
+        /// Transaction reference ID that were returned from the payment gateway. Returned only if the payment gateway provides this information
+        /// </summary>
         public string PGReferenceID { get; set; }
 
-        //Original response ID that was provided from by the payment gateway. Returned only if the payment gateway provides this information.
+        /// <summary>
+        /// Original response ID that was provided from by the payment gateway. Returned only if the payment gateway provides this information.
+        /// </summary>
         public string PGResponseID { get; set; }
 
+        /// <summary>
+        /// OK, Pending, Failed...
+        /// </summary>
         public eTransactionState State { get; set; }
 
-        // Adapter failure reason code 
+        /// <summary>
+        /// Adapter failure reason code 
+        /// </summary>
         public int FailReasonCode { get; set; }
+
+        public string PaymentDetails
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Credit card, cellular etc. (free text)
+        /// </summary>
+        public string PaymentMethod
+        {
+            get;
+            set;
+        }
 
         public TransactResult()
         {
