310e6ec169d6885a57dface9d41997b7993e448f
Sunny Simantov <sunny.simantov@kaltura.com>
2015-08-09 18:22:24 +0300

    Add new Pending Transaction Request class to Api Objects.

---
+++ ApiObjects/Billing/PendingTransactionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApiObjects.Billing
+{
+    public class PendingTransactionRequest
+    {
+        public PaymentGateway paymentGateway;
+        public int productId;
+        public eTransactionType productType;
+        public string siteGuid;
+    }
+}
