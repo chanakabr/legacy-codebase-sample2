045433314b511a54d4f8c14049e8270295ee8a7f
Oded Shafran <oded@shafranet.com>
2015-08-10 16:22:48 +0300

    KalturaFilterPager

---
+++ WebAPI/Models/General/KalturaFilterPager.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace WebAPI.Models.General
+{
+    /// <summary>
+    /// The KalturaFilterPager object enables paging management to be applied upon service list actions
+    /// </summary>
+    public class KalturaFilterPager : KalturaOTTObject
+    {
+        /// <summary>
+        /// The number of objects to retrieve
+        /// </summary>
+        [DataMember(Name = "pageSize")]
+        [JsonProperty(PropertyName = "pageSize")]
+        [XmlElement(ElementName = "pageSize")]
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// The page number for which {pageSize} of objects should be retrieved
+        /// </summary>
+        [DataMember(Name = "pageIndex")]
+        [JsonProperty(PropertyName = "pageIndex")]
+        [XmlElement(ElementName = "pageIndex")]
+        public int PageIndex { get; set; }
+    }
+}
