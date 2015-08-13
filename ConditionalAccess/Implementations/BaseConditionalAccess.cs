0cff7ec1f699c5ff77addcf158c69b3b3f32e4e5
Ira Yanai <iray@tvinci.com>
2015-08-13 17:13:51 +0300

    no message

--- ConditionalAccess/Implementations/BaseConditionalAccess.cs
+++ ConditionalAccess/Implementations/BaseConditionalAccess.cs
@@ -12211,14 +12211,6 @@ namespace ConditionalAccess
                 return response;
             }
 
-            // validate household
-            if (household < 1)
-            {
-                response.Status.Message = "Illegal household";
-                log.ErrorFormat("Error: {0}, data: {1}", response.Status.Message, logString);
-                return response;
-            }
-
             // validate productId
             if (productId < 1)
             {
@@ -12254,6 +12246,14 @@ namespace ConditionalAccess
                     // user validation failed
                     response.Status = SetResponseStatus(userValidStatus);
                     log.ErrorFormat("User validation failed: {0}, data: {1}", response.Status.Message, logString);
+                    return response;
+                }
+
+                // validate household
+                if (household < 1)
+                {
+                    response.Status.Message = "Illegal household";
+                    log.ErrorFormat("Error: {0}, data: {1}", response.Status.Message, logString);
                     return response;
                 }
 
