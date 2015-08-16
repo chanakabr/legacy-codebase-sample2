bc2b2625d6fa6b7d305e9429da970bba39eae430
Sunny Simantov <sunny.simantov@kaltura.com>
2015-08-16 17:09:47 +0300

    Parameter - check if date time type of value exceeds SQL Server minimum value. If yes, use SQL Server minimum value. It avoids exceptions in DB.

--- ODBCWrapper/Parameter.cs
+++ ODBCWrapper/Parameter.cs
@@ -7,27 +7,43 @@ namespace ODBCWrapper
 	/// </summary>
 	public class Parameter
 	{
-		private Parameter(string sParName , string sType , object sParVal)
+        /// <summary>
+        /// Minimum date time that is supported in SQL Server
+        /// </summary>
+		public static readonly DateTime MINIMUM_DATE_TIME = new DateTime(1753, 1, 1);
+
+		private Parameter(string name, string type, object value)
 		{
-			m_sParName = sParName;
-			m_sType = sType;
-			m_sParVal = sParVal;
+			m_sParName = name;
+			m_sType = type;
+
+			if (value is DateTime)
+			{
+                if ((DateTime)value < MINIMUM_DATE_TIME)
+                {
+                    value = MINIMUM_DATE_TIME;
+                }
+			}
+
+			m_sParVal = value;
 		}
 
-		~Parameter(){}
+		~Parameter()
+		{
+		}
 
 		public string m_sParName = "";
 		public string m_sType = "";
 		public object m_sParVal = null;
 
-		public static Parameter NEW_PARAM(string sParName , string sType , object sParVal)
+		public static Parameter NEW_PARAM(string sParName, string sType, object sParVal)
 		{
-			return new Parameter(sParName , sType , sParVal);
+			return new Parameter(sParName, sType, sParVal);
 		}
 
-		public static Parameter NEW_PARAM(string sParName , object sParVal)
+		public static Parameter NEW_PARAM(string sParName, object sParVal)
 		{
-			return NEW_PARAM(sParName , "" , sParVal);
+			return NEW_PARAM(sParName, "", sParVal);
 		}
 	}
 }
