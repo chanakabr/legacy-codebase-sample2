242bbe743dab2296fa9d294fec04c973c87a25ab
Sunny Simantov <sunny.simantov@kaltura.com>
2015-08-17 11:34:15 +0300

    Unified Search bug fix - truncate instead of sub-string.

--- Catalog/Catalog.cs
+++ Catalog/Catalog.cs
@@ -901,7 +901,7 @@ namespace Catalog
                             if (maxNGram > 0 && 
                                 (leaf.operand == ComparisonOperator.Contains || leaf.operand == ComparisonOperator.NotContains))
                             {
-                                leaf.value = leaf.value.ToString().Substring(0, maxNGram);
+                                leaf.value = leaf.value.ToString().Truncate(maxNGram);
                             }
 
                             #endregion
@@ -4254,7 +4254,5 @@ namespace Catalog
 
             searcherEpgSearch.m_oEpgChannelIDs = new List<long>(channelIds);
         }
-
-
     }
 }
