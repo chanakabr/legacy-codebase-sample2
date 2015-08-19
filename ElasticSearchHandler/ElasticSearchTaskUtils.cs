8f6f060888369effd6f5efa63a582c9b62602142
Sunny Simantov <sunny.simantov@kaltura.com>
2015-08-19 10:31:47 +0300

    Add references, add new utils

---
+++ ElasticSearchHandler/ElasticSearchTaskUtils.cs
@@ -0,0 +1,142 @@
+﻿using ApiObjects;
+using ApiObjects.SearchObjects;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Cache;
+using Tvinci.Core.DAL;
+
+namespace ElasticSearchHandler
+{
+    public static class ElasticSearchTaskUtils
+    {
+        public static string GetWSURL(string key)
+        {
+            return TVinciShared.WS_Utils.GetTcmConfigValue(key);
+        }
+
+        public static long UnixTimeStampNow()
+        {
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds, CultureInfo.CurrentCulture);
+        }
+
+        public static string GetEpgGroupAliasStr(int nGroupID)
+        {
+            return string.Format("{0}_epg", nGroupID);
+        }
+
+        public static string GetMediaGroupAliasStr(int nGroupID)
+        {
+            return nGroupID.ToString();
+        }
+
+        public static string GetNewEpgIndexStr(int nGroupID)
+        {
+            return string.Format("{0}_epg_{1}", nGroupID, DateTime.UtcNow.ToString("yyyyMMddHHmmss"));
+        }
+
+        public static string GetNewMediaIndexStr(int nGroupID)
+        {
+            return string.Format("{0}_{1}", nGroupID, DateTime.UtcNow.ToString("yyyyMMddHHmmss"));
+        }
+
+        public static string GetTanslationType(string sType, LanguageObj oLanguage)
+        {
+            if (oLanguage.IsDefault)
+            {
+                return sType;
+            }
+            else
+            {
+                return string.Concat(sType, "_", oLanguage.Code);
+            }
+        }
+
+        public static EpgCB GetEpgProgram(int nGroupID, int nEpgID)
+        {
+            EpgCB epg = null;
+
+            EpgBL.BaseEpgBL oEpgBL = EpgBL.Utils.GetInstance(nGroupID);
+            try
+            {
+                ulong uEpgID = ulong.Parse(nEpgID.ToString());
+                epg = oEpgBL.GetEpgCB(uEpgID);
+                return epg;
+            }
+            catch (Exception ex)
+            {
+                Logger.Logger.Log("Error (GetEpgProgram)", string.Format("epg:{0}, msg:{1}, st:{2}", nEpgID, ex.Message, ex.StackTrace), "ESFeeder");
+                return null;
+            }
+        }
+        public static List<EpgCB> GetEpgProgram(int nGroupID, int nEpgID, List<string> languages)
+        {  
+             List<EpgCB> epgs = null;
+
+            EpgBL.BaseEpgBL oEpgBL = EpgBL.Utils.GetInstance(nGroupID);
+            try
+            {
+                ulong uEpgID = ulong.Parse(nEpgID.ToString());
+                epgs = oEpgBL.GetEpgCB(uEpgID, languages);
+                return epgs;
+            }
+            catch (Exception ex)
+            {
+                Logger.Logger.Log("Error (GetEpgProgram)", string.Format("epg:{0}, msg:{1}, st:{2}", nEpgID, ex.Message, ex.StackTrace), "ESFeeder");
+                return new List<EpgCB>();
+            }
+        }
+        public static string GetPermittedWatchRules(int nGroupId)
+        {   
+            DataTable permittedWathRulesDt = Tvinci.Core.DAL.CatalogDAL.GetPermittedWatchRulesByGroupId(nGroupId, null);
+            List<string> lWatchRulesIds = null;
+            if (permittedWathRulesDt != null && permittedWathRulesDt.Rows.Count > 0)
+            {
+                lWatchRulesIds = new List<string>();
+                foreach (DataRow permittedWatchRuleRow in permittedWathRulesDt.Rows)
+                {
+                    lWatchRulesIds.Add(ODBCWrapper.Utils.GetSafeStr(permittedWatchRuleRow["RuleID"]));
+                }
+            }
+
+            string sRules = string.Empty;
+
+            if (lWatchRulesIds != null && lWatchRulesIds.Count > 0)
+            {
+                sRules = string.Join(" ", lWatchRulesIds);
+            }
+
+            return sRules;
+        }
+
+        public static List<LanguageObj> GetLanguages(int nGroupID)
+        {
+            List<LanguageObj> lLang = new List<LanguageObj>();
+            try
+            {
+                lLang = CatalogDAL.GetGroupLanguages(nGroupID);
+                return lLang;
+            }
+            catch (Exception ex)
+            {
+                //TO DO ADD LOGGER
+                return new List<LanguageObj>();
+            }
+        }
+    }
+
+    public enum eESFeederType
+    {
+        MEDIA,
+        EPG
+    }
+
+
+    
+}
