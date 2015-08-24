00d301e642d95db56ac3e24841a5117395232056
Oded Shafran <oded@shafranet.com>
2015-08-24 14:20:00 +0300

    removing redundant household_id

--- WebAPI/Controllers/HouseholdUserController.cs
+++ WebAPI/Controllers/HouseholdUserController.cs
@@ -15,8 +15,7 @@ namespace WebAPI.Controllers
     {
         /// <summary>
         /// Removes a user from household   
-        /// </summary>        
-        /// <param name="household_id">Household identifier</param>
+        /// </summary>                
         /// <param name="user_id">User identifier</param>
         /// <remarks>Possible status codes: Bad credentials = 500000, Internal connection = 500001, Timeout = 500002, Bad request = 500003, Forbidden = 500004, Unauthorized = 500005, Configuration error = 500006, Not found = 500007, Partner is invalid = 500008, 
         /// Household does not exists = 1006, Limitation period = 1014, User not exists in household = 1020, Invalid user = 1026, 
