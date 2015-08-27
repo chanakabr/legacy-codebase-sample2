1a8816bf148a4813fe68260fc229746029988509
Sunny Simantov <sunny.simantov@kaltura.com>
2015-08-27 16:35:31 +0300

    Add new Subscription Renewer project and start work on it. Very initial version

---
+++ SubscriptionRenewHandler/SubscriptionRenewRequest.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SubscriptionRenewHandler
+{
+    [Serializable]
+    public class SubscriptionRenewRequest
+    {
+        [JsonProperty("group_id")]
+        public int GroupID
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("billing_method")]
+        public int BillingMethod
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("site_guid")]
+        public string SiteGuid
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("price")]
+        public double Price
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("currency")]
+        public string Currency
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("subscription_code")]
+        public string SubscriptionCode
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("extra_params")]
+        public string ExtraParamas
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("purchase_id")]
+        public int PurchaseId
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("payment_number")]
+        public int PaymentNumber
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("number_of_payments")]
+        public int NumberOfPayments
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("total_number_of_payments")]
+        public int TotalNumberOfPayments
+        {
+            get;
+            set;
+        }
+
+        // End Date?
+
+        // Purchased with preview module?
+
+        [JsonProperty("billing_provider")]
+        public int BillingProvider
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("country_code")]
+        public string CountryCode
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("device_name")]
+        public string DeviceName
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("language_code")]
+        public string LanguageCode
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ SubscriptionRenewHandler/TaskHandler.cs
@@ -0,0 +1,71 @@
+﻿using ApiObjects.Response;
+using KLogMonitor;
+using Newtonsoft.Json;
+using RemoteTasksCommon;
+using SubscriptionRenewHandler.WS_CAS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using TVinciShared;
+
+namespace SubscriptionRenewHandler
+{
+    public class TaskHandler : ITaskHandler
+    {
+        private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+
+        #region ITaskHandler Members
+
+        public string HandleTask(string data)
+        {
+            string result = "failure";
+
+            try
+            {
+                log.InfoFormat("Renew subscription request. data={0}", data);
+
+                SubscriptionRenewRequest request = JsonConvert.DeserializeObject<SubscriptionRenewRequest>(data);
+
+                string url = WS_Utils.GetTcmConfigValue("WS_CAS");
+                string username = string.Empty;
+                string password = string.Empty;
+
+                TasksCommon.RemoteTasksUtils.GetCredentials(request.GroupID, ref username, ref password, ApiObjects.eWSModules.CONDITIONALACCESS);
+
+                module cas = new module();
+
+                if (!string.IsNullOrEmpty(url))
+                {
+                    cas.Url = url;
+                }
+
+                bool success = false;
+
+                
+                ApiObjects.Response.Status status = null;
+
+                if (status != null && status.Code == 0)
+                {
+                    success = true;
+                }
+
+                if (!success)
+                {
+                    // TODO: write format
+                    throw new Exception(string.Format(
+                        "Renew subscription request on {0} did not finish successfully.", request));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
