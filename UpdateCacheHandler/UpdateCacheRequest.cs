db4addaa20a82af1f580907c0eca0519b2061288
Sunny Simantov <sunny.simantov@kaltura.com>
2015-09-01 18:00:52 +0300

    Add update cache task.

---
+++ UpdateCacheHandler/UpdateCacheRequest.cs
@@ -0,0 +1,27 @@
+﻿using CouchbaseManager;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UpdateCacheHandler
+{
+    [Serializable]
+    public class UpdateCacheRequest
+    {
+        [JsonProperty("couchbase_bucket", Required=Required.AllowNull)]
+        public eCouchbaseBucket Bucket
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("keys")]
+        public List<string> Keys
+        {
+            get;
+            set;
+        }
+    }
+}
