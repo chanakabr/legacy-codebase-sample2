15b70696ed893fe92c7152c3ee24c78cc33aee24
Sunny Simantov <sunny.simantov@kaltura.com>
2015-09-02 16:03:50 +0300

    Refactor caching helper: create base class and make Geo Block Rules inherit from it.

---
+++ CachingHelpers/BaseCacheHelper.cs
@@ -0,0 +1,242 @@
+﻿using CachingProvider;
+using CouchbaseManager;
+using DAL;
+using KLogMonitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace CachingHelpers
+{
+    public abstract class BaseCacheHelper<T>
+    {
+        #region Consts
+
+        /// <summary>
+        /// 24 hours
+        /// </summary>
+        protected static readonly double DEFAULT_TIME_IN_CACHE_MINUTES = 1440d;
+        protected static readonly string DEFAULT_CACHE_NAME = "GroupsCache";
+
+        #endregion
+
+        #region Statis members
+
+        private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+        protected static object locker = new object();
+
+        #endregion
+
+        #region Private Members
+
+        protected ICachingService cacheService = null;
+        protected readonly double cacheTime;
+        protected string cacheGroupConfiguration;
+
+        #endregion
+
+        #region Ctor and initialization
+
+        protected BaseCacheHelper()
+        {
+            cacheGroupConfiguration = TVinciShared.WS_Utils.GetTcmConfigValue("GroupsCacheConfiguration");
+
+            switch (cacheGroupConfiguration)
+            {
+                case "CouchBase":
+                {
+                    cacheService = CouchBaseCache<T>.GetInstance("CACHE");
+
+                    //set ttl time for document 
+                    cacheTime = GetDocTTLSettings();
+                    break;
+                }
+                case "InnerCache":
+                {
+                    cacheTime = GetDefaultCacheTimeInMinutes();
+                    InitializeCachingService(GetCacheName(), cacheTime);
+                    break;
+                }
+                case "Hybrid":
+                {
+                    cacheTime = GetDefaultCacheTimeInMinutes();
+                    string cacheName = GetCacheName();
+                    cacheService = HybridCache<T>.GetInstance(eCouchbaseBucket.CACHE, cacheName);
+
+                    break;
+                }
+            }
+        }
+
+        private void InitializeCachingService(string cacheName, double cacheTime)
+        {
+            this.cacheService = new SingleInMemoryCache(cacheName, cacheTime);
+        }
+
+        private string GetCacheName()
+        {
+            string result = DEFAULT_CACHE_NAME;
+
+            string tcm = TVinciShared.WS_Utils.GetTcmConfigValue("GROUPS_CACHE_NAME");
+            
+            if (tcm.Length > 0)
+            {
+                result = tcm;
+            }
+
+            return result;
+        }
+
+        private double GetDefaultCacheTimeInMinutes()
+        {
+            double result = DEFAULT_TIME_IN_CACHE_MINUTES;
+            double tcm = 0d;
+
+            string timeString = TVinciShared.WS_Utils.GetTcmConfigValue("GROUPS_CACHE_TIME_IN_MINUTES");
+
+            if (timeString.Length > 0 && Double.TryParse(timeString, out tcm) && tcm > 0)
+            {
+                result = tcm;
+            }
+
+            return result;
+        }
+
+        private double GetDocTTLSettings()
+        {
+            double result;
+
+            if (!double.TryParse(TVinciShared.WS_Utils.GetTcmConfigValue("GroupsCacheDocTimeout"), out result))
+            {
+                result = 1440.0;
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region Abstract Methods
+
+        protected abstract T BuildValue(params object[] parameters);
+
+        #endregion
+
+        #region Public Methods
+
+        public T Get(string cacheKey, string mutexName, params object[] parameters)
+        {
+            T value = default(T);
+
+            try
+            {
+                BaseModuleCache baseModule = this.cacheService.Get(cacheKey);
+
+                if (baseModule != null && baseModule.result != null)
+                {
+                    value = (T)baseModule.result;
+                }
+                else
+                {
+                    bool inserted = false;
+                    bool createdNew = false;
+                    var mutexSecurity = Utils.CreateMutex();
+                    using (Mutex mutex = new Mutex(false, mutexName, out createdNew, mutexSecurity))
+                    {
+                        try
+                        {
+                            mutex.WaitOne(-1);
+
+                            VersionModuleCache versionModule = (VersionModuleCache)this.cacheService.GetWithVersion<T>(cacheKey);
+
+                            if (versionModule != null && versionModule.result != null)
+                            {
+                                value = (T)baseModule.result;
+                            }
+                            else
+                            {
+                                T tempValue = BuildValue(parameters);
+
+                                for (int i = 0; i < 3 && !inserted; i++)
+                                {
+                                    //try insert to Cache
+                                    versionModule.result = value;
+                                    inserted = this.cacheService.SetWithVersion<T>(cacheKey, versionModule, cacheTime);
+
+                                    if (inserted)
+                                    {
+                                        value = tempValue;
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            log.ErrorFormat("Get - " + string.Format("Couldn't get object in cache by key {0}. ex = {1}",
+                                cacheKey, ex.Message), ex);
+                        }
+                        finally
+                        {
+                            mutex.ReleaseMutex();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Get - " + string.Format("Couldn't get object in cache by key {0}. ex = {1}",
+                    cacheKey, ex.Message), ex);
+            }
+
+            return value;
+        }
+
+        public virtual bool Remove(string cacheKey, string mutexName)
+        {
+            bool isRemoveSucceeded = false;
+
+            try
+            {
+                for (int i = 0; i < 3 && !isRemoveSucceeded; i++)
+                {
+                    VersionModuleCache versionModule = (VersionModuleCache)cacheService.GetWithVersion<T>(cacheKey);
+
+                    if (versionModule != null && versionModule.result != null)
+                    {
+                        T cacheValue = (T)versionModule.result;
+
+                        bool createdNew = false;
+                        var mutexSecurity = Utils.CreateMutex();
+
+                        using (Mutex mutex = new Mutex(false, mutexName, out createdNew, mutexSecurity))
+                        {
+                            mutex.WaitOne(-1);
+
+                            //try update to CB
+                            BaseModuleCache bModule = cacheService.Remove(cacheKey);
+
+                            if (bModule != null && bModule.result != null)
+                            {
+                                isRemoveSucceeded = true;
+                            }
+
+                            mutex.ReleaseMutex();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Remove - " +
+                    string.Format("failed to Remove object from cache key={0}, ex={1}", cacheKey, ex.Message), ex);
+            }
+
+            return isRemoveSucceeded;
+        }
+
+        #endregion
+    }
+}
--- CachingHelpers/GeoBlockRules.cs
+++ CachingHelpers/GeoBlockRules.cs
@@ -12,7 +12,7 @@ using System.Threading;
 
 namespace CachingHelpers
 {
-    public class GeoBlockRules
+    public class GeoBlockRules : BaseCacheHelper<List<int>>
     {
         #region Consts
 
@@ -64,199 +64,45 @@ namespace CachingHelpers
         #region Ctor and initialization
 
         private GeoBlockRules()
+            : base()
         {
-            cacheGroupConfiguration = TVinciShared.WS_Utils.GetTcmConfigValue("GroupsCacheConfiguration");
-
-            switch (cacheGroupConfiguration)
-            {
-                case "CouchBase":
-                {
-                    rulesCache = CouchBaseCache<List<int>>.GetInstance("CACHE");
-
-                    //set ttl time for document 
-                    cacheTime = GetDocTTLSettings();     
-                    break;
-                }
-                case "InnerCache":
-                {
-                    cacheTime = GetDefaultCacheTimeInMinutes();
-                    InitializeCachingService(GetCacheName(), cacheTime);
-                    break;
-                }
-                case "Hybrid":
-                {
-                    cacheTime = GetDefaultCacheTimeInMinutes();
-                    string cacheName = GetCacheName();
-                    rulesCache = HybridCache<List<int>>.GetInstance(eCouchbaseBucket.CACHE, cacheName);
-
-                    break;
-                }
-            }
-        }
-
-        private void InitializeCachingService(string cacheName, double cacheTime)
-        {
-            this.rulesCache = new SingleInMemoryCache(cacheName, cacheTime);
-        }
-
-        private string GetCacheName()
-        {
-            string result = DEFAULT_CACHE_NAME;
-
-            string tcm = TVinciShared.WS_Utils.GetTcmConfigValue("GROUPS_CACHE_NAME");
-            
-            if (tcm.Length > 0)
-            {
-                result = tcm;
-            }
-
-            return result;
-        }
-
-        private double GetDefaultCacheTimeInMinutes()
-        {
-            double result = DEFAULT_TIME_IN_CACHE_MINUTES;
-            double tcm = 0d;
-
-            string timeString = TVinciShared.WS_Utils.GetTcmConfigValue("GROUPS_CACHE_TIME_IN_MINUTES");
-
-            if (timeString.Length > 0 && Double.TryParse(timeString, out tcm) && tcm > 0)
-            {
-                result = tcm;
-            }
-
-            return result;
-        }
-
-        private double GetDocTTLSettings()
-        {
-            double result;
-
-            if (!double.TryParse(TVinciShared.WS_Utils.GetTcmConfigValue("GroupsCacheDocTimeout"), out result))
-            {
-                result = 1440.0;
-            }
-
-            return result;
         }
 
         #endregion
 
-        #region Country To Rule cache
+        #region Override Methods
+
+        protected override List<int> BuildValue(params object[] parameters)
+        {
+            int groupId = (int)parameters[0];
+            int countryId = (int)parameters[1];
+
+            return ApiDAL.GetPermittedGeoBlockRulesByCountry(groupId, countryId);
+        }
+
+        #endregion
+
+        #region Public Methods
 
         public List<int> GetGeoBlockRulesByCountry(int groupId, int countryId)
         {
             List<int> rules = new List<int>();
 
-            BaseModuleCache baseModule = null;
-            try
-            {
-                string cacheKey = string.Format("country_to_rules_{0}_{1}", groupId, countryId);
+            string cacheKey = string.Format("country_to_rules_{0}_{1}", groupId, countryId);
+            string mutexName = string.Concat("Group GeoBlockRules GID_", groupId);
 
-                baseModule = this.rulesCache.Get(cacheKey);
-
-                if (baseModule != null && baseModule.result != null)
-                {
-                    rules = baseModule.result as List<int>;
-                }
-                else
-                {
-                    bool inserted = false;
-                    bool createdNew = false;
-                    var mutexSecurity = Utils.CreateMutex();
-                    using (Mutex mutex = new Mutex(false, string.Concat("Group GeoBlockRules GID_", groupId), out createdNew, mutexSecurity))
-                    {
-                        try
-                        {
-                            mutex.WaitOne(-1);
-
-                            VersionModuleCache versionModule = (VersionModuleCache)this.rulesCache.GetWithVersion<List<int>>(cacheKey);
-
-                            if (versionModule != null && versionModule.result != null)
-                            {
-                                rules = baseModule.result as List<int>;
-                            }
-
-                            else
-                            {
-                                List<int> tempRules = ApiDAL.GetPermittedGeoBlockRulesByCountry(groupId, countryId);
-
-                                for (int i = 0; i < 3 && !inserted; i++)
-                                {
-                                    //try insert to Cache
-                                    versionModule.result = tempRules;
-                                    inserted = this.rulesCache.SetWithVersion<List<int>>(cacheKey, versionModule, cacheTime);
-
-                                    if (inserted)
-                                    {
-                                        rules = tempRules;
-                                    }
-                                }
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            log.Error("GetGeoBlockRulesByCountry - " + string.Format("Couldn't get geo block rules for group {0} and country {1}, ex = {2}",
-                                groupId, countryId, ex.Message), ex);
-                        }
-                        finally
-                        {
-                            mutex.ReleaseMutex();
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                log.Error("GetGeoBlockRulesByCountry - " + string.Format("Couldn't get geo block rules for group {0} and country {1}, ex = {2}",
-                    groupId, countryId, ex.Message), ex);
-            }
+            rules = base.Get(cacheKey, mutexName, groupId, countryId);
 
             return rules;
         }
 
-        public bool RemoveGeoBlockRulesOfcountry(int groupId, int countryId)
+        public bool Remove(int groupId, int countryId)
         {
             bool isRemoveSucceeded = false;
-            VersionModuleCache versionModule = null;
+            string cacheKey = string.Format("country_to_rules_{0}_{1}", groupId, countryId);
+            string mutexName = string.Concat("Cache Delete GeoBlockRules_", groupId);
 
-            try
-            {
-                string cacheKey = string.Format("country_to_rules_{0}_{1}", groupId, countryId);
-
-                for (int i = 0; i < 3 && !isRemoveSucceeded; i++)
-                {
-                    versionModule = (VersionModuleCache)rulesCache.GetWithVersion<List<int>>(cacheKey);
-
-                    if (versionModule != null && versionModule.result != null)
-                    {
-                        List<int> rules = versionModule.result as List<int>;
-
-                        bool createdNew = false;
-                        var mutexSecurity = Utils.CreateMutex();
-
-                        using (Mutex mutex = new Mutex(false, string.Concat("Cache Delete GeoBlockRules_", groupId), out createdNew, mutexSecurity))
-                        {
-                            mutex.WaitOne(-1);
-
-                            //try update to CB
-                            BaseModuleCache bModule = rulesCache.Remove(cacheKey);
-
-                            if (bModule != null && bModule.result != null)
-                            {
-                                isRemoveSucceeded = true;
-                            }
-
-                            mutex.ReleaseMutex();
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                log.Error("RemoveGeoBlockRulesOfcountry - " +
-                    string.Format("failed to Remove geo block rules from cache GroupID={0}, country= {1}, ex={2}", groupId, countryId, ex.Message), ex);
-            }
+            isRemoveSucceeded = base.Remove(cacheKey, mutexName);
 
             return isRemoveSucceeded;
         }
