d38180ec244a82420e60be60144a1e6283d904e5
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2015-09-16 16:05:12 +0300

    no message

---
+++ ApiObjects/OSSAdapterResponse.cs
@@ -0,0 +1,26 @@
+﻿using ApiObjects.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApiObjects
+{
+    public class OSSAdapterResponse
+    {
+        public ApiObjects.Response.Status Status { get; set; }
+        public List<OSSAdapterBase> OSSAdapters  { get; set; }
+
+        public OSSAdapterResponse()
+        {
+            Status = new ApiObjects.Response.Status((int)eResponseStatus.Error, string.Empty);
+            OSSAdapters = new List<OSSAdapterBase>();
+        }
+
+        public OSSAdapterResponse(ApiObjects.Response.Status status, List<OSSAdapterBase> ossAdapters)
+        {
+            this.Status = status;
+            this.OSSAdapters = ossAdapters;
+        }
+    }
+}
