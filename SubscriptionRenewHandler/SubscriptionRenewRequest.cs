61110aa4b7c78c8a9e50901f539d5dfa03c6faef
Sunny Simantov <sunny.simantov@kaltura.com>
2015-09-20 09:36:18 +0300

    Add subscription renewer to Nova.

---
+++ SubscriptionRenewHandler/SubscriptionRenewRequest.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SubscriptionRenewHandler
+{
+    [Serializable]
+    public class SubscriptionRenewRequest
+    {
+        [JsonProperty("group_id")]
+        public int GroupID
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("billing_guid")]
+        public string BillingGuid
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("site_guid")]
+        public string SiteGuid
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("purchase_id")]
+        public long PurchaseId
+        {
+            get;
+            set;
+        }
+    }
+}
