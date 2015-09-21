3f183852d7f1bee8c1e3f8203a4b51dce1fc0f8c
Ira Yanai <iray@tvinci.com>
2015-09-21 18:09:12 +0300

    Renew - Pending - check renew interval > 0

--- ConditionalAccess/Implementations/BaseConditionalAccess.cs
+++ ConditionalAccess/Implementations/BaseConditionalAccess.cs
@@ -14252,16 +14252,19 @@ namespace ConditionalAccess
                         }
 
                         // enqueue renew transaction
-                        RenewTransactionsQueue queue = new RenewTransactionsQueue();
-                        RenewTransactionData data = new RenewTransactionData(m_nGroupID, siteguid, purchaseId, billingGuid, DateTime.UtcNow.AddMinutes(paymentGatewayResponse.RenewalIntervalMinutes));
-                        bool enqueueSuccessful = queue.Enqueue(data, string.Format(ROUTING_KEY_PROCESS_RENEW_SUBSCRIPTION, m_nGroupID));
-                        if (!enqueueSuccessful)
+                        if (paymentGatewayResponse.RenewalIntervalMinutes > 0)
                         {
-                            log.ErrorFormat("Failed enqueue of renew transaction {0}", data);
-                            return false;
+                            RenewTransactionsQueue queue = new RenewTransactionsQueue();
+                            RenewTransactionData data = new RenewTransactionData(m_nGroupID, siteguid, purchaseId, billingGuid, DateTime.UtcNow.AddMinutes(paymentGatewayResponse.RenewalIntervalMinutes));
+                            bool enqueueSuccessful = queue.Enqueue(data, string.Format(ROUTING_KEY_PROCESS_RENEW_SUBSCRIPTION, m_nGroupID));
+                            if (!enqueueSuccessful)
+                            {
+                                log.ErrorFormat("Failed enqueue of renew transaction {0}", data);
+                                return false;
+                            }
+                            else
+                                log.DebugFormat("New task created (upon renew pending response). data: {0}", data);
                         }
-                        else
-                            log.DebugFormat("New task created (upon renew pending response). data: {0}", data);
 
                         log.DebugFormat("pending renew returned. subID: {0}, price: {1}, currency: {2}, userID: {3}", productId, price, currency, siteguid);
                         return true;
