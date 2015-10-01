aca5c0bf1d0886e5352f80702363a818b869ce9f
Sunny Simantov <sunny.simantov@kaltura.com>
2015-10-01 16:42:45 +0300

    Add recommendation engine pages

---
+++ Web Sites/TVM/adm_recommendation_engine_adapter.aspx.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TVinciShared;
+
+public partial class adm_recommendation_engine_adapter : System.Web.UI.Page
+{
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+
+        if (LoginManager.IsPagePermitted() == false)
+            LoginManager.LogoutFromSite("login.html");
+
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        if (!IsPostBack)
+        {
+            Int32 nMenuID = 0;
+            m_sMenu = TVinciShared.Menu.GetMainMenu(6, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 1, false);
+            if (Request.QueryString["search_save"] != null)
+                Session["search_save"] = "1";
+            else
+                Session["search_save"] = null;
+        }
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetTableCSV()
+    {
+        string sOldOrderBy = "";
+        if (Session["order_by"] != null)
+            sOldOrderBy = Session["order_by"].ToString();
+        DBTableWebEditor theTable = new DBTableWebEditor(true, true, false, "", "adm_table_header", "adm_table_cell", "adm_table_alt_cell", "adm_table_link", "adm_table_pager", "adm_table", sOldOrderBy, 50);
+        FillTheTableEditor(ref theTable, sOldOrderBy);
+
+        string sCSVFile = theTable.OpenCSV();
+        theTable.Finish();
+        theTable = null;
+        return sCSVFile;
+    }
+
+    protected void FillTheTableEditor(ref DBTableWebEditor theTable, string sOrderBy)
+    {
+        Int32 groupID = LoginManager.GetLoginGroupID();
+
+        theTable += "select adapter.id, adapter.name, adapter.group_id, adapter.is_active, adapter.status, adapter.adapter_url as 'adapter url'";
+        theTable += ",adapter.external_identifier as 'external id'"; //, pg.pending_interval, pg.pending_retries, pg.shared_secret";
+        theTable += "from recommendation_engines adapter ";
+        theTable += "where";
+        theTable += ODBCWrapper.Parameter.NEW_PARAM("adapter.group_id", "=", groupID);
+        theTable += "and";
+        theTable += ODBCWrapper.Parameter.NEW_PARAM("adapter.status", "=", 1);
+        theTable += "order by id";
+        theTable.SetConnectionKey("main_connection_string");
+
+        theTable.AddHiddenField("is_active");
+        theTable.AddHiddenField("status");
+        theTable.AddHiddenField("group_id");
+        theTable.AddActivationField("is_active");
+        //theTable.AddActivationField("payment_gateway");
+
+        DataTableLinkColumn linkColumnKeParams = new DataTableLinkColumn("adm_recommendation_engines_settings.aspx", "Params", "");
+        linkColumnKeParams.AddQueryStringValue("paymentGW_id", "field=id");
+        linkColumnKeParams.AddQueryCounterValue("select count(*) as val from adm_recommendation_engines_settings where ( status=1 or status = 4 )  and recommendation_engine_id=", "field=id");
+        theTable.AddLinkColumn(linkColumnKeParams);
+
+        if (LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.EDIT))
+        {
+            DataTableLinkColumn linkColumn1 = new DataTableLinkColumn("adm_recommendation_engine_adapter_new.aspx", "Edit", "");
+            linkColumn1.AddQueryStringValue("adapter_id", "field=id");
+            theTable.AddLinkColumn(linkColumn1);
+        }
+
+        if (LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.REMOVE))
+        {
+            DataTableLinkColumn linkColumn = new DataTableLinkColumn("adm_generic_remove.aspx", "Delete", "STATUS=1;STATUS=3");
+            linkColumn.AddQueryStringValue("id", "field=id");
+            linkColumn.AddQueryStringValue("table", "recommendation_engines");
+            linkColumn.AddQueryStringValue("db", "main_connection_string");
+            linkColumn.AddQueryStringValue("confirm", "true");
+            linkColumn.AddQueryStringValue("main_menu", "6");
+            linkColumn.AddQueryStringValue("sub_menu", "1");
+            linkColumn.AddQueryStringValue("rep_field", "username");
+            linkColumn.AddQueryStringValue("rep_name", "Username");
+            theTable.AddLinkColumn(linkColumn);
+        }
+
+        if (LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.PUBLISH))
+        {
+            DataTableLinkColumn linkColumn = new DataTableLinkColumn("adm_generic_confirm.aspx", "Confirm", "STATUS=3;STATUS=4");
+            linkColumn.AddQueryStringValue("id", "field=id");
+            linkColumn.AddQueryStringValue("table", "recommendation_engines");
+            linkColumn.AddQueryStringValue("db", "main_connection_string");
+            linkColumn.AddQueryStringValue("confirm", "true");
+            linkColumn.AddQueryStringValue("main_menu", "6");
+            linkColumn.AddQueryStringValue("sub_menu", "1");
+            linkColumn.AddQueryStringValue("rep_field", "username");
+            linkColumn.AddQueryStringValue("rep_name", "Username");
+            theTable.AddLinkColumn(linkColumn);
+        }
+
+        if (LoginManager.IsActionPermittedOnPage(LoginManager.PAGE_PERMISION_TYPE.REMOVE))
+        {
+            DataTableLinkColumn linkColumn = new DataTableLinkColumn("adm_generic_confirm.aspx", "Cancel", "STATUS=3;STATUS=4");
+            linkColumn.AddQueryStringValue("id", "field=id");
+            linkColumn.AddQueryStringValue("table", "payment_gateway");
+            linkColumn.AddQueryStringValue("db", "billing_connection");
+            linkColumn.AddQueryStringValue("confirm", "false");
+            linkColumn.AddQueryStringValue("main_menu", "6");
+            linkColumn.AddQueryStringValue("sub_menu", "1");
+            linkColumn.AddQueryStringValue("rep_field", "username");
+            linkColumn.AddQueryStringValue("rep_name", "Username");
+            theTable.AddLinkColumn(linkColumn);
+        }
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        string sOldOrderBy = "";
+        if (Session["order_by"] != null)
+            sOldOrderBy = Session["order_by"].ToString();
+        DBTableWebEditor theTable = new DBTableWebEditor(true, true, true, "", 
+            "adm_table_header", "adm_table_cell", "adm_table_alt_cell", "adm_table_link", "adm_table_pager", "adm_table", sOldOrderBy, 50);
+        FillTheTableEditor(ref theTable, sOrderBy);
+
+        string sTable = theTable.GetPageHTML(int.Parse(sPageNum), sOrderBy);
+        theTable.Finish();
+        theTable = null;
+        return sTable;
+    }
+
+    public void GetHeader()
+    {
+        Response.Write(PageUtils.GetPreHeader() + ": " + "Recommendation Engine");
+    }
+}
---
+++ Web Sites/TVM/adm_recommendation_engine_adapter_new.aspx.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TVinciShared;
+
+public partial class adm_recommendation_engine_adapter_new : System.Web.UI.Page
+{
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        if (LoginManager.IsPagePermitted("adm_recommendation_engine_adapter.aspx") == false)
+            LoginManager.LogoutFromSite("login.html");
+        if (LoginManager.IsActionPermittedOnPage("adm_recommendation_engine_adapter.aspx", LoginManager.PAGE_PERMISION_TYPE.EDIT) == false)
+            LoginManager.LogoutFromSite("login.html");
+
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+
+        if (!IsPostBack)
+        {
+            bool flag = false;
+
+            if (Request.QueryString["submited"] != null && Request.QueryString["submited"].ToString().Trim() == "1")
+            {
+                int pgid = 0;
+                
+                // Validate uniqe external id
+                if (Session["adapter_id"] != null && Session["adapter_id"].ToString() != "" && int.Parse(Session["adapter_id"].ToString()) != 0)
+                {
+                    int.TryParse(Session["adapter_id"].ToString(), out pgid);
+                }
+
+                System.Collections.Specialized.NameValueCollection coll = HttpContext.Current.Request.Form;
+                
+                if (coll != null && coll.Count > 2 && !string.IsNullOrEmpty(coll["1_val"]))
+                {
+                    if (IsExternalIDExists(coll["1_val"], pgid))
+                    {
+                        Session["error_msg"] = "External Id must be unique";
+                        flag = true;
+                    }
+                    else
+                    {
+                        Int32 nID = DBManipulator.DoTheWork();
+
+                        //// set adapter configuration
+                        //Billing.module billing = new Billing.module();
+
+                        //string sIP = "1.1.1.1";
+                        //string sWSUserName = "";
+                        //string sWSPass = "";
+                        //TVinciShared.WS_Utils.GetWSUNPass(LoginManager.GetLoginGroupID(), "SetPaymentGatewayConfiguration", "billing", sIP, ref sWSUserName, ref sWSPass);
+                        //string sWSURL = GetWSURL("billing_ws");
+                        //if (sWSURL != "")
+                        //    billing.Url = sWSURL;
+                        //try
+                        //{
+                        //    Billing.Status status = billing.SetPaymentGatewayConfiguration(sWSUserName, sWSPass, nID);
+                        //    Logger.Logger.Log("SetPaymentGatewayConfiguration", string.Format("payment gateway ID:{0}, status:{1}", nID, status.Code), "SetPaymentGatewayConfiguration");
+                        //}
+                        //catch (Exception ex)
+                        //{
+                        //    Logger.Logger.Log("Exception", string.Format("payment gateway ID:{0}, ex msg:{1}, ex st: {2} ", nID, ex.Message, ex.StackTrace), "SetPaymentGatewayConfiguration");
+                        //}
+
+                        return;
+                    }
+                }
+            }
+
+            Int32 nMenuID = 0;
+            m_sMenu = TVinciShared.Menu.GetMainMenu(7, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 1, true);
+
+            if (Request.QueryString["adapter_id"] != null && Request.QueryString["adapter_id"].ToString() != "")
+            {
+                Session["adapter_id"] = int.Parse(Request.QueryString["adapter_id"].ToString());
+            }
+            else if (!flag)
+                Session["adapter_id"] = 0;
+        }
+    }
+
+    public void GetHeader()
+    {
+        Response.Write(PageUtils.GetPreHeader() + ": Recommendation Engine");
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        if (Session["error_msg"] != null && Session["error_msg"].ToString() != "")
+        {
+            Session["error_msg"] = "";
+            return Session["last_page_html"].ToString();
+        }
+
+        object adapterId = null;
+
+        if (Session["adapter_id"] != null && Session["adapter_id"].ToString() != "" && int.Parse(Session["adapter_id"].ToString()) != 0)
+        {
+            adapterId = Session["adapter_id"];
+        }
+
+        string backUrl = "adm_recommendation_engine_adapter.aspx?search_save=1";
+
+        object group_id = LoginManager.GetLoginGroupID();
+
+        DBRecordWebEditor theRecord = new DBRecordWebEditor("recommendation_engines", "adm_table_pager", backUrl, "", "ID", adapterId, backUrl, "");
+
+        DataRecordShortTextField dr_name = new DataRecordShortTextField("ltr", true, 60, 128);
+        dr_name.Initialize("Name", "adm_table_header_nbg", "FormInput", "NAME", true);
+        theRecord.AddRecord(dr_name);
+
+        DataRecordShortTextField dr_external_identifier = new DataRecordShortTextField("ltr", true, 60, 128);
+        dr_external_identifier.Initialize("External Identifier", "adm_table_header_nbg", "FormInput", "external_identifier", true);
+        theRecord.AddRecord(dr_external_identifier);
+
+        DataRecordShortTextField dr_adapter_url = new DataRecordShortTextField("ltr", true, 60, 128);
+        dr_adapter_url.Initialize("Adapter URL", "adm_table_header_nbg", "FormInput", "adapter_url", false);
+        theRecord.AddRecord(dr_adapter_url);
+
+        DataRecordShortTextField dr_shared_secret = new DataRecordShortTextField("ltr", true, 60, 128);
+        dr_shared_secret.Initialize("Shared Secret", "adm_table_header_nbg", "FormInput", "shared_secret", true);
+        theRecord.AddRecord(dr_shared_secret);
+
+        DataRecordShortIntField dr_groups = new DataRecordShortIntField(false, 9, 9);
+        dr_groups.Initialize("Group", "adm_table_header_nbg", "FormInput", "group_id", false);
+        dr_groups.SetValue(group_id.ToString());
+        theRecord.AddRecord(dr_groups);
+
+        string table = theRecord.GetTableHTML("adm_recommendation_engine_adapter_new.aspx?submited=1");
+
+        return table;
+    }
+
+    static public string GetWSURL(string sKey)
+    {
+        return TVinciShared.WS_Utils.GetTcmConfigValue(sKey);
+    }
+
+    static private bool IsExternalIDExists(string extId, int pgid)
+    {
+        int groupID = LoginManager.GetLoginGroupID();
+        bool result = false;
+
+        ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
+        selectQuery.SetConnectionKey("billing_connection");
+        selectQuery += "select ID from recommendation_engines where status=1 and";
+        selectQuery += ODBCWrapper.Parameter.NEW_PARAM("GROUP_ID", "=", groupID);
+        selectQuery += "and";
+        selectQuery += ODBCWrapper.Parameter.NEW_PARAM("external_identifier", "=", extId);
+        selectQuery += "and";
+        selectQuery += ODBCWrapper.Parameter.NEW_PARAM("ID", "<>", pgid);
+
+        if (selectQuery.Execute("query", true) != null)
+        {
+            int count = selectQuery.Table("query").DefaultView.Count;
+
+            if (count > 0)
+            {
+                result = true;
+                int adapterId = ODBCWrapper.Utils.GetIntSafeVal(selectQuery, "ID", 0);
+                string name = ODBCWrapper.Utils.GetStrSafeVal(selectQuery, "NAME", 0);
+                Logger.Logger.Log("IsExternalIDExists", string.Format("id:{0}, name:{1}", adapterId, name), "recommendation_engines");
+            }
+        }
+
+        selectQuery.Finish();
+        selectQuery = null;
+
+        return result;
+    }
+}
