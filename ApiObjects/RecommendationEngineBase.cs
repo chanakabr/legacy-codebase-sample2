38eb1d225db15ce27912484f607d2c67c30bb30b
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2015-10-01 14:21:48 +0300

    Add Files

---
+++ ApiObjects/RecommendationEngineBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApiObjects
+{
+    public class RecommendationEngineBase
+    {
+         public int ID { get; set; }
+        public string Name { get; set; }
+
+        public RecommendationEngineBase()
+        {
+        }
+
+        public RecommendationEngineBase(RecommendationEngineBase recommendationEngineBase)
+        {
+            this.ID = recommendationEngineBase.ID;
+            this.Name = recommendationEngineBase.Name;
+        }
+
+        public RecommendationEngineBase(int id, string name, bool isDefault)
+        {
+            this.ID = id;
+            this.Name = name;
+        }
+    }
+}
---
+++ ApiObjects/RecommendationEngineSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApiObjects
+{
+    public class RecommendationEngineSettings
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+
+        public RecommendationEngineSettings()
+        {
+
+        }
+
+        public RecommendationEngineSettings(string key, string value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+    }
+}
