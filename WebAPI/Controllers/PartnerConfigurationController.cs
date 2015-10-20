793117b82d56186deb388fe2c013c113086df7f3
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2015-10-20 18:08:44 +0300

    Set Partner Configuration ( group parameters )

---
+++ WebAPI/Controllers/PartnerConfigurationController.cs
@@ -0,0 +1,62 @@
+﻿using KLogMonitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using WebAPI.ClientManagers.Client;
+using WebAPI.Exceptions;
+using WebAPI.Managers.Models;
+using WebAPI.Models;
+using WebAPI.Models.Partner;
+using WebAPI.Utils;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("_service/PartnerConfiguration/action")]
+    public class PartnerConfigurationController : ApiController
+    {
+        private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+
+        /// <summary>
+        /// Update Partner Configuration
+        /// </summary>
+        /// <param name="configuration">Partner Configuration</param>        
+        [Route("update"), HttpPost]
+        [ApiAuthorize(true)]
+        public bool Update(KalturaPartnerConfigurationBase configuration)
+        {
+            bool response = false;
+
+            if (configuration == null)
+            {
+                throw new BadRequestException((int)WebAPI.Managers.Models.StatusCode.BadRequest, "configuration cannot be null");
+            }
+
+            int groupId = KS.GetFromRequest().GroupId;
+
+            try
+            {
+                // call client
+                KalturaBillingPartnerConfig partnerConfig = configuration as KalturaBillingPartnerConfig;
+                if (partnerConfig != null)
+                {
+                    response = ClientsManager.BillingClient().SetPartnerConfiguration(groupId, partnerConfig);
+                }
+                else
+                {
+                    throw new BadRequestException((int)WebAPI.Managers.Models.StatusCode.BadRequest, "Not implemented");
+                }
+            }
+            catch (ClientException ex)
+            {
+                ErrorUtils.HandleClientException(ex);
+            }
+
+            return response;
+        }
+
+    }
+
+}
---
+++ WebAPI/Models/Partner/KalturaBillingPartnerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using WebAPI.Models.Partner;
+
+namespace WebAPI.Models.Partner
+{
+    public class KalturaBillingPartnerConfig : KalturaPartnerConfigurationBase
+    {
+        /// <summary>
+        /// value
+        /// </summary>
+        [DataMember(Name = "value")]
+        [JsonProperty("value")]
+        [XmlElement(ElementName = "value")]
+        public string Value { get; set; }
+
+        /// <summary>
+        /// partner configuration type
+        /// </summary>
+        [DataMember(Name = "partner_configuration_type")]
+        [JsonProperty("partner_configuration_type")]
+        [XmlElement(ElementName = "partner_configuration_type")]
+        public KalturaPartnerConfigurationHolder Type { get; set; }
+    }
+}
