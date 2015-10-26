fb7ac411c0c9c3ab0e3fa259688bc98954235a6f
Irena Leibman <irena.leibman@kaltura.com>
2015-10-26 10:29:12 +0200

    BEO-373 - Resilient token refresh (use Refresh token only)

--- Libs/TVPApiModule/Objects/Authorization/APIToken.cs
+++ Libs/TVPApiModule/Objects/Authorization/APIToken.cs
@@ -77,6 +77,24 @@ namespace TVPApiModule.Objects.Authorization
             IsLongRefreshExpiration = token.IsLongRefreshExpiration;
         }
 
+        public APIToken(RefreshToken token, GroupConfiguration groupConfig)
+        {
+            AccessToken = Guid.NewGuid().ToString().Replace("-", string.Empty);
+            RefreshToken = token.RefreshTokenValue;
+            RefreshTokenExpiration = groupConfig.IsRefreshTokenExtendable ?
+                (token.IsLongRefreshExpiration ? token.RefreshTokenExpiration + groupConfig.RefreshExpirationForPinLoginSeconds : token.RefreshTokenExpiration + groupConfig.RefreshTokenExpirationSeconds) :
+                token.RefreshTokenExpiration;
+
+            // set access expiration time - no longer than refresh expiration
+            long accessExpiration = (long)TimeHelper.ConvertToUnixTimestamp(DateTime.UtcNow.AddSeconds(groupConfig.AccessTokenExpirationSeconds));
+            AccessTokenExpiration = accessExpiration >= RefreshTokenExpiration ? RefreshTokenExpiration : accessExpiration;
+
+            GroupID = token.GroupID;
+            SiteGuid = token.SiteGuid;
+            IsAdmin = token.IsAdmin;
+            IsLongRefreshExpiration = token.IsLongRefreshExpiration;
+        }
+
         public static string GetAPITokenId(string accessToken)
         {
             return string.Format("access_{0}", accessToken);
