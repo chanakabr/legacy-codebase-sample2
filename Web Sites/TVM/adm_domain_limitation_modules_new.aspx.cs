970ddf337070b7e124f697ea0ba2a3de31f694ce
unknown <Lior.Faingold@IL-LIORF-RND.Kaltura.com>
2015-11-03 10:09:34 +0200

    added support for duallist

--- Web Sites/TVM/adm_domain_limitation_modules_new.aspx.cs
+++ Web Sites/TVM/adm_domain_limitation_modules_new.aspx.cs
@@ -309,6 +309,13 @@ public partial class adm_domain_limitation_modules_new : System.Web.UI.Page
 
     public string initDualObj()
     {
+        Dictionary<string, object> dualList = new Dictionary<string, object>();
+        dualList.Add("FirstListTitle", "Device Families");
+        dualList.Add("SecondListTitle", "Available Device Families");
+
+        object[] resultData = null;
+        List<object> deviceFamilies = new List<object>();
+
         ODBCWrapper.DataSetSelectQuery selectQuery = null;
         List<UMObj> allFamilies = null;
         List<UMObj> limitFamilies = null;
@@ -317,12 +324,6 @@ public partial class adm_domain_limitation_modules_new : System.Web.UI.Page
         {
             Int32 nLogedInGroupID = LoginManager.GetLoginGroupID();
             int limitID = 0;
-            string sRet = string.Empty;
-            sRet += "Device Families";
-            sRet += "~~|~~";
-            sRet += "Available Device Families";
-            sRet += "~~|~~";
-            sRet += "<root>";
             if (Session["limit_id"] != null && Session["limit_id"].ToString().Length > 0)
             {
                 limitID = Int32.Parse(Session["limit_id"].ToString());
@@ -346,7 +347,14 @@ public partial class adm_domain_limitation_modules_new : System.Web.UI.Page
 
                     if (bIsOK)
                     {
-                        sRet += "<item id=\"" + sID + "\"  title=\"" + TVinciShared.ProtocolsFuncs.XMLEncode(allFamilies[i].m_title, true) + "\" description=\"" + TVinciShared.ProtocolsFuncs.XMLEncode(string.Empty, true) + "\" inList=\"false\" />";
+                        var data = new
+                        {
+                            ID = sID,
+                            Title = allFamilies[i].m_title,
+                            Description = allFamilies[i].m_title,
+                            InList = false
+                        };
+                        deviceFamilies.Add(data);                       
                     }
                 } // end bigger for
 
@@ -356,13 +364,26 @@ public partial class adm_domain_limitation_modules_new : System.Web.UI.Page
                     List<UMObj> umObjList = Session["device_families"] as List<UMObj>;
                     foreach (UMObj obj in umObjList)
                     {
-                        sRet += "<item id=\"" + obj.m_id + "\"  title=\"" + TVinciShared.ProtocolsFuncs.XMLEncode(obj.m_title, true) + "\" description=\"" + TVinciShared.ProtocolsFuncs.XMLEncode(obj.m_description, true) + "\" inList=\"true\" />";
+                        var data = new
+                        {
+                            ID = obj.m_id,
+                            Title = obj.m_title,
+                            Description = obj.m_description,
+                            InList = true
+                        };
+                        deviceFamilies.Add(data);                        
                     }
                 }
             }
 
-            sRet += "</root>";
-            return sRet;
+            resultData = new object[deviceFamilies.Count];
+            resultData = deviceFamilies.ToArray();
+
+            dualList.Add("Data", resultData);
+            dualList.Add("pageName", "adm_domain_limitation_modules_new.aspx");
+            dualList.Add("withCalendar", false);
+
+            return dualList.ToJSON();
 
         }
         finally
@@ -489,50 +510,44 @@ public partial class adm_domain_limitation_modules_new : System.Web.UI.Page
 
     }
 
-    public string changeItemStatus(string sID, string sAction, string index)
+    public string changeItemStatus(string sID, string sAction)
     {
         string retVal = string.Empty;
         if (Session["device_families"] != null && Session["device_families"] is List<UMObj>)
         {
             List<UMObj> umObjList = Session["device_families"] as List<UMObj>;
-            string action = sAction.ToLower();
-            switch (action)
+            bool isReorderNeeded = false;
+            int currentOrderNum = -1;
+            for (int i = 0; i < umObjList.Count; i++)
             {
-                case "remove":
-                    {
-                        for (int i = 0; i < umObjList.Count; i++)
-                        {
-                            UMObj obj = umObjList[i];
-                            if (obj.m_id.Equals(sID))
-                            {
-                                umObjList.Remove(obj);
-                                break;
-                            }
-                        }
-                        Session["device_families"] = umObjList;
-                        break;
-                    }
-                case "add":
-                    {
-                        UMObj obj = new UMObj(sID, string.Empty, string.Empty, true, int.Parse(index));
-                        int newOrder = int.Parse(index);
-
-                        foreach (UMObj umObj in umObjList)
-                        {
-                            int oldOrder = umObj.m_orderNum;
-
-                            if (oldOrder >= newOrder)
-                            {
-                                umObj.m_orderNum++;
-                            }
-
-                        }
-                        umObjList.Insert(int.Parse(index), obj);
-                        umObjList.Sort();
-                        Session["device_families"] = umObjList;
-                        break;
-                    }
+                UMObj obj = umObjList[i];
+                if (obj.m_id.Equals(sID))
+                {
+                    currentOrderNum = obj.m_orderNum;
+                    umObjList.Remove(obj);
+                    isReorderNeeded = true;
+                    break;
+                }
             }
+            if (isReorderNeeded)
+            {
+                foreach (UMObj umObj in umObjList)
+                {
+                    if (umObj.m_orderNum > currentOrderNum)
+                    {
+                        umObj.m_orderNum--;
+                    }
+                }
+            }
+            else
+            {
+                UMObj obj = new UMObj(sID, string.Empty, string.Empty, true, umObjList.Count);
+                int newOrder = umObjList.Count;
+                umObjList.Insert(umObjList.Count, obj);
+                umObjList.Sort();
+            }
+
+            Session["device_families"] = umObjList; 
 
             // delete from cache this DLM object    
             if (Session["limit_id"] != null && Session["limit_id"].ToString().Length > 0)
