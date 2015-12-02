ce509f1d4aec147b188321bbf769bbbffb5632ad
Lior Faingold <Lior.Faingold@kaltura.com>
2015-12-02 18:42:57 +0200

    added option to edit user permissions and group permissions (configuration -> admin accounts - > edit user permissions \ edit group permissions)

---
+++ Web Sites/TVM/adm_edit_group_permissions.aspx.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DAL;
+using TVinciShared;
+
+
+public partial class adm_edit_group_permissions : System.Web.UI.Page
+{
+
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        if (LoginManager.IsPagePermitted() == false)
+            LoginManager.LogoutFromSite("login.html");
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        if (!IsPostBack)
+        {
+            Int32 nMenuID = 0;
+            m_sMenu = TVinciShared.Menu.GetMainMenu(1, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 4, false);
+            if (Request.QueryString["selectedIndex"] != null && Request.QueryString["groupName"] != null)
+            {
+                int selectedIndex;
+                if (int.TryParse(Request.QueryString["selectedIndex"], out selectedIndex))
+                {
+                    Session["selectedGroupName"] = Request.QueryString["groupName"];
+                    GetAllGroupsList(selectedIndex);
+                }
+            }
+            else
+            {
+                GetAllGroupsList();
+            }
+        }
+    }
+
+    public void GetHeader()
+    {
+        string sRet = PageUtils.GetPreHeader() + ":";
+        Response.Write(sRet + " Edit Group Permissions");
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    private void GetAllGroupsList(int selectedIndex = -1)
+    {
+        if (Session["allGroupssTable"] != null)
+        {
+            groups.DataSource = (DataTable)Session["allGroupssTable"];
+        }
+        else
+        {
+            groups.DataSource = TvmDAL.GetAllGroups();
+            Session["allGroupssTable"] = groups.DataSource;
+        }
+        groups.DataValueField = "moto_text";
+        groups.DataTextField = "moto_text";
+        groups.DataBind();
+        if (selectedIndex > -1)
+        {
+            groups.SelectedIndex = selectedIndex;
+        }
+    }
+
+    public string initDualObj()
+    {
+        Dictionary<string, object> dualList = new Dictionary<string, object>();
+        dualList.Add("FirstListTitle", "Group Permissions");
+        dualList.Add("SecondListTitle", "Available Permissions");
+
+        object[] resultData = null;
+
+        string groupName = null;
+        if (Session["selectedGroupName"] != null)
+        {
+            groupName = Session["selectedGroupName"].ToString();
+        }
+        List<object> GroupPermissionsList = new List<object>();
+        if (!string.IsNullOrEmpty(groupName))
+        {
+            DataTable allowedTable = new DataTable();
+            DataTable notAllowedTable = TvmDAL.GetAllMenus();
+            HashSet<string> groupAllowedPermissionsHashSet = new HashSet<string>();
+            HashSet<string> groupNotAllowedPermissionsHashSet = new HashSet<string>();
+
+            foreach (DataRow dr in allowedTable.Rows)
+            {
+                groupAllowedPermissionsHashSet.Add(dr[0].ToString());
+                var data = new
+                {
+                    ID = dr[0],
+                    Title = string.Format("{0}, href: {1}", dr[1], dr[2]),
+                    Description = dr[2],
+                    InList = true
+                };
+                GroupPermissionsList.Add(data);
+            }
+
+            foreach (DataRow dr in notAllowedTable.Rows)
+            {
+                groupNotAllowedPermissionsHashSet.Add(dr[0].ToString());
+                var data = new
+                {
+                    ID = dr[0],
+                    Title = string.Format("{0}, href: {1}", dr[1], dr[2]),
+                    Description = dr[2],
+                    InList = false
+                };
+                GroupPermissionsList.Add(data);
+            }
+
+
+            Session["groupAllowedPermissions"] = groupAllowedPermissionsHashSet;
+            Session["groupNotAllowedPermissions"] = groupNotAllowedPermissionsHashSet;
+
+        }
+
+        resultData = new object[GroupPermissionsList.Count];
+        resultData = GroupPermissionsList.ToArray();
+
+        dualList.Add("Data", resultData);
+        dualList.Add("pageName", "adm_edit_group_permissions.aspx");
+        dualList.Add("withCalendar", false);
+
+        return dualList.ToJSON();
+    }
+
+    public string changeItemStatus(string sMenuID, string sAction)
+    {
+        string selectedGroupName = null;
+        if (Session["selectedGroupName"] != null)
+        {
+            selectedGroupName = Session["selectedGroupName"].ToString();
+            HashSet<string> groupAllowedPermissionsHashSet = new HashSet<string>();
+            HashSet<string> groupNotAllowedPermissionsHashSet = new HashSet<string>();
+            if (Session["groupAllowedPermissions"] != null && Session["groupNotAllowedPermissions"] != null)
+            {
+                groupAllowedPermissionsHashSet = (HashSet<string>)Session["groupAllowedPermissions"];
+                groupNotAllowedPermissionsHashSet = (HashSet<string>)Session["groupNotAllowedPermissions"];
+            }
+            int menuID = 0;
+            if (int.TryParse(sMenuID, out menuID) && !string.IsNullOrEmpty(selectedGroupName))
+            {
+                if (groupAllowedPermissionsHashSet.Contains(sMenuID) && TvmDAL.RemoveMenuFromGroup(selectedGroupName, menuID))
+                {
+                    groupAllowedPermissionsHashSet.Remove(sMenuID);
+                    groupNotAllowedPermissionsHashSet.Add(sMenuID);
+                }
+
+                else if (groupNotAllowedPermissionsHashSet.Contains(sMenuID) && TvmDAL.AddMenuToGroup(selectedGroupName, menuID))
+                {
+                    groupNotAllowedPermissionsHashSet.Remove(sMenuID);
+                    groupAllowedPermissionsHashSet.Add(sMenuID);
+                }
+            }
+
+            Session["groupAllowedPermissions"] = groupAllowedPermissionsHashSet;
+            Session["groupNotAllowedPermissions"] = groupNotAllowedPermissionsHashSet;
+        }
+
+        return "";
+    }
+
+}
---
+++ Web Sites/TVM/adm_edit_user_permissions.aspx.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DAL;
+using TVinciShared;
+
+
+public partial class adm_edit_user_permissions : System.Web.UI.Page
+{
+    
+    protected string m_sMenu;
+    protected string m_sSubMenu;    
+    public int testIndex;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        if (LoginManager.IsPagePermitted() == false)
+            LoginManager.LogoutFromSite("login.html");
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        if (!IsPostBack)
+        {
+            Int32 nMenuID = 0;
+            m_sMenu = TVinciShared.Menu.GetMainMenu(1, true, ref nMenuID);
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 4, false);
+            if (Request.QueryString["selectedIndex"] != null && Request.QueryString["userID"] != null)
+            {
+                int userID, selectedIndex;
+                if (int.TryParse(Request.QueryString["selectedIndex"], out selectedIndex) && int.TryParse(Request.QueryString["userID"], out userID))
+                {
+                    Session["selectedUserID"] = userID;
+                    GetAllUsersList(selectedIndex);
+                }
+            }
+            else
+            {
+                GetAllUsersList();
+            }
+        }
+    }
+
+    public void GetHeader()
+    {
+        string sRet = PageUtils.GetPreHeader() + ":";
+        Response.Write(sRet + " Edit User Permissions");
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    private void GetAllUsersList(int selectedIndex = -1)
+    {
+        if (Session["allUsersTable"] != null)
+        {
+            users.DataSource = (DataTable)Session["allUsersTable"];
+        }
+        else
+        {
+            users.DataSource = TvmDAL.GetAllUsers();
+            Session["allUsersTable"] = users.DataSource;
+        }        
+        users.DataValueField = "id";
+        users.DataTextField = "username";
+        users.DataBind();
+        if (selectedIndex > -1)
+        {
+            users.SelectedIndex = selectedIndex;
+        }
+    }
+
+    public string initDualObj()
+    {
+        Dictionary<string, object> dualList = new Dictionary<string, object>();
+        dualList.Add("FirstListTitle", "User Permissions");
+        dualList.Add("SecondListTitle", "Available Permissions");        
+
+        object[] resultData = null;
+
+        int selectedUserID = 0;
+        if (Session["selectedUserID"] != null)
+        {
+            selectedUserID = int.Parse(Session["selectedUserID"].ToString());
+        }
+        List<object> userPermissionsList = new List<object>();
+        if (selectedUserID > 0)
+        {
+            DataTable allowedTable = TvmDAL.GetUsersAllowedMenus(selectedUserID);
+            DataTable notAllowedTable = TvmDAL.GetUsersNotAllowedMenus(selectedUserID);
+            HashSet<string> userAllowedPermissionsHashSet = new HashSet<string>();
+            HashSet<string> userNotAllowedPermissionsHashSet = new HashSet<string>();
+
+            foreach (DataRow dr in allowedTable.Rows)
+            {
+                userAllowedPermissionsHashSet.Add(dr[0].ToString());
+                var data = new
+                {
+                    ID = dr[0],
+                    Title = string.Format("{0}, href: {1}", dr[1], dr[2]),
+                    Description = dr[2],
+                    InList = true
+                };
+                userPermissionsList.Add(data);
+            }
+
+            foreach (DataRow dr in notAllowedTable.Rows)
+            {
+                userNotAllowedPermissionsHashSet.Add(dr[0].ToString());
+                var data = new
+                {
+                    ID = dr[0],
+                    Title = string.Format("{0}, href: {1}", dr[1], dr[2]),
+                    Description = dr[2],
+                    InList = false
+                };
+                userPermissionsList.Add(data);
+            }
+
+
+            Session["userAllowedPermissions"] = userAllowedPermissionsHashSet;
+            Session["userNotAllowedPermissions"] = userNotAllowedPermissionsHashSet;
+
+        }
+
+        resultData = new object[userPermissionsList.Count];
+        resultData = userPermissionsList.ToArray();
+
+        dualList.Add("Data", resultData);
+        dualList.Add("pageName", "adm_edit_user_permissions.aspx");
+        dualList.Add("withCalendar", false);
+
+        return dualList.ToJSON();
+    }       
+
+    public string changeItemStatus(string sMenuID, string sAction)
+    {
+        int selectedUserID = 0;
+        if (Session["selectedUserID"] != null)
+        {
+            selectedUserID = int.Parse(Session["selectedUserID"].ToString());
+            HashSet<string> userAllowedPermissionsHashSet = new HashSet<string>();
+            HashSet<string> userNotAllowedPermissionsHashSet = new HashSet<string>();
+            if (Session["userAllowedPermissions"] != null && Session["userNotAllowedPermissions"] != null)
+            {
+                userAllowedPermissionsHashSet = (HashSet<string>)Session["userAllowedPermissions"];
+                userNotAllowedPermissionsHashSet = (HashSet<string>)Session["userNotAllowedPermissions"];
+            }
+            int menuID = 0;
+            if (int.TryParse(sMenuID, out menuID))
+            {
+                if (userAllowedPermissionsHashSet.Contains(sMenuID) && TvmDAL.RemoveMenuFromUser(selectedUserID, menuID))
+                {
+                    userAllowedPermissionsHashSet.Remove(sMenuID);
+                    userNotAllowedPermissionsHashSet.Add(sMenuID);
+                }
+
+                else if (userNotAllowedPermissionsHashSet.Contains(sMenuID) && TvmDAL.AddMenuToUser(selectedUserID, menuID))
+                {
+                    userNotAllowedPermissionsHashSet.Remove(sMenuID);
+                    userAllowedPermissionsHashSet.Add(sMenuID);
+                }
+            }
+
+            Session["userAllowedPermissions"] = userAllowedPermissionsHashSet;
+            Session["userNotAllowedPermissions"] = userNotAllowedPermissionsHashSet;
+        }
+
+        return "";
+    }        
+   
+}
