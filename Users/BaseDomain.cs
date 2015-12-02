31241a440bcc2ae31f4e948df188092d9eda9137
Lior Faingold <Lior.Faingold@kaltura.com>
2015-12-02 13:51:24 +0200

    fixed suspend\resume domain to remove users from cache

--- Users/BaseDomain.cs
+++ Users/BaseDomain.cs
@@ -737,7 +737,16 @@ namespace Users
 
             // remove from cache
             if (SuspendSucceed)
+            {
+                // Remove Domain
                 oDomainCache.RemoveDomain(nDomainID);
+                UsersCache usersCache = UsersCache.Instance();
+                foreach (int userID in domain.m_UsersIDs)
+                {
+                    // Remove Users
+                    usersCache.RemoveUser(userID);
+                }
+            }
 
             // update result
             if (SuspendSucceed)
@@ -780,7 +789,16 @@ namespace Users
 
             // remove from cache
             if (ResumeSucceed)
+            {
+                // Remove Domain
                 oDomainCache.RemoveDomain(nDomainID);
+                UsersCache usersCache = UsersCache.Instance();
+                foreach (int userID in domain.m_UsersIDs)
+                {
+                    // Remove Users
+                    usersCache.RemoveUser(userID);
+                }
+            }
 
             // update result
             if (ResumeSucceed)
