f79de07c6a1e2c4b7b68d7debeda947b4fa295f2
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2015-12-02 14:19:04 +0200

    BEO-608 Adjust HH in Phoenix API

--- WebAPI/Controllers/PaymentGatewayController.cs
+++ WebAPI/Controllers/PaymentGatewayController.cs
@@ -1,17 +1,11 @@
 ﻿using KLogMonitor;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Web;
 using System.Web.Http;
-using System.Web.Http.Description;
-using System.Web.Http.ModelBinding;
 using WebAPI.ClientManagers.Client;
 using WebAPI.Exceptions;
 using WebAPI.Managers.Models;
-using WebAPI.Models.Billing;
-using WebAPI.Models.Catalog;
 using WebAPI.Utils;
 
 namespace WebAPI.Controllers
@@ -26,22 +20,29 @@ namespace WebAPI.Controllers
         /// </summary>
         /// <remarks>
         /// Possible status codes:       
-        ///   User Does Not Exist = 2000, User Not In Domain = 1005, User With No Domain = 2024, User Suspended = 2001, 
-        /// Domain Not Exists = 1006, Household Not Set To Payment Gateway = 6027 
-        /// </remarks>
-        /// <param name="household_id">Household Identifier</param>
+        /// User Suspended = 2001, Household Not Set To Payment Gateway = 6027 
+        /// </remarks>        
         [Route("list"), HttpPost]
         [ApiAuthorize]
-        public List<Models.Billing.KalturaPaymentGatewayBaseProfile> List(long household_id)
+        public List<Models.Billing.KalturaPaymentGatewayBaseProfile> List()
         {
             List<Models.Billing.KalturaPaymentGatewayBaseProfile> response = null;
 
             int groupId = KS.GetFromRequest().GroupId;
 
+            // get domain       
+            var domain = HouseholdUtils.GetHouseholdIDByKS(groupId);
+
+            // check if the user performing the action is domain master
+            if (domain == 0)
+            {
+                throw new ForbiddenException();
+            }
+
             try
             {
                 // call client
-                response = ClientsManager.BillingClient().GetHouseholdPaymentGateways(groupId, KS.GetFromRequest().UserId, household_id);
+                response = ClientsManager.BillingClient().GetHouseholdPaymentGateways(groupId, KS.GetFromRequest().UserId, domain);
             }
             catch (ClientException ex)
             {
@@ -56,22 +57,29 @@ namespace WebAPI.Controllers
         /// </summary>
         /// <remarks>
         /// Possible status codes:       
-        ///   User Does Not Exist = 2000, User Not In Domain = 1005, User With No Domain = 2024, User Suspended = 2001, 
-        /// Domain Not Exists = 1006, Household Not Set To Payment Gateway = 6027
+        /// User Suspended = 2001, Household Not Set To Payment Gateway = 6027
         /// </remarks>
-        /// <param name="household_id">Household Identifier</param>        
         [Route("get"), HttpPost]
         [ApiAuthorize]
-        public Models.Billing.KalturaPaymentGateway Get(long household_id)
+        public Models.Billing.KalturaPaymentGateway Get()
         {
             Models.Billing.KalturaPaymentGateway response = null;
 
             int groupId = KS.GetFromRequest().GroupId;
 
+            // get domain       
+            var domain = HouseholdUtils.GetHouseholdIDByKS(groupId);
+
+            // check if the user performing the action is domain master
+            if (domain == 0)
+            {
+                throw new ForbiddenException();
+            }
+
             try
             {
                 // call client
-                response = ClientsManager.BillingClient().GetSelectedHouseholdPaymentGateway(groupId, household_id);
+                response = ClientsManager.BillingClient().GetSelectedHouseholdPaymentGateway(groupId, domain);
             }
             catch (ClientException ex)
             {
@@ -86,15 +94,13 @@ namespace WebAPI.Controllers
         /// </summary>
         /// <remarks>
         /// Possible status codes:       
-        /// user does not exist = 2000, user not in domain = 1005, user with no domain = 2024, user suspended = 2001, domain not exists = 1006, 
-        /// error saving payment gateway household = 6017, household already set to payment gateway = 6024, payment gateway selection is disabled = 6028,
+        /// user suspended = 2001, error saving payment gateway household = 6017, household already set to payment gateway = 6024, payment gateway selection is disabled = 6028,
         /// payment gateway not valid = 6043
         /// </remarks>
         /// <param name="payment_gateway_id">Payment Gateway Identifier</param> 
-        /// <param name="household_id">Household Identifier</param>
         [Route("set"), HttpPost]
         [ApiAuthorize]
-        public bool Set(int payment_gateway_id, long household_id)
+        public bool Set(int payment_gateway_id)
         {
             bool response = false;
 
@@ -114,7 +120,7 @@ namespace WebAPI.Controllers
                 }
 
                 // call client
-                response = ClientsManager.BillingClient().SetHouseholdPaymentGateway(groupId, payment_gateway_id, KS.GetFromRequest().UserId, household_id);
+                response = ClientsManager.BillingClient().SetHouseholdPaymentGateway(groupId, payment_gateway_id, userID, domain.Id);
             }
             catch (ClientException ex)
             {
@@ -129,14 +135,12 @@ namespace WebAPI.Controllers
         /// </summary>
         /// <remarks>
         /// Possible status codes:       
-        ///   User Does Not Exist = 2000, User Not In Domain = 1005, User Suspended = 2001, Domain Not Exists = 1006
-        /// Payment Gateway Identifier is Missing = 6005, Payment gateway not exist = 6008, Household not set to payment gateway = 6027
-        /// PaymentGatewaySelectionIsDisabled = 6028, ServiceForbidden = 500004
+        /// user suspended = 2001, payment gateway identifier is missing = 6005, payment gateway not exist = 6008, household not set to payment gateway = 6027
+        /// payment gateway selection is disabled = 6028, service forbidden = 500004
         /// </remarks>
         /// <param name="payment_gateway_id">Payment Gateway Identifier</param>
-        /// <param name="household_id">Household Identifier</param>
         [Route("delete"), HttpPost]     
-        public bool Delete(int payment_gateway_id, long household_id)
+        public bool Delete(int payment_gateway_id)
         {
             bool response = false;
 
@@ -156,7 +160,7 @@ namespace WebAPI.Controllers
                 }
 
                 // call client
-                response = ClientsManager.BillingClient().DeleteHouseholdPaymentGateway(groupId, payment_gateway_id, KS.GetFromRequest().UserId, household_id);
+                response = ClientsManager.BillingClient().DeleteHouseholdPaymentGateway(groupId, payment_gateway_id, userID, domain.Id);
             }
             catch (ClientException ex)
             {
