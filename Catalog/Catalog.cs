27aa415628a7dea70e1764f3f5e362d5a52c7eb0
Michael <Yaron!>
2015-12-06 18:05:24 +0200

    BEO-617

--- Catalog/Catalog.cs
+++ Catalog/Catalog.cs
@@ -4945,7 +4945,7 @@ namespace Catalog
                 int mediaTypeID = Catalog.GetMediaTypeID(request.m_nMediaID);
                 if (mediaTypeID == 0)
                 {
-                    status.Message = "Media type doesn’t exist";
+                    status.Message = "Media doesn’t exist";
                     status.Code = (int)eResponseStatus.BadSearchRequest;
                     return status;
                 }
