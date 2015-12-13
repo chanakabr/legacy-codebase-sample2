c86914471a61ce238b96b53713b041c87413ec17
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2015-12-13 14:54:16 +0200

    BEO-556 Delete a user Add error messages

--- WebAPI/Controllers/OTTUserController.cs
+++ WebAPI/Controllers/OTTUserController.cs
@@ -498,11 +498,12 @@ namespace WebAPI.Controllers
         }
 
         /// <summary>
-        /// Permanently delete a user. User to delete cannot be an exclusive household master, and cannot be default user.
+        /// Permanently delete a user.
         /// </summary>        
         /// <param name="user">The user model to delete</param>        
         /// <remarks>        
         /// Possible status codes: 
+        /// Household does not exists = 1006, Household suspended = 1009, User not exists in domain = 1020, User does not exist = 2000, Default user cannot be deleted = 2030, Master user cannot be deleted = 2031
         /// </remarks>        
         [Route("delete"), HttpPost]
         public bool Delete()
