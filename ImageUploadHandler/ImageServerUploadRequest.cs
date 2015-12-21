8bbeb56957a10c1961dd0a121cb6bcc974b7384d
Ira Yanai <ira.yanai@kaltura.com>
2015-12-21 07:26:26 +0000

    new image server

--- ImageUploadHandler/ImageServerUploadRequest.cs
+++ ImageUploadHandler/ImageServerUploadRequest.cs
@@ -10,16 +10,12 @@ namespace ImageUploadHandler
     [Serializable]
     public class ImageServerUploadRequest
     {
-        [JsonProperty("group_id")]        
         public int GroupId { get; set; }
 
-        [JsonProperty("id")]
         public string Id { get; set; }
 
-        [JsonProperty("version")]        
         public int Version { get; set; }
 
-        [JsonProperty("source_path")]        
         public string SourcePath { get; set; }
     }
 }
