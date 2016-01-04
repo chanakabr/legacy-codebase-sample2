4f3c9013f11388278a1f6323e594f739ddaa3355
Lior Faingold <Lior.Faingold@kaltura.com>
2016-01-04 19:11:36 +0200

    Continue BEO-742, added unified bookmarks with catch-up (Get only)

--- Catalog/Response/AssetsBookmarksResponse.cs
+++ Catalog/Response/AssetsBookmarksResponse.cs
@@ -73,28 +73,4 @@ namespace Catalog.Response
         }
     }
 
-    [DataContract]
-    public class LastPosition
-    {
-        [DataMember]
-        public int m_nUserID;
-
-        [DataMember]
-        public eUserType m_eUserType;
-
-        [DataMember]
-        public int m_nLocation;
-
-        public LastPosition()
-        {
-        }
-
-        public LastPosition(int nUserID, eUserType eUserType, int nLocation)
-        {
-            this.m_nUserID = nUserID;
-            this.m_eUserType = eUserType;
-            this.m_nLocation = nLocation;
-        }
-    }
-
 }
