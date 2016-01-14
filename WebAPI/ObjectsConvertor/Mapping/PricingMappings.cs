146162fb6d4500ab0f8f7e83a1a06e34998714f2
Michael Yaron <Michael.Yaron@kaltura.com>
2016-01-14 17:31:13 +0200

    BEO-805 - corrected subs id mapping

--- WebAPI/ObjectsConvertor/Mapping/PricingMappings.cs
+++ WebAPI/ObjectsConvertor/Mapping/PricingMappings.cs
@@ -162,7 +162,7 @@ namespace WebAPI.Mapping.ObjectsConvertor
                .ForMember(dest => dest.PurchaseUserId, opt => opt.MapFrom(src => src.m_sPurchasedBySiteGuid))
                .ForMember(dest => dest.RelatedMediaFileIds, opt => opt.MapFrom(src => src.m_lRelatedMediaFileIDs))
                .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => src.m_dtStartDate.HasValue ? SerializationUtils.ConvertToUnixTimestamp(src.m_dtStartDate.Value) : 0))
-               .ForMember(dest => dest.SubscriptionId, opt => opt.MapFrom(src => src.m_relevantSub))
+               .ForMember(dest => dest.SubscriptionId, opt => opt.MapFrom(src => src.m_relevantSub.m_sObjectCode))
                .ForMember(dest => dest.ProductCode, opt => opt.MapFrom(src => src.m_sProductCode));
 
             // ItemPriceContainer to PPVItemPriceDetails
