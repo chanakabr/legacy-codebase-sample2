249179cc6fbf2b32f9b0e581b1c6d1a086ddee84
Irena Leibman <irena.leibman@kaltura.com>
2016-01-25 19:44:46 +0200

    added api_v3/service/version/action/get

---
+++ WebAPI/Controllers/VersionController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.ClientManagers.Client;
+using WebAPI.Exceptions;
+using WebAPI.Managers.Models;
+using WebAPI.Models.General;
+using WebAPI.Models.Pricing;
+using WebAPI.Utils;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("_service/version/action")]
+    public class VersionController : ApiController
+    {
+        /// <summary>
+        /// Returns information about the current version
+        /// </summary>
+        /// </remarks>
+        [Route("get"), HttpPost]
+        [ApiAuthorize]
+        public KalturaProjectVersion Get()
+        {
+            KalturaProjectVersion version = new KalturaProjectVersion();
+
+            version.Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            return version;
+        }
+    }
+}
