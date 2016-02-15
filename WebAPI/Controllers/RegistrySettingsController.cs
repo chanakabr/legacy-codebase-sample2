179bc5544d5dc88ff22566c88b3c652086ed5d62
Liat Regev <liatr@tvinci.com>
2016-02-15 11:54:39 +0200

    BEO-971 : Registry System

---
+++ WebAPI/Controllers/RegistrySettingsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebAPI.ClientManagers.Client;
+using WebAPI.Exceptions;
+using WebAPI.Managers.Models;
+using WebAPI.Models.API;
+using WebAPI.Utils;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("_service/RegistrySettings/action")]
+    public class RegistrySettingsController : ApiController
+    {
+        /// <summary>
+        /// Retrieve the registry settings.        
+        /// </summary>        
+        /// <remarks>        
+        /// </remarks>
+        /// <returns>The registry settings that apply for the partner</returns>
+        /// 
+        [Route("list"), HttpPost]
+        [ApiAuthorize]
+        public List<KalturaRegistrySettings> List()
+        {
+            List<KalturaRegistrySettings>  response = null;
+
+            try
+            {
+                int groupId = KS.GetFromRequest().GroupId;
+                // call client                
+                response = ClientsManager.ApiClient().GetAllRegistry(groupId);              
+            }
+            catch (ClientException ex)
+            {
+                ErrorUtils.HandleClientException(ex);
+            }
+            return response;
+        }
+    }
+}
---
+++ WebAPI/Models/API/KalturaRegistrySettings.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml.Serialization;
+using WebAPI.Models.General;
+
+namespace WebAPI.Models.API
+{
+    public class KalturaRegistrySettings : KalturaOTTObject    
+    {
+        /// <summary>
+        /// Permission item identifier
+        /// </summary>
+        [DataMember(Name = "key")]
+        [JsonProperty("key")]
+        [XmlElement(ElementName = "key")]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Permission item name
+        /// </summary>
+        [DataMember(Name = "value")]
+        [JsonProperty("value")]
+        [XmlElement(ElementName = "value")]
+        public string Value { get; set; }
+    }
+}
