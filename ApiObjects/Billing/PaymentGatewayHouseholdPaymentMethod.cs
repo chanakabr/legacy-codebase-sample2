b984ab4d97b33952bd8b5cf690c75c94733a06ae
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2016-02-17 14:09:45 +0200

    BEO-876 Multiple payment methods on each PG

--- ApiObjects/Billing/PaymentGatewayHouseholdPaymentMethod.cs
+++ ApiObjects/Billing/PaymentGatewayHouseholdPaymentMethod.cs
@@ -6,7 +6,7 @@ namespace ApiObjects.Billing
         public int Id { get; set; }
         public int PaymentGatewayId { get; set; }
         public long HouseholdId { get; set; }
-        public int PaymentMethoId { get; set; }
+        public int PaymentMethodId { get; set; }
         public string paymentMethodExternalId { get; set; }
         public string PaymentDetails { get; set; }
 
