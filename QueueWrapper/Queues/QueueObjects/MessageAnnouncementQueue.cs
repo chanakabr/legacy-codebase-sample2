ae7582a54b96f4403664d04003ae1f783231ec6c
Michael <Yaron!>
2016-02-18 11:22:36 +0200

    latest changes announcement

--- QueueWrapper/Queues/QueueObjects/MessageAnnouncementQueue.cs
+++ QueueWrapper/Queues/QueueObjects/MessageAnnouncementQueue.cs
@@ -12,7 +12,6 @@ namespace QueueWrapper.Queues.QueueObjects
             : base()
         {
             this.Implementation = new RabbitQueue(ConfigType.DefaultConfig, true);
-            storeForRecovery = true;
         }
 
         public override bool Enqueue(ApiObjects.QueueObject record, string sRouteKey)
