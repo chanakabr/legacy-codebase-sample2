912a21f57bc957e774f78ef640b0fb3959ddab8e
Irena Leibman <irena.leibman@kaltura.com>
2016-03-02 11:20:43 +0200

    Added objects for price plan and multi price plan

---
+++ ApiObjects/IngestBusinessModules/IngestPricePlan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiObjects
+{
+    public class IngestPricePlan
+    {
+        public string Code { get; set; }
+
+        public string FullLifeCycle { get; set; }
+
+        public string ViewLifeCycle { get; set; }
+
+        public string MaxViews { get; set; }
+
+        public string PriceCode { get; set; }
+
+        public string CouponGroup { get; set; }
+
+        public bool IsRenewable { get; set; }
+
+        public string RecurringPeriods { get; set; }
+
+        public eIngestAction Action { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
