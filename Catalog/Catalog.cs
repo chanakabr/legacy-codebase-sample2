e77c6039485c745531c360e199d399d6a292a135
Lior Faingold <Lior.Faingold@kaltura.com>
2016-03-03 14:02:19 +0200

    fix BEO-886, MediaConcurrency rule doesn't work + mediamark returns empty for MediaConcurrency

--- Catalog/Catalog.cs
+++ Catalog/Catalog.cs
@@ -3658,6 +3658,7 @@ namespace Catalog
                     switch (domainsResp.m_eStatus)
                     {
                         case WS_Domains.DomainResponseStatus.ConcurrencyLimitation:
+                        case WS_Domains.DomainResponseStatus.MediaConcurrencyLimitation:
                             {
                                 res = true;
                                 break;
