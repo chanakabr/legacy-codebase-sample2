49e218d94ce808dd48e386617f6375c0c4a1b1df
Amishai Guilburd <amishai.guilburd@kaltura.com>
2016-03-10 09:22:43 +0200

    updated monitor message

--- KLogMonitor/KLogMonitor/KLogger.cs
+++ KLogMonitor/KLogMonitor/KLogger.cs
@@ -163,21 +163,36 @@ namespace KLogMonitor
                 logs.Add(le);
         }
 
+        //private string formatMessage(string msg, DateTime creationDate)
+        //{
+        //    return string.Format("{0} - class: {1}, line: {2}, method: {3}, server:{4} ip:{5} reqid:{6} partner:{7} action:{8} client:{9} uid:{10} msg:{11}",
+        //        creationDate,                                  // 0
+        //        ClassName != null ? ClassName : string.Empty,  // 1
+        //        Line,                                          // 2
+        //        MethodName != null ? MethodName : string.Empty,// 3
+        //        Server != null ? Server : string.Empty,        // 4
+        //        IPAddress != null ? IPAddress : string.Empty,  // 5
+        //        UniqueID != null ? UniqueID : string.Empty,    // 6
+        //        PartnerID != null ? PartnerID : string.Empty,  // 7
+        //        Action != null ? Action : string.Empty,        // 8
+        //        ClientTag != null ? ClientTag : string.Empty,  // 9
+        //        UserID != null ? UserID : "0",                 // 10
+        //        msg != null ? msg : string.Empty);             // 11
+        //}
+
         private string formatMessage(string msg, DateTime creationDate)
         {
-            return string.Format("{0} - class: {1}, line: {2}, method: {3}, server:{4} ip:{5} reqid:{6} partner:{7} action:{8} client:{9} uid:{10} msg:{11}",
-                creationDate,                                  // 0
-                ClassName != null ? ClassName : string.Empty,  // 1
-                Line,                                          // 2
-                MethodName != null ? MethodName : string.Empty,// 3
-                Server != null ? Server : string.Empty,        // 4
-                IPAddress != null ? IPAddress : string.Empty,  // 5
-                UniqueID != null ? UniqueID : string.Empty,    // 6
-                PartnerID != null ? PartnerID : string.Empty,  // 7
-                Action != null ? Action : string.Empty,        // 8
-                ClientTag != null ? ClientTag : string.Empty,  // 9
-                UserID != null ? UserID : "0",                 // 10
-                msg != null ? msg : string.Empty);             // 11
+            return string.Format("class: {0} line: {1} method: {2} server:{3} ip:{4} reqid:{5} partner:{6} action:{7} uid:{8} msg:{9}",
+                ClassName != null ? ClassName : string.Empty,  // 0
+                Line,                                          // 1
+                MethodName != null ? MethodName : string.Empty,// 2
+                Server != null ? Server : string.Empty,        // 3
+                IPAddress != null ? IPAddress : string.Empty,  // 4
+                UniqueID != null ? UniqueID : string.Empty,    // 5
+                PartnerID != null ? PartnerID : string.Empty,  // 6
+                Action != null ? Action : string.Empty,        // 7
+                UserID != null ? UserID : "0",                 // 8
+                msg != null ? msg : string.Empty);             // 9
         }
 
         private void sendLog(LogEvent logEvent)
