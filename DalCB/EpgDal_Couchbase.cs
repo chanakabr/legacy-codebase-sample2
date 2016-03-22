ad906b003159871f7bfb967099364451b49168c9
Sunny Simantov <sunny.simantov@kaltura.com>
2016-03-22 10:41:19 +0200

    Settings EPGs - serialize object before inserting

--- DalCB/EpgDal_Couchbase.cs
+++ DalCB/EpgDal_Couchbase.cs
@@ -47,8 +47,9 @@ namespace DalCB
             {
                 try
                 {
-                    bRes = (dtExpiresAt.HasValue) ? cbManager.Set(sDocID, epg, (uint)(dtExpiresAt.Value - DateTime.UtcNow).TotalSeconds) :
-                                                   cbManager.Set(sDocID, epg);
+                    bRes = (dtExpiresAt.HasValue) ?
+                        cbManager.Set(sDocID, JsonConvert.SerializeObject(epg, Formatting.None), (uint)(dtExpiresAt.Value - DateTime.UtcNow).TotalSeconds) :
+                        cbManager.Set(sDocID, JsonConvert.SerializeObject(epg, Formatting.None));
                 }
                 catch (Exception ex)
                 {
@@ -78,8 +79,9 @@ namespace DalCB
                 try
                 {
                     // TODO  : add here the json serialize 
-                    bRes = (dtExpiresAt.HasValue) ? cbManager.SetWithVersion(sDocID, epg, cas, (uint)(dtExpiresAt.Value - DateTime.UtcNow).TotalSeconds) :
-                                                    cbManager.SetWithVersion(sDocID, epg, cas);
+                    bRes = (dtExpiresAt.HasValue) ?
+                        cbManager.SetWithVersion(sDocID, JsonConvert.SerializeObject(epg, Formatting.None), cas, (uint)(dtExpiresAt.Value - DateTime.UtcNow).TotalSeconds) :
+                        cbManager.SetWithVersion(sDocID, JsonConvert.SerializeObject(epg, Formatting.None), cas);
                 }
                 catch (Exception ex)
                 {
@@ -109,8 +111,9 @@ namespace DalCB
             {
                 try
                 {
-                    bRes = (dtExpiresAt.HasValue) ? cbManager.Set(sDocID, epg, (uint)(dtExpiresAt.Value - DateTime.UtcNow).TotalSeconds) :
-                                                    cbManager.Set(sDocID, epg);
+                    bRes = (dtExpiresAt.HasValue) ?
+                        cbManager.Set(sDocID, JsonConvert.SerializeObject(epg, Formatting.None), (uint)(dtExpiresAt.Value - DateTime.UtcNow).TotalSeconds) :
+                        cbManager.Set(sDocID, JsonConvert.SerializeObject(epg, Formatting.None));
                 }
                 catch (Exception ex)
                 {
@@ -138,8 +141,9 @@ namespace DalCB
             {
                 try
                 {
-                    bRes = (dtExpiresAt.HasValue) ? cbManager.SetWithVersion(sDocID, epg, cas, (uint)(dtExpiresAt.Value - DateTime.UtcNow).TotalSeconds) :
-                                                    cbManager.SetWithVersion(sDocID, epg, cas);
+                    bRes = (dtExpiresAt.HasValue) ?
+                        cbManager.SetWithVersion(sDocID, JsonConvert.SerializeObject(epg, Formatting.None), cas, (uint)(dtExpiresAt.Value - DateTime.UtcNow).TotalSeconds) :
+                        cbManager.SetWithVersion(sDocID, JsonConvert.SerializeObject(epg, Formatting.None), cas);
                 }
                 catch (Exception ex)
                 {
