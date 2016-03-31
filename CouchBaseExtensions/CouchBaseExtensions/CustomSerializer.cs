05106e19c95115bfda9a76a19682c35321e65d0e
Lior Faingold <Lior.Faingold@kaltura.com>
2016-03-31 17:39:54 +0300

    added CouchBaseExtension

---
+++ CouchBaseExtensions/CouchBaseExtensions/CustomSerializer.cs
@@ -0,0 +1,42 @@
+﻿using Couchbase.Core.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CouchBaseExtensions
+{
+    public class CustomSerializer : ITypeSerializer
+    {
+
+        Couchbase.Core.Serialization.DefaultSerializer DefaultSerializator;
+
+        public CustomSerializer()
+        {
+            DefaultSerializator = new DefaultSerializer(new JsonSerializerSettings() { ContractResolver = new DefaultContractResolver() },
+                                                        new JsonSerializerSettings() { ContractResolver = new DefaultContractResolver() });            
+        }
+
+        public CustomSerializer(JsonSerializerSettings deserializationSettings, JsonSerializerSettings serializerSettings)
+        {
+            DefaultSerializator = new DefaultSerializer(deserializationSettings, serializerSettings);
+        }
+
+        public T Deserialize<T>(System.IO.Stream stream)
+        {
+            return DefaultSerializator.Deserialize<T>(stream);
+        }
+
+        public T Deserialize<T>(byte[] buffer, int offset, int length)
+        {
+            return DefaultSerializator.Deserialize<T>(buffer, offset, length);
+        }
+
+        public byte[] Serialize(object obj)
+        {
+            return DefaultSerializator.Serialize(obj);
+        }
+    }
+}
---
+++ CouchBaseExtensions/CouchBaseExtensions/CustomTranscoder.cs
@@ -0,0 +1,83 @@
+﻿using Couchbase.Core.Serialization;
+using Couchbase.Core.Transcoders;
+using Couchbase.IO.Converters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CouchBaseExtensions
+{
+    public class CustomTranscoder : ITypeTranscoder
+    {
+        Couchbase.Core.Transcoders.DefaultTranscoder DefualtTranscoder;
+
+        public CustomTranscoder()
+        {
+            DefualtTranscoder = new DefaultTranscoder();
+        }
+
+        public CustomTranscoder(IByteConverter converter)
+        {
+            DefualtTranscoder = new DefaultTranscoder(converter);
+        }
+
+        public CustomTranscoder(IByteConverter converter, ITypeSerializer serializer)
+        {
+            DefualtTranscoder = new DefaultTranscoder(converter, serializer);
+        }
+
+
+
+        public IByteConverter Converter
+        {
+            get
+            {
+                return DefualtTranscoder.Converter;
+            }
+            set
+            {
+                DefualtTranscoder.Converter = value;
+            }
+        }
+
+        public T Decode<T>(byte[] buffer, int offset, int length, Couchbase.IO.Operations.Flags flags, Couchbase.IO.Operations.OperationCode opcode)
+        {
+            return DefualtTranscoder.Decode<T>(buffer, offset, length, flags, opcode);
+        }
+
+        public T Decode<T>(ArraySegment<byte> buffer, int offset, int length, Couchbase.IO.Operations.Flags flags, Couchbase.IO.Operations.OperationCode opcode)
+        {
+            return DefualtTranscoder.Decode<T>(buffer, offset, length, flags, opcode);
+        }
+
+        public byte[] Encode<T>(T value, Couchbase.IO.Operations.Flags flags, Couchbase.IO.Operations.OperationCode opcode)
+        {
+            return DefualtTranscoder.Encode<T>(value, flags, opcode);
+        }
+
+        public Couchbase.IO.Operations.Flags GetFormat<T>(T value)
+        {
+            Couchbase.IO.Operations.Flags flags = DefualtTranscoder.GetFormat<T>(value);            
+            if (flags.TypeCode == TypeCode.Object) 
+            { 
+                flags.TypeCode = TypeCode.String; 
+            }
+            return flags;
+        }
+
+        public Couchbase.Core.Serialization.ITypeSerializer Serializer
+        {
+            get
+            {
+                return DefualtTranscoder.Serializer;
+            }
+            set
+            {
+                DefualtTranscoder.Serializer = value;
+            }
+        }
+    }
+}
