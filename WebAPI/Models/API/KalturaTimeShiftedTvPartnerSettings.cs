c385c84f1726b1874616b6255900199c4888beb9
Lior Faingold <Lior.Faingold@kaltura.com>
2016-04-04 14:42:33 +0300

    updated TSTV partner settings

--- WebAPI/Models/API/KalturaTimeShiftedTvPartnerSettings.cs
+++ WebAPI/Models/API/KalturaTimeShiftedTvPartnerSettings.cs
@@ -27,5 +27,37 @@ namespace WebAPI.Models.API
         [XmlElement(ElementName = "cdvr_enabled", IsNullable = true)]
         public bool? CdvrEnabled { get; set; }
 
+        /// <summary>
+        /// Is start-over enabled
+        /// </summary>
+        [DataMember(Name = "start_over_enabled")]
+        [JsonProperty("start_over_enabled")]
+        [XmlElement(ElementName = "start_over_enabled", IsNullable = true)]
+        public bool? StartOverEnabled { get; set; }
+
+        /// <summary>
+        /// Is trick-play
+        /// </summary>
+        [DataMember(Name = "trick_play_enabled")]
+        [JsonProperty("trick_play_enabled")]
+        [XmlElement(ElementName = "trick_play_enabled", IsNullable = true)]
+        public bool? TrickPlayEnabled { get; set; }
+
+        /// <summary>
+        /// Catch-up buffer length
+        /// </summary>
+        [DataMember(Name = "start_over_enabled")]
+        [JsonProperty("start_over_enabled")]
+        [XmlElement(ElementName = "start_over_enabled", IsNullable = true)]
+        public int CatchUpBufferLength { get; set; }
+
+        /// <summary>
+        /// Trick play buffer length
+        /// </summary>
+        [DataMember(Name = "trick_play_enabled")]
+        [JsonProperty("trick_play_enabled")]
+        [XmlElement(ElementName = "trick_play_enabled", IsNullable = true)]
+        public int TrickPlayBufferLength { get; set; }
+
     }
 }
