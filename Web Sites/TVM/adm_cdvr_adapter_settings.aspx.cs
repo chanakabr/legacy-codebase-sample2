e0ad3ec6602953bf8d0c07d514bd6891c98052df
Irena Leibman <irena.leibman@kaltura.com>
2016-04-05 16:06:33 +0300

    CDVR adapter pages

---
+++ Web Sites/TVM/adm_cdvr_adapter_settings.aspx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TVinciShared;
+
+public partial class adm_cdvr_adapter_settings : System.Web.UI.Page
+{
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        if (LoginManager.IsPagePermitted("adm_cdvr_adapter.aspx") == false)
+            LoginManager.LogoutFromSite("login.html");
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+
+        if (string.IsNullOrEmpty(Request.QueryString["cdvr_adapter_id"]))
+        {   
+            Session["cdvr_adapter_id"] = null;            
+        }
+        if (!IsPostBack)
+        {
+            Int32 nMenuID = 0;
+            m_sMenu = TVinciShared.Menu.GetMainMenu(7, true, ref nMenuID, "adm_cdvr_adapter.aspx");
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 1, false);
+
+            if (Request.QueryString["cdvr_adapter_id"] != null && Request.QueryString["cdvr_adapter_id"].ToString() != "")
+            {
+                Session["cdvr_adapter_id"] = int.Parse(Request.QueryString["cdvr_adapter_id"].ToString());                                
+            }           
+        }
+    }
+
+    public void GetHeader()
+    {
+        Response.Write(PageUtils.GetPreHeader() + ":" + " CDVR Adapter values");
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetTableCSV()
+    {
+        string sOldOrderBy = "";
+        if (Session["order_by"] != null)
+            sOldOrderBy = Session["order_by"].ToString();
+        DBTableWebEditor theTable = new DBTableWebEditor(true, true, false, "", "adm_table_header", "adm_table_cell", "adm_table_alt_cell", "adm_table_link", "adm_table_pager", "adm_table", sOldOrderBy, 50);
+        FillTheTableEditor(ref theTable, sOldOrderBy);
+
+        string sCSVFile = theTable.OpenCSV();
+        theTable.Finish();
+        theTable = null;
+        return sCSVFile;
+    }
+
+    protected void FillTheTableEditor(ref DBTableWebEditor theTable, string sOrderBy)
+    {
+        Int32 groupID = LoginManager.GetLoginGroupID();        
+        theTable += "select ID, group_id, status, key_Name as 'key', [value] ";
+        theTable += " from cdvr_adapters_settings ";
+        theTable += " where ( status = 1 or status = 4 ) AND " ;
+        theTable += ODBCWrapper.Parameter.NEW_PARAM("adapter_id", "=", int.Parse(Session["cdvr_adapter_id"].ToString()));
+        theTable += "AND";
+        theTable += ODBCWrapper.Parameter.NEW_PARAM("group_id", "=", groupID);
+        if (sOrderBy != "")
+        {
+            theTable += " order by ";
+            theTable += sOrderBy;
+        }
+        else
+        {
+            theTable += " order by id ";
+        }
+
+        theTable.AddHiddenField("ID");
+        theTable.AddHiddenField("group_id");
+        theTable.AddHiddenField("status");
+        theTable.SetConnectionKey("CA_CONNECTION_STRING");
+
+
+        if (LoginManager.IsActionPermittedOnPage("adm_cdvr_adapter.aspx", LoginManager.PAGE_PERMISION_TYPE.EDIT))
+        {
+            DataTableLinkColumn linkColumn1 = new DataTableLinkColumn("adm_cdvr_adapter_settings_new.aspx", "Edit", "");
+            linkColumn1.AddQueryStringValue("cdvr_adapter_setting_id", "field=id");
+            theTable.AddLinkColumn(linkColumn1);
+        }
+        if (LoginManager.IsActionPermittedOnPage("adm_cdvr_adapter.aspx", LoginManager.PAGE_PERMISION_TYPE.REMOVE))
+        {
+            DataTableLinkColumn linkColumn = new DataTableLinkColumn("adm_generic_remove.aspx", "Delete", "STATUS=1;STATUS=3");
+            linkColumn.AddQueryStringValue("id", "field=id");
+            linkColumn.AddQueryStringValue("table", "cdvr_adapters_settings");
+            linkColumn.AddQueryStringValue("db", "CA_CONNECTION_STRING");
+            linkColumn.AddQueryStringValue("confirm", "true");
+            linkColumn.AddQueryStringValue("main_menu", "7");
+            linkColumn.AddQueryStringValue("sub_menu", "1");
+            linkColumn.AddQueryStringValue("rep_field", "NAME");
+            linkColumn.AddQueryStringValue("rep_name", "ùí");
+            theTable.AddLinkColumn(linkColumn);
+        }
+
+        if (LoginManager.IsActionPermittedOnPage("adm_cdvr_adapter.aspx", LoginManager.PAGE_PERMISION_TYPE.PUBLISH))
+        {
+            DataTableLinkColumn linkColumn = new DataTableLinkColumn("adm_generic_confirm.aspx", "Confirm", "STATUS=3;STATUS=4");
+            linkColumn.AddQueryStringValue("id", "field=id");
+            linkColumn.AddQueryStringValue("table", "cdvr_adapters_settings");
+            linkColumn.AddQueryStringValue("db", "CA_CONNECTION_STRING");
+            linkColumn.AddQueryStringValue("confirm", "true");
+            linkColumn.AddQueryStringValue("main_menu", "7");
+            linkColumn.AddQueryStringValue("sub_menu", "1");
+            linkColumn.AddQueryStringValue("rep_field", "NAME");
+            linkColumn.AddQueryStringValue("rep_name", "ùí");
+            theTable.AddLinkColumn(linkColumn);
+        }
+
+        if (LoginManager.IsActionPermittedOnPage("adm_cdvr_adapter.aspx", LoginManager.PAGE_PERMISION_TYPE.PUBLISH))
+        {
+            DataTableLinkColumn linkColumn = new DataTableLinkColumn("adm_generic_confirm.aspx", "Cancel", "STATUS=3;STATUS=4");
+            linkColumn.AddQueryStringValue("id", "field=id");
+            linkColumn.AddQueryStringValue("table", "cdvr_adapters_settings");
+            linkColumn.AddQueryStringValue("db", "CA_CONNECTION_STRING");
+            linkColumn.AddQueryStringValue("confirm", "false");
+            linkColumn.AddQueryStringValue("main_menu", "7");
+            linkColumn.AddQueryStringValue("sub_menu", "1");
+            linkColumn.AddQueryStringValue("rep_field", "NAME");
+            linkColumn.AddQueryStringValue("rep_name", "ùí");
+            theTable.AddLinkColumn(linkColumn);
+        }
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        string sOldOrderBy = "";
+        if (Session["order_by"] != null)
+            sOldOrderBy = Session["order_by"].ToString();
+
+        DBTableWebEditor theTable = new DBTableWebEditor(true, true, true, "", "adm_table_header", "adm_table_cell", "adm_table_alt_cell", "adm_table_link", "adm_table_pager", "adm_table", sOldOrderBy, 50);
+        FillTheTableEditor(ref theTable, sOrderBy);
+
+        string sTable = theTable.GetPageHTML(int.Parse(sPageNum), sOrderBy, false);
+        Session["ContentPage"] = "adm_cdvr_adapter.aspx";
+        Session["LastContentPage"] = "adm_cdvr_adapter.aspx?search_save=1";
+        Session["order_by"] = sOldOrderBy;
+        theTable.Finish();
+        theTable = null;
+        return sTable;
+    }
+}
---
+++ Web Sites/TVM/adm_cdvr_adapter_settings_new.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using KLogMonitor;
+using TVinciShared;
+
+public partial class adm_cdvr_adapter_settings_new : System.Web.UI.Page
+{
+    private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+            LoginManager.LogoutFromSite("login.html");
+
+        else if (LoginManager.IsPagePermitted("adm_cdvr_adapter.aspx") == false)
+            LoginManager.LogoutFromSite("login.html");
+
+        else if (LoginManager.IsActionPermittedOnPage("adm_cdvr_adapter.aspx", LoginManager.PAGE_PERMISION_TYPE.EDIT) == false)
+            LoginManager.LogoutFromSite("login.html");
+
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        Int32 nMenuID = 0;
+        if (!IsPostBack)
+        {
+            m_sMenu = TVinciShared.Menu.GetMainMenu(7, true, ref nMenuID, "adm_cdvr_adapter.aspx");
+            m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 1, true);
+            if (Request.QueryString["submited"] != null && Request.QueryString["submited"].ToString() == "1")
+            {
+                DBManipulator.DoTheWork("CA_CONNECTION_STRING");
+                int cdvrAdapterId = 0;
+
+                if (Session["cdvr_adapter_id"] != null && !string.IsNullOrEmpty(Session["cdvr_adapter_id"].ToString()) && int.TryParse(Session["cdvr_adapter_id"].ToString(), out cdvrAdapterId))
+                {
+                    //// set adapter configuration
+                    //apiWS.API api = new apiWS.API();
+
+                    //string sIP = "1.1.1.1";
+                    //string sWSUserName = "";
+                    //string sWSPass = "";
+                    //TVinciShared.WS_Utils.GetWSUNPass(LoginManager.GetLoginGroupID(), "SetCDVRAdapterConfiguration", "api", sIP, ref sWSUserName, ref sWSPass);
+                    //string sWSURL = TVinciShared.WS_Utils.GetTcmConfigValue("api_ws");
+                    //if (!string.IsNullOrEmpty(sWSURL))
+                    //    api.Url = sWSURL;
+                    //try
+                    //{
+                    //    apiWS.Status status = api.SetOSSAdapterConfiguration(sWSUserName, sWSPass, cdvrAdapterId);
+                    //    log.Debug("SetOSSAdapterConfiguration - " + string.Format("oass adapter id:{0}, status:{1}", cdvrAdapterId, status.Code));
+                    //}
+                    //catch (Exception ex)
+                    //{
+                    //    log.Debug("Exception - " + string.Format("oass adapter id:{0}, ex msg:{1}, ex st: {2} ", cdvrAdapterId, ex.Message, ex.StackTrace), ex);
+                    //}
+                }
+
+                return;
+            }
+            if (Request.QueryString["cdvr_adapter_setting_id"] != null && Request.QueryString["cdvr_adapter_setting_id"].ToString() != "")
+            {
+                Session["cdvr_adapter_setting_id"] = int.Parse(Request.QueryString["cdvr_adapter_setting_id"].ToString());
+            }
+            else
+            {
+                Session["cdvr_adapter_setting_id"] = 0;
+            }
+
+
+            if (Session["cdvr_adapter_id"] == null || Session["cdvr_adapter_id"].ToString() == "" || Session["cdvr_adapter_id"].ToString() == "0")
+            {
+                LoginManager.LogoutFromSite("login.html");
+                return;
+            }
+        }
+    }
+
+    public void GetHeader()
+    {
+        if (Session["cdvr_adapter_setting_id"] != null && Session["cdvr_adapter_setting_id"].ToString() != "" && int.Parse(Session["cdvr_adapter_setting_id"].ToString()) != 0)
+            Response.Write(PageUtils.GetPreHeader() + ":CDVR Adapter Settings - Edit");
+        else
+            Response.Write(PageUtils.GetPreHeader() + ":CDVR Adapter Settings - New");
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        if (Session["error_msg"] != null && Session["error_msg"].ToString() != "")
+        {
+            Session["error_msg"] = "";
+            return Session["last_page_html"].ToString();
+        }
+        Int32 groupID = LoginManager.GetLoginGroupID();
+
+        object t = null; ;
+        if (Session["cdvr_adapter_setting_id"] != null && Session["cdvr_adapter_setting_id"].ToString() != "" && int.Parse(Session["cdvr_adapter_setting_id"].ToString()) != 0)
+            t = Session["cdvr_adapter_setting_id"];
+        string sBack = "adm_cdvr_adapter_settings.aspx?search_save=1&cdvr_adapter_id=" + Session["cdvr_adapter_id"].ToString();
+
+        DBRecordWebEditor theRecord = new DBRecordWebEditor("cdvr_adapters_settings", "adm_table_pager", sBack, "", "ID", t, sBack, "cdvr_adapter_id");
+
+        theRecord.SetConnectionKey("CA_CONNECTION_STRING");
+
+        DataRecordShortTextField dr_key = new DataRecordShortTextField("ltr", true, 60, 128);
+        dr_key.Initialize("Key", "adm_table_header_nbg", "FormInput", "key_name", false);
+        theRecord.AddRecord(dr_key);
+
+        DataRecordLongTextField dr_value = new DataRecordLongTextField("ltr", true, 60, 4);
+        dr_value.Initialize("Value", "adm_table_header_nbg", "FormInput", "value", false);
+        theRecord.AddRecord(dr_value);
+
+        DataRecordShortIntField dr_groups = new DataRecordShortIntField(false, 9, 9);
+        dr_groups.Initialize("Group", "adm_table_header_nbg", "FormInput", "GROUP_ID", false);
+        dr_groups.SetValue(groupID.ToString());
+        theRecord.AddRecord(dr_groups);
+
+        DataRecordShortIntField dr_cdvr_adapter_id = new DataRecordShortIntField(false, 9, 9);
+        dr_cdvr_adapter_id.Initialize("adapter_id", "adm_table_header_nbg", "FormInput", "adapter_id", false);
+        dr_cdvr_adapter_id.SetValue(Session["cdvr_adapter_id"].ToString());
+        theRecord.AddRecord(dr_cdvr_adapter_id);
+
+
+
+        string sTable = theRecord.GetTableHTML("adm_cdvr_adapter_settings_new.aspx?submited=1");
+        return sTable;
+    }
+}
