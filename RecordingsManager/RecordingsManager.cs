2299e78d0b6ab7b760b80ced260808e6f312b326
Sunny Simantov <sunny.simantov@kaltura.com>
2016-04-13 10:28:30 +0300

    add recordings manager

---
+++ RecordingsManager/RecordingsManager.cs
@@ -0,0 +1,26 @@
+﻿using ApiObjects.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordingsManager
+{
+    public class RecordingsManager
+    {
+        public int Record(long programId, DateTime startDate, DateTime endDate, string siteGuid, int domainId)
+        {
+            int recordingId = 0;
+
+            return recordingId;
+        }
+
+        public Status CancelRecord(long programId, DateTime startDate, DateTime endDate, string siteGuid, int domainId)
+        {
+            Status status = new Status();
+
+            return status;
+        }
+    }
+}
