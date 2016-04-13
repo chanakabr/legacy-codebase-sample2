522c38341c15e012a318cebb0bab34c526e2d27f
Sunny Simantov <sunny.simantov@kaltura.com>
2016-04-13 11:26:56 +0300

    continue progress

--- RecordingsManager/RecordingsManager.cs
+++ RecordingsManager/RecordingsManager.cs
@@ -1,16 +1,56 @@
 ﻿using ApiObjects;
 using ApiObjects.Response;
 using ApiObjects.TimeShiftedTv;
+using KLogMonitor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RecordingsManager
+namespace Recordings
 {
     public class RecordingsManager
     {
+        #region Static Members
+
+        private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+
+        #endregion
+
+        #region Singleton
+
+        private RecordingsManager()
+        {
+        }
+
+        private static object locker = new object();
+        private RecordingsManager instance;
+
+        public RecordingsManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (locker)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new RecordingsManager();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
         public Recording Record(int groupId, long programId, DateTime startDate, DateTime endDate, string siteGuid, int domainId)
         {
             Recording recording = null;
@@ -26,5 +66,7 @@ namespace RecordingsManager
 
             return status;
         }
+
+        #endregion
     }
 }
