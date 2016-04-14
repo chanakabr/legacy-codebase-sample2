ef5c55f1f5ac466d11eac44d5fa842f07a894c8e
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2016-04-14 09:19:07 +0300

    no message

---
+++ WebAPI/Controllers/FollowTemplateController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebAPI.ClientManagers.Client;
+using WebAPI.Exceptions;
+using WebAPI.Managers.Models;
+using WebAPI.Models.Notification;
+using WebAPI.Utils;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("_service/FollowTemplate/action")]
+    public class FollowTemplate : ApiController
+    {
+        /// <summary>
+        /// TBD
+        /// </summary>
+        /// <param name="push_token">TBD</param>
+        /// <remarks>
+        /// 
+        /// </remarks>
+        /// <returns></returns>
+        [Route("add"), HttpPost]
+        [ApiAuthorize]
+        public KalturaFollowTemplate Add(KalturaFollowTemplate follow_template)
+        {
+            KalturaFollowTemplate response = null;
+
+            int groupId = KS.GetFromRequest().GroupId;
+
+            try
+            {
+                // call client
+                response = ClientsManager.NotificationClient().InsertFollowTemplate(groupId, follow_template);
+            }
+            catch (ClientException ex)
+            {
+                ErrorUtils.HandleClientException(ex);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ WebAPI/Models/Notification/KalturaFollowTemplate.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Xml.Serialization;
+using WebAPI.Models.General;
+
+namespace WebAPI.Models.Notification
+{
+    [Serializable]
+    public class KalturaFollowTemplate : KalturaOTTObject
+    {
+        [DataMember(Name = "id")]
+        [JsonProperty(PropertyName = "id")]
+        [XmlElement(ElementName = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "message")]
+        [JsonProperty(PropertyName = "message")]
+        [XmlElement(ElementName = "message")]
+        public string Message { get; set; }
+
+        [DataMember(Name = "Date_Format")]
+        [JsonProperty(PropertyName = "Date_Format")]
+        [XmlElement(ElementName = "Date_Format")]
+        public string DateFormat { get; set; }
+    }
+}
