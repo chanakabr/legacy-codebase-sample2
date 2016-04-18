09b61a042f62ab72e8b08722f783796f6095952f
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2016-04-18 13:29:46 +0300

    Add adm_series_template.aspx for creating FollowTemplate

---
+++ Web Sites/TVM/adm_series_template.aspx.cs
@@ -0,0 +1,188 @@
+﻿using ApiObjects;
+using ApiObjects.Notification;
+using ApiObjects.Response;
+using System;
+using System.Collections.Specialized;
+using System.Data;
+using TvinciImporter;
+using TvinciImporter.WSCatalog;
+using TVinciShared;
+
+public partial class adm_series_template : System.Web.UI.Page
+{
+    protected FollowTemplate followTemplate;
+    protected string m_sMenu;
+    protected string m_sSubMenu;
+    protected string m_sLangMenu;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LoginManager.CheckLogin() == false)
+            Response.Redirect("login.html");
+        else if (LoginManager.IsPagePermitted("adm_series_template.aspx") == false)
+            LoginManager.LogoutFromSite("login.html");
+        if (AMS.Web.RemoteScripting.InvokeMethod(this))
+            return;
+        Int32 nMenuID = 0;
+        m_sMenu = TVinciShared.Menu.GetMainMenu(14, true, ref nMenuID);
+        m_sSubMenu = TVinciShared.Menu.GetSubMenu(nMenuID, 3, true);
+
+        if (!IsPostBack)
+        {
+            if (Request.QueryString["submited"] != null && Request.QueryString["submited"].ToString().Trim() == "1")
+            {
+                int groupId = LoginManager.GetLoginGroupID();
+
+                ApiObjects.Response.Status result = new ApiObjects.Response.Status((int)eResponseStatus.Error, eResponseStatus.Error.ToString());
+
+                followTemplate = GetPageData();
+                if (followTemplate != null)
+                {
+                    ImporterImpl.SetFollowTemplate(groupId, ref followTemplate);
+                }
+            }
+        }
+    }
+
+    protected void GetMainMenu()
+    {
+        Response.Write(m_sMenu);
+    }
+
+    public void GetHeader()
+    {
+        Response.Write(PageUtils.GetPreHeader() + ": Series Template");
+    }
+
+    protected void GetLangMenu()
+    {
+        Response.Write(m_sLangMenu);
+    }
+
+    protected void GetSubMenu()
+    {
+        Response.Write(m_sSubMenu);
+    }
+
+    public string GetPageContent(string sOrderBy, string sPageNum)
+    {
+        object t = null;
+        if (Session["error_msg"] != null && Session["error_msg"].ToString() != "")
+        {
+            Session["error_msg"] = "";
+            return Session["last_page_html"].ToString();
+        }
+
+        object groupId = LoginManager.GetLoginGroupID();
+
+        int tableID = GetSeriesTemplateId(ODBCWrapper.Utils.GetIntSafeVal(groupId));
+        bool isParentGroup = IsParentGroup(ODBCWrapper.Utils.GetIntSafeVal(groupId));
+
+        string sTable = string.Empty;
+        if (!isParentGroup)
+        {
+            sTable = (PageUtils.GetPreHeader() + ": Module is not implemented");
+        }
+        else
+        {
+            if (tableID > 0) // a new record
+            {
+                t = tableID;
+            }
+
+            string sBack = "adm_series_template.aspx?search_save=1";
+            DBRecordWebEditor theRecord = new DBRecordWebEditor("follow_templates", "adm_table_pager", sBack, "", "ID", t, sBack, "");
+            theRecord.SetConnectionKey("notifications_connection");
+
+            DataRecordLongTextField dr_message = new DataRecordLongTextField("ltr", true, 60, 4);
+            dr_message.Initialize("Message", "adm_table_header_nbg", "FormInput", "message", true);
+            theRecord.AddRecord(dr_message);
+
+            DataRecordShortTextField dr_dateFormat = new DataRecordShortTextField("ltr", true, 60, 256);
+            dr_dateFormat.Initialize("Date format", "adm_table_header_nbg", "FormInput", "date_format", true);
+            theRecord.AddRecord(dr_dateFormat);
+
+            sTable = theRecord.GetTableHTML("adm_series_template.aspx?submited=1");
+        }
+        return sTable;
+    }
+
+    private int GetSeriesTemplateId(int groupID)
+    {
+
+        int seriesTemplateId = 0;
+        try
+        {
+            ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
+            selectQuery.SetConnectionKey("notifications_connection");
+            selectQuery += "select ID, Message, date_format from follow_templates where status=1 and asset_type=0 and";
+            selectQuery += ODBCWrapper.Parameter.NEW_PARAM("GROUP_ID", "=", groupID);
+            if (selectQuery.Execute("query", true) != null)
+            {
+                Int32 nCount = selectQuery.Table("query").DefaultView.Count;
+                if (nCount > 0)
+                {
+                    seriesTemplateId = ODBCWrapper.Utils.GetIntSafeVal(selectQuery.Table("query").DefaultView[0].Row, "ID");
+                }
+            }
+            selectQuery.Finish();
+            selectQuery = null;
+        }
+        catch
+        {
+            seriesTemplateId = 0;
+        }
+        return seriesTemplateId;
+
+    }
+
+    private bool IsParentGroup(int groupID)
+    {
+        bool res = false;
+        try
+        {
+            ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
+            selectQuery.SetConnectionKey("MAIN_CONNECTION_STRING");
+            selectQuery += "select PARENT_GROUP_ID from groups where";
+            selectQuery += ODBCWrapper.Parameter.NEW_PARAM("ID", "=", groupID);
+            if (selectQuery.Execute("query", true) != null)
+            {
+                Int32 nCount = selectQuery.Table("query").DefaultView.Count;
+                if (nCount > 0)
+                {
+                    int parentGroupID = ODBCWrapper.Utils.GetIntSafeVal(selectQuery.Table("query").DefaultView[0].Row, "PARENT_GROUP_ID");
+                    if (parentGroupID == 1)
+                    {
+                        res = true;
+                    }
+                }
+            }
+            selectQuery.Finish();
+            selectQuery = null;
+        }
+        catch (Exception ex)
+        {
+            res = false;
+        }
+        return res;
+    }
+
+    private FollowTemplate GetPageData()
+    {
+        NameValueCollection nvc = Request.Form;
+
+        FollowTemplate followTemplate = new FollowTemplate() { AssetType = eOTTAssetTypes.Series };
+
+        if (!string.IsNullOrEmpty(nvc["0_val"]))
+        {
+            followTemplate.Message = nvc["0_val"];
+        }
+
+        if (!string.IsNullOrEmpty(nvc["1_val"]))
+        {
+            followTemplate.DateFormat = nvc["1_val"];
+        }
+
+        return followTemplate;
+    }
+}
