3af920c24b6c3e3807a6f851222e53f70fa8dabd
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2016-04-26 12:07:31 +0300

    remove toString

--- ApiObjects/Notification/MessageTemplate.cs
+++ ApiObjects/Notification/MessageTemplate.cs
@@ -10,16 +10,5 @@ namespace ApiObjects.Notification
         public string Sound { get; set; }
         public string Action { get; set; }
         public string URL { get; set; }
-
-        public override string ToString()
-        {
-            if (this == null)
-            {
-                return string.Empty;
-            }
-
-            return string.Format("MessageTemplate: Id: {0}, message: {1}, DateFormat: {2}, AssetType: {3}, Sound: {4}, Action: {5}, URL: {6}",
-                Id, Message, DateFormat, AssetType.ToString(), Sound, Action, URL);
-        }
     }
 }
