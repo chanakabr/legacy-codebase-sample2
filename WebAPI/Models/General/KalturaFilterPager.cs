0cf01cd2f7b0c08ec85d0938e6fe67bb954c6981
Lior Faingold <Lior.Faingold@kaltura.com>
2016-05-08 09:50:07 +0300

    set page index default if none is specified (otherwise it will be 0)

--- WebAPI/Models/General/KalturaFilterPager.cs
+++ WebAPI/Models/General/KalturaFilterPager.cs
@@ -16,6 +16,7 @@ namespace WebAPI.Models.General
     {
         private static int maxPageSize;
         private const int DEFAULT_PAGE_SIZE = 30;
+        private const int DEFAULT_PAGE_INDEX = 1;
         private int pageSize;
         private int pageIndex;
 
@@ -96,6 +97,7 @@ namespace WebAPI.Models.General
             }
 
             pageSize = DEFAULT_PAGE_SIZE;
+            pageIndex = DEFAULT_PAGE_INDEX;
         }
 
         internal int getPageIndex()
