1947391c4806b717aaaecbfe34e7b3c673a90340
Lior Faingold <Lior.Faingold@kaltura.com>
2016-05-09 10:15:05 +0300

    changed response value

--- ApiObjects/Response/EnumResponseStatus.cs
+++ ApiObjects/Response/EnumResponseStatus.cs
@@ -260,7 +260,7 @@ namespace ApiObjects.Response
         InvalidUsageModule = 9013,
         InvalidCouponGroup = 9014,
         InvalidCurrency = 9015,
-        PpvModuleNotFound = 9016,
+        ModuleNotExists = 9016,
 
         // Adapters 10000-11000
         AdapterNotExists = 10000,
