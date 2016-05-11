4e5eaff443cb7b51331388e842fe79c1e1a38fb3
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2016-05-11 11:24:07 +0300

    BEO-1430 Notifications - Inbox Add validation for max  MessageTTL

--- ApiObjects/Response/EnumResponseStatus.cs
+++ ApiObjects/Response/EnumResponseStatus.cs
@@ -225,7 +225,7 @@ namespace ApiObjects.Response
 
         // notification 8000-899
         NoNotificationSettingsSent = 8000,
-        PushNotificationFalse = 8001, //"push notification false can't comabin with push system announcements true";
+        PushNotificationFalse = 8001, //"push notification false can't combine with push system announcements true";
         NoNotificationPartnerSettings = 8002,
         NoNotificationSettings = 8003,        
         AnnouncementMessageIsEmpty = 8004,
@@ -242,6 +242,7 @@ namespace ApiObjects.Response
         DatetimeFormatIsInvalid = 8015,
         MessageTemplateNotFound = 8016,
         URLPlaceholdersInvalid = 8017,
+        InvalidMessageTTL = 8018,
 
 
         //Pricing 9000-9999
