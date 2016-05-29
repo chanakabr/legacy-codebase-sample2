8c666248e777e837c95ba7f9f5669d3a12f785aa
Lior Faingold <Lior.Faingold@kaltura.com>
2016-05-29 16:27:56 +0300

    ODBC read/write changes

--- ODBCWrapper/Utils.cs
+++ ODBCWrapper/Utils.cs
@@ -993,8 +993,7 @@ namespace ODBCWrapper
             //m_bIsWritable || m_bUseWritable
             if (!useWriteable)
             {                
-                //Utils.UseWritable = ReadWriteLock(sKey, oValue, executer, isWritable);
-                Utils.UseWritable = true;
+                Utils.UseWritable = ReadWriteLock(sKey, oValue, executer, isWritable);
 
                 //if (useWriteable) Logger.Logger.Log("DBLock ", "m_bUseWritable changed to '" + Utils.UseWritable + "', for: " + executer, "ODBC_DBLock");
             }
