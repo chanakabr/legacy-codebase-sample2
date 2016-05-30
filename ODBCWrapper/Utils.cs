07602c42d290f4a2e852a27ce413a3aeda58be60
Amishai Guilburd <amishai.guilburd@kaltura.com>
2016-05-30 08:49:34 +0300

    fixed Lior and Elram something something  +                 Utils.UseWritable = true;

--- ODBCWrapper/Utils.cs
+++ ODBCWrapper/Utils.cs
@@ -994,6 +994,7 @@ namespace ODBCWrapper
             if (!useWriteable)
             {                
                 Utils.UseWritable = ReadWriteLock(sKey, oValue, executer, isWritable);
+                Utils.UseWritable = true;
 
                 //if (useWriteable) Logger.Logger.Log("DBLock ", "m_bUseWritable changed to '" + Utils.UseWritable + "', for: " + executer, "ODBC_DBLock");
             }
