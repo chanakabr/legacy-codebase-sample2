40bb9e07c1c0267394b407adb4b1a687450ed35f
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2016-06-09 10:29:40 +0300

    BEO-1661 Push to any device

---
+++ WebAPI/Models/Notification/KalturaPushWebParametersResponse.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using WebAPI.Models.General;
+
+namespace WebAPI.Models.Notification
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract(Name = "KalturaPushWebParametersResponse", Namespace = "")]
+    [XmlRoot("KalturaPushWebParametersResponse")]
+    public class KalturaPushWebParametersResponse : KalturaListResponse
+    {
+        /// push web parameters
+        /// </summary>
+        [DataMember(Name = "objects")]
+        [JsonProperty("objects")]
+        [XmlArray(ElementName = "objects", IsNullable = true)]
+        [XmlArrayItem(ElementName = "item")]
+        public List<KalturaPushWebParameters> PushWebParameters { get; set; }
+    }
+}
