4810374d9dcca27847fad798272b326975df663f
tan-tan-kanarek <Jonathan.Kanarek@Kaltura.com>
2016-06-13 14:08:45 +0300

    Added addDevice method

--- WebAPI/Clients/DomainsClient.cs
+++ WebAPI/Clients/DomainsClient.cs
@@ -459,6 +459,45 @@ namespace WebAPI.Clients
             return result;
         }
 
+        internal KalturaDevice AddDevice(int groupId, int domainId, string deviceName, string udid, int deviceBrandId)
+        {
+            WebAPI.Domains.DeviceResponse response = null;
+
+            Group group = GroupsManager.GetGroup(groupId);
+
+            try
+            {
+                using (KMonitor km = new KMonitor(Events.eEvent.EVENT_WS))
+                {
+                    response = Domains.AddDevice(group.DomainsCredentials.Username, group.DomainsCredentials.Password, domainId, udid, deviceName, deviceBrandId);
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Exception received while calling domains service. ws address: {0}, exception: {1}", Domains.Url, ex);
+                ErrorUtils.HandleWSException(ex);
+            }
+
+            if (response == null || response.Status == null)
+            {
+                throw new ClientException((int)StatusCode.Error, StatusCode.Error.ToString());
+            }
+
+            if (response.Status.Code != (int)StatusCode.OK)
+            {
+                throw new ClientException(response.Status.Code, response.Status.Message);
+            }
+
+            if (response.Device == null)
+            {
+                throw new ClientException((int)StatusCode.Error, StatusCode.Error.ToString());
+            }
+
+            KalturaDevice result = Mapper.Map<KalturaDevice>(response.Device.m_oDevice);
+
+            return result;
+        }
+
         internal KalturaDevicePin GetPinForDevice(int groupId, string udid, int deviceBrandId)
         {
             KalturaDevicePin result;
