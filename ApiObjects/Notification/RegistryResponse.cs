1b43b5db4831a47ff76dd12f084eda1706f57d57
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2016-06-16 14:13:44 +0300

    BEO-1661 Push to any device rename

--- ApiObjects/Notification/RegistryResponse.cs
+++ ApiObjects/Notification/RegistryResponse.cs
@@ -1,9 +1,5 @@
 ﻿using ApiObjects.Response;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ApiObjects.Notification
 {
