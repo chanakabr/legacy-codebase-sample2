db4e722d729d668ab262c35359112046145c3e33
Sunny Simantov <sunny.simantov@kaltura.com>
2016-07-04 07:38:37 +0300

    add forgotten "," to EPG serialization

--- ElasticSearch/Common/ESSerializer.cs
+++ ElasticSearch/Common/ESSerializer.cs
@@ -735,7 +735,7 @@ namespace ElasticSearch.Common
 
             sRecord.AppendFormat("\"epg_id\": {0}, \"group_id\": {1}, \"epg_channel_id\": {2}, \"is_active\": {3}, \"start_date\": \"{4}\", \"end_date\": \"{5}\"," +
                 " \"name\": \"{6}\", \"description\": \"{7}\", \"cache_date\": \"{8}\", \"date_routing\": \"{9}\", \"create_date\": \"{10}\", \"update_date\": \"{11}\","+
-                "\"search_end_date\": \"{12}\", \"crid\": \"{13}\"",
+                "\"search_end_date\": \"{12}\", \"crid\": \"{13}\",",
                 oEpg.EpgID, oEpg.GroupID, oEpg.ChannelID, (oEpg.isActive) ? 1 : 0, oEpg.StartDate.ToString("yyyyMMddHHmmss"), oEpg.EndDate.ToString("yyyyMMddHHmmss"),
                 Common.Utils.ReplaceDocumentReservedCharacters(ref name), Common.Utils.ReplaceDocumentReservedCharacters(ref description),
                 /* cache_date*/ DateTime.UtcNow.ToString("yyyyMMddHHmmss"), /* date_routing */ oEpg.StartDate.ToUniversalTime().ToString("yyyyMMdd"),
