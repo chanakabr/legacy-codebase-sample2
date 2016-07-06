56057e421a4d8ff1583ee81a54fcfd18e37c22fd
Liat Regev <liatr@tvinci.com>
2016-07-06 13:54:53 +0300

    add : BEO-1830 , Enhanced concurrency calculation stop, finish, swoosh

--- ApiObjects/enums.cs
+++ ApiObjects/enums.cs
@@ -329,4 +329,21 @@ namespace ApiObjects
         [EnumMember]
         All
     }
+
+    public enum MediaPlayActions
+    {
+        PLAY = 1,
+        STOP = 2,
+        PAUSE = 3,
+        FIRST_PLAY = 4,
+        SWOOSH = 5,
+        FULL_SCREEN = 6,
+        SEND_TO_FRIEND = 7,
+        LOAD = 8,
+        FULL_SCREEN_EXIT = 9,
+        FINISH = 10,
+        BITRATE_CHANGE = 40,
+        ERROR = 18,
+        NONE = 99
+    }
 }
