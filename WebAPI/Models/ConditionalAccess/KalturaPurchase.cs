f834141a09eda57ed70f205a779a3524817ed6f7
tan-tan-kanarek <Jonathan.Kanarek@Kaltura.com>
2016-07-11 19:24:25 +0300

    Align transaction controller

---
+++ WebAPI/Models/ConditionalAccess/KalturaPurchase.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using WebAPI.Models.General;
+using WebAPI.Managers.Schema;
+using WebAPI.Exceptions;
+
+namespace WebAPI.Models.ConditionalAccess
+{
+    public class KalturaPurchaseBase : KalturaOTTObject
+    {
+        /// <summary>
+        /// Identifier for the package from which this content is offered
+        /// </summary>
+        [DataMember(Name = "productId")]
+        [JsonProperty("productId")]
+        [XmlElement(ElementName = "productId")]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Identifier for the content to purchase. Relevant only if Product type = PPV
+        /// </summary>
+        [DataMember(Name = "contentId")]
+        [JsonProperty("contentId")]
+        [XmlElement(ElementName = "contentId")]
+        public int? ContentId { get; set; }
+
+        /// <summary>
+        /// Package type. Possible values: PPV, Subscription, Collection
+        /// </summary>
+        [DataMember(Name = "productType")]
+        [JsonProperty("productType")]
+        [XmlElement(ElementName = "productType")]
+        public KalturaTransactionType ProductType { get; set; }
+
+        internal int getContentId()
+        {
+            return ContentId.HasValue ? ContentId.Value : 0;
+        }
+    }
+
+    public class KalturaPurchase : KalturaPurchaseBase
+    {
+        /// <summary>
+        /// Identifier for paying currency, according to ISO 4217
+        /// </summary>
+        [DataMember(Name = "currency")]
+        [JsonProperty("currency")]
+        [XmlElement(ElementName = "currency")]
+        public string Currency { get; set; }
+
+        /// <summary>
+        /// Net sum to charge – as a one-time transaction. Price must match the previously provided price for the specified content.
+        /// </summary>
+        [DataMember(Name = "price")]
+        [JsonProperty("price")]
+        [XmlElement(ElementName = "price")]
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Identifier for a pre-entered payment method. If not provided – the household’s default payment method is used
+        /// </summary>
+        [DataMember(Name = "paymentMethodId")]
+        [JsonProperty("paymentMethodId")]
+        [XmlElement(ElementName = "paymentMethodId")]
+        public int? PaymentMethodId { get; set; }
+
+        /// <summary>
+        /// Identifier for a pre-associated payment gateway. If not provided – the account’s default payment gateway is used
+        /// </summary>
+        [DataMember(Name = "paymentGatewayId")]
+        [JsonProperty("paymentGatewayId")]
+        [XmlElement(ElementName = "paymentGatewayId")]
+        public int? PaymentGatewayId { get; set; }
+
+        /// <summary>
+        /// Coupon code
+        /// </summary>
+        [DataMember(Name = "coupon")]
+        [JsonProperty("coupon")]
+        [XmlElement(ElementName = "coupon")]
+        public string Coupon { get; set; }
+        
+        internal void Validate()
+        {
+            // validate purchase token
+            if (string.IsNullOrEmpty(Currency))
+                throw new BadRequestException((int)WebAPI.Managers.Models.StatusCode.BadRequest, "currency cannot be empty");
+
+            //// validate price
+            if (Price < 0)
+                throw new BadRequestException((int)WebAPI.Managers.Models.StatusCode.BadRequest, "price is illegal");
+
+            //// validate product_id
+            if (ProductId <= 0)
+                throw new BadRequestException((int)WebAPI.Managers.Models.StatusCode.BadRequest, "productId is illegal");
+
+            //// validate payment_method_id
+            if (PaymentMethodId.HasValue && PaymentMethodId.Value == 0)
+                throw new BadRequestException((int)WebAPI.Managers.Models.StatusCode.BadRequest, "payment_method_id cannot be 0");
+        }
+
+        internal int getPaymentMethodId()
+        {
+            return PaymentMethodId.HasValue ? PaymentMethodId.Value : 0;
+        }
+
+        internal int getPaymentGatewayId()
+        {
+            return PaymentGatewayId.HasValue ? PaymentGatewayId.Value : 0;
+        }
+
+        internal string getCoupon()
+        {
+            return Coupon == null ? string.Empty : Coupon;
+        }
+    }
+
+    public class KalturaPurchaseSession : KalturaPurchase
+    {
+        /// <summary>
+        /// Preview module identifier (relevant only for subscription)
+        /// </summary>
+        [DataMember(Name = "previewModuleId")]
+        [JsonProperty("previewModuleId")]
+        [XmlElement(ElementName = "previewModuleId")]
+        public int? PreviewModuleId { get; set; }
+
+        internal int getPreviewModuleId()
+        {
+            return PreviewModuleId.HasValue ? PreviewModuleId.Value : 0;
+        }
+    }
+
+    public class KalturaExternalReceipt : KalturaPurchaseBase
+    {
+        /// <summary>
+        /// A unique identifier that was provided by the In-App billing service to validate the purchase
+        /// </summary>
+        [DataMember(Name = "receiptId")]
+        [JsonProperty("receiptId")]
+        [XmlElement(ElementName = "receiptId")]
+        public string ReceiptId { get; set; }
+
+        /// <summary>
+        /// The payment gateway name for the In-App billing service to be used. Possible values: Google/Apple
+        /// </summary>
+        [DataMember(Name = "paymentGatewayName")]
+        [JsonProperty("paymentGatewayName")]
+        [XmlElement(ElementName = "paymentGatewayName")]
+        public string PaymentGatewayName { get; set; }
+
+        internal void Validate()
+        {
+            // validate purchase token
+            if (string.IsNullOrEmpty(ReceiptId))
+                throw new BadRequestException((int)WebAPI.Managers.Models.StatusCode.BadRequest, "purchase receipt cannot be empty");
+
+            // validate payment gateway id
+            if (string.IsNullOrEmpty(PaymentGatewayName))
+                throw new BadRequestException((int)WebAPI.Managers.Models.StatusCode.BadRequest, "payment gateway type cannot be empty");
+        }
+    }
+}
---
+++ WebAPI/Models/ConditionalAccess/KalturaTransactionStatus.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using WebAPI.Models.General;
+using WebAPI.Managers.Schema;
+using WebAPI.Exceptions;
+
+namespace WebAPI.Models.ConditionalAccess
+{
+    public enum KalturaTransactionAdapterStatus
+    {
+        OK = 0,
+        PENDING = 1,
+        FAILED = 2
+    }
+
+    public class KalturaTransactionStatus : KalturaOTTObject
+    {
+        /// <summary>
+        /// Payment gateway adapter application state for the transaction to update
+        /// </summary>
+        [DataMember(Name = "adapterTransactionStatus")]
+        [JsonProperty("adapterTransactionStatus")]
+        [XmlElement(ElementName = "adapterTransactionStatus")]
+        public KalturaTransactionAdapterStatus AdapterStatus { get; set; }
+
+        /// <summary>
+        /// External transaction identifier
+        /// </summary>
+        [DataMember(Name = "externalId")]
+        [JsonProperty("externalId")]
+        [XmlElement(ElementName = "externalId")]
+        public string ExternalId { get; set; }
+
+        /// <summary>
+        /// Payment gateway transaction status
+        /// </summary>
+        [DataMember(Name = "externalStatus")]
+        [JsonProperty("externalStatus")]
+        [XmlElement(ElementName = "externalStatus")]
+        public string ExternalStatus { get; set; }
+
+        /// <summary>
+        /// Payment gateway message
+        /// </summary>
+        [DataMember(Name = "externalMessage")]
+        [JsonProperty("externalMessage")]
+        [XmlElement(ElementName = "externalMessage")]
+        public string ExternalMessage { get; set; }
+
+        /// <summary>
+        /// The reason the transaction failed
+        /// </summary>
+        [DataMember(Name = "failReason")]
+        [JsonProperty("failReason")]
+        [XmlElement(ElementName = "failReason")]
+        public int FailReason { get; set; }
+    }
+}
