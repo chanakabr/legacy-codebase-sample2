9baaff9c4aaa5e6e2a59334dee38978539c8a576
Lior Faingold <Lior.Faingold@kaltura.com>
2016-07-27 16:15:01 +0300

    add series cancel and series delete status to show on list response

--- WebAPI/Models/ConditionalAccess/KalturaRecordingStatus.cs
+++ WebAPI/Models/ConditionalAccess/KalturaRecordingStatus.cs
@@ -17,6 +17,8 @@ namespace WebAPI.Models.ConditionalAccess
         RECORDED = 2,
         CANCELED = 3,
         FAILED = 4,
-        DELETED = 5        
+        DELETED = 5,
+        SERIES_CANCLED = 6,
+        SERIES_DELETED = 7
     } 
 }
