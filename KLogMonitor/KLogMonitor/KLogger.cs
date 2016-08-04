66eb4c392e3eb1a7bd251a564d6132f7931f50e1
Amishai Guilburd <amishai.guilburd@kaltura.com>
2016-08-04 12:16:27 +0300

    updated logs monitor

--- KLogMonitor/KLogMonitor/KLogger.cs
+++ KLogMonitor/KLogMonitor/KLogger.cs
@@ -195,16 +195,16 @@ namespace KLogMonitor
         private string formatMessage(string msg, DateTime creationDate)
         {
             return string.Format("class:{0} topic:{1} method:{2} server:{3} ip:{4} reqid:{5} partner:{6} action:{7} uid:{8} msg:{9}",
-                ClassName != null ? ClassName : "null",  // 0
-                Topic != null ? Topic : "null",          // 1
-                MethodName != null ? MethodName : "null",// 2
-                Server != null ? Server : "null",        // 3
-                IPAddress != null ? IPAddress : "null",  // 4
-                UniqueID != null ? UniqueID : "null",    // 5
-                PartnerID != null ? PartnerID : "null",  // 6
-                Action != null ? Action : "null",        // 7
-                UserID != null ? UserID : "0",           // 8
-                msg != null ? msg : "null");             // 9
+                !string.IsNullOrWhiteSpace(ClassName) ? ClassName : "null",  // 0
+                !string.IsNullOrWhiteSpace(Topic) ? Topic : "null",          // 1
+                !string.IsNullOrWhiteSpace(MethodName) ? MethodName : "null",// 2
+                !string.IsNullOrWhiteSpace(Server) ? Server : "null",        // 3 
+                !string.IsNullOrWhiteSpace(IPAddress) ? IPAddress : "null",  // 4
+                !string.IsNullOrWhiteSpace(UniqueID) ? UniqueID : "null",    // 5
+                !string.IsNullOrWhiteSpace(PartnerID) ? PartnerID : "null",  // 6
+                !string.IsNullOrWhiteSpace(Action) ? Action : "null",        // 7
+                !string.IsNullOrWhiteSpace(UserID) ? UserID : "0",           // 8
+                !string.IsNullOrWhiteSpace(msg) ? msg : "null");             // 9
         }
 
         private void sendLog(LogEvent logEvent)
