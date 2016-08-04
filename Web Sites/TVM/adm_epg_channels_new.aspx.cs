57d700a0d2fd5efa561a90a2fc086769aebb1660
Liat Regev <liatr@tvinci.com>
2016-08-04 07:44:31 +0300

    fix missspelling - BEO-2139

--- Web Sites/TVM/adm_epg_channels_new.aspx.cs
+++ Web Sites/TVM/adm_epg_channels_new.aspx.cs
@@ -289,7 +289,7 @@ public partial class adm_epg_channels_new : System.Web.UI.Page
         theRecord.AddRecord(dr_enableRecordingPlaybackNonEntitled);
 
         DataRecordDropDownField dr_enableRecordingPlaybackNonExisting = new DataRecordDropDownField("lu_epg_field_enable", "DESCRIPTION", "id", "", null, 60, false);
-        dr_enableRecordingPlaybackNonExisting.Initialize("Enable Recording Playback (for non-entitled channel)", "adm_table_header_nbg", "FormInput", "enable_recording_playback_non_existing", false);
+        dr_enableRecordingPlaybackNonExisting.Initialize("Enable Recording Playback (for non-existing channel)", "adm_table_header_nbg", "FormInput", "enable_recording_playback_non_existing", false);
         theRecord.AddRecord(dr_enableRecordingPlaybackNonExisting);
 
         string sTable = theRecord.GetTableHTML("adm_epg_channels_new.aspx?submited=1");
