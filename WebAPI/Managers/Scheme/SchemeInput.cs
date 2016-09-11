1d97d0c7146487acc1ffd2741dbf0d140136c231
tan-tan-kanarek <Jonathan.Kanarek@Kaltura.com>
2016-09-11 10:57:18 +0300

    Validate read actions inputs

--- WebAPI/Managers/Scheme/SchemeInput.cs
+++ WebAPI/Managers/Scheme/SchemeInput.cs
@@ -40,76 +40,73 @@ namespace WebAPI.Managers.Scheme
             if(HttpContext.Current.Items[RequestParser.REQUEST_TYPE] != null)
                 requiresPermission = (RequestType)HttpContext.Current.Items[RequestParser.REQUEST_TYPE];
 
-            if (isA(requiresPermission, RequestType.WRITE))
+            if (DynamicType != null)
             {
-                if (DynamicType != null)
+                string sValue = (string)Convert.ChangeType(value, typeof(string));
+                try
                 {
-                    string sValue = (string)Convert.ChangeType(value, typeof(string));
-                    try
-                    {
-                        Enum.Parse(DynamicType, sValue, true);
-                    }
-                    catch (ArgumentException e)
-                    {
-                        throw new BadRequestException(BadRequestException.ARGUMENT_STRING_SHOULD_BE_ENUM, name, DynamicType.Name);
-                    }
+                    Enum.Parse(DynamicType, sValue, true);
                 }
+                catch (ArgumentException e)
+                {
+                    throw new BadRequestException(BadRequestException.ARGUMENT_STRING_SHOULD_BE_ENUM, name, DynamicType.Name);
+                }
+            }
 
-                if (MaxLength > 0)
-                {
-                    string sValue = (string)Convert.ChangeType(value, typeof(string));
-                    if (sValue.Length > MaxLength)
-                        throw new BadRequestException(BadRequestException.ARGUMENT_MAX_LENGTH_CROSSED, name, MaxLength.ToString());
-                }
+            if (MaxLength > 0)
+            {
+                string sValue = (string)Convert.ChangeType(value, typeof(string));
+                if (sValue.Length > MaxLength)
+                    throw new BadRequestException(BadRequestException.ARGUMENT_MAX_LENGTH_CROSSED, name, MaxLength.ToString());
+            }
 
-                if (MinLength >= 0)
-                {
-                    string sValue = (string)Convert.ChangeType(value, typeof(string));
-                    if (sValue.Length < MinLength)
-                        throw new BadRequestException(BadRequestException.ARGUMENT_MIN_LENGTH_CROSSED, name, MinLength.ToString());
-                }
+            if (MinLength >= 0)
+            {
+                string sValue = (string)Convert.ChangeType(value, typeof(string));
+                if (sValue.Length < MinLength)
+                    throw new BadRequestException(BadRequestException.ARGUMENT_MIN_LENGTH_CROSSED, name, MinLength.ToString());
+            }
 
-                if (MaxInteger < int.MaxValue)
-                {
-                    int iValue = (int)Convert.ChangeType(value, typeof(int));
-                    if (iValue > MaxInteger)
-                        throw new BadRequestException(BadRequestException.ARGUMENT_MAX_VALUE_CROSSED, name, MaxInteger.ToString());
-                }
+            if (MaxInteger < int.MaxValue)
+            {
+                int iValue = (int)Convert.ChangeType(value, typeof(int));
+                if (iValue > MaxInteger)
+                    throw new BadRequestException(BadRequestException.ARGUMENT_MAX_VALUE_CROSSED, name, MaxInteger.ToString());
+            }
 
-                if (MinInteger > int.MinValue)
-                {
-                    long lValue = (long)Convert.ChangeType(value, typeof(long));
-                    if (lValue < MinInteger)
-                        throw new BadRequestException(BadRequestException.ARGUMENT_MIN_VALUE_CROSSED, name, MinInteger.ToString());
-                }
+            if (MinInteger > int.MinValue)
+            {
+                long lValue = (long)Convert.ChangeType(value, typeof(long));
+                if (lValue < MinInteger)
+                    throw new BadRequestException(BadRequestException.ARGUMENT_MIN_VALUE_CROSSED, name, MinInteger.ToString());
+            }
 
-                if (MaxLong < long.MaxValue)
-                {
-                    long lValue = (long)Convert.ChangeType(value, typeof(long));
-                    if (lValue > MaxLong)
-                        throw new BadRequestException(BadRequestException.ARGUMENT_MAX_VALUE_CROSSED, name, MaxLong.ToString());
-                }
+            if (MaxLong < long.MaxValue)
+            {
+                long lValue = (long)Convert.ChangeType(value, typeof(long));
+                if (lValue > MaxLong)
+                    throw new BadRequestException(BadRequestException.ARGUMENT_MAX_VALUE_CROSSED, name, MaxLong.ToString());
+            }
 
-                if (MinLong > long.MinValue)
-                {
-                    int lValue = (int)Convert.ChangeType(value, typeof(int));
-                    if (lValue < MinLong)
-                        throw new BadRequestException(BadRequestException.ARGUMENT_MIN_VALUE_CROSSED, name, MinLong.ToString());
-                }
+            if (MinLong > long.MinValue)
+            {
+                int lValue = (int)Convert.ChangeType(value, typeof(int));
+                if (lValue < MinLong)
+                    throw new BadRequestException(BadRequestException.ARGUMENT_MIN_VALUE_CROSSED, name, MinLong.ToString());
+            }
 
-                if (MaxFloat < float.MaxValue)
-                {
-                    float fValue = (float)Convert.ChangeType(value, typeof(float));
-                    if (fValue > MaxFloat)
-                        throw new BadRequestException(BadRequestException.ARGUMENT_MAX_VALUE_CROSSED, name, MaxFloat.ToString());
-                }
+            if (MaxFloat < float.MaxValue)
+            {
+                float fValue = (float)Convert.ChangeType(value, typeof(float));
+                if (fValue > MaxFloat)
+                    throw new BadRequestException(BadRequestException.ARGUMENT_MAX_VALUE_CROSSED, name, MaxFloat.ToString());
+            }
 
-                if (MinFloat > float.MinValue)
-                {
-                    float fValue = (float)Convert.ChangeType(value, typeof(float));
-                    if (fValue < MinFloat)
-                        throw new BadRequestException(BadRequestException.ARGUMENT_MIN_VALUE_CROSSED, name, MinFloat.ToString());
-                }
+            if (MinFloat > float.MinValue)
+            {
+                float fValue = (float)Convert.ChangeType(value, typeof(float));
+                if (fValue < MinFloat)
+                    throw new BadRequestException(BadRequestException.ARGUMENT_MIN_VALUE_CROSSED, name, MinFloat.ToString());
             }
         }
 
