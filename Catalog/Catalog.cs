845a1a3ebf6dbcf61612386e4871a5fd473726cb
Sunny Simantov <sunny.simantov@kaltura.com>
2016-09-20 15:09:11 +0300

    fix issue in Get Media Mark Hit Initial Data

--- Catalog/Catalog.cs
+++ Catalog/Catalog.cs
@@ -4164,6 +4164,7 @@ namespace Catalog
             #region  try get values from catalog cache
 
             double cacheTime = TVinciShared.WS_Utils.GetTcmDoubleValue("CATALOG_HIT_CACHE_TIME_IN_MINUTES");
+
             if (cacheTime == 0)
             {
                 cacheTime = 120d;
@@ -4171,11 +4172,11 @@ namespace Catalog
 
             CatalogCache catalogCache = CatalogCache.Instance();
             string ipKey = string.Format("{0}_userIP_{1}", eWSModules.CATALOG, userIP);
+
             object oCountryID = catalogCache.Get(ipKey);
+
             bool bIP = false;
             bool bMedia = false;
-
-            bool isValid = false;
 
             if (oCountryID != null)
             {
@@ -4185,44 +4186,49 @@ namespace Catalog
 
             string m_mf_Key = string.Format("{0}_media_{1}_mediaFile_{2}", eWSModules.CATALOG, mediaID, mediaFileID);
             List<KeyValuePair<string, int>> lMedia = catalogCache.Get<List<KeyValuePair<string, int>>>(m_mf_Key);
+
             if (lMedia != null && lMedia.Count > 0)
             {
                 InitMediaMarkHitDataFromCache(ref ownerGroupID, ref cdnID, ref qualityID, ref formatID, ref mediaTypeID, ref billingTypeID, ref fileDuration, lMedia);
 
+                log.DebugFormat("GetMediaMarkHitInitialData, get media mark hit datafrom cache: " +
+                    "cdnID {0}, qualityID {1}, formatId {2}, mediaTypeId {3}, billingTypeId {4}, fileDuration {5}",
+                    cdnID, qualityID, formatID, mediaTypeID, billingTypeID, fileDuration);
+
                 if (cdnID == -1 && qualityID == -1 && formatID == -1 && mediaTypeID == -1 && billingTypeID == -1 && fileDuration == -1)
                 {
-                    isValid = false;
-                }
-                else
-                {
-                    isValid = true;
+                    return false;
                 }
 
                 bMedia = true;
-
             }
 
             #endregion
 
-            if (!isValid)
-            {
-                return false;
-            }
-
             if (!bIP) // try getting countryID from ES, if it fails get countryID from DB
             {
+                log.DebugFormat("GetMediaMarkHitInitialData, try getting countryID from ES, if it fails get countryID from DB");
+
                 countryID = ElasticSearch.Utilities.IpToCountry.GetCountryByIp(userIP);
                 //getting from ES failed
                 if (countryID == 0)
                 {
+                    log.DebugFormat("GetMediaMarkHitInitialData, getting country from ES failed, getting from DB");
+
                     long ipVal = 0;
                     ipVal = ParseIPOutOfString(userIP);
                     CatalogDAL.Get_IPCountryCode(ipVal, ref countryID);
                 }
                 if (countryID > 0)
                 {
+                    log.DebugFormat("GetMediaMarkHitInitialData, setting countryId in cache");
+
                     catalogCache.Set(ipKey, countryID, cacheTime);
                     bIP = true;
+                }
+                else
+                {
+                    log.DebugFormat("GetMediaMarkHitInitialData, setting countryId in cache");
                 }
             }
 
@@ -4231,11 +4237,14 @@ namespace Catalog
                 if (CatalogDAL.GetMediaPlayData(mediaID, mediaFileID, ref ownerGroupID, ref cdnID, ref qualityID, ref formatID, ref mediaTypeID, ref billingTypeID, ref fileDuration))
                 {
                     InitMediaMarkHitDataToCache(ownerGroupID, cdnID, qualityID, formatID, mediaTypeID, billingTypeID, fileDuration, ref lMedia);
+
                     catalogCache.Set(m_mf_Key, lMedia, cacheTime);
                     bMedia = true;
                 }
                 else
                 {
+                    log.DebugFormat("GetMediaMarkHitInitialData, GetMediaPlayData failed, setting in cache invalid media");
+
                     // If couldn't get media - create an "invalid" record and save it in cache
                     InitMediaMarkHitDataToCache(ownerGroupID, -1, -1, -1, -1, -1, -1, ref lMedia);
                     catalogCache.Set(m_mf_Key, lMedia, cacheTime);
