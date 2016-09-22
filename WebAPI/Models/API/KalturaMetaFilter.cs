79c1c56c4568b3f97a5e7ebddfa05d9c482d730c
Irena Leibman <irena.leibman@kaltura.com>
2016-09-22 18:03:44 +0300

    no message

---
+++ WebAPI/Models/API/KalturaMetaFilter.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Xml.Serialization;
+using WebAPI.Exceptions;
+using WebAPI.Managers.Scheme;
+using WebAPI.Models.Catalog;
+using WebAPI.Models.General;
+
+namespace WebAPI.Models.API
+{
+    /// <summary>
+    /// Meta filter
+    /// </summary>
+    public class KalturaMetaFilter : KalturaFilter<KalturaMetaOrderBy>
+    {
+        /// <summary>
+        /// Meta system field name to filter by
+        /// </summary>
+        [DataMember(Name = "fieldNameEqual")]
+        [JsonProperty("fieldNameEqual")]
+        [XmlElement(ElementName = "fieldNameEqual")]
+        public KalturaMetaFieldName FieldNameEqual { get; set; }
+
+        /// <summary>
+        /// Meta system field name to filter by
+        /// </summary>
+        [DataMember(Name = "fieldNameNotEqual")]
+        [JsonProperty("fieldNameNotEqual")]
+        [XmlElement(ElementName = "fieldNameNotEqual")]
+        public KalturaMetaFieldName FieldNameNotEqual { get; set; }
+
+        /// <summary>
+        /// Meta type to filter by
+        /// </summary>
+        [DataMember(Name = "typeEqual")]
+        [JsonProperty("typeEqual")]
+        [XmlElement(ElementName = "typeEqual")]
+        public KalturaMetaType? TypeEqual { get; set; }
+
+        /// <summary>
+        /// Asset type to filter by
+        /// </summary>
+        [DataMember(Name = "assetTypeEqual")]
+        [JsonProperty("assetTypeEqual")]
+        [XmlElement(ElementName = "assetTypeEqual")]
+        public KalturaAssetType? AssetTypeEqual { get; set; }
+        
+
+
+        public override KalturaMetaOrderBy GetDefaultOrderByValue()
+        {
+            return KalturaMetaOrderBy.NONE;
+        }
+    }
+}
---
+++ WebAPI/Models/API/KalturaMetaListResponse.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml.Serialization;
+using WebAPI.Managers.Scheme;
+using WebAPI.Models.Catalog;
+using WebAPI.Models.General;
+
+namespace WebAPI.Models.API
+{
+    /// <summary>
+    /// Meta list response
+    /// </summary>
+    public class KalturaMetaListResponse : KalturaListResponse
+    {
+        /// <summary>
+        /// A list asset meta
+        /// </summary>
+        [DataMember(Name = "objects")]
+        [JsonProperty("objects")]
+        [XmlArray(ElementName = "objects", IsNullable = true)]
+        [XmlArrayItem(ElementName = "item")]
+        public List<KalturaMeta> Objects { get; set; }
+    }
+}
