771fb45e411969b7a12414efebb737d120cd9659
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2016-10-10 17:02:44 +0300

    no message

--- WebAPI/Models/DMS/KalturaConfiguration.cs
+++ WebAPI/Models/DMS/KalturaConfiguration.cs
@@ -75,16 +75,5 @@ namespace WebAPI.Models.DMS
         IllegalPostData = 5,
         Success = 6,
         VersionNotFound = 7
-    }
-
-    public enum KalturaPlatform
-    {
-        Android = 0,
-        iOS = 1,
-        WindowsPhone = 2,
-        Blackberry = 3,
-        STB = 4,
-        CTV = 5,
-        Other = 6
-    }
+    }  
 }
