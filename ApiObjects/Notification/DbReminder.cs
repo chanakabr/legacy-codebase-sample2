78ea1bed46756ad85c275c08cb00f8ac74cb3459
Amishai Guilburd <amishai.guilburd@kaltura.com>
2016-12-08 14:17:58 +0200

    added ReminderDB object

---
+++ ApiObjects/Notification/DbReminder.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ApiObjects.Notification
+{
+    public class DbReminder
+    {
+        public int ID { get; set; }
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public long SendTime { get; set; }
+        public string Phrase { get; set; }
+        public string Reference { get; set; }
+        public int QueueId { get; set; }
+        public string QueueName { get; set; }
+        public bool IsSent { get; set; }
+    }
+}
