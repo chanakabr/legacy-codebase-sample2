0ef6e2998d42cb7711bb00b5c1ae1d6f82680a80
Sunny Simantov <sunny.simantov@kaltura.com>
2016-12-20 16:55:04 +0200

    continue progress

--- EventManager/Events/KalturaObjectChangedEvent.cs
+++ EventManager/Events/KalturaObjectChangedEvent.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,6 +27,49 @@ namespace EventManager.Events
         {
             this.PreviousObject = previousObject;
             this.ChangedFields = changedFields;
+
+            GetChangedFields(newObject, previousObject);
+        }
+
+        private void GetChangedFields(ApiObjects.CoreObject newObject, ApiObjects.CoreObject previousObject)
+        {
+            if (this.ChangedFields == null)
+            {
+                this.ChangedFields = new List<string>();
+
+                var properties = GetProperties(newObject);
+
+                foreach (var property in properties)
+                {
+                    string name = property.Name;
+                    var newValue = property.GetValue(newObject, null);
+                    var oldValue = property.GetValue(previousObject, null);
+
+                    if (newValue != null)
+                    {
+                        if (oldValue != null)
+                        {
+                            if (!newValue.Equals(oldValue))
+                            {
+                                this.ChangedFields.Add(property.Name);
+                            }
+                        }
+                        else
+                        {
+                            this.ChangedFields.Add(property.Name);
+                        }
+                    }
+                    else if (oldValue != null)
+                    {
+                        this.ChangedFields.Add(property.Name);
+                    }
+                }
+            }
+        }
+
+        private static PropertyInfo[] GetProperties(object obj)
+        {
+            return obj.GetType().GetProperties();
         }
     }
 }
