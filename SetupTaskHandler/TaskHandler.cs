141ab367563f251f704faaafb419ce6feb6f16de
Amishai Guilburd <amishai.guilburd@kaltura.com>
2016-12-21 17:26:26 +0200

    fixed success flag on  announcements

--- SetupTaskHandler/TaskHandler.cs
+++ SetupTaskHandler/TaskHandler.cs
@@ -87,7 +87,10 @@ namespace SetupTaskHandler
 
                             var status = service.DeleteAnnouncementsOlderThan(string.Empty, string.Empty);
                             if (status != null && status.Code == (int)ApiObjects.Response.eResponseStatus.OK)
+                            {
                                 log.Debug("NotificationCleanupIteration: Successfully run cleanup notifications");
+                                success = true;
+                            }
                             else
                                 log.Error("NotificationCleanupIteration: Error received when trying to run cleanup notifications");
                         }
