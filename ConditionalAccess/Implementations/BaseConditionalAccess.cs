83dea733430080e36e568f97f2dac426e2d366c8
Lior Faingold <Lior.Faingold@kaltura.com>
2016-12-22 13:58:31 +0200

    add CAS log

--- ConditionalAccess/Implementations/BaseConditionalAccess.cs
+++ ConditionalAccess/Implementations/BaseConditionalAccess.cs
@@ -41,6 +41,7 @@ namespace ConditionalAccess
     public abstract class BaseConditionalAccess
     {
         private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+        private static readonly KLogger casLog = new KLogger("casLogAppender", true);
 
         protected string m_sPurchaseMailTemplate;
         protected string m_sMailFromName;
