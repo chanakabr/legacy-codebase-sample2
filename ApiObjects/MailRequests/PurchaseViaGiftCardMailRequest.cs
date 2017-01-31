afa726a8ab8a7476c7085e2ca8e3ca60566eb49c
Ira Yanai <iray@tvinci.com>
2017-01-31 16:33:18 +0200

    gift card

--- ApiObjects/MailRequests/PurchaseViaGiftCardMailRequest.cs
+++ ApiObjects/MailRequests/PurchaseViaGiftCardMailRequest.cs
@@ -22,54 +22,6 @@ namespace ApiObjects
 
             result.Add(mergeType);
 
-            MCGlobalMergeVars mergeOfferName = new MCGlobalMergeVars()
-            {
-                name = "offerName",
-                content = this.m_sItemName
-            };
-
-            result.Add(mergeOfferName);
-
-            MCGlobalMergeVars mergePrice = new MCGlobalMergeVars()
-            {
-                name = "price",
-                content = this.m_sPrice
-            };
-
-            result.Add(mergePrice);
-
-            MCGlobalMergeVars mergePurchaseDate = new MCGlobalMergeVars()
-            {
-                name = "purchaseDate",
-                content = this.m_sPurchaseDate
-            };
-
-            result.Add(mergePurchaseDate);
-
-            MCGlobalMergeVars mergeEmail = new MCGlobalMergeVars()
-            {
-                name = "email",
-                content = this.m_sSenderTo
-            };
-
-            result.Add(mergeEmail);
-
-            MCGlobalMergeVars mergeFirstName = new MCGlobalMergeVars()
-            {
-                name = "firstName",
-                content = this.m_sFirstName
-            };
-
-            result.Add(mergeFirstName);
-
-            MCGlobalMergeVars mergeLastName = new MCGlobalMergeVars()
-            {
-                name = "lastName",
-                content = this.m_sLastName
-            };
-
-            result.Add(mergeLastName);
-
             return result;
         }
     }
