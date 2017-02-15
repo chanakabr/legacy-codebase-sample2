89a50bfd8cfc47ab64713056e7bc572abf25a12e
Irena Leibman <irena.leibman@kaltura.com>
2017-02-15 12:16:38 +0200

    Changed Fairplay certificate configuration to be by group

--- WebAPI/Managers/Models/Group.cs
+++ WebAPI/Managers/Models/Group.cs
@@ -105,6 +105,9 @@ namespace WebAPI.Managers.Models
         [JsonProperty("media_prep_account_id")]
         public int MediaPrepAccountId { get; set; }
 
+        [JsonProperty("fairplay_certificate")]
+        public string FairplayCertificate { get; set; }
+
         [JsonIgnore]
         public List<Language> Languages { get; set; }
 
