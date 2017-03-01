fc7752ff4309dfb5e1add9e678ec78a6bfe8cb48
Sunny Simantov <sunny.simantov@kaltura.com>
2017-03-01 16:00:05 +0200

    start progress

---
+++ WebAPI/Controllers/AssetCountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebAPI.Models.Catalog;
+using WebAPI.Models.General;
+
+namespace WebAPI.Controllers
+{
+    public class AssetCountController : ApiController
+    {
+        /*
+         Like list of AssetController
+         KalturaAssetInfoFilter
+         KalturaAssetGroupBy
+         * KalturaAssetMetaGroupBy
+         * 
+         Returns: List KalturaAssetCount
+         * Count
+         * String - the value of the meta
+         * 
+         */
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filter">Filtering the assets request</param>
+        /// <param name="pager">Paging the request</param>
+        /// <remarks></remarks>
+        [Route("list"), HttpPost]
+        [ApiAuthorize]
+        public KalturaAssetCountListResponse List(KalturaAssetFilter filter = null, KalturaFilterPager pager = null)
+        {
+            return null;
+        }
+    }
+}
---
+++ WebAPI/Models/Catalog/KalturaAssetCount.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Xml.Serialization;
+using WebAPI.Models.General;
+
+namespace WebAPI.Models.Catalog
+{
+    /// <summary>
+    /// Asset wrapper
+    /// </summary>
+    [Serializable]
+    public class KalturaAssetCountListResponse : KalturaListResponse
+    {
+        /// <summary>
+        /// Assets
+        /// </summary>
+        [DataMember(Name = "objects")]
+        [JsonProperty(PropertyName = "objects")]
+        [XmlArray(ElementName = "objects", IsNullable = true)]
+        [XmlArrayItem("item")]
+        public List<KalturaAssetCount> Objects
+        {
+            get;
+            set;
+        }
+    }
+
+    public class KalturaAssetCount
+    {
+    }
+}
