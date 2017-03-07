8c2cfdd5ffdc40dcfa9dcec0f2aa6d4559877e80
Sunny Simantov <sunny.simantov@kaltura.com>
2017-03-07 15:39:45 +0200

    start progress

---
+++ ActionRuleHandler/ActionRuleRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using ApiObjects.Json.Converters;
+
+namespace ActionRuleHandler
+{
+    [Serializable]
+    public class ActionRuleRequest
+    {
+        [JsonProperty("group_id")]
+        public int GroupId
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("rule_ids")]
+        public List<int> RuleIds
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ActionRuleHandler/TaskHandler.cs
@@ -0,0 +1,37 @@
+﻿using KLogMonitor;
+using Newtonsoft.Json;
+using RemoteTasksCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ActionRuleHandler
+{
+    public class TaskHandler : ITaskHandler
+    {
+        private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+
+        public string HandleTask(string data)
+        {
+            string result = "failure";
+
+            try
+            {
+                log.DebugFormat("starting action rule request. data={0}", data);
+
+                ActionRuleRequest request = JsonConvert.DeserializeObject<ActionRuleRequest>(data);
+                
+            }
+            catch (Exception ex)
+            {
+                log.Error(string.Format("Failed performing action rule request. data = {0} message = {1}, stack trace= {2}, target site = {3}",
+                    data, ex.Message, ex.StackTrace, ex.TargetSite), ex);
+                throw ex;
+            }
+
+            return result;
+        }
+    }
+}
