cbd85f3a4db73fec1181ec79801de645706eda4c
Irena Leibman <irena.leibman@kaltura.com>
2017-03-27 17:46:55 +0300

    BEO-3461

--- WebAPI/ObjectsConvertor/Mapping/CatalogMappings.cs
+++ WebAPI/ObjectsConvertor/Mapping/CatalogMappings.cs
@@ -147,8 +147,8 @@ namespace WebAPI.ObjectsConvertor.Mapping
             //Media to SlimAssetInfo
             Mapper.CreateMap<ProgramObj, KalturaBaseAssetInfo>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.AssetId))
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.m_oProgram.ProgrammeName))
-                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.m_oProgram.ProgrammeDescription))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => new KalturaMultilingualString(src.m_oProgram.ProgrammeName)))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => new KalturaMultilingualString(src.m_oProgram.ProgrammeDescription)))
                 .ForMember(dest => dest.Type, opt => opt.MapFrom(src => (int)AssetType.epg));
 
             //channelObj to Channel
