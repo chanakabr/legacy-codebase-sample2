d371f5960eea840d7f019a861f40c184590ab4b7
Anat Grossberg <Anat.Grossberg@Kaltura.com>
2017-03-28 13:05:49 +0300

    BEO-3427 Sign in - message about blocking account appear after enter correct password

--- Logic/Users/User.cs
+++ Logic/Users/User.cs
@@ -1113,8 +1113,12 @@ namespace Core.Users
                         }
                         else
                         {
-                            ret = ResponseStatus.WrongPasswordOrUserName;
                             UpdateFailCount(1, nID);
+
+                            if (nFailCount >= nMaxFailCount && ((TimeSpan)(dNow - dLastFailDate)).TotalMinutes < nLockMinutes)
+                                ret = ResponseStatus.InsideLockTime;
+                            else
+                                ret = ResponseStatus.WrongPasswordOrUserName;
                         }
                     }
                     else
