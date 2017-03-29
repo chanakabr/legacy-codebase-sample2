786245f65136fa09a8d9ff1a10664132098d274c
Liat Regev <liatr@tvinci.com>
2017-03-29 12:33:05 +0300

    missing one case

--- Logic/Api/api.cs
+++ Logic/Api/api.cs
@@ -8292,7 +8292,7 @@ namespace Core.Api
                                          && settings.RecordingScheduleWindow == null && settings.PaddingBeforeProgramStarts == null && settings.PaddingAfterProgramEnds == null
                                          && settings.ProtectionPeriod == null && settings.ProtectionQuotaPercentage == null && settings.IsSeriesRecordingEnabled == null
                                          && settings.IsRecordingPlaybackNonEntitledChannelEnabled == null && settings.IsRecordingPlaybackNonExistingChannelEnabled == null
-                                         && !settings.QuotaOveragePolicy.HasValue && !settings.ProtectionPolicy.HasValue ))
+                                         && !settings.QuotaOveragePolicy.HasValue && !settings.ProtectionPolicy.HasValue && !settings.RecoveryGracePeriod.HasValue))
                 {
                     response.Code = (int)ApiObjects.Response.eResponseStatus.TimeShiftedTvPartnerSettingsNotSent;
                     response.Message = ApiObjects.Response.eResponseStatus.TimeShiftedTvPartnerSettingsNotSent.ToString();
