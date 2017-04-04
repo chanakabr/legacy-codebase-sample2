9436fd49a192aa4b3a2fdc7e03409db19485fd16
Liat Regev <liatr@tvinci.com>
2017-04-04 09:06:06 +0300

    change social privacy setting - per action with default

--- ApiObjects/Social/SocialPrivacySettings.cs
+++ ApiObjects/Social/SocialPrivacySettings.cs
@@ -10,16 +10,18 @@ namespace ApiObjects.Social
     {
         public List<SocialNetwork> SocialNetworks { get; set; }
 
-        public eSocialActionPrivacy InternalPrivacy { get; set; }
+        public List<SocialActionPrivacy> InternalPrivacy { get; set; }
 
         public SocialPrivacySettings()
         {
-            InternalPrivacy = eSocialActionPrivacy.ALLOW; //Default value : Enabled
+            InternalPrivacy = new List<SocialActionPrivacy>();
         }
 
         public static SocialPrivacySettings SetDefultPrivacySettings()
         {
             SocialPrivacySettings settings = new SocialPrivacySettings();
+
+            settings.InternalPrivacy = SetDefaultInternalPrivacy();
 
             settings.SocialNetworks = new List<SocialNetwork>();
             foreach (SocialPlatform platform in Enum.GetValues(typeof(SocialPlatform)))
@@ -28,7 +30,7 @@ namespace ApiObjects.Social
                      new SocialNetwork
                      {
                          Network = platform,
-                         Privacy = eSocialActionPrivacy.DONT_ALLOW,
+                         SocialAction = SetDefaultNetworkPlatformPrivacy(),
                          SocialPrivacy = eSocialPrivacy.SELF
                      }
                      );
@@ -41,24 +43,55 @@ namespace ApiObjects.Social
             settings.SocialNetworks = new List<SocialNetwork>();
             foreach (SocialPlatform platform in Enum.GetValues(typeof(SocialPlatform)))
             {
+               
                 settings.SocialNetworks.Add(
                      new SocialNetwork
                      {
                          Network = platform,
-                         Privacy = eSocialActionPrivacy.DONT_ALLOW,
+                         SocialAction  = SetDefaultNetworkPlatformPrivacy(),
                          SocialPrivacy = eSocialPrivacy.SELF
                      }
                      );
             }
+        }
+        
+        private static List<SocialActionPrivacy> SetDefaultInternalPrivacy()
+        {
+            List<SocialActionPrivacy> internalPrivacy = new List<SocialActionPrivacy>();
+            foreach (eUserAction action in Enum.GetValues(typeof(eUserAction)))
+            {
+                SocialActionPrivacy socialActionPrivacy = new SocialActionPrivacy()
+                {
+                    Action = action,
+                    Privacy = action == eUserAction.WATCHES ? eSocialActionPrivacy.DONT_ALLOW : eSocialActionPrivacy.ALLOW
+                };
+                internalPrivacy.Add(socialActionPrivacy);
+            }            
+            return internalPrivacy;
+        }
+
+        private static List<SocialActionPrivacy> SetDefaultNetworkPlatformPrivacy()
+        {
+            List<SocialActionPrivacy> socialNetwork = new List<SocialActionPrivacy>();
+            foreach (eUserAction action in Enum.GetValues(typeof(eUserAction)))
+            {
+                SocialActionPrivacy socialActionPrivacy = new SocialActionPrivacy()
+                {
+                    Action = action,
+                    Privacy = eSocialActionPrivacy.DONT_ALLOW
+                };
+                socialNetwork.Add(socialActionPrivacy);
+            }
+            return socialNetwork;
         }
 
         public override string ToString()
         {
             string res = string.Empty;
 
-            res = string.Format("InternalPrivacy : {0}", InternalPrivacy.ToString());
+            res = string.Format("InternalPrivacy : {0}", string.Join(",",InternalPrivacy.Select(x=>x.ToString()).ToList<string>()));
             res = string.Format("{0}, SocialNetworks: {1}, ", res, SocialNetworks!= null? 
-                string.Join(",", SocialNetworks.SelectMany(o => o.Network + " - " + o.Privacy.ToString()+" -" +o.SocialPrivacy.ToString()).ToList()) : string.Empty);
+                string.Join(",", SocialNetworks.SelectMany(o => o.Network + " - " + o.SocialAction.ToString()+" -" +o.SocialPrivacy.ToString()).ToList()) : string.Empty);
 
             return res;
         }
