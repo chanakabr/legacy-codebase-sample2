6bc78868c8225ec11a57219f803be6682c858487
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2017-04-05 12:43:32 +0300

    BEO-3463 ottuser - unactivated user state returns 0 "ok" should be "not active" change IsUserActivated

--- Libs/TVPApiModule/Services/ApiUsersService.cs
+++ Libs/TVPApiModule/Services/ApiUsersService.cs
@@ -192,7 +192,7 @@ namespace TVPApiModule.Services
             {
                 using (KMonitor km = new KMonitor(KLogMonitor.Events.eEvent.EVENT_WS, null, null, null, null))
                 {
-                    var res = m_Module.GetUserFavorites(m_wsUserName, m_wsPassword, sSiteGuid, iDomainID, string.Empty, sItemType);
+                    var res = m_Module.GetUserFavorites(m_wsUserName, m_wsPassword, sSiteGuid, iDomainID, string.Empty, sItemType,  FavoriteOrderBy.CreateDateAsc);
                     response = res.Favorites;
                 }
             }
@@ -868,7 +868,7 @@ namespace TVPApiModule.Services
                 UserItemList userItemList = new UserItemList()
                 {
                     itemObj = itemObjects,
-                    itemType = itemType,
+                    itemType = ConvertItemTypeToListItemType(itemType),
                     listType = listType,
                     siteGuid = siteGuid
 
@@ -897,7 +897,7 @@ namespace TVPApiModule.Services
                 UserItemList userItemList = new UserItemList()
                 {
                     itemObj = itemObjects,
-                    itemType = itemType,
+                    itemType = ConvertItemTypeToListItemType(itemType),
                     listType = listType,
                     siteGuid = siteGuid
 
@@ -926,7 +926,7 @@ namespace TVPApiModule.Services
                 UserItemList userItemList = new UserItemList()
                 {
                     itemObj = itemObjects,
-                    itemType = itemType,
+                    itemType = ConvertItemTypeToListItemType(itemType),
                     listType = listType,
                     siteGuid = siteGuid
 
@@ -955,7 +955,7 @@ namespace TVPApiModule.Services
                 UserItemList userItemList = new UserItemList()
                 {
                     itemObj = itemObjects,
-                    itemType = itemType,
+                    itemType = ConvertItemTypeToListItemType(itemType),
                     listType = listType,
                     siteGuid = siteGuid
 
@@ -986,7 +986,7 @@ namespace TVPApiModule.Services
                 UserItemList userItemList = new UserItemList()
                 {
                     itemObj = itemObjects,
-                    itemType = itemType,
+                    itemType = ConvertItemTypeToListItemType(itemType),
                     listType = listType,
                     siteGuid = siteGuid
 
@@ -1004,6 +1004,20 @@ namespace TVPApiModule.Services
             }
 
             return res;
+        }
+
+        private ListItemType ConvertItemTypeToListItemType(ItemType itemType)
+        {
+            switch (itemType)
+            {
+                case ItemType.All:
+                    return ListItemType.All;
+                case ItemType.Media:
+                    return ListItemType.Media;
+                default:
+                    logger.ErrorFormat("Unknown itemType: {0}", itemType.ToString());
+                    throw new Exception(string.Format("Unknown itemType: {0}", itemType.ToString()));
+            }
         }
 
         public ResponseStatus SetUserTypeByUserID(string sSiteGuid, int userTypeID)
@@ -1192,6 +1206,14 @@ namespace TVPApiModule.Services
 
             return clientResponse;
         }
+    }
+}
 
+namespace TVPPro.SiteManager.TvinciPlatform.Users
+{
+    public enum ItemType
+    {
+        All = 0,
+        Media = 2
     }
 }
