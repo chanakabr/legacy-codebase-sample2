fdd3a579da71dcfbb514f18b1939972435a87550
Lior Faingold <Lior.Faingold@kaltura.com>
2017-04-09 10:10:20 +0300

    fix BEO-3258

--- ConditionalAccess/Implementations/BaseConditionalAccess.cs
+++ ConditionalAccess/Implementations/BaseConditionalAccess.cs
@@ -15846,6 +15846,13 @@ namespace ConditionalAccess
                     return response;
                 }
 
+                if (!IsServiceAllowed((int)domainId, eService.NPVR))
+                {
+                    log.DebugFormat("Premium Service not allowed, DomainID: {0}, UserID: {1}, Service: {2}", domainId, userId, eService.NPVR.ToString());
+                    response.Status = new ApiObjects.Response.Status((int)eResponseStatus.ServiceNotAllowed, "Service not allowed");
+                    return response;
+                }
+
                 MediaFileItemPricesContainer price = null;
                 bool isItemPurchased = false;
                 // validate entitlements if needed 
