36085039d3d260e03d13adabf652b6af5e364ccb
Amishai Guilburd <amishai.guilburd@kaltura.com>
2017-04-24 18:02:29 +0300

    Added engagement logic

--- Logic/Notification/Adapters/EngagementAdapterClient.cs
+++ Logic/Notification/Adapters/EngagementAdapterClient.cs
@@ -19,41 +19,41 @@ namespace APILogic.Notification.Adapters
         {
             try
             {
-                if (engagementAdapter != null && !string.IsNullOrEmpty(engagementAdapter.ProviderUrl))
+                if (engagementAdapter == null || string.IsNullOrEmpty(engagementAdapter.AdapterUrl))
                 {
-                    //set unixTimestamp
-                    long unixTimestamp = ODBCWrapper.Utils.DateTimeToUnixTimestamp(DateTime.UtcNow);
+                    log.ErrorFormat("Adapter URL was not found. group ID: {0}, adapter: {1}",
+                        groupId,
+                        engagementAdapter != null ? JsonConvert.SerializeObject(engagementAdapter) : "null");
+                    return false;
+                }
 
-                    //set signature
-                    string signature = string.Concat(engagementAdapter.ID, engagementAdapter.Settings != null ? string.Concat(engagementAdapter.Settings.Select(s => string.Concat(s.Key, s.Value))) : string.Empty,
-                        groupId, unixTimestamp);
+                //set unixTimestamp
+                long unixTimestamp = ODBCWrapper.Utils.DateTimeToUnixTimestamp(DateTime.UtcNow);
 
-                    using (APILogic.EngagementAdapterService.ServiceClient client = new APILogic.EngagementAdapterService.ServiceClient(string.Empty, engagementAdapter.ProviderUrl))
+                //set signature
+                string signature = string.Concat(engagementAdapter.ID, engagementAdapter.Settings != null ? string.Concat(engagementAdapter.Settings.Select(s => string.Concat(s.Key, s.Value))) : string.Empty,
+                    groupId, unixTimestamp);
+
+                using (APILogic.EngagementAdapterService.ServiceClient client = new APILogic.EngagementAdapterService.ServiceClient(string.Empty, engagementAdapter.AdapterUrl))
+                {
+                    APILogic.EngagementAdapterService.AdapterStatus adapterResponse = client.SetConfiguration(
+                        engagementAdapter.ID,
+                        engagementAdapter.ProviderUrl,
+                        engagementAdapter.Settings != null ? engagementAdapter.Settings.Select(s => new APILogic.EngagementAdapterService.KeyValue() { Key = s.Key, Value = s.Value }).ToArray() : null,
+                        groupId,
+                        unixTimestamp,
+                        System.Convert.ToBase64String(TVinciShared.EncryptUtils.AesEncrypt(engagementAdapter.SharedSecret, TVinciShared.EncryptUtils.HashSHA1(signature))));
+
+                    if (adapterResponse != null && adapterResponse.Code == (int)EngagementAdapterStatus.OK)
                     {
-                        if (!string.IsNullOrEmpty(engagementAdapter.ProviderUrl))
-                        {
-                            client.Endpoint.Address = new System.ServiceModel.EndpointAddress(engagementAdapter.ProviderUrl);
-                        }
-
-                        APILogic.EngagementAdapterService.AdapterStatus adapterResponse = client.SetConfiguration(
-                            engagementAdapter.ID,
-                            engagementAdapter.ProviderUrl,
-                            engagementAdapter.Settings != null ? engagementAdapter.Settings.Select(s => new APILogic.EngagementAdapterService.KeyValue() { Key = s.Key, Value = s.Value }).ToArray() : null,
-                            groupId,
-                            unixTimestamp,
-                            System.Convert.ToBase64String(TVinciShared.EncryptUtils.AesEncrypt(engagementAdapter.SharedSecret, TVinciShared.EncryptUtils.HashSHA1(signature))));
-
-                        if (adapterResponse != null && adapterResponse.Code == (int)EngagementAdapterStatus.OK)
-                        {
-                            log.DebugFormat("Engagement Adapter SetConfiguration Result: AdapterID = {0}, AdapterStatus = {1}", engagementAdapter.ID, adapterResponse.Code);
-                            return true;
-                        }
-                        else
-                        {
-                            log.ErrorFormat("Engagement Adapter SetConfiguration Result: AdapterID = {0}, AdapterStatus = {1}",
-                                engagementAdapter.ID, adapterResponse != null ? adapterResponse.Code.ToString() : "ERROR");
-                            return false;
-                        }
+                        log.DebugFormat("Successfully set configuration of engagement adapter. Result: AdapterID = {0}, AdapterStatus = {1}", engagementAdapter.ID, adapterResponse.Code);
+                        return true;
+                    }
+                    else
+                    {
+                        log.ErrorFormat("Failed to set engagement Adapter configuration. Result: AdapterID = {0}, AdapterStatus = {1}",
+                            engagementAdapter.ID, adapterResponse != null ? adapterResponse.Code.ToString() : "ERROR");
+                        return false;
                     }
                 }
             }
@@ -72,31 +72,36 @@ namespace APILogic.Notification.Adapters
             try
             {
                 // validate adapter exists
-                if (engagementAdapter != null && !string.IsNullOrEmpty(engagementAdapter.ProviderUrl))
+                if (engagementAdapter == null || string.IsNullOrEmpty(engagementAdapter.AdapterUrl))
                 {
-                    long unixTimeNow = ODBCWrapper.Utils.DateTimeToUnixTimestamp(DateTime.UtcNow);
+                    log.ErrorFormat("Adapter URL was not found. group ID: {0}, adapter: {1}",
+                        groupId,
+                        engagementAdapter != null ? JsonConvert.SerializeObject(engagementAdapter) : "null");
+                    return null;
+                }
 
-                    // set signature
-                    string signature = string.Concat(engagementAdapter.ID, adapterDynamicData, unixTimeNow);
+                long unixTimeNow = ODBCWrapper.Utils.DateTimeToUnixTimestamp(DateTime.UtcNow);
 
-                    using (APILogic.EngagementAdapterService.ServiceClient client = new APILogic.EngagementAdapterService.ServiceClient(string.Empty, engagementAdapter.ProviderUrl))
+                // set signature
+                string signature = string.Concat(engagementAdapter.ID, adapterDynamicData, unixTimeNow);
+
+                using (APILogic.EngagementAdapterService.ServiceClient client = new APILogic.EngagementAdapterService.ServiceClient(string.Empty, engagementAdapter.AdapterUrl))
+                {
+                    // get list
+                    adapterResponse = client.GetList(engagementAdapter.ID, adapterDynamicData, unixTimeNow, signature);
+                    if (adapterResponse != null && adapterResponse.Status != null && adapterResponse.Status.Code == (int)EngagementAdapterStatus.OK)
                     {
-                        // get list
-                        adapterResponse = client.GetList(engagementAdapter.ID, adapterDynamicData, unixTimeNow, signature);
-                        if (adapterResponse != null && adapterResponse.Status != null && adapterResponse.Status.Code == (int)EngagementAdapterStatus.OK)
-                        {
-                            log.DebugFormat("Successfully received engagement Adapter List Result: AdapterID = {0}, AdapterStatus = {1}, number of results: {2}",
-                                            engagementAdapter.ID,
-                                            ((EngagementAdapterStatus)adapterResponse.Status.Code).ToString(),
-                                            adapterResponse.UserIds != null ? adapterResponse.UserIds.Count() : 0);
-                        }
-                        else
-                        {
-                            log.ErrorFormat("Engagement Adapter GetList error. Adapter: {0}, result: {1}",
-                                     JsonConvert.SerializeObject(engagementAdapter),
-                                     adapterResponse != null ? JsonConvert.SerializeObject(adapterResponse) : "null");
-                            return null;
-                        }
+                        log.DebugFormat("Successfully received engagement Adapter List Result: AdapterID = {0}, AdapterStatus = {1}, number of results: {2}",
+                                        engagementAdapter.ID,
+                                        ((EngagementAdapterStatus)adapterResponse.Status.Code).ToString(),
+                                        adapterResponse.UserIds != null ? adapterResponse.UserIds.Count() : 0);
+                    }
+                    else
+                    {
+                        log.ErrorFormat("Engagement Adapter GetList error. Adapter: {0}, result: {1}",
+                                 JsonConvert.SerializeObject(engagementAdapter),
+                                 adapterResponse != null ? JsonConvert.SerializeObject(adapterResponse) : "null");
+                        return null;
                     }
                 }
             }
