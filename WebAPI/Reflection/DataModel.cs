aa641d5741617ee227e9d5e915a1628f4022fd79
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2017-04-25 13:30:41 +0300

    BEO-3478 UserChurnNotificationType  - changeeOttAssetType to MessageTemplateTYpe

--- WebAPI/Reflection/DataModel.cs
+++ WebAPI/Reflection/DataModel.cs
@@ -519,6 +519,19 @@ namespace WebAPI.Reflection
                                 ret.Add("messageType", "assetType");
                             }
                             break;
+                        case "Update":
+                            ret = new Dictionary<string, string>() { 
+                                 {"messageType", "asset_Type"},
+                            };
+                            if (currentVersion != null && currentVersion.CompareTo(new Version("3.6.2094.15157")) < 0 && currentVersion.CompareTo(new Version(OldStandardAttribute.Version)) > 0)
+                            {
+                                if (ret.ContainsKey("messageType"))
+                                {
+                                    ret.Remove("messageType");
+                                }
+                                ret.Add("messageType", "assetType");
+                            }
+                            break;
                     }
                     break;
                     
