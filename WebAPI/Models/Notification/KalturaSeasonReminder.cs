cf7f5a7554cb451eab362d6fde8ff7ea6eafea38
Irena Leibman <irena.leibman@kaltura.com>
2017-04-30 16:22:56 +0300

    new objects

---
+++ WebAPI/Models/Notification/KalturaSeasonReminder.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace WebAPI.Models.Notifications
+{
+    [Serializable]
+    public class KalturaSeasonReminder : KalturaReminder
+    {
+        /// <summary>
+        /// Series identifier
+        /// </summary>
+        [DataMember(Name = "seriesId")]
+        [JsonProperty(PropertyName = "seriesId")]
+        [XmlElement(ElementName = "seriesId")]
+        public long SeriesId { get; set; }
+
+        /// <summary>
+        /// Season identifier
+        /// </summary>
+        [DataMember(Name = "seasonId")]
+        [JsonProperty(PropertyName = "seasonId")]
+        [XmlElement(ElementName = "seasonId")]
+        public long SeasonId { get; set; }
+    }    
+}
---
+++ WebAPI/Models/Notification/KalturaSeriesReminder.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace WebAPI.Models.Notifications
+{
+    [Serializable]
+    public class KalturaSeriesReminder : KalturaReminder
+    {
+        /// <summary>
+        /// Series identifier
+        /// </summary>
+        [DataMember(Name = "seriesId")]
+        [JsonProperty(PropertyName = "seriesId")]
+        [XmlElement(ElementName = "seriesId")]
+        public long SeriesId { get; set; }
+    }    
+}
