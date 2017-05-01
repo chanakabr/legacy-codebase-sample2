3495dd3cd8f34c92952ed5794db0849096bd6901
Amishai Guilburd <amishai.guilburd@kaltura.com>
2017-05-01 10:00:24 +0300

    Added log to engagement task

--- EngagementHandler/TaskHandler.cs
+++ EngagementHandler/TaskHandler.cs
@@ -27,10 +27,16 @@ namespace EngagementHandler
 
                 bool success = false;
                 if (request.EngagementBulkId > 0)
+                {
+                    log.DebugFormat("Calling SendEngagementBulk in remote tasks. Engagement ID: {0}, Engagement Bulk ID :{1}", request.EngagementId, request.EngagementBulkId);
                     success = Core.Notification.Module.SendEngagementBulk(request.GroupId, request.EngagementId, request.EngagementBulkId, request.StartTime);
+                }
                 else
+                {
+                    log.DebugFormat("Calling SendEngagement message in remote tasks. Engagement ID: {0}", request.EngagementId, request.EngagementBulkId);
                     success = Core.Notification.Module.SendEngagement(request.GroupId, request.EngagementId, request.StartTime);
-                    
+                }
+
 
                 if (!success)
                     throw new Exception(string.Format("Engagement did not finish successfully. Data: {0}", data));
