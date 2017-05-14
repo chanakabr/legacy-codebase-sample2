31458bfd47c39044c358a32115162528fa2036ad
Amishai Guilburd <amishai.guilburd@kaltura.com>
2017-05-14 10:29:40 +0300

    Changed back initiate push action so it will work through Rabbit

--- Logic/Users/User.cs
+++ Logic/Users/User.cs
@@ -848,7 +848,7 @@ namespace Core.Users
 
             if (retVal.m_RespStatus == ResponseStatus.SessionLoggedOut)
             {
-                Utils.AddInitiateNotificationAction(nGroupID, eUserMessageAction.Logout, siteGuid, sDeviceUDID);
+                Utils.AddInitiateNotificationActionToQueue(nGroupID, eUserMessageAction.Logout, siteGuid, sDeviceUDID);
             }
             else
                 log.ErrorFormat("SignOut: error while logging user out: user: {0}, group: {1}, error: {2}", siteGuid, nGroupID, retVal.m_RespStatus);
