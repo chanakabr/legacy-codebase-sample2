2eeb96669204f1d0125ef7c7c4735044fc93296a
Amishai Guilburd <amishai.guilburd@kaltura.com>
2017-05-25 11:45:52 +0300

    added interests notification objects

---
+++ ApiObjects/Notification/InterestNotificationMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiObjects.Notification
+{
+    public class InterestNotificationMessage
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime SendTime { get; set; }
+
+        public string TopicInterestsNotificationsId { get; set; }
+    }
+}
