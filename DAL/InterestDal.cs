7362e77e31fac4205f163d59b70b345305ead5de
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2017-05-28 12:01:41 +0300

    BEO-3517 personal channels based on user interests CB

--- DAL/InterestDal.cs
+++ DAL/InterestDal.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using ApiObjects;
+﻿using ApiObjects;
 using ApiObjects.Notification;
 using CouchbaseManager;
 using KLogMonitor;
+using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Reflection;
+using System.Threading;
 
 namespace DAL
 {
@@ -20,6 +18,7 @@ namespace DAL
         private const string CB_DESIGN_DOC_ENGAGEMENT = "interests";
         private const int SLEEP_BETWEEN_RETRIES_MILLI = 1000;
         private const int NUM_OF_TRIES = 3;
+        private const int TTL_USER_INTEREST_DAYS = 30;       
 
         public static bool DeleteTopicInterestNotification(int groupId, long id)
         {
@@ -88,5 +87,108 @@ namespace DAL
                 TemplateType = Enum.IsDefined(typeof(MessageTemplateType), templateType) ? (MessageTemplateType)templateType : MessageTemplateType.None
             };
         }
+
+        private static string GetUserInterestKey(int partnerId, int userId)
+        {
+            return string.Format("user_interests:{0}:{1}", partnerId, userId);
+        }
+
+        public static bool SetUserInterest(UserInterest userInterest)
+        {
+            bool result = false;
+            try
+            {
+                // get user interest TTL
+                int userInterestTtl = TCMClient.Settings.Instance.GetValue<int>("ttl_user_interest_days");
+                if (userInterestTtl == 0)
+                    userInterestTtl = TTL_USER_INTEREST_DAYS;
+
+                int numOfTries = 0;
+                while (!result && numOfTries < NUM_OF_TRIES)
+                {
+                    result = cbManager.Set(GetUserInterestKey(userInterest.PartnerId, userInterest.UserId), userInterest, (uint)TimeSpan.FromDays(userInterestTtl).TotalSeconds);
+                    if (!result)
+                    {
+                        numOfTries++;
+                        log.ErrorFormat("Error while setting user interest document. number of tries: {0}/{1}. User interest object: {2}",
+                             numOfTries,
+                            NUM_OF_TRIES,
+                            JsonConvert.SerializeObject(userInterest));
+
+                        Thread.Sleep(SLEEP_BETWEEN_RETRIES_MILLI);
+                    }
+                    else
+                    {
+                        // log success on retry
+                        if (numOfTries > 0)
+                        {
+                            numOfTries++;
+                            log.DebugFormat("successfully set user interest document. number of tries: {0}/{1}. User interest object {2}",
+                            numOfTries,
+                            NUM_OF_TRIES,
+                            JsonConvert.SerializeObject(userInterest));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Error while setting user interest document.  User interest object: {0}, ex: {1}", JsonConvert.SerializeObject(userInterest), ex);
+            }
+
+            return result;
+        }
+
+        public static UserInterest GetUserInterest(int partnerId, int userId)
+        {
+            UserInterest userInterest = null;
+            Couchbase.IO.ResponseStatus status = Couchbase.IO.ResponseStatus.None;
+            string key = GetUserInterestKey(partnerId, userId);
+
+            try
+            {
+                bool result = false;
+                int numOfTries = 0;
+                while (!result && numOfTries < NUM_OF_TRIES)
+                {
+                    userInterest = cbManager.Get<UserInterest>(key, out status);
+                    if (userInterest == null)
+                    {
+                        if (status != Couchbase.IO.ResponseStatus.KeyNotFound)
+                        {
+                            numOfTries++;
+                            log.ErrorFormat("Error while getting user interest data. number of tries: {0}/{1}. key: {2}",
+                                numOfTries,
+                                NUM_OF_TRIES,
+                                key);
+
+                            Thread.Sleep(SLEEP_BETWEEN_RETRIES_MILLI);
+                        }
+                    }
+                    else
+                    {
+                        result = true;
+
+                        // log success on retry
+                        if (numOfTries > 0)
+                        {
+                            numOfTries++;
+                            log.DebugFormat("successfully received user interest data. number of tries: {0}/{1}. key {2}",
+                            numOfTries,
+                            NUM_OF_TRIES,
+                            key);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Error while trying to get user interest data. key: {0}, ex: {1}", key, ex);
+            }
+
+            return userInterest;
+        }
+
+    
     }
 }
