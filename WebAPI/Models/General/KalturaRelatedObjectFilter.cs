c42aee0af45fb4600242c23714e74ca4a6bcaaba
tan-tan-kanarek <Jonathan.Kanarek@Kaltura.com>
2017-07-26 14:56:19 +0300

    generate interfaces in XML

--- WebAPI/Models/General/KalturaRelatedObjectFilter.cs
+++ WebAPI/Models/General/KalturaRelatedObjectFilter.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebAPI.Managers.Scheme;
 
 namespace WebAPI.Models.General
 {
+    enum KalturaDummyOrderBy
+    {
+    }
+
     /// <summary>
     /// Define KalturaRelatedObjectFilter
     /// </summary>
-    public interface KalturaRelatedObjectFilter
+    [SchemeBase(typeof(KalturaFilter<KalturaDummyOrderBy>))]
+    public interface KalturaRelatedObjectFilter : IKalturaFilter
     {
         // no implementation 
     }
