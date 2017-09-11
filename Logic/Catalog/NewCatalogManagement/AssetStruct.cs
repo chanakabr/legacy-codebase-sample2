4f2d7b5d24fa818ce82a40ba932de7150a8925fd
Lior Faingold <Lior.Faingold@Kaltura.com>
2017-09-11 19:32:20 +0300

    continue working on BEO-4158

--- Logic/Catalog/NewCatalogManagement/AssetStruct.cs
+++ Logic/Catalog/NewCatalogManagement/AssetStruct.cs
@@ -12,7 +12,7 @@ namespace Core.Catalog.NewCatalogManagement
         public string Name { get; set; }
         public string SystemName { get; set; }
         public List<long> MetaIds { get; set; }
-        public bool  IsPredefined { get; set; }
+        public bool? IsPredefined { get; set; }
         public long CreateDate { get; set; }
         public long UpdateDate { get; set; }
 
@@ -44,7 +44,7 @@ namespace Core.Catalog.NewCatalogManagement
             sb.AppendFormat("Name: {0}, ", Name);
             sb.AppendFormat("SystemName: {0}, ", SystemName);
             sb.AppendFormat("MetaIds: {0}, ", MetaIds != null ? string.Join(",", MetaIds) : string.Empty);
-            sb.AppendFormat("IsPredefined: {0}, ", IsPredefined);
+            sb.AppendFormat("IsPredefined: {0}, ", IsPredefined.HasValue ? IsPredefined.Value.ToString() : string.Empty);
             sb.AppendFormat("CreateDate: {0}, ", CreateDate);
             sb.AppendFormat("UpdateDate: {0}", UpdateDate);
 
--- Logic/Catalog/NewCatalogManagement/CatalogManager.cs
+++ Logic/Catalog/NewCatalogManagement/CatalogManager.cs
@@ -15,8 +15,87 @@ namespace Core.Catalog.NewCatalogManagement
     {
         private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
 
-        private static void CreateAssetStructResponseFromDataSet(AssetStructListResponse response, DataSet ds)
+        private static Status CreateAssetStructResponseStatusFromResult(long result)
         {
+            Status responseStatus = new Status((int)eResponseStatus.Error, eResponseStatus.Error.ToString());
+            switch (result)
+            {
+                case -111:
+                    responseStatus = new Status((int)eResponseStatus.AssetStructNameAlreadyInUse, eResponseStatus.AssetStructNameAlreadyInUse.ToString());
+                    break;
+                case -222:
+                    responseStatus = new Status((int)eResponseStatus.AssetStructSystemNameAlreadyInUse, eResponseStatus.AssetStructSystemNameAlreadyInUse.ToString());
+                    break;
+                case -333:
+                    responseStatus = new Status((int)eResponseStatus.AssetStructDoesNotExist, eResponseStatus.AssetStructDoesNotExist.ToString());
+                    break;
+                default:
+                    break;
+            }
+
+            return responseStatus;
+        }
+
+        private static AssetStruct CreateAssetStructFromIdAndDataRow(long id, DataRow dr)
+        {
+            AssetStruct result = null;
+            if (id > 0)
+            {
+                string name = ODBCWrapper.Utils.GetSafeStr(dr, "DESCRIPTION");
+                string systemName = ODBCWrapper.Utils.GetSafeStr(dr, "NAME");
+                bool isPredefined = ODBCWrapper.Utils.ExtractBoolean(dr, "IS_BASIC");
+                DateTime? createDate = ODBCWrapper.Utils.GetNullableDateSafeVal(dr, "CREATE_DATE");
+                DateTime? updateDate = ODBCWrapper.Utils.GetNullableDateSafeVal(dr, "UPDATE_DATE");
+                result = new AssetStruct(id, name, systemName, isPredefined, createDate.HasValue ? ODBCWrapper.Utils.DateTimeToUnixTimestampUtc(createDate.Value) : 0,
+                                         updateDate.HasValue ? ODBCWrapper.Utils.DateTimeToUnixTimestampUtc(updateDate.Value) : 0);
+            }
+
+            return result;
+        }
+
+        private static AssetStructResponse CreateAssetStructResponseFromDataSet(DataSet ds)
+        {
+            AssetStructResponse response = new AssetStructResponse();
+            if (ds != null && ds.Tables != null && ds.Tables.Count > 0)
+            {                
+                DataTable dt = ds.Tables[0];
+                if (dt != null && dt.Rows != null && dt.Rows.Count == 1)
+                {
+                    long id = ODBCWrapper.Utils.GetLongSafeVal(dt.Rows[0], "ID", 0);
+                    if (id > 0)
+                    {
+                        response.AssetStruct = CreateAssetStructFromIdAndDataRow(id, dt.Rows[0]);                        
+                    }
+                    else
+                    {
+                        response.Status = CreateAssetStructResponseStatusFromResult(id);
+                    }
+                }
+
+                if (response.AssetStruct != null && ds.Tables.Count == 2)
+                {
+                    DataTable metasDt = ds.Tables[1];
+                    if (response.AssetStruct != null && metasDt != null && metasDt.Rows != null && metasDt.Rows.Count > 0)
+                    {
+                        foreach (DataRow dr in metasDt.Rows)
+                        {
+                            long assetStructId = ODBCWrapper.Utils.GetLongSafeVal(dr, "TEMPLATE_ID", 0);
+                            long metaId = ODBCWrapper.Utils.GetLongSafeVal(dr, "TOPIC_ID", 0);
+                            if (!response.AssetStruct.MetaIds.Contains(metaId))
+                            {
+                                response.AssetStruct.MetaIds.Add(metaId);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return response;
+        }
+
+        private static AssetStructListResponse CreateAssetStructListResponseFromDataSet(DataSet ds)
+        {
+            AssetStructListResponse response = new AssetStructListResponse();
             if (ds != null && ds.Tables != null && ds.Tables.Count == 2)
             {
                 Dictionary<long, AssetStruct> idToAssetStructMap = new Dictionary<long, AssetStruct>();
@@ -26,16 +105,13 @@ namespace Core.Catalog.NewCatalogManagement
                     foreach (DataRow dr in dt.Rows)
                     {
                         long id = ODBCWrapper.Utils.GetLongSafeVal(dr, "ID", 0);
-                        string name = ODBCWrapper.Utils.GetSafeStr(dr, "DESCRIPTION");
-                        string systemName = ODBCWrapper.Utils.GetSafeStr(dr, "NAME");
-                        bool isPredefined = ODBCWrapper.Utils.ExtractBoolean(dr, "IS_BASIC");
-                        DateTime? createDate = ODBCWrapper.Utils.GetNullableDateSafeVal(dr, "CREATE_DATE");
-                        DateTime? updateDate = ODBCWrapper.Utils.GetNullableDateSafeVal(dr, "UPDATE_DATE");
                         if (id > 0 && !idToAssetStructMap.ContainsKey(id))
                         {
-                            AssetStruct assetStruct = new AssetStruct(id, name, systemName, isPredefined, createDate.HasValue ? ODBCWrapper.Utils.DateTimeToUnixTimestampUtc(createDate.Value) : 0,
-                                                                        updateDate.HasValue ? ODBCWrapper.Utils.DateTimeToUnixTimestampUtc(updateDate.Value) : 0);
-                            idToAssetStructMap.Add(id, assetStruct);
+                            AssetStruct assetStruct = CreateAssetStructFromIdAndDataRow(id, dr);
+                            if (assetStruct != null)
+                            {
+                                idToAssetStructMap.Add(id, assetStruct);
+                            }
                         }
                     }
                 }
@@ -57,6 +133,8 @@ namespace Core.Catalog.NewCatalogManagement
                 response.AssetStructs = idToAssetStructMap.Values.ToList();
                 response.Status = new ApiObjects.Response.Status((int)eResponseStatus.OK, eResponseStatus.OK.ToString());
             }
+
+            return response;
         }
 
         public static AssetStructListResponse GetAssetStructsByIds(int groupId, List<long> ids)
@@ -65,7 +143,7 @@ namespace Core.Catalog.NewCatalogManagement
             try
             {
                 DataSet ds = CatalogDAL.GetAssetStructsByIds(groupId, ids);
-                CreateAssetStructResponseFromDataSet(response, ds);
+                response = CreateAssetStructListResponseFromDataSet(ds);
             }
             catch (Exception ex)
             {
@@ -81,7 +159,7 @@ namespace Core.Catalog.NewCatalogManagement
             try
             {
                 DataSet ds = CatalogDAL.GetAssetStructsByMetaIds(groupId, metaIds);
-                CreateAssetStructResponseFromDataSet(response, ds);
+                response = CreateAssetStructListResponseFromDataSet(ds);
             }
             catch (Exception ex)
             {
@@ -107,28 +185,7 @@ namespace Core.Catalog.NewCatalogManagement
                     }
                 }
                 DataSet ds = CatalogDAL.InsertAssetStruct(groupId, assetStructToadd.Name, assetStructToadd.SystemName, metaIdsToPriority, assetStructToadd.IsPredefined);
-                List<AssetStruct> assetStructs = null;
-                long id = 0;
-                if (id > 0)
-                {
-                    result.Status = new Status((int)eResponseStatus.OK, eResponseStatus.OK.ToString());
-                    assetStructToadd.Id = id;
-                    result.AssetStruct = assetStructToadd;
-                }
-                else
-                {
-                    switch (id)
-                    {
-                        case -1:
-                            result.Status = new Status((int)eResponseStatus.AssetStructNameAlreadyInUse, eResponseStatus.AssetStructNameAlreadyInUse.ToString());
-                            break;
-                        case -2:
-                            result.Status = new Status((int)eResponseStatus.AssetStructSystemNameAlreadyInUse, eResponseStatus.AssetStructSystemNameAlreadyInUse.ToString());
-                            break;
-                        default:
-                            break;
-                    }
-                }
+                result = CreateAssetStructResponseFromDataSet(ds);
             }
             catch (Exception ex)
             {
@@ -140,12 +197,46 @@ namespace Core.Catalog.NewCatalogManagement
 
         public static AssetStructResponse UpdateAssetStruct(int groupId, AssetStruct assetStructToUpdate, bool shouldUpdateMetaIds)
         {
-            throw new NotImplementedException();
+            AssetStructResponse result = new AssetStructResponse();
+            try
+            {
+                List<KeyValuePair<long, int>> metaIdsToPriority = null;
+                if (assetStructToUpdate.MetaIds != null)
+                {
+                    metaIdsToPriority = new List<KeyValuePair<long, int>>();                                           
+                    int priority = 1;
+                    foreach (long metaId in assetStructToUpdate.MetaIds)
+                    {
+                        metaIdsToPriority.Add(new KeyValuePair<long, int>(metaId, priority));
+                        priority++;
+                    }
+                }
+                DataSet ds = CatalogDAL.UpdateAssetStruct(groupId, assetStructToUpdate.Name, assetStructToUpdate.SystemName, metaIdsToPriority, assetStructToUpdate.IsPredefined);
+                result = CreateAssetStructResponseFromDataSet(ds);
+            }
+            catch (Exception ex)
+            {
+                log.Error(string.Format("Failed UpdateAssetStruct for groupId: {0} and assetStruct: {1}", groupId, assetStructToUpdate.ToString()), ex);
+            }
+
+            return result;
         }
 
-        public static Status DeleteAssetStruct(object groupId, long id)
+        public static Status DeleteAssetStruct(int groupId, long id)
         {
-            throw new NotImplementedException();
+            Status result = new Status((int)eResponseStatus.Error, eResponseStatus.Error.ToString());
+            try
+            {
+                long deleteResult = CatalogDAL.DeleteAssetStruct(groupId, id);
+                result = CreateAssetStructResponseStatusFromResult(deleteResult);
+            }
+            catch (Exception ex)
+            {
+                log.Error(string.Format("Failed UpdateAssetStruct for groupId: {0} and assetStructId: {1}", groupId, id), ex);
+            }
+
+            return result;
         }
+
     }
 }
