173574266226cffbda4d1ada7f6135bbe018674c
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2017-11-22 21:59:31 +0200

    BEO-4300 EU Regulation - OPT-OUT Split update/add/delete for seperte events

--- Logic/Users/UserDynamicData.cs
+++ Logic/Users/UserDynamicData.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using ApiObjects;
+using KLogMonitor;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 using System.Xml;
 using System.Xml.Linq;
 using TVinciShared;
 
 namespace Core.Users
 {
-    public class UserDynamicData : ICloneable
+    public class UserDynamicData : ApiObjects.CoreObject
     {
+        private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+
+        Dictionary<int, KeyValuePair<string, string>> dynamicDataToUpdate = new Dictionary<int, KeyValuePair<string, string>>(); //rows to update in DB
+        List<int> dynamicDataToRemoved = new List<int>(); //rows to remove from DB
+        List<KeyValuePair<string, string>> dynamicDataToInsert = new List<KeyValuePair<string, string>>(); //rows to insert in DB
+        int groupId = 0;
+        public int UserId = 0;
+            
         public UserDynamicData()
         {
             m_sUserData = null;
@@ -23,7 +35,7 @@ namespace Core.Users
         protected bool UpdateUserDynamicDataStatus(Int32 nUserID, Int32 nStatus)
         {
             bool res = false;
-            
+
             try
             {
                 ODBCWrapper.UpdateQuery updateQuery = new ODBCWrapper.UpdateQuery("users_dynamic_data");
@@ -45,69 +57,6 @@ namespace Core.Users
             return false;
         }
 
-        protected bool UpdateUserDynamicData(Int32 nUserID, string sType , string sValue , Int32 nGroupID)
-        {
-            bool res = false;
-
-            try
-            {
-                Int32 nID = 0;
-                ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
-                selectQuery.SetConnectionKey("USERS_CONNECTION_STRING");
-                selectQuery.SetCachedSec(0);
-                selectQuery += "SELECT ID FROM USERS_DYNAMIC_DATA WITH (NOLOCK) WHERE STATUS=0 AND ";
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("USER_ID", "=", nUserID);
-                selectQuery += "AND";
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("DATA_TYPE", "=", sType);
-                if (selectQuery.Execute("query", true) != null)
-                {
-                    Int32 nCount = selectQuery.Table("query").DefaultView.Count;
-                    if (nCount > 0)
-                    {
-                        nID = int.Parse(selectQuery.Table("query").DefaultView[0].Row["ID"].ToString());
-                    }
-                }
-                selectQuery.Finish();
-                selectQuery = null;
-
-                if (nID != 0)
-                {
-                    ODBCWrapper.UpdateQuery updateQuery = new ODBCWrapper.UpdateQuery("users_dynamic_data");
-                    updateQuery += ODBCWrapper.Parameter.NEW_PARAM("STATUS", "=", 1);
-                    updateQuery += ODBCWrapper.Parameter.NEW_PARAM("DATA_TYPE", "=", sType);
-                    updateQuery += ODBCWrapper.Parameter.NEW_PARAM("DATA_VALUE", "=", sValue);
-                    updateQuery += ODBCWrapper.Parameter.NEW_PARAM("IS_ACTIVE", "=", 1);
-                    updateQuery += ODBCWrapper.Parameter.NEW_PARAM("UPDATE_DATE", "=", DateTime.UtcNow);
-                    updateQuery += " WHERE ";
-                    updateQuery += ODBCWrapper.Parameter.NEW_PARAM("ID", "=", nID);
-                    res = updateQuery.Execute();
-                    updateQuery.Finish();
-                    updateQuery = null;
-                }
-                else
-                {
-                    ODBCWrapper.InsertQuery inserQuery = new ODBCWrapper.InsertQuery("users_dynamic_data");
-                    inserQuery += ODBCWrapper.Parameter.NEW_PARAM("STATUS", "=", 1);
-                    inserQuery += ODBCWrapper.Parameter.NEW_PARAM("DATA_TYPE", "=", sType);
-                    inserQuery += ODBCWrapper.Parameter.NEW_PARAM("DATA_VALUE", "=", sValue);
-                    inserQuery += ODBCWrapper.Parameter.NEW_PARAM("IS_ACTIVE", "=", 1);
-                    inserQuery += ODBCWrapper.Parameter.NEW_PARAM("USER_ID", "=", nUserID);
-                    inserQuery += ODBCWrapper.Parameter.NEW_PARAM("group_id", "=", nGroupID);
-                    res = inserQuery.Execute();
-                    inserQuery.Finish();
-                    inserQuery = null;
-                }
-
-                return res;
-            }
-            catch (Exception ex)
-            {
-                HandleException(ex);
-            }
-
-            return false;
-        }
-
         public bool Save(Int32 nUserID)
         {
             bool saved = false;
@@ -117,13 +66,10 @@ namespace Core.Users
             string sDataValue = ""; //the data_value from the DB                   
             List<int> lTypesNotInDB = new List<int>(); //types that are in the dynamic data and not in DB        
             //by default, each cell has the value of its index, but if this dataType is already in DB, it is marked with -1
-            int [] TypesNotInDB = new int[m_sUserData.Length];
-            for (int h = 0; h <m_sUserData.Length ; h++) 
+            int[] TypesNotInDB = new int[m_sUserData.Length];
+            for (int h = 0; h < m_sUserData.Length; h++)
                 TypesNotInDB[h] = h;
-    
-            List<int> lToRemove = new List<int>(); //rows to remove from DB
-            Dictionary<int, KeyValuePair<string, string>> dUpdate = new Dictionary<int, KeyValuePair<string, string>>();//rows to update in DB
-            List<KeyValuePair<string, string>> lInsert = new List<KeyValuePair<string, string>>();//rows to insert in DB
+
             KeyValuePair<string, string> kvp;
             try
             {
@@ -131,8 +77,8 @@ namespace Core.Users
                 ODBCWrapper.DataSetSelectQuery selectQuery = new ODBCWrapper.DataSetSelectQuery();
                 selectQuery.SetConnectionKey("USERS_CONNECTION_STRING");
                 selectQuery.SetCachedSec(0);
-                selectQuery += "SELECT ID, DATA_TYPE, DATA_VALUE FROM USERS_DYNAMIC_DATA WITH (NOLOCK) WHERE STATUS=1 AND "; 
-                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("USER_ID", "=", nUserID);                
+                selectQuery += "SELECT ID, DATA_TYPE, DATA_VALUE FROM USERS_DYNAMIC_DATA WITH (NOLOCK) WHERE STATUS=1 AND ";
+                selectQuery += ODBCWrapper.Parameter.NEW_PARAM("USER_ID", "=", nUserID);
                 if (selectQuery.Execute("query", true) != null)
                 {
                     int nCount = selectQuery.Table("query").DefaultView.Count;
@@ -148,32 +94,33 @@ namespace Core.Users
                             UserDynamicDataContainer t = (UserDynamicDataContainer)(m_sUserData[i]);
                             string sType = t.m_sDataType;
                             string sVal = t.m_sValue;
-                            
+
                             if (sType != "" && sType == sDataType)
-                            {  
+                            {
                                 TypesNotInDB[i] = -1;
                                 bFound = true;
                                 if (sVal != sDataValue) //update only rows that have differrent data 
                                 {
                                     kvp = new KeyValuePair<string, string>(sType, sVal);
-                                    dUpdate.Add(nID, kvp);                                   
+                                    dynamicDataToUpdate.Add(nID, kvp);
                                 }
-                            }                       
-                        }                        
+                            }
+                        }
                         if (!bFound) //set status to 2 in DB
-                        {                            
-                            lToRemove.Add(nID);                                                     
-                        }                    
-                    }    
-                
+                        {
+                            dynamicDataToRemoved.Add(nID);
+                        }
+                    }
+
                     //insert new dynamic data            
                     lTypesNotInDB = TypesNotInDB.Where(x => x > -1).ToList();
-                    foreach(int index in lTypesNotInDB)
+                    foreach (int index in lTypesNotInDB)
                     {
                         kvp = new KeyValuePair<string, string>(m_sUserData[index].m_sDataType, m_sUserData[index].m_sValue);
-                        lInsert.Add(kvp);
-                    }  
-                    saved = UpdateAllDynamicData(nUserID, nGroupID, dUpdate, lInsert, lToRemove);            
+                        dynamicDataToInsert.Add(kvp);
+                    }
+
+                    saved = UpdateAllDynamicData(nUserID, nGroupID);
                 }
                 selectQuery.Finish();
                 selectQuery = null;
@@ -244,7 +191,7 @@ namespace Core.Users
                         cElement.SetAttributeValue("data_type", typeVal.Key);
                         cElement.SetAttributeValue("data_value", typeVal.Value);
                         //cElement.SetAttributeValue("group_id", nGroupID);                       
-                        
+
                         root.Add(cElement);
                     }
                 }
@@ -260,45 +207,42 @@ namespace Core.Users
         }
 
         //generate a temporary table and insert to it all the data that need to be updated\inserted
-        private bool UpdateAllDynamicData(int nUserID, int nGroupID, Dictionary<int, KeyValuePair<string, string>> dUpdate, List<KeyValuePair<string, string>> lInsert, List<int> lToRemove)
+        private bool UpdateAllDynamicData(int nUserID, int nGroupID)
         {
-            ODBCWrapper.DirectQuery directQuery = new ODBCWrapper.DirectQuery();
-            directQuery.SetConnectionKey("USERS_CONNECTION_STRING");
-            //directQuery += "declare @UpdateDate datetime";
-            //directQuery += "set  @UpdateDate= '" + DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss") + "'";
+            bool insertResult = false;
+            bool updateResult = false;
+            bool deleteResult = false;
+            UserId = nUserID;
+            groupId = nGroupID;
 
-            directQuery += "create table #x ( id int, user_id int, data_type nvarchar(50), data_value nvarchar(512), is_active int,";
-            directQuery += "status int, group_id int, create_date datetime, update_date datetime,  publish_date datetime)";
-       
-            foreach (int id in dUpdate.Keys)
+            if (dynamicDataToInsert?.Count > 0)
             {
-                directQuery += "insert into #x (id, data_type, data_value,status, update_date) values (" + id.ToString() + ", '" + dUpdate[id].Key + "' , '" + dUpdate[id].Value + "', 1, getdate())";  
-            }
-            //set the status of rows that needs to be removed to 2            
-            foreach (int id in lToRemove)
-            {
-                directQuery += "insert into #x (id, data_type, data_value,status, update_date) values (" + id.ToString() + ",'','', 2 , getdate())";   
-            }
-            
-            foreach (KeyValuePair<string, string> kvp in lInsert)
-            {
-                directQuery += "insert into #x (id, user_id, data_type, data_value, is_active, status, group_id, create_date, update_date, publish_date) values";
-                directQuery += "(0, " + nUserID.ToString() + ", '" + kvp.Key + "' , '" + kvp.Value + "' ,1 , 1, " + nGroupID.ToString() + ",";
-                directQuery += "getdate() , getdate(), getdate() )";
+                insertResult = this.Insert();
+                if (!insertResult)
+                {
+                    log.ErrorFormat("Error inserting dynamicData: {0}. userId: {1}, groupId: {2}", JsonConvert.SerializeObject(dynamicDataToInsert), UserId, groupId);
+                }
             }
 
-            //update the rows that already exist
-            directQuery += "update users_dynamic_data set DATA_TYPE = #x.data_type, data_value = #x.data_value, STATUS = #x.status, UPDATE_DATE = #x.update_date";
-            directQuery += "from  #x inner join [users_dynamic_data] on #x.id = [users_dynamic_data].id";
+            if (dynamicDataToRemoved?.Count > 0)
+            {
+                deleteResult = this.Delete();
+                if (!deleteResult)
+                {
+                    log.ErrorFormat("Error deleting dynamicData: {0}. userId: {1}, groupId: {2}", string.Join(",", dynamicDataToRemoved), UserId, groupId);
+                }
+            }
 
-            //insert the new rows
-            directQuery += "insert into [users_dynamic_data] (USER_ID, DATA_TYPE, DATA_VALUE, IS_ACTIVE, STATUS, group_id, CREATE_DATE, UPDATE_DATE, PUBLISH_DATE)";
-            directQuery += "select user_id, DATA_TYPE, DATA_VALUE, IS_ACTIVE ,STATUS, group_id, CREATE_DATE, UPDATE_DATE, PUBLISH_DATE from #x where #x.id = 0";
+            if (dynamicDataToUpdate?.Count > 0)
+            {
+                updateResult = this.Update();
+                if (!updateResult)
+                {
+                    log.ErrorFormat("Error updating dynamicData: {0}. userId: {1}, groupId: {2}", string.Join(",", dynamicDataToUpdate), UserId, groupId);
+                }
+            }
 
-            bool res = directQuery.Execute();
-            directQuery.Finish();
-            directQuery = null;
-            return res;
+            return insertResult & deleteResult & updateResult;
         }
 
         public bool Initialize(string sXML)
@@ -411,8 +355,8 @@ namespace Core.Users
             return string.Empty;
         }
 
-        
-        public object Clone()
+
+        public UserDynamicData Clone()
         {
             return CloneImpl();
         }
@@ -423,12 +367,99 @@ namespace Core.Users
 
             return copy;
         }
-        
+
         private void HandleException(Exception ex)
         {
             //throw new NotImplementedException();
         }
-        
+
+        protected override bool DoInsert()
+        {
+            bool success = false;
+
+            ODBCWrapper.DirectQuery directQuery = new ODBCWrapper.DirectQuery();
+            directQuery.SetConnectionKey("USERS_CONNECTION_STRING");
+
+            directQuery += "create table #x ( id int, user_id int, data_type nvarchar(50), data_value nvarchar(512), is_active int,";
+            directQuery += "status int, group_id int, create_date datetime, update_date datetime,  publish_date datetime)";
+
+            foreach (KeyValuePair<string, string> kvp in dynamicDataToInsert)
+            {
+                directQuery += "insert into #x (id, user_id, data_type, data_value, is_active, status, group_id, create_date, update_date, publish_date) values";
+                directQuery += "(0, " + UserId.ToString() + ", '" + kvp.Key + "' , '" + kvp.Value + "' ,1 , 1, " + groupId.ToString() + ",";
+                directQuery += "getdate() , getdate(), getdate() )";
+            }
+
+            //insert the new rows
+            directQuery += "insert into [users_dynamic_data] (USER_ID, DATA_TYPE, DATA_VALUE, IS_ACTIVE, STATUS, group_id, CREATE_DATE, UPDATE_DATE, PUBLISH_DATE)";
+            directQuery += "select user_id, DATA_TYPE, DATA_VALUE, IS_ACTIVE ,STATUS, group_id, CREATE_DATE, UPDATE_DATE, PUBLISH_DATE from #x where #x.id = 0";
+
+            success = directQuery.Execute();
+            directQuery.Finish();
+            directQuery = null;
+
+            return success;
+        }
+
+        protected override bool DoUpdate()
+        {
+            bool success = false;
+
+            ODBCWrapper.DirectQuery directQuery = new ODBCWrapper.DirectQuery();
+            directQuery.SetConnectionKey("USERS_CONNECTION_STRING");
+
+            directQuery += "create table #x ( id int, user_id int, data_type nvarchar(50), data_value nvarchar(512), is_active int,";
+            directQuery += "status int, group_id int, create_date datetime, update_date datetime,  publish_date datetime)";
+
+            foreach (int id in dynamicDataToUpdate.Keys)
+            {
+                directQuery += "insert into #x (id, data_type, data_value,status, update_date) values (" + id.ToString() + ", '" + dynamicDataToUpdate[id].Key + "' , '" + dynamicDataToUpdate[id].Value + "', 1, getdate())";
+            }
+
+            //update the rows that already exist
+            directQuery += "update users_dynamic_data set DATA_TYPE = #x.data_type, data_value = #x.data_value, STATUS = #x.status, UPDATE_DATE = #x.update_date";
+            directQuery += "from  #x inner join [users_dynamic_data] on #x.id = [users_dynamic_data].id";
+
+            success = directQuery.Execute();
+            directQuery.Finish();
+            directQuery = null;
+
+            return success;
+        }
+
+        protected override bool DoDelete()
+        {
+            bool success = false;
+
+            ODBCWrapper.DirectQuery directQuery = new ODBCWrapper.DirectQuery();
+            directQuery.SetConnectionKey("USERS_CONNECTION_STRING");
+
+            directQuery += "create table #x ( id int, user_id int, data_type nvarchar(50), data_value nvarchar(512), is_active int,";
+            directQuery += "status int, group_id int, create_date datetime, update_date datetime,  publish_date datetime)";
+
+            //set the status of rows that needs to be removed to 2            
+            foreach (int id in dynamicDataToRemoved)
+            {
+                directQuery += "insert into #x (id, data_type, data_value,status, update_date) values (" + id.ToString() + ",'','', 2 , getdate())";
+            }
+
+            //update the rows that already exist
+            directQuery += "update users_dynamic_data set DATA_TYPE = #x.data_type, data_value = #x.data_value, STATUS = #x.status, UPDATE_DATE = #x.update_date";
+            directQuery += "from  #x inner join [users_dynamic_data] on #x.id = [users_dynamic_data].id";
+
+            success = directQuery.Execute();
+            directQuery.Finish();
+            directQuery = null;
+
+            return success;
+
+        }
+
+        public override CoreObject CoreClone()
+        {
+            return this.Clone();
+        }
+
         public UserDynamicDataContainer[] m_sUserData;
     }
 }
