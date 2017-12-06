072e8393b5d9ff4853523c6432527846364a3ac8
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2017-12-06 11:11:07 +0200

    Add TagSearchDefinitions Obj

---
+++ ApiObjects/SearchObjects/TagSearchDefinitions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiObjects.SearchObjects
+{
+    public class TagSearchDefinitions
+    {
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+
+        public string Tag { get; set; }
+
+        public string SearchValue { get; set; }
+
+        public int TopicId { get; set; }
+
+    }
+}
