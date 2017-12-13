ba09436c1f90f59675bd9bc155c42f187c173102
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2017-12-13 12:24:04 +0200

    Add Image type

---
+++ Logic/Catalog/CatalogManagement/ImageTypeListResponse.cs
@@ -0,0 +1,18 @@
+﻿using ApiObjects.Response;
+using System.Collections.Generic;
+
+namespace Core.Catalog.CatalogManagement
+{
+    public class ImageTypeListResponse
+    {
+        public Status Status { get; set; }
+
+        public List<Topic> ImageTypes { get; set; }
+
+        public ImageTypeListResponse()
+        {
+            Status = new Status((int)eResponseStatus.Error, eResponseStatus.Error.ToString());
+            ImageTypes = new List<Topic>();
+        }
+    }
+}
