ba6a1078bfc9e555fa69fa0bff514562e249211d
Lior Faingold <Lior.Faingold@Kaltura.com>
2017-12-14 20:56:08 +0200

    fix build index  - add co_guid and catalog_start_date as strings

--- ElasticSearch/Common/Serializer/BaseESSeralizer.cs
+++ ElasticSearch/Common/Serializer/BaseESSeralizer.cs
@@ -27,7 +27,7 @@ namespace ElasticSearch.Common
             recordBuilder.AppendFormat("\"media_id\": {0}, \"group_id\": {1}, \"media_type_id\": {2}, \"wp_type_id\": {3}, \"is_active\": {4}, " +
                 "\"device_rule_id\": {5}, \"like_counter\": {6}, \"views\": {7}, \"rating\": {8}, \"votes\": {9}, \"start_date\": \"{10}\", " +
                 "\"end_date\": \"{11}\", \"final_date\": \"{12}\", \"create_date\": \"{13}\", \"update_date\": \"{14}\", \"{19}\": \"{15}\", " +
-                "\"{20}\": \"{16}\", \"cache_date\": \"{17}\", \"geo_block_rule_id\": {18}, \"co_guid\": {19}, \"catalog_start_date\": {20}, ",
+                "\"{20}\": \"{16}\", \"cache_date\": \"{17}\", \"geo_block_rule_id\": {18}, \"co_guid\": \"{19}\", \"catalog_start_date\": \"{20}\", ",
                 media.m_nMediaID, media.m_nGroupID, media.m_nMediaTypeID, media.m_nWPTypeID, media.m_nIsActive,
                 media.m_nDeviceRuleId, media.m_nLikeCounter, media.m_nViews, media.m_dRating, media.m_nVotes, media.m_sStartDate,
                 media.m_sEndDate, media.m_sFinalEndDate, media.m_sCreateDate, media.m_sUpdateDate, Common.Utils.ReplaceDocumentReservedCharacters(media.m_sName, shouldLowerCase),
