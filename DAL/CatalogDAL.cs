b5f7dc24dc890002f2c8e25b6970116e2b52748b
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2017-12-17 13:30:31 +0200

    no message

--- DAL/CatalogDAL.cs
+++ DAL/CatalogDAL.cs
@@ -4959,14 +4959,14 @@ namespace Tvinci.Core.DAL
             return sp.ExecuteDataSet();
         }
 
-        public static DataSet InsertImageType(int groupId, string name, string systemName, long rationId, string helpText, long userId, long? defaultImageId)
+        public static DataSet InsertImageType(int groupId, string name, string systemName, long ratioId, string helpText, long userId, long? defaultImageId)
         {
             ODBCWrapper.StoredProcedure sp = new ODBCWrapper.StoredProcedure("InsertImageType");
             sp.SetConnectionKey("MAIN_CONNECTION_STRING");
             sp.AddParameter("@groupId", groupId);
             sp.AddParameter("@name", name);
             sp.AddParameter("@systemName", systemName);
-            sp.AddParameter("@rationId", rationId);
+            sp.AddParameter("@ratioId", ratioId);
             sp.AddParameter("@helpText", helpText);
             sp.AddParameter("@updaterId", userId);
             if (defaultImageId.HasValue)
@@ -4986,7 +4986,7 @@ namespace Tvinci.Core.DAL
             return sp.ExecuteReturnValue<int>() > 0;
         }
 
-        public static DataSet UpdateImageType(int groupId, long id, string name, string systemName, long rationId, string helpText, long userId, long? defaultImageId)
+        public static DataSet UpdateImageType(int groupId, long id, string name, string systemName, long ratioId, string helpText, long userId, long? defaultImageId)
         {
             ODBCWrapper.StoredProcedure sp = new ODBCWrapper.StoredProcedure("UpdateImageType");
             sp.SetConnectionKey("MAIN_CONNECTION_STRING");
@@ -4994,7 +4994,7 @@ namespace Tvinci.Core.DAL
             sp.AddParameter("@groupId", groupId);
             sp.AddParameter("@name", name);
             sp.AddParameter("@systemName", systemName);
-            sp.AddParameter("@rationId", rationId);
+            sp.AddParameter("@ratioId", ratioId);
             sp.AddParameter("@helpText", helpText);
             sp.AddParameter("@updaterId", userId);
             if (defaultImageId.HasValue)
