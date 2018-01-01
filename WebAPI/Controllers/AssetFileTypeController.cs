b930e62e27f69aa817f82417afb1fdef9bfba2e4
KALTURA\Irena.Leibman <irena.leibman@kaltura.com>
2018-01-01 19:31:06 +0200

    finishing asset file types

---
+++ WebAPI/Controllers/AssetFileTypeController.cs
@@ -0,0 +1,126 @@
+﻿using KLogMonitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using WebAPI.ClientManagers.Client;
+using WebAPI.Exceptions;
+using WebAPI.Filters;
+using WebAPI.Models.API;
+using WebAPI.Utils;
+using WebAPI.Managers.Models;
+using WebAPI.Models.ConditionalAccess;
+using WebAPI.Models.General;
+using WebAPI.Managers.Scheme;
+using WebAPI.Models.Catalog;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("_service/assetFileType/action")]
+    public class AssetFileTypeController : ApiController
+    {
+        private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+       
+        /// <summary>
+        /// Returns a list of asset-file types
+        /// </summary>
+        [Route("list"), HttpPost]
+        [ApiAuthorize]
+        public KalturaAssetFileTypeListResponse List()
+        {
+            KalturaAssetFileTypeListResponse response = new KalturaAssetFileTypeListResponse();
+
+            int groupId = KS.GetFromRequest().GroupId;
+            string udid = KSUtils.ExtractKSPayload().UDID;
+
+            try
+            {
+                response = ClientsManager.CatalogClient().GetAssetFileTypes(groupId);
+            }
+            catch (ClientException ex)
+            {
+                throw new ApiException(ex);
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Add new asset-file type
+        /// </summary>
+        /// <param name="assetFileType">Asset-file type</param>
+        /// <remarks>
+        /// </remarks>
+        /// <returns></returns>
+        [Route("add"), HttpPost]
+        [ApiAuthorize]
+        public KalturaAssetFileType Add(KalturaAssetFileType assetFileType)
+        {
+            int groupId = KS.GetFromRequest().GroupId;
+            long userId = Utils.Utils.GetUserIdFromKs();
+
+            assetFileType.validateForInsert();
+
+            try
+            {
+                return ClientsManager.CatalogClient().AddAssetFileType(groupId, userId, assetFileType);
+            }
+            catch (ClientException ex)
+            {
+                throw new ApiException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Update existing asset-file type
+        /// </summary>
+        /// <param name="id">Asset-file type identifier</param>
+        /// <param name="assetFileType">Asset-file type</param>
+        /// <remarks>
+        /// </remarks>
+        /// <returns></returns>
+        [Route("update"), HttpPost]
+        [ApiAuthorize]
+        public KalturaAssetFileType Update(int id, KalturaAssetFileType assetFileType)
+        {
+            int groupId = KS.GetFromRequest().GroupId;
+            long userId = Utils.Utils.GetUserIdFromKs();
+
+            try
+            {
+                return ClientsManager.CatalogClient().UpdateAssetFileType(groupId, userId, id, assetFileType);
+            }
+            catch (ClientException ex)
+            {
+                throw new ApiException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Delete asset-file type by id
+        /// </summary>
+        /// <param name="id">Asset-file type identifier</param>
+        /// <remarks>
+        /// </remarks>
+        /// <returns></returns>
+        [Route("delete"), HttpPost]
+        [ApiAuthorize]
+        public bool Delete(int id)
+        {
+            int groupId = KS.GetFromRequest().GroupId;
+            long userId = Utils.Utils.GetUserIdFromKs();
+
+            try
+            {
+                return ClientsManager.CatalogClient().DeleteAssetFileType(groupId, userId, id);
+            }
+            catch (ClientException ex)
+            {
+                throw new ApiException(ex);
+            }
+        }
+    }
+}
---
+++ WebAPI/Models/Catalog/KalturaAssetFileType.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Xml.Serialization;
+using WebAPI.Exceptions;
+using WebAPI.Managers.Scheme;
+using WebAPI.Models.Catalog;
+using WebAPI.Models.ConditionalAccess;
+using WebAPI.Models.General;
+using WebAPI.Models.Users;
+
+namespace WebAPI.Models.Catalog
+{
+    /// <summary>
+    /// Asset-file type
+    /// </summary>
+    public class KalturaAssetFileType : KalturaOTTObject
+    {
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
+        [DataMember(Name = "id")]
+        [JsonProperty("id")]
+        [XmlElement(ElementName = "id")]
+        [SchemeProperty(ReadOnly = true)]
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// Unique description
+        /// </summary>
+        [DataMember(Name = "description")]
+        [JsonProperty("description")]
+        [XmlElement(ElementName = "description")]
+        [SchemeProperty(MinLength = 1, MaxLength = 50)]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Indicates if asset-file type is active or disabled
+        /// </summary>
+        [DataMember(Name = "status")]
+        [JsonProperty("status")]
+        [XmlElement(ElementName = "status")]
+        public bool? Status { get; set; }
+
+        /// <summary>
+        /// Specifies when was the type was created. Date and time represented as epoch.
+        /// </summary>
+        [DataMember(Name = "createDate")]
+        [JsonProperty(PropertyName = "createDate")]
+        [XmlElement(ElementName = "createDate")]
+        [SchemeProperty(ReadOnly = true)]
+        public long? CreateDate { get; set; }
+
+        /// <summary>
+        /// Specifies when was the type last updated. Date and time represented as epoch.
+        /// </summary>
+        [DataMember(Name = "updateDate")]
+        [JsonProperty(PropertyName = "updateDate")]
+        [XmlElement(ElementName = "updateDate")]
+        [SchemeProperty(ReadOnly = true)]
+        public long? UpdateDate { get; set; }
+
+        /// <summary>
+        /// Specifies whether playback as trailer is allowed
+        /// </summary>
+        [DataMember(Name = "isTrailer")]
+        [JsonProperty(PropertyName = "isTrailer")]
+        [XmlElement(ElementName = "isTrailer")]
+        [SchemeProperty(InsertOnly = true)]
+        public bool? IsTrailer { get; set; }
+
+        /// <summary>
+        /// Defines playback streamer type
+        /// </summary>
+        [DataMember(Name = "streamerType")]
+        [JsonProperty(PropertyName = "streamerType")]
+        [XmlElement(ElementName = "streamerType")]
+        [SchemeProperty(InsertOnly = true)]
+        public KalturaAssetFileStreamerType? StreamerType { get; set; }
+
+        /// <summary>
+        /// DRM adapter-profile identifier, use -1 for uDRM, NULL for no DRM.
+        /// </summary>
+        [DataMember(Name = "drmAdapterProfileId")]
+        [JsonProperty(PropertyName = "drmAdapterProfileId")]
+        [XmlElement(ElementName = "drmAdapterProfileId")]
+        [SchemeProperty(MinInteger = -1)]
+        public int? DrmAdapterProfileId { get; set; }
+
+        public void validateForInsert()
+        {
+            if (Description == null || Description.Trim().Length == 0)
+            {
+                throw new BadRequestException(BadRequestException.ARGUMENT_CANNOT_BE_EMPTY, "assetFileType.description");
+            }
+
+            if (StreamerType == null)
+            {
+                throw new BadRequestException(BadRequestException.ARGUMENT_CANNOT_BE_EMPTY, "assetFileType.streamerType");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Asset-file types list
+    /// </summary>
+    [DataContract(Name = "Collections", Namespace = "")]
+    [XmlRoot("Collections")]
+    public class KalturaAssetFileTypeListResponse : KalturaListResponse
+    {
+        /// <summary>
+        /// A list of asset-file types
+        /// </summary>
+        [DataMember(Name = "objects")]
+        [JsonProperty("objects")]
+        [XmlArray(ElementName = "objects", IsNullable = true)]
+        [XmlArrayItem("item")]
+        public List<KalturaAssetFileType> Types { get; set; }
+    }
+    
+    public enum KalturaAssetFileStreamerType
+    {
+        APPLE_HTTP = 0,
+        MPEG_DASH = 1,
+        URL = 2,
+        SMOOTH_STREAMING = 3
+    }
+}
