0911f6eefd82eef4c77e59acedd29a49962bec67
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2018-01-02 12:27:11 +0200

    no message

--- WebAPI/Controllers/PromotionController.cs
+++ WebAPI/Controllers/PromotionController.cs
@@ -1,86 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Http;
-using WebAPI.ClientManagers.Client;
-using WebAPI.Exceptions;
-using WebAPI.Filters;
-using WebAPI.Managers.Models;
-using WebAPI.Managers.Scheme;
-using WebAPI.Models.ConditionalAccess;
-using WebAPI.Models.General;
-using WebAPI.Models.Pricing;
-using WebAPI.Utils;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
     [RoutePrefix("_service/promotion/action")]
-    public class PromotionController
+    public class PromotionController : ApiController
     {
         /// <summary>
         /// Returns promotions list regarding to mediaId
-        /// </summary>
-        /// <param name="filter">Request filter</param>
+        /// </summary>        
         /// <remarks></remarks>
-        [Route("list"), HttpPost]        
-        public KalturaProductPriceListResponse List(KalturaProductPriceFilter filter)
+        [Route("list"), HttpPost]
+        [ApiAuthorize]
+        public KalturaPromotionListResponse List()
         {
-            List<KalturaProductPrice> productPrices = new List<KalturaProductPrice>();
-            List<KalturaSubscriptionPrice> subscriptionPrices = new List<KalturaSubscriptionPrice>();
-            List<KalturaPpvPrice> ppvPrices = new List<KalturaPpvPrice>();
-            List<KalturaCollectionPrice> collectiontPrices = new List<KalturaCollectionPrice>();
-
-            filter.Validate();
-
-            int groupId = KS.GetFromRequest().GroupId;
-            string udid = KSUtils.ExtractKSPayload().UDID;
-            string language = Utils.Utils.GetLanguageFromRequest();
-            string currency = Utils.Utils.GetCurrencyFromRequest();
-            string userId = KS.GetFromRequest().UserId;
-
-            try
-            {
-                if (filter.SubscriptionIdIn != null && filter.SubscriptionIdIn.Count() > 0)
-                {
-                    subscriptionPrices = ClientsManager.ConditionalAccessClient().GetSubscriptionsPrices(groupId, filter.getSubscriptionIdIn(), userId, filter.CouponCodeEqual,
-                                                                                                            udid, language, filter.getShouldGetOnlyLowest(), currency);
-                    productPrices.AddRange(subscriptionPrices);
-                }
-
-                if (filter.FileIdIn != null && filter.FileIdIn.Count() > 0)
-                {
-                    ppvPrices = ClientsManager.ConditionalAccessClient().GetPpvPrices(groupId, filter.getFileIdIn(), userId, filter.CouponCodeEqual, udid, language,
-                                                                                        filter.getShouldGetOnlyLowest(), currency);
-                    productPrices.AddRange(ppvPrices);
-                }
-
-                if (filter.CollectionIdIn != null && filter.CollectionIdIn.Count() > 0)
-                {
-                    collectiontPrices = ClientsManager.ConditionalAccessClient().GetCollectionPrices(groupId, filter.getCollectionIdIn(), userId, filter.CouponCodeEqual, udid, language,
-                                                                                        filter.getShouldGetOnlyLowest(), currency);
-                    productPrices.AddRange(collectiontPrices);
-                }
-
-                // order
-                switch (filter.OrderBy)
-                {
-                    case KalturaProductPriceOrderBy.PRODUCT_ID_ASC:
-                        productPrices = productPrices.OrderBy(p => p.ProductId).ToList();
-                        break;
-                    case KalturaProductPriceOrderBy.PRODUCT_ID_DESC:
-                        productPrices = productPrices.OrderByDescending(p => p.ProductId).ToList();
-                        break;
-                    default:
-                        break;
-                }
-            }
-            catch (ClientException ex)
-            {
-                ErrorUtils.HandleClientException(ex);
-            }
-
-            return new KalturaProductPriceListResponse() { ProductsPrices = productPrices, TotalCount = productPrices.Count() };
+            KalturaPromotionListResponse response = new KalturaPromotionListResponse();
+            response.Promotions = new List<KalturaPromotion>();
+            response.Promotions.Add(new KalturaPromotion() { Link = "http://www.next.co.il/en/g222610s2#185058", Text = "Game over", StartTime = 5, EndTime=7 });
+            return response;
         }
     }
 }
