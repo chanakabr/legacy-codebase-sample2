36ad67dd0f0a078e31e4fa504367eed5550ca651
Lior Faingold <Lior.Faingold@kaltura.com>
2018-01-31 18:24:33 +0200

    working on BEO-4662 and BEO-4663

--- DAL/CatalogDAL.cs
+++ DAL/CatalogDAL.cs
@@ -5191,12 +5191,16 @@ namespace Tvinci.Core.DAL
             return sp.ExecuteReturnValue<int>() > 0;
         }
 
-        public static DataTable InsertGroupImageRatios(int groupId, long userId, string ratio)
+        public static DataTable InsertGroupImageRatios(int groupId, long userId, string ratio, int height, int width,
+                                                        int acceptedErrorMarginPrecentage)
         {
-            ODBCWrapper.StoredProcedure sp = new ODBCWrapper.StoredProcedure("InsertGroupImageRatios");
+            ODBCWrapper.StoredProcedure sp = new ODBCWrapper.StoredProcedure("InsertGroupImageRatio");
             sp.SetConnectionKey("MAIN_CONNECTION_STRING");
             sp.AddParameter("@groupId", groupId);
             sp.AddParameter("@ratio", ratio);
+            sp.AddParameter("@height", height);
+            sp.AddParameter("@width", width);
+            sp.AddParameter("@acceptedErrorMarginPrecentage", acceptedErrorMarginPrecentage);
             sp.AddParameter("@updaterId", userId);
 
             return sp.Execute();
