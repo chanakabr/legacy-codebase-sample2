601ab47665bdc009d598fdfda6abd7082f0cfce5
Amishai Guilburd <amishai.guilburd@kaltura.com>
2018-02-27 16:55:11 +0200

    Added action extraction from celery received task for PS second level DLL routing

---
+++ RemoteTasksService/Action.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace RemoteTasksService
+{
+    [Serializable]
+    public class Action
+    {
+        [JsonProperty("action")]
+        public string ActionImp { get; set; }
+    }
+}
