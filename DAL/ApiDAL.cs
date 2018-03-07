09945b6a0db8e3a448a30a762dedeb8bd46256b9
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2018-03-07 15:50:25 +0200

    BEO-4767 [TVM: Business Media Types] Cannot modify 'Streamer type' from 'applehttp' to 'none' & vice versa

--- DAL/ApiDAL.cs
+++ DAL/ApiDAL.cs
@@ -4208,12 +4208,16 @@ namespace DAL
                         Id = ODBCWrapper.Utils.GetLongSafeVal(dr, "id"),
                         Type = ODBCWrapper.Utils.GetSafeStr(dr, "DESCRIPTION"), 
                         IsTrailer = ODBCWrapper.Utils.GetIntSafeVal(dr, "IS_TRAILER") == 1 ? true : false,
-                        CdnId = ODBCWrapper.Utils.GetIntSafeVal(dr, "STREAMING_SUPLIER_ID"),
-                        StreamerType = (StreamerType)ODBCWrapper.Utils.GetIntSafeVal(dr, "streamer_type"),
+                        CdnId = ODBCWrapper.Utils.GetIntSafeVal(dr, "STREAMING_SUPLIER_ID"),                        
                         Url = ODBCWrapper.Utils.GetSafeStr(dr, "STREAMING_CODE"),
                         DrmId = ODBCWrapper.Utils.GetIntSafeVal(dr, "DRM_ID"),
                         MediaId = mediaId,
                     };
+
+                    if(ODBCWrapper.Utils.GetNullableInt(dr, "streamer_type").HasValue)
+                    {
+                        file.StreamerType = (StreamerType)ODBCWrapper.Utils.GetNullableInt(dr, "streamer_type");
+                    }
 
                     files.Add(file);
                 }
--- ODBCWrapper/Utils.cs
+++ ODBCWrapper/Utils.cs
@@ -50,6 +50,20 @@ namespace ODBCWrapper
             }
         }
 
+        public static int? GetNullableInt(DataRow dr, string sField)
+        {
+            try
+            {
+                if (dr != null && dr[sField] != DBNull.Value)
+                    return int.Parse(dr[sField].ToString());
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
         public static int GetIntSafeVal(DataRowView dr, string sField)
         {
             try
