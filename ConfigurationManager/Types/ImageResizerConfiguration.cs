fc8088e8e54be776836c0cda3ebb925529b8c0fe
KALTURA\Sunny.Simantov <sunny.simantov@kaltura.com>
2018-03-13 10:46:22 +0200

    add image resizer handler configuration

---
+++ ConfigurationManager/Types/ImageResizerConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationManager
+{
+    public class ImageResizerConfiguration : ConfigurationValue
+    {
+        public StringConfigurationValue ProxyAddress;
+        public StringConfigurationValue ProxyUsername;
+        public StringConfigurationValue ProxyPassword;
+        public BooleanConfigurationValue UseFileSystem;
+        public StringConfigurationValue ImagesBasePath;
+
+        public ImageResizerConfiguration(string key) : base(key)
+        {
+            ProxyAddress = new ConfigurationManager.StringConfigurationValue("proxy_address")
+            {
+                ShouldAllowEmpty = true
+            };
+            ProxyUsername = new ConfigurationManager.StringConfigurationValue("proxy_username")
+            {
+                ShouldAllowEmpty = true
+            };
+            ProxyPassword = new StringConfigurationValue("proxy_password")
+            {
+                ShouldAllowEmpty = true
+            };
+            UseFileSystem = new ConfigurationManager.BooleanConfigurationValue("use_file_system")
+            {
+                ShouldAllowEmpty = true,
+                DefaultValue = true
+            };
+            ImagesBasePath = new ConfigurationManager.StringConfigurationValue("images_base_path")
+            {
+                ShouldAllowEmpty = true
+            };
+        }
+    }
+}
