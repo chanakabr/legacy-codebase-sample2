636815f4869a21cf87a0fc2ac4809c9ed13b581d
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2018-03-14 13:40:51 +0200

    ApplicationConfiguration.Version.Value

--- Web Sites/TVM/adm_recommendation_engine_adapter.aspx.cs
+++ Web Sites/TVM/adm_recommendation_engine_adapter.aspx.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using ConfigurationManager;
+using System;
 using TVinciShared;
 
 public partial class adm_recommendation_engine_adapter : System.Web.UI.Page
