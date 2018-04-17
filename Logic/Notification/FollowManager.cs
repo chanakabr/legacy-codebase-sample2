ca93a8dfd0bd15b7e172a2a1ae309fb42319cce2
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2018-04-17 11:52:18 +0300

    no message

--- Logic/Notification/FollowManager.cs
+++ Logic/Notification/FollowManager.cs
@@ -4,7 +4,6 @@ using ApiObjects;
 using ApiObjects.Notification;
 using ApiObjects.Response;
 using ApiObjects.SearchObjects;
-using ConfigurationManager;
 using Core.Catalog;
 using Core.Catalog.Request;
 using Core.Catalog.Response;
