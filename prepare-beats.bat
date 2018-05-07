396725f17b861ef14f8126568743c5671e644789
jonathan.amit-kanarek <jonathan.amit-kanarek@kaltura.com>
2018-05-07 14:52:37 +0300

    Enable destination folder

--- prepare-beats.bat
+++ prepare-beats.bat
@@ -1,13 +1,13 @@
+set DESTINATION=%1
 
 powershell Invoke-WebRequest -OutFile filebeat.zip https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.2.4-windows-x86_64.zip
 powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('filebeat.zip', 'filebeat-tmp'); }"
 
 dir /B filebeat-tmp >tempfilelist.txt
-mkdir filebeat
+mkdir %DESTINATION%\filebeat
 
 for /f "tokens=1 delims=¬" %%b in (tempfilelist.txt) do (
-    echo "copy filebeat-tmp\%%b"
-    copy "filebeat-tmp\%%b" filebeat\
+    copy "filebeat-tmp\%%b" %DESTINATION%\filebeat\
     GOTO CLEANUP
 )
 
