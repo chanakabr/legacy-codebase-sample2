1d07e8b95440abe5041ed1cb99108dff3f0a8544
KALTURA\Ira.Yanai <ira.yanai@kaltura.com>
2018-05-07 14:40:56 +0300

    https://kaltura.atlassian.net/browse/BEO-4984

--- NPVR/AlcatelLucentNPVRV2.cs
+++ NPVR/AlcatelLucentNPVRV2.cs
@@ -528,7 +528,10 @@ namespace NPVR
                     {
                         if (httpStatusCode == HTTP_STATUS_OK)
                         {
-                            GetRecordSeriesResponse(responseJson, args, res);
+                            res.entityID = args.EntityID;
+                            res.msg = string.Empty;
+                            res.status = RecordStatus.OK;
+                            res.recordingID = args.AssetID;
                         }
                         else
                         {
@@ -1257,7 +1260,7 @@ namespace NPVR
 
         private bool IsSetAssetAlreadyWatchedInputValid(NPVRParamsObj args)
         {
-            return args != null && !string.IsNullOrEmpty(args.EntityID) && !string.IsNullOrEmpty(args.AssetID) && args.Value > 0;
+            return args != null && !string.IsNullOrEmpty(args.EntityID) && !string.IsNullOrEmpty(args.AssetID) && (args.Value == 0 || args.Value == 1);
         }
 
         private void GetSetAssetProtectionStatusResponse(string responseJson, NPVRParamsObj args, NPVRProtectResponse response)
