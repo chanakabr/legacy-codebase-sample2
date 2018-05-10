078e5a49ce5b879141114960d7f79ea8ae94477b
Lior Faingold <lior.faingold@Kaltura.com>
2018-05-10 12:01:54 +0300

    support search by linear_media_id on epg

--- WebAPI/Models/Catalog/KalturaRelatedFilter.cs
+++ WebAPI/Models/Catalog/KalturaRelatedFilter.cs
@@ -20,7 +20,8 @@ namespace WebAPI.Models.Catalog
         /// geo_block - only valid value is "true": When enabled, only assets that are not restriced to the user by geo-block rules will return.
         /// parental_rules - only valid value is "true": When enabled, only assets that the user doesn't need to provide PIN code will return.
         /// user_interests - only valid value is "true". When enabled, only assets that the user defined as his interests (by tags and metas) will return.
-        /// epg_channel_id – the channel identifier of the EPG program.
+        /// epg_channel_id – the channel identifier of the EPG program. *****Deprecated, please use linear_media_id instead*****
+        /// linear_media_id – the linear media identifier of the EPG program.
         /// entitled_assets - valid values: "free", "entitled", "both". free - gets only free to watch assets. entitled - only those that the user is implicitly entitled to watch.
         /// Comparison operators: for numerical fields =, >, >=, <, <=, : (in). 
         /// For alpha-numerical fields =, != (not), ~ (like), !~, ^ (any word starts with), ^= (phrase starts with), + (exists), !+ (not exists).
