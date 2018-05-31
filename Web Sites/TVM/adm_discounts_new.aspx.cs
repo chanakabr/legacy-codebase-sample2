6d246aef3b11522735fe5afc965aebaab4b04922
KALTURA\Irena.Leibman <irena.leibman@kaltura.com>
2018-05-31 14:37:03 +0300

    BEO-5156 - invalidation key

--- Web Sites/TVM/adm_discounts_new.aspx.cs
+++ Web Sites/TVM/adm_discounts_new.aspx.cs
@@ -1,9 +1,9 @@
 ﻿using CachingProvider.LayeredCache;
 using KLogMonitor;
-using RabbitMQ.Client.Impl;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
