ab6ab3facbfe1e660c06b92ba8b74b06aea5d26e
Shir Bruchim <shir.bruchim@kaltura.com>
2018-06-27 16:03:29 +0300

    fix BEO-5263

---
+++ TVinciShared/GenericExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVinciShared
+{
+    public static class GenericExtensionMethods
+    {
+        /// <summary>
+        /// Check if struct object is default (null)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsDefault<T>(this T value) where T : struct
+        {
+            bool isDefault = value.Equals(default(T));
+
+            return isDefault;
+        }
+    }
+}
