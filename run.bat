c462be0896397d0b2d16a87e5fd9f859da143a51
jonathan.amit-kanarek <jonathan.amit-kanarek@kaltura.com>
2018-07-16 19:53:43 +0300

    Revert logging to hostname dir

--- run.bat
+++
@@ -1,4 +0,0 @@
-mkdir C:\log\iis\%COMPUTERNAME%
-mklink /J C:\log\iis\%COMPUTERNAME%\%API_VERSION% C:\log\iisInternal
-mklink /J C:\log\api\%COMPUTERNAME% C:\log\RestfulApi
-C:\ServiceMonitor.exe w3svc
