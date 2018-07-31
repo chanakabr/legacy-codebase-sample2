ebc31611a9adb074ceb300b1e03c10619469b3b0
Lior Faingold <lior.faingold@kaltura.com>
2018-07-31 11:15:53 +0300

    fix BEO-5418

--- CachingProvider/LayeredCache/LayeredCache.cs
+++ CachingProvider/LayeredCache/LayeredCache.cs
@@ -174,8 +174,18 @@ namespace CachingProvider.LayeredCache
                         }
                     }
                 }
+                else
+                {
+                    res = true;
+                }
+
                 if (shouldAddSessionResults)
                 {
+                    if (results == null)
+                    {
+                        results = new Dictionary<string, T>();
+                    }
+
                     foreach (KeyValuePair<string, T> pair in sessionResultMapping)
                     {
                         results.Add(pair.Key, pair.Value);
