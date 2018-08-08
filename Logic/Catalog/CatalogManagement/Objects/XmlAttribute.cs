b5dfb1ca9edc7e09b380e915933b0e1336134032
Shir Bruchim <shir.bruchim@kaltura.com>
2018-08-08 12:45:32 +0300

    no message

---
+++ Logic/Catalog/CatalogManagement/Objects/XmlAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Catalog.CatalogManagement
+{
+    // TODO SHIR - FINISH 
+    public class XmlAttribute : Attribute
+    {
+        public string PropertyName { get; set; }
+
+        public bool IsKeyProperty { get; set; }
+
+        public bool PropertyValueRequired { get; set; }        
+
+        public bool IgnoreWhenGeneratingTemplate { get; set; }        
+    }
+}
