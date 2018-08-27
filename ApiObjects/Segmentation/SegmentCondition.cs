db56db8cae5868c299aeb65d7dd8658a3c26ab99
Sunny Simantov <sunny.simantov@kaltura.com>
2018-08-27 16:39:13 +0300

    develop BEO-5449 - add methods for add/update/delete/list segmentation types

---
+++ ApiObjects/Segmentation/SegmentCondition.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ApiObjects.Segmentation
+{
+    public class SegmentCondition
+    {
+    }
+
+    public class SegmentUserDataCondition : SegmentCondition
+    {
+        [JsonProperty()]
+        public string Field;
+
+        [JsonProperty()]
+        public string Value;
+    }
+
+    public class ContentScoreCondition : SegmentCondition
+    {
+        [JsonProperty()]
+        public int Score { get; set; }
+
+        [JsonProperty()]
+        public List<ContentActionCondition> Actions { get; set; }
+    }
+
+    public class ContentActionCondition
+    {
+        [JsonProperty()]
+        public ContentAction Action { get; set; }
+
+        [JsonProperty()]
+        public int Length { get; set; }
+
+        [JsonProperty()]
+        public int Multiplier { get; set; }
+    }
+
+    public class MonetizationScoredCondition : SegmentCondition
+    {
+        [JsonProperty()]
+        public int Score { get; set; }
+
+        [JsonProperty()]
+        public List<MonetizationCondition> Actions { get; set; }
+    }
+
+    public class MonetizationCondition
+    {
+        [JsonProperty()]
+        public MonetizationType Type;
+
+        [JsonProperty()]
+        public int MinimumPrice;
+
+        [JsonProperty()]
+        public int Multiplier;
+    }
+}
---
+++ ApiObjects/Segmentation/SegmentValues.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiObjects.Segmentation
+{
+    public class SegmentValue
+    {
+        [JsonProperty()]
+        public int Id;
+
+        [JsonProperty()]
+        public string Name;
+
+        // Name in other languages other then default (when language="*")        
+        [JsonProperty()]
+        public List<LanguageContainer> NamesWithLanguages { get; set; }
+
+        [JsonProperty()]
+        public string Value;
+
+        [JsonProperty()]
+        public int? Threshold;
+    }
+
+    public class SegmentValues : SegmentBaseValue
+    {
+        [JsonProperty(ItemTypeNameHandling = TypeNameHandling.All)]
+        public SegmentSource Source;
+
+        [JsonProperty()]
+        public int? Threshold;
+
+        [JsonProperty()]
+        public List<SegmentValue> Values;
+    }
+}
