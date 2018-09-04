a1bc5233950387a98c6b26f41d824fa41a165008
Sunny Simantov <sunny.simantov@kaltura.com>
2018-09-04 16:22:33 +0300

    BEO-5548 temporary fix - date meta parsing goes wrong for live assets

--- WebAPI/ObjectsConvertor/Mapping/CatalogMappings.cs
+++ WebAPI/ObjectsConvertor/Mapping/CatalogMappings.cs
@@ -2140,7 +2140,15 @@ namespace WebAPI.ObjectsConvertor.Mapping
                 {
                     if (!string.IsNullOrEmpty(meta.m_sValue))
                     {
-                        value = new KalturaLongValue() { value = SerializationUtils.ConvertToUnixTimestamp(DateTime.Parse(meta.m_sValue, CultureInfo.InvariantCulture)) };
+                        DateTime parsedDate;
+                        if (DateTime.TryParse(meta.m_sValue, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate))
+                        {
+                            value = new KalturaLongValue() { value = SerializationUtils.ConvertToUnixTimestamp(parsedDate) };
+                        }
+                        else
+                        {
+                            value = new KalturaLongValue() { value = 0 };
+                        }
                     }
                 }
                 else
