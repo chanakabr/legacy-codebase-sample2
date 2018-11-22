17fa9a4f32b17c37e4b32fd17797eb4f9fef83db
Sunny Simantov <sunny.simantov@kaltura.com>
2018-11-22 12:08:12 +0200

    TTV - add segmentation task handler

---
+++ SegmentationTaskHandler/SegmentationTaskRequest.cs
@@ -0,0 +1,41 @@
+﻿using ApiObjects;
+using ApiObjects.JsonSerializers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SegmentationTaskHandler
+{
+    [Serializable]
+    public class SegmentationTaskRequest
+    {
+        [JsonProperty("group_id")]
+        public int GroupID
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("task_type")]
+        public SegmentationTaskType TaskType
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("users_segments")]
+        public Dictionary<string, List<long>> UsersSegments
+        {
+            get;
+            set;
+        }
+    }
+
+    public enum SegmentationTaskType
+    {
+        update_user_segments
+    }
+}
