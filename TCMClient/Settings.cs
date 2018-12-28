6a93b9dc765c2f13907be3cfc6eb573d6eb8da9f
Jonathan Kanarek <jonathan.amit-kanarek@Kaltura.com>
2018-12-28 15:51:07 +0200

    Fixed dependencies

---
+++ TCMClient/Settings.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+#if NET45
+using System.Configuration;
+#endif
+
+namespace TCMClient
+{
+    public class Settings
+    {
+        private static object locker = new object();
+        private static Settings _Instance;
+        public static Settings Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    lock (locker)
+                    {
+                        if (_Instance == null)
+                        {
+                            _Instance = new Settings();
+                        }
+                    }
+                }
+                return _Instance;
+            }
+        }
+
+        private JObject m_Settings = new JObject();
+        
+        private string m_URL;
+        private string m_Application;
+        private string m_Host;
+        private string m_Environment;
+        private string m_AppID;
+        private string m_AppSecret;
+        private bool m_VerifySSL;
+        private string m_LocalPath;
+
+#region Constructors
+
+        private Settings()
+        {
+
+        }
+
+#endregion
+
+#region Public Methods
+
+#if NET45
+        /// <summary>
+        /// Initializes settings with data from local / remote source according to config
+        /// </summary>
+        /// <param name="fromLocal">determines the source</param>
+        public void Init(bool? fromLocal = null)
+        {
+            TCMConfiguration config = (TCMConfiguration)ConfigurationManager.GetSection("TCMConfig");
+            if (fromLocal.HasValue)
+            {
+                config.FromLocal = fromLocal.Value;
+            }
+            Init(config);
+        }
+#endif
+
+        /// <summary>
+        /// Initializes settings with data from local / remote source according to config
+        /// </summary>
+        /// <param name="config">all configuration properties in one object</param>
+        public void Init(TCMConfiguration config)
+        {
+            config.OverrideEnvironmentVariable();
+            if (config.FromLocal)
+            {
+                m_LocalPath = config.LocalPath;
+
+                //Populate settings from local
+                PopulateSettings(true);
+            }
+            else
+            {
+                //Populate settings from remote
+                Init(config.URL, config.Application, config.Host, config.Environment, config.AppID, config.AppSecret, config.VerifySSL, config.LocalPath);
+            }
+        }
+
+        /// <summary>
+        /// Initializes settings with data from remote source 
+        /// </summary>
+        /// <param name="url">Remote Server URL</param>
+        /// <param name="application">Application</param>
+        /// <param name="host">Host</param>
+        /// <param name="environment">Environment</param>
+        /// <param name="appID">App ID</param>
+        /// <param name="appSecret">App Secret</param>
+        /// <param name="localPath">Local Path</param>
+        [Obsolete]
+        public void Init(string url, string application, string host, string environment, string appID, string appSecret, string localPath = null)
+        {
+            m_URL = url;
+            m_Application = application;
+            m_Host = host;
+            m_Environment = environment;
+            m_AppID = appID;
+            m_AppSecret = appSecret;
+            m_LocalPath = localPath;
+
+            PopulateSettings(false);
+        }
+
+        /// <summary>
+        /// Initializes settings with data from remote source 
+        /// </summary>
+        /// <param name="url">Remote Server URL</param>
+        /// <param name="application">Application</param>
+        /// <param name="host">Host</param>
+        /// <param name="environment">Environment</param>
+        /// <param name="appID">App ID</param>
+        /// <param name="appSecret">App Secret</param>
+        /// <param name="verifySSL">App Secret</param>
+        /// <param name="localPath">Local Path</param>
+        public void Init(string url, string application, string host, string environment, string appID, string appSecret, bool verifySSL, string localPath = null)
+        {
+            m_URL = url;
+            m_Application = application;
+            m_Host = host;
+            m_Environment = environment;
+            m_AppID = appID;
+            m_AppSecret = appSecret;
+            m_VerifySSL = verifySSL;
+            m_LocalPath = localPath;
+
+            PopulateSettings(false);
+        }
+
+        /// <summary>
+        /// Returns the value from settings
+        /// </summary>
+        /// <typeparam name="T">Value type</typeparam>
+        /// <param name="key">Key</param>
+        /// <returns></returns>
+        public T GetValue<T>(string key)
+        {
+            try
+            {
+                JToken token = m_Settings.SelectToken(key.ToLower());
+                if (token != null)
+                {
+                    return token.ToObject<T>();
+                }
+                else return default(T);
+            }
+            catch (Exception ex)
+            {
+                return default(T);
+            }
+
+        }
+
+        /// <summary>
+        /// Return the JSON object
+        /// </summary>
+        /// <returns></returns>
+        public JObject Get()
+        {
+            return m_Settings;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private string getSettingsFromServer()
+        {
+            string settings = null;
+
+            HttpWebResponse httpWebResponse = null;
+
+            try
+            {
+                if (!m_VerifySSL)
+                {
+                    ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                }
+
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(string.Format("{0}/{1}/{2}/{3}?app_id={4}&app_secret={5}", m_URL, m_Application, m_Host, m_Environment, m_AppID, m_AppSecret));
+                httpWebRequest.Method = "GET";
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Timeout = 5000;
+
+                httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                using (StreamReader sr = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    settings = sr.ReadToEnd();
+                }
+
+                string pathToLocalFile = getPathToLocalFile();
+
+                using (StreamWriter sw = new StreamWriter(pathToLocalFile))
+                {
+                    sw.Write(settings);
+                }
+            }
+            catch
+            {
+            }
+            finally
+            {
+                if (httpWebResponse != null)
+                    httpWebResponse.Close();
+            }
+
+            return settings;
+        }
+
+        private string getSettingsFromLocal()
+        {
+            string settings = null;
+            string pathToLocalFile = getPathToLocalFile();
+
+            if (File.Exists(pathToLocalFile))
+            {
+                using (StreamReader sr = new StreamReader(pathToLocalFile))
+                {
+                    settings = sr.ReadToEnd();
+                }
+            }
+
+            return settings;
+        }
+
+        private string getSettings(bool fromLocal)
+        {
+            string settings = null;
+
+            if (fromLocal)
+            {
+                settings = getSettingsFromLocal();
+            }
+            else
+            {
+                settings = getSettingsFromServer();
+
+                //if no data from server, try to take data from local file
+                if (string.IsNullOrEmpty(settings))
+                {
+                    settings = getSettingsFromLocal();
+                }
+            }
+
+            return settings;
+        }
+
+        private void PopulateSettings(bool fromLocal)
+        {
+            string settings = getSettings(fromLocal);
+
+            if (!string.IsNullOrEmpty(settings) && !settings.ToLower().Equals("null"))
+            {
+                try
+                {
+                    foreach (var record in JsonConvert.DeserializeObject<Dictionary<string, object>>(settings))
+                    {
+                        if (record.Value is JObject)
+                        {
+                            m_Settings.Add(record.Key.ToLower(), ParseNestedObjects((JObject)record.Value)); 
+                        }
+                        else
+                        {
+                            string _key = record.Key;
+                            m_Settings.Add(_key.ToLower(), new JValue(record.Value));    
+                        }
+                        
+                    }
+                }
+                catch
+                {
+                    throw new Exception("Source is corrupted.");
+                }
+            }
+            else
+            {
+                throw new Exception("Source is empty / not avaiable.");
+            }
+        }
+
+        private JObject ParseNestedObjects(JObject settings)
+        {
+            JObject retJObject = new JObject();
+            //Iterate siblings
+            foreach (var record in settings)
+            {
+                if (record.Value is JObject)
+                {
+                    //if JObject (complex), go deeper in the hierarchy
+                    JObject parsedJObject = ParseNestedObjects((JObject) record.Value);
+                    retJObject.Add(record.Key.ToLower(), parsedJObject);
+                }
+                else
+                {
+                    JProperty token = new JProperty(record.Key.ToLower(), record.Value);
+                    retJObject.Add(token);
+                }
+            }
+            return retJObject;
+        }
+
+        private string getPathToLocalFile()
+        {
+            string pathToLocalFile = null;
+
+            if (string.IsNullOrEmpty(m_LocalPath))
+            {
+                pathToLocalFile = Environment.CurrentDirectory + "/tcm.config";
+            }
+            else
+            {
+                pathToLocalFile = m_LocalPath + "/tcm.config";
+            }
+
+            return pathToLocalFile;
+        }
+
+#endregion
+    }
+}
---
+++ TCMClient/TCMConfiguration.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Configuration;
+
+namespace TCMClient
+{
+#if NET45
+    public class TCMConfiguration : ConfigurationSection
+#else
+    public class TCMConfiguration
+#endif
+    {
+        private string m_URL = null;
+
+        private string m_Application = null;
+
+        private string m_Host = null;
+
+        private string m_Environment = null;
+
+        private string m_AppID = null;
+
+        private string m_AppSecret = null;
+
+        private bool? m_VerifySSL = null;
+
+        private bool? m_FromLocal = null;
+
+        private string m_LocalPath = null;
+
+        public void OverrideEnvironmentVariable()
+        {
+            string application = System.Environment.GetEnvironmentVariable("TCM_APP");
+            string url = System.Environment.GetEnvironmentVariable("TCM_URL");
+            string environment = System.Environment.GetEnvironmentVariable("TCM_SECTION");
+            string appID = System.Environment.GetEnvironmentVariable("TCM_APP_ID");
+            string appSecret = System.Environment.GetEnvironmentVariable("TCM_APP_SECRET");
+            string verifySSL = System.Environment.GetEnvironmentVariable("TCM_VERIFY_SSL");
+
+            if (application != null)
+            {
+                Application = application;
+            }
+
+            if (url != null)
+            {
+                URL = url;
+            }
+
+            if (environment != null)
+            {
+                Environment = environment;
+            }
+
+            if (appID != null)
+            {
+                AppID = appID;
+            }
+
+            if (appSecret != null)
+            {
+                AppSecret = appSecret;
+            }
+
+            if (verifySSL != null && (verifySSL.Equals("1") || verifySSL.ToLower().Equals("true")))
+            {
+                VerifySSL = true;
+            }
+            else
+            {
+                VerifySSL = false;
+            }
+        }
+
+#if NET45
+        [ConfigurationProperty("URL", IsRequired = true)]
+#endif
+        public string URL
+        {
+            get =>
+#if NET45
+                m_URL != null ? m_URL : this["URL"] as string;
+#else
+                m_URL;
+#endif
+
+
+            set
+            {
+                m_URL = value;
+            }
+        }
+
+#if NET45
+        [ConfigurationProperty("Application", IsRequired = true)]
+#endif
+        public string Application
+        {
+            get =>
+#if NET45
+                m_Application != null ? m_Application : this["Application"] as string;
+#else
+                m_Application;
+#endif
+
+            set
+            {
+                m_Application = value;
+            }
+        }
+
+#if NET45
+        [ConfigurationProperty("Host", IsRequired = true)]
+#endif
+        public string Host
+        {
+            get =>
+#if NET45
+                m_Host != null ? m_Host : this["Host"] as string;
+#else
+                m_Host;
+#endif
+
+            set
+            {
+                m_Host = value;
+            }
+        }
+
+#if NET45
+        [ConfigurationProperty("Environment", IsRequired = true)]
+#endif
+        public string Environment
+        {
+            get =>
+#if NET45
+                m_Environment != null ? m_Environment : this["Environment"] as string;
+#else
+                m_Environment;
+#endif
+
+            set
+            {
+                m_Environment = value;
+            }
+        }
+
+#if NET45
+        [ConfigurationProperty("AppID", IsRequired = true)]
+#endif
+        public string AppID
+        {
+            get =>
+#if NET45
+                m_AppID != null ? m_AppID : this["AppID"] as string;
+#else
+                m_AppID;
+#endif
+
+            set
+            {
+                m_AppID = value;
+            }
+        }
+
+#if NET45
+        [ConfigurationProperty("AppSecret", IsRequired = true)]
+#endif
+        public string AppSecret
+        {
+            get =>
+#if NET45
+               m_AppSecret != null ? m_AppSecret : this["AppSecret"] as string;
+#else
+                m_AppSecret;
+#endif
+
+            set
+            {
+                m_AppSecret = value;
+            }
+        }
+
+#if NET45
+        [ConfigurationProperty("VerifySSL", DefaultValue = "false", IsRequired = false)]
+#endif
+        public bool VerifySSL
+        {
+            get =>
+#if NET45
+                m_VerifySSL.HasValue ? m_VerifySSL.Value : (Boolean)this["VerifySSL"];
+#else
+                m_VerifySSL.Value;
+#endif
+
+            set
+            {
+                m_VerifySSL = value;
+            }
+        }
+
+#if NET45
+        [ConfigurationProperty("FromLocal", DefaultValue = "false", IsRequired = false)]
+#endif
+        public Boolean FromLocal
+        {
+            get =>
+#if NET45
+                m_FromLocal.HasValue ? m_FromLocal.Value : (Boolean)this["FromLocal"];
+#else
+                m_FromLocal.Value;
+#endif
+
+            set
+            {
+                m_FromLocal = value;
+            }
+        }
+
+#if NET45
+        [ConfigurationProperty("LocalPath", IsRequired = false)]
+#endif
+        public string LocalPath
+        {
+            get =>
+#if NET45
+                m_LocalPath != null ? m_LocalPath : this["LocalPath"] as string;
+#else
+                m_LocalPath;
+#endif
+
+            set
+            {
+                m_LocalPath = value;
+            }
+        }
+    }
+}
