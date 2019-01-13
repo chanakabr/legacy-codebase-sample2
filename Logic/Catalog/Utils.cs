9e13f1acc5fb9f3b08953021546a2127df08f5e4
Lior Faingold <lior.faingold@kaltura.com>
2019-01-13 15:44:08 +0200

    fix assets cache for medias that don't exist

--- Logic/Catalog/Utils.cs
+++ Logic/Catalog/Utils.cs
@@ -29,6 +29,7 @@ namespace Core.Catalog
         private static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
 
         public const int DEFAULT_CATALOG_LOG_THRESHOLD_MILLISEC = 500; // half a second
+        public const string EMPTY_ASSET_ID = "0";
 
         public static string GetSignature(string sSigningString, Int32 nGroupID)
         {
@@ -1634,7 +1635,7 @@ namespace Core.Catalog
                     }
 
                     string keyFormat = "{0}_{1}"; // mapped asset key format = assetType_assetId
-                    Dictionary<string, BaseObject> mappedAssets = unOrderedAssets.ToDictionary(x => string.Format(keyFormat, x.AssetType.ToString(), x.AssetId), x => x);
+                    Dictionary<string, BaseObject> mappedAssets = unOrderedAssets.Where(x => x.AssetId != EMPTY_ASSET_ID).ToDictionary(x => string.Format(keyFormat, x.AssetType.ToString(), x.AssetId), x => x);                    
                     foreach (BaseObject baseAsset in assets)
                     {
                         string key = string.Empty;
@@ -1652,6 +1653,7 @@ namespace Core.Catalog
                         {
                             key = string.Format(keyFormat, baseAsset.AssetType.ToString(), baseAsset.AssetId);
                         }
+
                         if (mappedAssets.ContainsKey(key))
                         {
                             if (isNpvr)
@@ -1675,6 +1677,11 @@ namespace Core.Catalog
                             {
                                 result.Add(mappedAssets[key]);
                             }
+                        }
+                        // support for TVPAPI (returns empty object for assets that don't exist)
+                        else
+                        {
+                            result.Add(null);
                         }
                     }
                 }
@@ -1881,12 +1888,20 @@ namespace Core.Catalog
                     if (ids != null && groupId.HasValue)
                     {
                         assets = CatalogLogic.CompleteMediaDetails(ids, groupId.Value, filter, false);
-                        res = assets.Count() == ids.Count();
+                        res = assets.Count == ids.Count;
                     }
 
                     if (res)
                     {
-                        result = assets.ToDictionary(x => LayeredCacheKeys.GetAssetWithLanguageKey(eAssetTypes.MEDIA.ToString(), x.AssetId, filter.m_nLanguage), x => x);
+                        result = assets.Where(x => x != null).ToDictionary(x => LayeredCacheKeys.GetAssetWithLanguageKey(eAssetTypes.MEDIA.ToString(), x.AssetId, filter.m_nLanguage), x => x);
+                        List<string> unvalidMediaIdsToAdd = ids.Select(x => x.ToString()).Except(assets.Where(x => x != null).Select(x => x.AssetId)).ToList();
+                        if (unvalidMediaIdsToAdd != null && unvalidMediaIdsToAdd.Count > 0)
+                        {
+                            foreach (string mediaId in unvalidMediaIdsToAdd)
+                            {
+                                result.Add(LayeredCacheKeys.GetAssetWithLanguageKey(eAssetTypes.MEDIA.ToString(), mediaId, filter.m_nLanguage), new MediaObj() { AssetId = "0" });
+                            }
+                        }
                     }
                     else
                     {
