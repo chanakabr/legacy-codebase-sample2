bdb55fd1a73c8245e7dfc3f836e35b472eaa7c1d
Shir Bruchim <shir.bruchim@kaltura.com>
2019-01-24 14:28:03 +0200

    Date refactoring code

--- StreamingProvider/HarmonicProvider.cs
+++ StreamingProvider/HarmonicProvider.cs
@@ -198,12 +198,10 @@ namespace StreamingProvider
             return streamType;
         }
 
-        // TODO SHIR - DELETE
         private long ConvertDateToEpochTimeInMilliseconds(DateTime dateTime)
         {
-            return long.Parse((Math.Floor(dateTime.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds).ToString()));
+            return long.Parse((Math.Floor(dateTime.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds).ToString()));
         }
-
 
         protected override bool ValidParameters(Dictionary<string, object> dParams)
         {
