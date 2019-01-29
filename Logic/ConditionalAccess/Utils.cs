1cc6c523082c01b365f328eebc27a5b9ea38df4a
guykatznelson <guy.katznelson@kaltura.com>
2019-01-29 15:20:52 +0200

    added new filter for ExternalRecording, supports metadata

--- Logic/ConditionalAccess/Utils.cs
+++ Logic/ConditionalAccess/Utils.cs
@@ -5511,12 +5511,14 @@ namespace Core.ConditionalAccess
 
         private static Dictionary<long, Recording> FilterViewableRecordingsOnly(Dictionary<long, Recording> domainRecordingIdToRecordingMap)
         {
-            if (domainRecordingIdToRecordingMap == null||!domainRecordingIdToRecordingMap.Any())
+            if (domainRecordingIdToRecordingMap == null || !domainRecordingIdToRecordingMap.Any())
                 return domainRecordingIdToRecordingMap;
+
+            var epoc = DateTime.UtcNow.ToUtcUnixTimestampSeconds();
 
             return domainRecordingIdToRecordingMap
                 .Where(x => x.Value.ViewableUntilDate.HasValue &&
-                            x.Value.ViewableUntilDate.Value - DateTime.UtcNow.ToUtcUnixTimestampSeconds() > 0)
+                            x.Value.ViewableUntilDate.Value > epoc)
                 .ToDictionary(x => x.Key, x => x.Value);
         }
 
