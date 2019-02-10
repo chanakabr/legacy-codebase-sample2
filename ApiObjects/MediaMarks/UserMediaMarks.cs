2dc5c6458ccdaf239b17c6bb2edeb187d3e0b07a
Sunny <sunny.simantov@kaltura.com>
2019-02-10 15:09:06 +0200

    add user media marks

---
+++ ApiObjects/MediaMarks/UserMediaMarks.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiObjects.MediaMarks
+{
+    [JsonObject()]
+    [Serializable]
+    public class UserMediaMarks
+    {
+        [JsonProperty("mediaMarks")]
+        public List<AssetAndLocation> mediaMarks;
+
+        public UserMediaMarks()
+        {
+            mediaMarks = new List<AssetAndLocation>();
+        }
+    }
+
+    public class AssetAndLocation
+    {
+        [JsonProperty("assetId")]
+        public int AssetId { get; set; }
+        
+        [JsonProperty("assetType")]
+        public eAssetTypes AssetType { get; set; }
+
+        [JsonProperty("createdAt")]
+        public long CreatedAt { get; set; }
+
+        public AssetAndLocation()
+        {
+
+        }
+    }
+}
