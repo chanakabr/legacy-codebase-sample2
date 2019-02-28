99d2a290cff6a1f141543f21be612bc025d9b353
Jonathan Kanarek <jonathan.amit-kanarek@Kaltura.com>
2019-02-28 11:25:07 +0200

    Trigger auto increment

--- Ingest/Properties/AssemblyInfo.cs
+++ Ingest/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.1.2.*")]
-[assembly: AssemblyFileVersion("5.1.2.0")]
+[assembly: AssemblyInformationalVersion("5.1-2-72-g155b66bb-dirty")]
