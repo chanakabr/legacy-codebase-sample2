8b7dcbd62a30462322654e0742572d07a8c01644
Shir Bruchim <shir.bruchim@kaltura.com>
2019-03-19 15:08:27 +0200

    no message

--- TVinciShared/DateUtils.cs
+++ TVinciShared/DateUtils.cs
@@ -400,7 +400,6 @@ namespace TVinciShared
         public static DateTime ExtractDate(string dateTime, string format)
         {
             DateTime result = DateTime.ParseExact(dateTime, format, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
-            //DateTime result = DateTime.Parse(dateTime);
             return result;
         }
     }
