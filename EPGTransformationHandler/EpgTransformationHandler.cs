d67e55032815de053366ad82960f679ca2af364b
Arthur Vaverko <Arthur.Vaverko@kaltura.com>
2019-03-27 23:42:30 +0200

    BEO-6417 Core Ingest - Transformation service (added EventBus)

---
+++ EPGTransformationHandler/EpgTransformationHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using ApiObjects.EventBus;
+using EventBus.Abstraction;
+using KLogMonitor;
+using Microsoft.Extensions.Logging;
+
+namespace EPGTransformationHandler
+{
+    public class SimpleMessageServiceEventEventHandler : IServiceEventHandler<EpgTransformationEvent>
+    {
+        private static readonly KLogger _Logger = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
+
+        public SimpleMessageServiceEventEventHandler()
+        {
+        }
+        public Task Handle(EpgTransformationEvent serviceEvent)
+        {
+            _Logger.Info($"I'm handling it: [{serviceEvent.Data}]");
+            // TODO: Download file from s3
+            // TODO: send file data to transformation adapater
+            //...
+
+            return Task.CompletedTask;
+        }
+     
+    }
+
+}
---
+++ EPGTransformationHandler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using EventBus.RabbitMQ;
+using Microsoft.Extensions.Hosting;
+
+namespace EPGTransformationHandler
+{
+    internal static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureEventBustConsumer(c =>
+                {
+                    c.HandlerUniqueId = "13232";
+                    c.ConcurrentConsumers = 3;
+                });
+            await builder.RunConsoleAsync();
+
+        }
+    }
+}
