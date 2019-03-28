6f4363e77701d81e41077962cfe4550b0aa7c922
Sunny <sunny.simantov@kaltura.com>
2019-03-28 17:23:11 +0200

    Continue work on transformation handler

--- EPGTransformationHandler/EpgTransformationHandler.cs
+++ EPGTransformationHandler/EpgTransformationHandler.cs
@@ -5,6 +5,7 @@ using ApiObjects.EventBus;
 using EventBus.Abstraction;
 using KLogMonitor;
 using Microsoft.Extensions.Logging;
+using Core.Catalog.CatalogManagement;
 
 namespace EPGTransformationHandler
 {
@@ -18,11 +19,13 @@ namespace EPGTransformationHandler
 
         public Task Handle(EpgTransformationEvent serviceEvent)
         {
-            _Logger.Info($"I'm handling it, requestId:[{serviceEvent.RequestId}], jobId:[{serviceEvent.BulkUploadData.Id}]");
+            _Logger.Info($"I'm handling it, requestId:[{serviceEvent.RequestId}], jobId:[{serviceEvent.BulkUpload.Id}]");
             // TODO: Download file from s3
             // TODO: send file data to transformation adapater
             //...
 
+            BulkUploadManager.ProcessBulkUpload(serviceEvent.GroupId, serviceEvent.UserId, serviceEvent.BulkUpload.Id);
+            
             return Task.CompletedTask;
         }
      
