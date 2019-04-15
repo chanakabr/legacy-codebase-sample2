072a344a95b57916f6690cb91fad733007823853
Shir Bruchim <shir.bruchim@kaltura.com>
2019-04-15 23:42:48 +0300

    no message

--- EpgIngest/Utils.cs
+++ EpgIngest/Utils.cs
@@ -217,7 +217,7 @@ namespace EpgIngest
 
                     if (fieldTypes == FieldTypes.Meta)
                     {
-                        // TODO SHIR - DO THE SAME IM OPC
+                        // TODO - DO THE SAME In OPC account
                         item.isProtectFromUpdates = ODBCWrapper.Utils.GetIntSafeVal(dr, "is_protect_from_updates") == 0 ? false : true;
                     }
                 }
