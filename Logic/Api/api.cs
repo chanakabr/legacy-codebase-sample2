45bf6c3d1f1769cbde20b642f6a018a660449317
guykatznelson <guy.katznelson@kaltura.com>
2019-04-23 11:59:54 +0300

    BEO-6388

--- Logic/Api/api.cs
+++ Logic/Api/api.cs
@@ -8440,7 +8440,7 @@ namespace Core.Api
             return result;
         }
 
-        public static ApiObjects.Roles.PermissionsResponse GetGroupPermissions(int groupId)
+        public static ApiObjects.Roles.PermissionsResponse GetGroupPermissions(int groupId, long? roleIdIn)
         {
             PermissionsResponse response = new PermissionsResponse()
             {
@@ -8449,7 +8449,7 @@ namespace Core.Api
 
             try
             {
-                response.Permissions = APILogic.Api.Managers.RolesPermissionsManager.GetGroupPermissions(groupId);
+                response.Permissions = APILogic.Api.Managers.RolesPermissionsManager.GetGroupPermissions(groupId, roleIdIn);
                 if (response.Permissions != null)
                 {
                     response.Permissions = response.Permissions.OrderBy(x => x.Id).ToList();
