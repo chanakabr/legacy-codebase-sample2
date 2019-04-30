5adb946386a3af65535c9079cdfa036779853c82
Arthur Vaverko <Arthur.Vaverko@kaltura.com>
2019-04-30 22:14:15 +0300

    updated BulkUploadIngestEvent to include relevant data

---
+++ ApiObjects/BulkUpload/EpgProgramBulkUploadObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ApiObjects.BulkUpload
+{
+    public class EpgProgramBulkUploadObject : IBulkUploadObject
+    {
+        public programme ParsedProgramObject { get; set; }
+        public IList<EpgCB> EpgCbObjects { get; set; }
+        public int ParentGroupId { get; set; }
+        public int GroupId { get; set; }
+        public int ChannelId { get; set; }
+        public string ChannelExternalId { get; set; }
+        public long LinearMediaId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
