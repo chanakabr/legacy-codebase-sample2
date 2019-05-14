bfb071d1da5104c7a9f19ce0ae29e30a06b7b624
Arthur Vaverko <Arthur.Vaverko@kaltura.com>
2019-05-14 14:18:33 +0300

    moved epgIngestVersion to groupFeatures

--- Ingest/IngestController.cs
+++ Ingest/IngestController.cs
@@ -101,8 +101,8 @@ namespace Ingest
                                     OperationContext.Current.IncomingMessageProperties[Constants.TOPIC] = "EPG Ingest";
                                 }
 
-                                var epgIngestVersion = Core.GroupManagers.GroupSettingsManager.GetEpgIngestVersion(groupID);
-                                if (epgIngestVersion > 1)
+                                var doesGroupUseNewEpgIngest = Core.GroupManagers.GroupSettingsManager.DoesGroupUseNewEpgIngest(groupID);
+                                if (doesGroupUseNewEpgIngest)
                                 {
                                     IngestEpgUsingBulkUpoad(groupID, request);
                                     ingestResponse.Status = "Ingest redirected to Add from bulk upload";
