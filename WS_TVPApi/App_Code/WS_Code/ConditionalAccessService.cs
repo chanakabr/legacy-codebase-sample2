07b7c00a0b623aa044c74f971d706c9e9d110a52
AnatGr <anat.grossberg@kaltura.com>
2019-05-16 12:04:35 +0300

    Add log

--- WS_TVPApi/App_Code/WS_Code/ConditionalAccessService.cs
+++ WS_TVPApi/App_Code/WS_Code/ConditionalAccessService.cs
@@ -1601,10 +1601,12 @@ namespace TVPApiServices
             {
                 try
                 {
-                    userIP = SiteHelper.GetClientIP();
+                    string ip = SiteHelper.GetClientIP();
+
+                    logger.DebugFormat("BEO-6653 - GetClientIP:{0}, useriP:{1}", ip, userIP == null ? "NULL" : userIP);
 
                     response = new ApiConditionalAccessService(groupId, initObj.Platform).
-                        GetEPGLicensedLink(initObj.SiteGuid, mediaFileID, EPGItemID, startTime, basicLink, userIP, refferer, countryCd2, languageCode3, initObj.UDID, formatType);
+                        GetEPGLicensedLink(initObj.SiteGuid, mediaFileID, EPGItemID, startTime, basicLink, ip, refferer, countryCd2, languageCode3, initObj.UDID, formatType);
                 }
                 catch (Exception ex)
                 {
