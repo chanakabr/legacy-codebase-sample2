597a66bccbcc3c2ccc73cb814d4163acdff72c2a
Jonathan Kanarek <jonathan.amit-kanarek@Kaltura.com>
2019-05-31 13:58:52 +0300

    5.2.3

--- WebAPI/Properties/AssemblyInfo.cs
+++ WebAPI/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.2.0.*")]
+[assembly: AssemblyVersion("5.2.3.*")]
 [assembly: AssemblyInformationalVersion("5.2-0-72-g155b66bb-dirty")]
