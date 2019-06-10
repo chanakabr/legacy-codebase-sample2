4b5567688c1e98c137cae55ea0997896a7cd74d5
Shir Bruchim <shir.bruchim@kaltura.com>
2019-06-10 16:45:04 +0300

    fix BEO-6778

--- Logic/Catalog/CatalogManagement/Managers/AssetManager.cs
+++ Logic/Catalog/CatalogManagement/Managers/AssetManager.cs
@@ -1176,7 +1176,7 @@ namespace Core.Catalog.CatalogManagement
                 }
 
                 // Update asset catalogStartDate and finalEndDate
-                assetToUpdate.CatalogStartDate = assetCatalogStartDate;
+                assetToUpdate.CatalogStartDate = assetCatalogStartDate ?? assetToUpdate.CatalogStartDate;
                 assetToUpdate.FinalEndDate = assetFinalEndDate ?? assetToUpdate.FinalEndDate;
 
                 // Add Name meta values (for languages that are not default), Name can only be updated
