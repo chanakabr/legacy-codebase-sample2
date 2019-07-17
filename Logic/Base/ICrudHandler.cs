c15e23dcf7c9032d1c883a79f2a53fd504f334ce
Shir Bruchim <shir.bruchim@kaltura.com>
2019-07-17 11:57:08 +0300

    working on best practice for CRUD actions

--- Logic/Base/ICrudHandler.cs
+++ Logic/Base/ICrudHandler.cs
@@ -1,33 +1,16 @@
 ﻿using ApiObjects.Base;
 using ApiObjects.Response;
 using System;
-using System.Collections.Generic;
 
 namespace ApiLogic.Base
 {
-    // TODO SHIR - CRUD changes
     public interface ICrudHandler<ICrudHandeledObject, IdentifierT, ICrudFilter>
         where IdentifierT : IConvertible
     {
-        GenericResponse<ICrudHandeledObject> Add(int groupId, ICrudHandeledObject objectToAdd, Dictionary<string, object> extraParams = null);
-        GenericResponse<ICrudHandeledObject> Update(int groupId, ICrudHandeledObject objectToUpdate, Dictionary<string, object> extraParams = null);
-        Status Delete(int groupId, IdentifierT id, Dictionary<string, object> extraParams = null);
-        GenericResponse<ICrudHandeledObject> Get(int groupId, IdentifierT id, Dictionary<string, object> extraParams = null);
-        // TODO SHIR - FINISH GENERIC LIST METHOD in ICrudHandler
+        GenericResponse<ICrudHandeledObject> Add(ContextData contextData, ICrudHandeledObject objectToAdd);
+        GenericResponse<ICrudHandeledObject> Update(ContextData contextData, ICrudHandeledObject objectToUpdate);
+        Status Delete(ContextData contextData, IdentifierT id);
+        GenericResponse<ICrudHandeledObject> Get(ContextData groupId, IdentifierT id);
         GenericListResponse<ICrudHandeledObject> List(ICrudFilter filter);
     }
-
-    //public abstract class BaseCrudHandler<CoreT> where CoreT : ICrudHandeledObject
-    //{
-    //    //protected static readonly KLogger log = new KLogger(MethodBase.GetCurrentMethod().DeclaringType.ToString());
-
-    //    public abstract GenericResponse<CoreT> Add(int groupId, CoreT objectToAdd, Dictionary<string, object> extraParams = null);
-    //    public abstract GenericResponse<CoreT> Update(int groupId, CoreT objectToUpdate, Dictionary<string, object> extraParams = null);
-    //    public abstract Status Delete(int groupId, long id, Dictionary<string, object> funcParams);
-    //    public abstract GenericResponse<CoreT> Get(int groupId, long id);
-    //    private BaseCrudHandler() { }
-
-        
-    //    //GenericListResponse<CoreT> List(U filter, KalturaFilter);
-    //}
 }
