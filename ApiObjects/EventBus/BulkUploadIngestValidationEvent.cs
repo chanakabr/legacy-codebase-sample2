6133c35fa68be66780525adefd36a41080d4a1b3
guykatznelson <guy.katznelson@kaltura.com>
2019-07-30 17:48:53 +0300

    BEO-6975

---
+++ ApiObjects/EventBus/BulkUploadIngestValidationEvent.cs
@@ -0,0 +1,18 @@
+﻿using ApiObjects.BulkUpload;
+using EventBus.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApiObjects.EventBus
+{
+    public class BulkUploadIngestValidationEvent : BulkUploadEvent
+    {
+        public List<EpgProgramBulkUploadObject> EPGs { get; set; }
+        public List<EpgProgramBulkUploadObject> EdgeProgramsToUpdate { get; set; }
+        public DateTime DateOfProgramsToIngest { get; set; }
+        public IDictionary<string, LanguageObj> Languages { get; set; }
+        public Dictionary<string, BulkUploadProgramAssetResult> Results { get; set; }
+    }
+}
