9702de485197cdf18c65573a66b22989569eef6b
Sunny <sunny.simantov@kaltura.com>
2019-09-11 17:11:06 +0300

    convert export handler to message bus

---
+++ ApiObjects/EventBus/ExportRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventBus.Abstraction;
+using Newtonsoft.Json;
+
+namespace ApiObjects.EventBus
+{
+    [Serializable]
+    public class ExportRequest : ServiceEvent
+    {
+        [JsonProperty("task_id")]
+        public long TaskId { get; set; }
+
+        [JsonProperty("version")]
+        public string Version { get; set; }
+    }
+}
