abf0d3cf81b99cc88f89926ad2c7d945adbd775a
Matan Oved <matan.oved@Kaltura.com>
2019-09-11 13:01:21 +0300

    Add eResponseStatus of passwordPolicy service

--- WebAPI/Controllers/PasswordPolicyController.cs
+++ WebAPI/Controllers/PasswordPolicyController.cs
@@ -1,14 +1,15 @@
 ﻿using ApiObjects;
+using ApiObjects.Response;
 using WebAPI.Managers.Scheme;
 using WebAPI.Models.Users;
 
 namespace WebAPI.Controllers
 {
     [Service("passwordPolicy")]
-    [AddAction]
-    [UpdateAction]
-    [DeleteAction]
-    [ListAction]
+    [AddAction(ClientThrows = new eResponseStatus[] { eResponseStatus.Error, eResponseStatus.RoleDoesNotExists })]
+    [UpdateAction(ClientThrows = new eResponseStatus[] { eResponseStatus.Error, eResponseStatus.PasswordPolicyDoesNotExist, eResponseStatus.RoleDoesNotExists })]
+    [DeleteAction(ClientThrows = new eResponseStatus[] { eResponseStatus.Error, eResponseStatus.PasswordPolicyDoesNotExist })]
+    [ListAction(ClientThrows = new eResponseStatus[] { eResponseStatus.Error, eResponseStatus.PasswordPolicyDoesNotExist })]
     public class PasswordPolicyController : KalturaCrudController<KalturaPasswordPolicy, KalturaPasswordPolicyListResponse, PasswordPolicy, long, KalturaPasswordPolicyFilter, PasswordPolicyFilter>
     {
     }
