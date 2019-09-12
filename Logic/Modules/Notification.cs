9b96a98168dee962a91e2652d1b29dc4901a4199
Sunny <sunny.simantov@kaltura.com>
2019-09-12 14:53:15 +0300

    convert initiate notification handler to message bus

--- Logic/Modules/Notification.cs
+++ Logic/Modules/Notification.cs
@@ -12,6 +12,8 @@ using Newtonsoft.Json;
 using QueueWrapper.Queues.QueueObjects;
 using APILogic.Notification;
 using Core.Catalog;
+using ApiObjects.EventBus;
+using ConfigurationManager;
 
 namespace Core.Notification
 {
@@ -537,15 +539,32 @@ namespace Core.Notification
 
         public static bool AddInitiateNotificationActionToQueue(int groupId, eUserMessageAction userAction, int userId, string udid, string pushToken = "")
         {
-            InitiateNotificationActionQueue que = new InitiateNotificationActionQueue();
-            ApiObjects.QueueObjects.UserNotificationData messageAnnouncementData = new ApiObjects.QueueObjects.UserNotificationData(groupId, (int)userAction, userId, udid, pushToken);
+            bool res = true;
 
-            bool res = que.Enqueue(messageAnnouncementData, ROUTING_KEY_INITIATE_NOTIFICATION_ACTION);
+            var eventBus = EventBus.RabbitMQ.EventBusPublisherRabbitMQ.GetInstanceUsingTCMConfiguration();
+            var serviceEvent = new InitiateNotificationActionRequest()
+            {
+                GroupId = groupId,
+                pushToken = pushToken,
+                Udid = udid,
+                UserAction = userAction,
+                UserId = userId
+            };
 
-            if (res)
-                log.DebugFormat("Successfully inserted a message to notification action queue. user id: {0}, device id: {1}, push token: {2}, group id: {3}, user action: {4}", userId, udid, pushToken, groupId, userAction);
-            else
-                log.ErrorFormat("Error while inserting to notification action queue.  user id: {0}, device id: {1}, push token: {2}, group id: {3}, user action: {4}", userId, udid, pushToken, groupId, userAction);
+            eventBus.Publish(serviceEvent);
+
+            if (ApplicationConfiguration.ShouldSupportCeleryMessages.Value)
+            {
+                InitiateNotificationActionQueue que = new InitiateNotificationActionQueue();
+                ApiObjects.QueueObjects.UserNotificationData messageAnnouncementData = new ApiObjects.QueueObjects.UserNotificationData(groupId, (int)userAction, userId, udid, pushToken);
+
+                res = que.Enqueue(messageAnnouncementData, ROUTING_KEY_INITIATE_NOTIFICATION_ACTION);
+
+                if (res)
+                    log.DebugFormat("Successfully inserted a message to notification action queue. user id: {0}, device id: {1}, push token: {2}, group id: {3}, user action: {4}", userId, udid, pushToken, groupId, userAction);
+                else
+                    log.ErrorFormat("Error while inserting to notification action queue.  user id: {0}, device id: {1}, push token: {2}, group id: {3}, user action: {4}", userId, udid, pushToken, groupId, userAction);
+            }
 
             return res;
         }
--- Logic/Users/Utils.cs
+++ Logic/Users/Utils.cs
@@ -19,7 +19,7 @@ using System.Security.Principal;
 using System.Text;
 using APILogic.Users;
 using TVinciShared;
-
+using ApiObjects.EventBus;
 
 namespace Core.Users
 {
@@ -1047,15 +1047,30 @@ namespace Core.Users
         {
             if (Notification.NotificationSettings.IsNotificationSettingsExistsForPartner(groupId))
             {
-                InitiateNotificationActionQueue que = new InitiateNotificationActionQueue();
-                ApiObjects.QueueObjects.UserNotificationData messageAnnouncementData = new ApiObjects.QueueObjects.UserNotificationData(groupId, (int)userAction, userId, udid, pushToken);
+                var eventBus = EventBus.RabbitMQ.EventBusPublisherRabbitMQ.GetInstanceUsingTCMConfiguration();
+                var serviceEvent = new InitiateNotificationActionRequest()
+                {
+                    GroupId = groupId,
+                    pushToken = pushToken,
+                    Udid = udid,
+                    UserAction = userAction,
+                    UserId = userId
+                };
 
-                bool res = que.Enqueue(messageAnnouncementData, ROUTING_KEY_INITIATE_NOTIFICATION_ACTION);
+                eventBus.Publish(serviceEvent);
 
-                if (res)
-                    log.DebugFormat("Successfully inserted a message to notification action queue. user id: {0}, device id: {1}, push token: {2}, group id: {3}, user action: {4}", userId, udid, pushToken, groupId, userAction);
-                else
-                    log.ErrorFormat("Error while inserting to notification action queue.  user id: {0}, device id: {1}, push token: {2}, group id: {3}, user action: {4}", userId, udid, pushToken, groupId, userAction);
+                if (ApplicationConfiguration.ShouldSupportCeleryMessages.Value)
+                {
+                    InitiateNotificationActionQueue que = new InitiateNotificationActionQueue();
+                    ApiObjects.QueueObjects.UserNotificationData messageAnnouncementData = new ApiObjects.QueueObjects.UserNotificationData(groupId, (int)userAction, userId, udid, pushToken);
+
+                    bool res = que.Enqueue(messageAnnouncementData, ROUTING_KEY_INITIATE_NOTIFICATION_ACTION);
+
+                    if (res)
+                        log.DebugFormat("Successfully inserted a message to notification action queue. user id: {0}, device id: {1}, push token: {2}, group id: {3}, user action: {4}", userId, udid, pushToken, groupId, userAction);
+                    else
+                        log.ErrorFormat("Error while inserting to notification action queue.  user id: {0}, device id: {1}, push token: {2}, group id: {3}, user action: {4}", userId, udid, pushToken, groupId, userAction);
+                }
             }
         }
 
