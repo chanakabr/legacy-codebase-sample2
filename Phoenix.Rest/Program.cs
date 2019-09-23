d056a7707e84c136b521e2f54839c5ed344d28bf
arthurvaverko <arthur.vaverko@kaltura.com>
2019-09-23 10:10:07 +0300

    fixed content type recognition with charecter set on net core

--- Phoenix.Rest/Program.cs
+++ Phoenix.Rest/Program.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using KLogMonitor;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Phoenix.Rest
 {
@@ -15,7 +9,7 @@ namespace Phoenix.Rest
     {
         public static async Task Main(string[] args)
         {
-            string apiVersion = System.Configuration.ConfigurationManager.AppSettings.Get("apiVersion");
+            var apiVersion = System.Configuration.ConfigurationManager.AppSettings.Get("apiVersion");
             var defaultLogDir = $@"C:\log\phoenix\{apiVersion}";
             KLogger.InitLogger("log4net.config", KLogEnums.AppType.WS, defaultLogDir);
             ConfigurationManager.ApplicationConfiguration.Initialize(shouldLoadDefaults: true, silent: true);
