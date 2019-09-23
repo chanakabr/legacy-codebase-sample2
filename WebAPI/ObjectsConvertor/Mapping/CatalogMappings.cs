e1c3628f68bfbbf95eb47e87f0db7787a7df44ff
Matan Oved <matan.oved@Kaltura.com>
2019-09-23 18:21:21 +0300

    Fix BEO-7037

--- WebAPI/ObjectsConvertor/Mapping/CatalogMappings.cs
+++ WebAPI/ObjectsConvertor/Mapping/CatalogMappings.cs
@@ -1477,7 +1477,7 @@ namespace WebAPI.ObjectsConvertor.Mapping
                 {
                     KalturaLongValue metaValue = meta.Value as KalturaLongValue;
                     metaToAdd.m_oTagMeta.m_sType = ApiObjects.MetaType.DateTime.ToString();
-                    metaToAdd.m_sValue = DateUtils.UtcUnixTimestampSecondsToDateTime(metaValue.value).ToString("dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                    metaToAdd.m_sValue = DateUtils.UtcUnixTimestampSecondsToDateTime(metaValue.value).ToString("dd/MM/yyyy HH:mm:ss");
                 }
                 else if (metaType == typeof(KalturaIntegerValue))
                 {
