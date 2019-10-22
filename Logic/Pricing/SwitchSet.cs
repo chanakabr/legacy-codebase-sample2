61d78098acf137b5b0e781a125b1dd11f20e75ce
Lior Faingold <lior.faingold@kaltura.com>
2019-10-22 18:39:00 +0300

    fix GEN-427

--- Logic/Pricing/SwitchSet.cs
+++ Logic/Pricing/SwitchSet.cs
@@ -1,4 +1,5 @@
 ﻿using ApiObjects;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace Core.Pricing
 {
+    [JsonObject(ItemTypeNameHandling = TypeNameHandling.All)]
     public class SwitchSet : SubscriptionSet
     {
         //public List<KeyValuePair> SubscriptionSetIdsToPriority;  
