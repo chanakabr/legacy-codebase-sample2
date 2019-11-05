2d5365ad31c0d5d81e9d902358933bd7ea1ef04d
guykatznelson <guy.katznelson@kaltura.com>
2019-11-05 14:17:15 +0200

    BEO-7279

--- ApiObjects/Segmentation/SegmentAction.cs
+++ ApiObjects/Segmentation/SegmentAction.cs
@@ -2,11 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ApiObjects.Response;
+using System.Linq;
 
 namespace ApiObjects.Segmentation
 {
     public class SegmentAction
     {
+        public virtual Status ValidateForInsert()
+        {
+            return new Status(eResponseStatus.OK);
+        }
     }
 
     public class SegmentAssetOrderAction : SegmentAction
@@ -16,5 +22,29 @@ namespace ApiObjects.Segmentation
 
         [JsonProperty()]
         public List<string> Values { get; set; }
+
+        public override Status ValidateForInsert()
+        {
+            var status = new Status(eResponseStatus.OK);
+
+            if (string.IsNullOrEmpty(Name))
+            {
+                status.Set(eResponseStatus.InvalidParameters, "missing name of segment order action");
+            }
+
+            if (Values == null)
+            {
+                status.Set(eResponseStatus.InvalidParameters, "missing values of segment order action");
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(Values.FirstOrDefault()))
+                {
+                    status.Set(eResponseStatus.InvalidParameters, "value of segment order action can't be empty");
+                }
+            }
+
+            return status;
+        }
     }
 }
