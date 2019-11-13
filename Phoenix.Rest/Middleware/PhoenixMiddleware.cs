f6910b13ea18d10b740305dd374234b8ad09fcce
arthurvaverko <arthur.vaverko@kaltura.com>
2019-11-13 07:45:13 +0200

    Added ability to configure max \ min thread pool and servicepont connection limit using environment vars

--- Phoenix.Rest/Middleware/PhoenixMiddleware.cs
+++ Phoenix.Rest/Middleware/PhoenixMiddleware.cs
@@ -26,11 +26,45 @@ namespace Phoenix.Rest.Middleware
         /// </summary>
         public static IServiceCollection ConfigurePhoenix(this IServiceCollection services)
         {
+
+            System.Threading.ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxCompletionThreads);
+            System.Threading.ThreadPool.GetMinThreads(out int minWorkerThreads, out int minCompletionThreads);
+
+            _Logger.Info($"Current ServicePointManager.DefaultConnectionLimit:{ServicePointManager.DefaultConnectionLimit}");
+            _Logger.Info($"Current Max workThreads/IO Threads :{maxWorkerThreads} / {maxCompletionThreads}");
+            _Logger.Info($"Current Min workThreads/IO Threads :{minWorkerThreads} / {minCompletionThreads}");
+
+            var minWorkerThreadsToSet = GetEnvironmnetVariableInt("MIN_WORKER_THREADS", defaultVal: minWorkerThreads);
+            var minIOWorkerThreadsToSet = GetEnvironmnetVariableInt("MIN_IO_WORKER_THREADS", defaultVal: maxCompletionThreads);
+            var maxWorkerThreadsToSet = GetEnvironmnetVariableInt("MAX_WORKER_THREADS", defaultVal: maxWorkerThreads);
+            var maxIOWorkerThreadsToSet = GetEnvironmnetVariableInt("MAX_IO_WORKER_THREADS", defaultVal: minCompletionThreads);
+            var connectionLimitToSet = GetEnvironmnetVariableInt("MAX_CONN_LIMIT", defaultVal: ServicePointManager.DefaultConnectionLimit);
+
+            System.Threading.ThreadPool.SetMinThreads(minWorkerThreadsToSet, minIOWorkerThreadsToSet); // or higher
+            System.Threading.ThreadPool.SetMaxThreads(maxWorkerThreadsToSet, maxIOWorkerThreadsToSet); // or higher
+            System.Net.ServicePointManager.DefaultConnectionLimit = connectionLimitToSet; // Max concurrent outbound requests
+
+            _Logger.Info($"Current ServicePointManager.DefaultConnectionLimit:{connectionLimitToSet}");
+            _Logger.Info($"Current Max workThreads/IO Threads :{maxWorkerThreadsToSet} / {maxIOWorkerThreadsToSet}");
+            _Logger.Info($"Current Min workThreads/IO Threads :{minWorkerThreadsToSet} / {minIOWorkerThreadsToSet}");
+
             services.AddHttpContextAccessor();
             services.AddKalturaApplicationSessionContext();
             services.AddSingleton<IResponseFromatterProvider, ResponseFromatterProvider>();
 
             return services;
+        }
+
+        private static int GetEnvironmnetVariableInt(string key, int defaultVal)
+        {
+            if (int.TryParse(Environment.GetEnvironmentVariable(key), out var outValue))
+            {
+                return outValue;
+            }
+            else
+            {
+                return defaultVal;
+            }
         }
 
         /// <summary>
