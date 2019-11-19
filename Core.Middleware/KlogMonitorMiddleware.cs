63129a22cd3f0edde3ca564aa623ace4d084842e
arthurvaverko <arthur.vaverko@kaltura.com>
2019-11-19 20:56:52 +0200

    Move common middelware: Cors, ExceptionHandler and Klogger related

---
+++ Core.Middleware/KlogMonitorMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using KLogMonitor;
+
+namespace Core.Middleware
+{
+    public static class KlogMonitorMiddleware
+    {
+        public const string SESSION_HEADER_KEY = KLogMonitor.Constants.REQUEST_ID_KEY;
+
+        public static IApplicationBuilder UseKlogerMonitor(this IApplicationBuilder app)
+        {
+
+            return app.Use(async (context, _next) =>
+            {
+                using (var km = new KMonitor(Events.eEvent.EVENT_CLIENT_API_START))
+                {
+                    await _next.Invoke();
+                }
+
+            });
+        }
+    }
+}
---
+++ Core.Middleware/StaticHttpContextAccessor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Middleware
+{
+    public static class StaticHttpContextAccessor
+    {
+
+        public static IServiceCollection AddStaticHttpContextAccessor(this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            var provider = services.BuildServiceProvider();
+            var httpContextAccessor = provider.GetService<IHttpContextAccessor>();
+
+            // Confgure the shim of System.Web.HttpContext, this is actualy the project StaticHttpContextForNetCore
+            System.Web.HttpContext.Configure(httpContextAccessor);
+
+            return services;
+        }
+    }
+}
