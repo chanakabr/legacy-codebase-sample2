175014191841c3c94b4a4ef5aca5e0cb67469344
Lior Faingold <lior.faingold@kaltura.com>
2019-11-26 15:41:38 +0200

    fix GEN-535 and in general all clear_cache.aspx?action=clear_all requests

--- Phoenix.Legacy/clear_cache.aspx.cs
+++ Phoenix.Legacy/clear_cache.aspx.cs
@@ -18,130 +18,16 @@ namespace WebAPI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string sHost = "";
-            string sRefferer = "";
-            if (HttpContext.Current.Request.ServerVariables["REMOTE_HOST"] != null)
-                sHost = HttpContext.Current.Request.ServerVariables["REMOTE_HOST"].ToLower();
-            if (HttpContext.Current.Request.ServerVariables["HTTP_REFERER"] != null)
-                sRefferer = HttpContext.Current.Request.ServerVariables["HTTP_REFERER"].ToLower();
-            bool bAdmin = true;
-            if (sHost.ToLower().IndexOf("admin.tvinci.com") != -1 ||
-                sHost.ToLower().IndexOf("62.128.54.164") != -1 ||
-                sHost.ToLower().IndexOf("62.128.54.165") != -1 ||
-                sHost.ToLower().IndexOf("62.128.54.166") != -1 ||
-                sHost.ToLower().IndexOf("62.128.54.167") != -1 ||
-                sHost.ToLower().IndexOf("62.128.54.168") != -1 ||
-                sHost.ToLower().IndexOf("80.179.194.132") != -1 ||
-                sHost.ToLower().IndexOf("213.8.115.108") != -1 ||
-                sHost.ToLower().StartsWith("72.26.211") == true ||
-                sHost.ToLower().IndexOf("127.0.0.1") != -1 ||
-                sRefferer.ToLower().IndexOf("tvinci.com") != -1 ||
-            sHost.ToLower().IndexOf("173.231.146.34") != -1)
-                bAdmin = true;
+            string action = "";
+            string key = "";
             if (Request.QueryString["action"] != null)
-            {
-                Response.Clear();
-                Response.Write("Clear cache request from host: " + sHost + " , Refferer: " + sRefferer + "<br/>");
-                if (Request.QueryString["action"].ToString().ToLower().Trim() == "clear_all")
-                {
-                    if (bAdmin == true)
-                    {
-                        try
-                        {
-                            CachingManager.CachingManager.RemoveFromCache("");
-                            TvinciCache.WSCache.ClearAll();
-                            CatalogCache.ClearAll();
-                            //bool layeredCacheCleared = false;
-                            //bool addGroupIdInResponse = false;
-                            //int groupId;
-                            //if (context.Request.QueryString["groupId"] != null)
-                            //{                                    
-                            //    if (int.TryParse(context.Request.QueryString["groupId"].ToString().Trim(), out groupId))
-                            //    {
-                            //        addGroupIdInResponse = true;
-                            //        WS_API.API api = new WS_API.API();
-                            //        layeredCacheCleared = ClientsManager.ApiClient().ClearLayeredCache(groupId);
-                            //    }
-                            //}
-                            //else
-                            //{
-                            //    layeredCacheCleared = true;
-                            //}
-                            //string addGroupIdResult = addGroupIdInResponse ? string.Format("for groupId: {0}", groupId) : string.Empty;
-                            //string clearCacheResult = layeredCacheCleared ? string.Format("Cleared all cache {0}", addGroupIdResult) : string.Format("Cache cleared but LayeredCache failed {0}", addGroupIdResult);
-                            //context.Response.Write(clearCacheResult);
-                            Response.Write("Cache cleared");
-                        }
-                        catch (Exception ex)
-                        {
-                            Response.Write("Error : " + ex.Message);
-                            Response.StatusCode = 404;
-                        }
-                    }
-                    else
-                    {
-                        Response.StatusCode = 404;
-                    }
-                }
-                if (Request.QueryString["action"].ToString().ToLower().Trim() == "keys")
-                {
-                    System.Collections.Generic.List<string> keys = CachingManager.CachingManager.GetCachedKeys();
-                    TvinciCache.WSCache cache = TvinciCache.WSCache.Instance;
-                    if (cache != null)
-                    {
-                        keys.AddRange(cache.GetKeys());
-                    }
-                    Response.Write(string.Join(",", keys));
-                }
-            }
-            else if (Request.QueryString["key"] != null)
-            {
-                Response.Clear();
-                if (bAdmin == true)
-                {
-                    try
-                    {
-                        string key = Request.QueryString["key"].ToString().Trim();
-                        object o = CachingManager.CachingManager.GetCacheObject(key);
-                        if (o != null)
-                        {
-                            Response.Write(string.Format("{0} value is: {1}", key, o.ToString()));
-                        }
-                        else
-                        {
-                            TvinciCache.WSCache cache = TvinciCache.WSCache.Instance;
-                            if (cache != null)
-                            {
-                                object value;
-                                bool isExists = cache.TryGet<object>(key, out value);
-                                if (isExists)
-                                {
-                                    Response.Write(string.Format("{0} value is: {1}", key, value));
-                                }
-                                else
-                                {
-                                    Response.Write(string.Format("Key {0} not found", key));
-                                }
-                            }
-                            else
-                            {
-                                Response.Write(string.Format("Key {0} not found", key));
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Response.Write("Error : " + ex.Message);
-                        Response.StatusCode = 404;
-                    }
-                }
-                else
-                {
-                    Response.StatusCode = 404;
-                }
-            }
-            else
-                Response.StatusCode = 404;
+                action = Request.QueryString["action"].ToString().ToLower().Trim();
+            if (Request.QueryString["key"] != null)
+                key = Request.QueryString["key"].ToString().Trim();
+            ApiObjects.Response.Status status = Core.Api.api.ClearLocalServerCache(action, key);
+            Response.Clear();
+            Response.Write(status.Message);
+            Response.StatusCode = status.Code == 0 ? 200 : status.Code;
         }
     }
 }
