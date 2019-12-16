354d3aa33bcfceb1f02b0ae2067b9e388b44df91
Tzachi Hartuv <tzachi.hartuv@Kaltura.com>
2019-12-16 10:42:13 +0200

    Add tests

--- ConfigTest/DefaultConfigurationTests.cs
+++ ConfigTest/DefaultConfigurationTests.cs
@@ -12,6 +12,9 @@ namespace Tests.ConfigTest
     [TestClass]
     public class DefaultConfigurationTests
     {
+        const int BaseValueKeyLocation = 0;
+        const int BaseValueDefaultValueLocation = 1;
+        const int BaseValueMustBeOverwriteInTcmLocation = 2;
 
         [TestMethod]
         public void Test_AllConfigurationSettingsMustHaveDefultValue()
@@ -22,6 +25,111 @@ namespace Tests.ConfigTest
             var result = TestIfConainNullDefaultValue(type, configuration);
             Assert.IsTrue(result);
 
+        }
+
+
+        [TestMethod]
+        public void Test_StubInCaseOfMustBeOverwriteInTcmInTcm()
+        {
+            ApplicationConfiguration configuration = ApplicationConfiguration.Current;
+            Type type = typeof(ApplicationConfiguration);
+            TestStubKey(type, configuration);
+        }
+
+
+
+        [TestMethod]
+        public void Test_ValidateDuplicateKeys()
+        {
+            ApplicationConfiguration configuration = ApplicationConfiguration.Current;
+            Type type = typeof(ApplicationConfiguration);
+            TestIfConainsDuplicateKey(type, configuration);
+        }
+
+
+        private void TestStubKey(Type type, IBaseConfig baseConfig)
+        {
+            List<FieldInfo> fields = type.GetFields().ToList();
+            foreach (var field in fields)
+            {
+                object baseValueData = field.GetValue(baseConfig);
+                if (baseValueData == null)
+                {
+
+                    Assert.Fail($"object [{type.Name}] holds null objcet {field.Name}");
+                }
+                else if (field.FieldType.Name.StartsWith("BaseValue"))
+                {
+
+                    try
+                    {
+                        var value = baseValueData.GetType().GetRuntimeFields().ToList()[BaseValueDefaultValueLocation].GetValue(baseValueData) as string;
+                        bool? mustBeOverwriteInTcmLocation = baseValueData.GetType().GetRuntimeFields().ToList()[BaseValueMustBeOverwriteInTcmLocation].GetValue(baseValueData) as bool?;
+                        if ( mustBeOverwriteInTcmLocation.Value && value != TcmObjectKeys.Stub)
+                        {
+                            Assert.Fail($"case of mustBeOverwriteInTcm key must be Stub key.  object: [{type.Name}]");
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                else if (field.FieldType == type && field.Name == "Current")
+                {
+                    continue;
+                }
+                else if (IsBaseStartWithName(field.FieldType, "BaseConfig") &&
+                    field.FieldType.GetInterface("IBaseConfig") != null)
+                {
+                    TestStubKey(field.FieldType, baseValueData as IBaseConfig);
+                }
+                else
+                {
+                    Assert.Fail($"{baseConfig.ToString()} contains unkown param");
+                }
+            }
+        }
+
+        private void TestIfConainsDuplicateKey(Type type, IBaseConfig baseConfig)
+        {
+            List<FieldInfo> fields = type.GetFields().ToList();
+            Dictionary<string,string> uniqueKeys = new Dictionary<string,string>();
+            foreach (var field in fields)
+            {
+                object baseValueData = field.GetValue(baseConfig);
+                if (baseValueData == null)
+                {
+
+                    Assert.Fail($"object [{type.Name}] holds null objcet {field.Name}");
+                }
+                else if (field.FieldType.Name.StartsWith("BaseValue"))
+                {
+                    var key = baseValueData.GetType().GetRuntimeFields().ToList()[BaseValueKeyLocation].GetValue(baseValueData) as string;
+                    if (uniqueKeys.TryGetValue(key, out var decleredType))
+                    {
+                        if (decleredType == field.DeclaringType.Name)
+                        {
+                            Assert.Fail($"Duplicate key exist for the same object: [{type.Name}], Key; {key}");
+                        }
+                        continue;
+                    }
+                    uniqueKeys.Add(key,field.DeclaringType.Name);
+                }
+                else if (field.FieldType == type && field.Name == "Current")
+                {
+                    continue;
+                }
+                else if (IsBaseStartWithName(field.FieldType, "BaseConfig") &&
+                    field.FieldType.GetInterface("IBaseConfig") != null)
+                {
+                    TestIfConainsDuplicateKey(field.FieldType, baseValueData as IBaseConfig);
+                }
+                else
+                {
+                   Assert.Fail($"{baseConfig.ToString()} contains unkown param");
+                }
+            }
         }
 
         private bool TestIfConainNullDefaultValue(Type type, IBaseConfig baseConfig)
@@ -40,9 +148,9 @@ namespace Tests.ConfigTest
                 }
                 else if (field.FieldType.Name.StartsWith("BaseValue"))
                 {
-                    var key = baseValueData.GetType().GetRuntimeFields().ToList()[0].GetValue(baseValueData);
-                    var value = baseValueData.GetType().GetProperty("Value").GetValue(baseValueData);
-                    
+                    var key = baseValueData.GetType().GetRuntimeFields().ToList()[BaseValueKeyLocation].GetValue(baseValueData);
+                    var value = baseValueData.GetType().GetRuntimeFields().ToList()[BaseValueDefaultValueLocation].GetValue(baseValueData);
+
                     if (value == null || string.IsNullOrEmpty(value.ToString()) )
                     {
                         sb.AppendLine($"object [{type.Name}] holds null default value [{field.Name}], Json key [{key}]");
