48b89c37f36424aa4453c937ebc5ffcefae30107
arthurvaverko <arthur.vaverko@kaltura.com>
2019-12-25 10:56:35 +0200

    GEN-592 Configure Klogger as the asp.net core logging provider

---
+++ KLogMonitor/KloggerProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KLogMonitor
+{
+    public class KLoggerProvider : ILoggerProvider
+    {
+        private readonly ConcurrentDictionary<string, KLogger> _loggers = new ConcurrentDictionary<string, KLogger>();
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, name => new KLogger(name));
+        }
+
+        public void Dispose()
+        {
+            _loggers.Clear();
+        }
+    }
+
+}
