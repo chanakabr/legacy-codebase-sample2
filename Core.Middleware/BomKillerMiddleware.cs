b02977cf77f21213fd5750b8d1aee1aef10a6346
Sunny <sunny.simantov@kaltura.com>
2020-01-02 12:06:08 +0200

    Add bom killer middleware See: https://www.freecodecamp.org/news/a-quick-tale-about-feff-the-invisible-character-cd25cd4630e7/

---
+++ Core.Middleware/BomKillerMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using KLogMonitor;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Middleware
+{
+    public static class BomKillerMiddleware
+    {
+        /// <summary>
+        /// LATERS
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseBomKiller(this IApplicationBuilder app)
+        {
+            return app.Use(async (context, _next) =>
+            {
+                using (var inMemoryResponse = new MemoryStream())
+                {
+                    var originalResponseStream = context.Response.Body;
+                    context.Response.Body = inMemoryResponse;
+
+                    await _next.Invoke();
+
+                    inMemoryResponse.Seek(0, SeekOrigin.Begin);
+
+                    using (var streamReader = new StreamReader(inMemoryResponse))
+                    {
+                        var bodyAsText = await streamReader.ReadToEndAsync();
+
+                        context.Response.Body = originalResponseStream;
+                        await context.Response.WriteAsync(bodyAsText, Encoding.UTF8);
+                    }
+                }
+            });
+        }
+    }
+}
