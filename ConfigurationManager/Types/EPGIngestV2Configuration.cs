f3b4039543811df7ba963c69fe442f613c104319
arthurvaverko <arthur.vaverko@kaltura.com>
2020-02-06 16:46:50 +0200

    Added DistributedLock class to be used in ingest v2
    (cherry picked from commit c2440b88c6dd08e76679feba68dd7767dc8b0c05)
    
    # Conflicts:
    #	ConfigurationManager/ApplicationConfiguration.cs
    #	EventBus.RabbitMQ/KalturaEventBusRabbitMQConsumerServiceExtensions.cs
    #	Logic/Catalog/BulkUpload/BulkUploadEpgAssetData.cs
    #	Logic/Catalog/BulkUpload/BulkUploadIngestJobData.cs
    (cherry picked from commit ba7e9356d6b231415f0748bdb42c3aaabb210f89)

---
+++ ConfigurationManager/Types/EPGIngestV2Configuration.cs
@@ -0,0 +1,36 @@
+﻿using ConfigurationManager.ConfigurationSettings.ConfigurationBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace ConfigurationManager
+{
+    public class EPGIngestV2Configuration : BaseConfig<EPGIngestV2Configuration>
+    {
+        public BaseValue<int> LockNumOfRetries = new BaseValue<int>("lock_num_of_retries", 600);
+        public BaseValue<int> LockRetryIntervalMS = new BaseValue<int>("lock_retry_interval_ms", 10000);
+        public BaseValue<int> LockTTLSeconds = new BaseValue<int>("lock_ttl_seconds", 10800);
+
+        public override string TcmKey => TcmObjectKeys.OTTUserControllerConfiguration;
+
+        public override string[] TcmPath => new string[] { TcmKey };
+
+
+        public override bool Validate()
+        {
+            var isValid = LockNumOfRetries.ActualValue > 0;
+            isValid &= LockRetryIntervalMS.ActualValue > 0;
+            isValid &= LockTTLSeconds.ActualValue > 0;
+
+            // TODO: Check with lior if its okay to add klogger and print additional logs to this chekc .. 
+            //if (LockTTLSeconds.ActualValue < 10800)
+            //{
+                //log.Warn($"Setting the distributed lock to less than 6 hours is not recommended, this is a failsafe mechanisms to prevent locking indefinetlly");
+            //}
+
+            return isValid;
+        }
+    }
+}
