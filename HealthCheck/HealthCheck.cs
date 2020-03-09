98df9c16ca6eba68019d462c1e95bfd41d269507
Sunny <sunny.simantov@kaltura.com>
2020-03-09 09:58:59 +0200

    small progress with health check

--- HealthCheck/HealthCheck.cs
+++ HealthCheck/HealthCheck.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace HealthCheck
 {
@@ -12,23 +16,57 @@ namespace HealthCheck
         public static void AddHealthCheckService(this IServiceCollection services)
         {
             var healthCheckBuilder = services.AddHealthChecks();
-            healthCheckBuilder = healthCheckBuilder.AddCheck<ElasticSearchHealthCheck>("elastic_search_check");
+            healthCheckBuilder = healthCheckBuilder.AddCheck<ElasticSearchHealthCheck>("elastic_search");
         }
 
         public static void UseHealthCheck(this IApplicationBuilder app, string path)
         {
             var options = new HealthCheckOptions();
-            //options.ResponseWriter = 
-            //options.ResultStatusCodes.Add(Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Healthy, 200);
-            //options.ResultStatusCodes.Add(Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy, 503);
-            //options.ResultStatusCodes.Add(Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Degraded, 503);
-            
+            options.ResponseWriter = WriteResponse;
+
             app.UseHealthChecks(path, options);
         }
 
-        //public static HttpContext GetHealthCheckResponse(HealthReport report)
-        //{
-        //    HttpContext.
-        //}
+        private static Task WriteResponse(HttpContext context, HealthReport result)
+        {
+            context.Response.ContentType = "application/json; charset=utf-8";
+
+            var options = new JsonWriterOptions
+            {
+                Indented = true
+            };
+
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new Utf8JsonWriter(stream, options))
+                {
+                    writer.WriteStartObject();
+                    writer.WriteString("status", result.Status.ToString());
+                    writer.WriteStartObject("results");
+                    foreach (var entry in result.Entries)
+                    {
+                        writer.WriteStartObject(entry.Key);
+                        writer.WriteString("status", entry.Value.Status.ToString());
+                        writer.WriteString("description", entry.Value.Description);
+                        writer.WriteStartObject("data");
+                        foreach (var item in entry.Value.Data)
+                        {
+                            writer.WritePropertyName(item.Key);
+                            JsonSerializer.Serialize(
+                                writer, item.Value, item.Value?.GetType() ??
+                                typeof(object));
+                        }
+                        writer.WriteEndObject();
+                        writer.WriteEndObject();
+                    }
+                    writer.WriteEndObject();
+                    writer.WriteEndObject();
+                }
+
+                var json = Encoding.UTF8.GetString(stream.ToArray());
+
+                return context.Response.WriteAsync(json);
+            }
+        }
     }
 }
---
+++ HealthCheck/HealthCheckDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthCheck
+{
+    public class HealthCheckDefinition
+    {
+        public HealthCheckDefinition(HealthCheckType type, object[] args)
+        {
+
+        }
+    }
+
+    public enum HealthCheckType
+    {
+        SQL,
+        CouchBase,
+        ElasticSearch,
+        RabbitMQ,
+        ThirdParty
+    }
+}
