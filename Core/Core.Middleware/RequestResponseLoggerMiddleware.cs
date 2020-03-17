a65ae27cb097184aff3a1df499d38705a7b781f1
arthurvaverko <arthur.vaverko@kaltura.com>
2020-03-18 00:13:40 +0200

    completed response \ request logger

--- Core/Core.Middleware/RequestResponseLoggerMiddleware.cs
+++ Core/Core.Middleware/RequestResponseLoggerMiddleware.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -21,38 +22,45 @@ namespace Core.Middleware
         {
             app.Use(async (context, _next) =>
             {
-                await _next.Invoke();
+                // Get Request Information
+                context.Request.EnableBuffering();
                 var requestUrl = context.Request.GetDisplayUrl();
-                var requestHeaders = GetRequestHeadersStr(context.Request);
-                var requestLogStr = await FormatRequestLogStr(context.Request);
-                var responseLogStr = await FormatResponseLogStr(context.Response);
-                _Logger.Info($"{requestLogStr} {context.Response.StatusCode} {responseLogStr}");
-                var builder = new StringBuilder();
+                var requestHeadersStr = GetHeadersStr(context.Request.Headers);
+                var requestBodyStr = await TryGetBodyStr(context.Request.Body, Convert.ToInt32(context.Request.ContentLength));
 
+                // Prepare Response Capturing (Replace the response stream with a memeory stream so that we can re-read it later)
+                var originalResponseStream = context.Response.Body;
+                var memoryResponseStream = new MemoryStream();
+                context.Response.Body = memoryResponseStream;
+
+                // On Repsonse starting delegate will read the memory stream and log it, then will redirect the memory copy stream of the response
+                // To the origianl response stream the client is waiting for
+                context.Response.OnStarting(async () =>
+                {
+                    memoryResponseStream.Seek(0, SeekOrigin.Begin);
+                    await memoryResponseStream.CopyToAsync(originalResponseStream);
+
+                    // Logging req\resp async after the response returned
+                    _ = Task.Run(async () =>
+                    {
+                        requestBodyStr = requestBodyStr.Replace("\n", string.Empty).Replace("\t", string.Empty).Replace("\r", string.Empty);
+                        var responseBodyStr = await TryGetBodyStr(context.Response.Body, Convert.ToInt32(context.Response.Body.Length));
+                        var responseHeadersStr = GetHeadersStr(context.Response.Headers);
+                        var responseStatusStr = context.Response.StatusCode.ToString();
+                        await memoryResponseStream.DisposeAsync();
+                        _Logger.Info($"{requestUrl} | {requestHeadersStr} | {requestBodyStr} | {responseStatusStr} | {responseHeadersStr} | {responseBodyStr}");
+                    });
+
+                });
+
+                await _next.Invoke();
             });
             return app;
         }
 
-        private static async Task<string> FormatResponseLogStr(HttpResponse response)
+        private static string GetHeadersStr(IHeaderDictionary headers)
         {
-            string bodyAsText = await TryGetBodyStr(response.Body, Convert.ToInt32(response.ContentLength));
-            //TODO: Add response headers...
-            return bodyAsText;
-        }
-
-        private static async Task<string> FormatRequestLogStr(HttpRequest request)
-        {
-            request.EnableBuffering();
-            string bodyAsText = await TryGetBodyStr(request.Body, Convert.ToInt32(request.ContentLength));
-
-            builder.AppendLine($" {bodyAsText}");
-
-            return builder.ToString();
-        }
-
-        private static string GetRequestHeadersStr(HttpRequest request)
-        {
-            var headersStrs = request.Headers.Select(h => $"{h.Key}:{h.Value}");
+            var headersStrs = headers.Select(h => $"{h.Key}:{h.Value}");
             return string.Join(",", headersStrs);
         }
 
@@ -60,7 +68,9 @@ namespace Core.Middleware
         {
             try
             {
+
                 var buffer = new byte[contentLength];
+                bodyStream.Seek(0, SeekOrigin.Begin);
                 await bodyStream.ReadAsync(buffer, 0, buffer.Length);
                 bodyStream.Seek(0, SeekOrigin.Begin);
                 var bodyAsText = Encoding.UTF8.GetString(buffer);
---
+++ Phoenix.Dev.Dockerfile
@@ -0,0 +1,39 @@
+﻿FROM mcr.microsoft.com/dotnet/core/sdk:3.1 as builder
+WORKDIR /src
+
+RUN apt-get install git
+RUN dotnet tool install --global dotnet-counters
+ENV PATH="${PATH}:/root/.dotnet/tools"
+
+COPY [".git", ".git"]
+COPY ["Core", "Core"]
+COPY ["phoenix", "phoenix"]
+
+#version patch
+WORKDIR /src/Core
+RUN bash DllVersioning.Core.sh .
+RUN bash get-version-tag.sh > version
+
+WORKDIR /src/phoenix
+RUN bash /src/Core/DllVersioning.Core.sh .
+RUN dotnet publish -c Release "./Phoenix.Rest/Phoenix.Rest.csproj" -o /src/published/phoenix-rest
+
+ARG API_LOG_DIR=/var/log/phoenix/
+ENV API_LOG_DIR ${API_LOG_DIR}
+ENV API_STD_OUT_LOG_LEVEL "Off"
+
+WORKDIR /opt/phoenix-rest
+RUN cp -r /src/published/phoenix-rest /opt
+
+ENV ARGS "--urls http://0.0.0.0:80"
+
+EXPOSE 80
+EXPOSE 443
+
+ENTRYPOINT [ "sh", "-c", "dotnet Phoenix.Rest.dll ${ARGS}" ]
+
+
+
+
+
+
