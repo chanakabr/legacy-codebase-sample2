873135851a5e0a48016bc6ee6c7349d88fea0ebb
gilgaldi <61379872+gilgaldi@users.noreply.github.com>
2020-05-21 12:56:25 +0300

    added validation on lang in IngestTransformation (#41)
    * added validation on lang in IngestTransformation
    
    * adding null check

--- Core/Logic/Catalog/BulkUpload/Validators/ProgramValidor.cs
+++ Core/Logic/Catalog/BulkUpload/Validators/ProgramValidor.cs
@@ -39,20 +39,20 @@ namespace ApiLogic.Catalog.BulkUpload.Validators
 
         private static bool ValidateTitleLang(EpgProgramBulkUploadObject program)
         {
-            return program.ParsedProgramObject.title.All(x => !string.IsNullOrEmpty(x.lang));
+            return program.ParsedProgramObject.title != null && program.ParsedProgramObject.title.All(x => !string.IsNullOrEmpty(x.lang));
         }
 
         public static bool ValidateMetadataLang(EpgProgramBulkUploadObject program)
         {
             //verify does not contain empty lang 
-            return program.ParsedProgramObject.metas.SelectMany(x => x.MetaValues)
+            return program.ParsedProgramObject.metas != null && program.ParsedProgramObject.metas.SelectMany(x => x.MetaValues)
                 .All(x => !string.IsNullOrEmpty(x.lang));
         }
 
         public static bool ValidateTagsLang(EpgProgramBulkUploadObject program)
         {
             //verify does not contain empty lang 
-            return program.ParsedProgramObject.tags.SelectMany(x => x.TagValues)
+            return program.ParsedProgramObject.tags != null && program.ParsedProgramObject.tags.SelectMany(x => x.TagValues)
                 .All(x => !string.IsNullOrEmpty(x.lang));
         }
 
