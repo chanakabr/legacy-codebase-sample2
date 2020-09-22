60dcd7371d581f642a6f4baea322ba06decbac5b
vstatkevich <Vladimir.Statkevich@kaltura.com>
2020-09-22 11:12:03 +0300

    BEO-8565 Fix Content-Type parsing (#229)
    Motivation:
    `Content-Type` header could contain extra info, e.g. `charset`.
    So we should compare media-type part of content-type header only

--- phoenix/Phoenix.Rest/Middleware/PhoenixRequestContextBuilder.cs
+++ phoenix/Phoenix.Rest/Middleware/PhoenixRequestContextBuilder.cs
@@ -11,13 +11,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Mime;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using TVinciShared;
 using WebAPI;
 using WebAPI.Filters;
-using WebAPI.Managers;
 using WebAPI.Models.General;
 using WebAPI.Reflection;
 
@@ -264,15 +265,15 @@ namespace Phoenix.Rest.Middleware
             {
                 return await ParseFormDataBody(request, context);
             }
-            else if (request.ContentType.Equals("application/json", StringComparison.OrdinalIgnoreCase))
+
+            if (MediaTypeHeaderValue.TryParse(request.ContentType, out var contentType) &&
+                contentType.MediaType.Equals(MediaTypeNames.Application.Json, StringComparison.OrdinalIgnoreCase))
             {
                 return await ParseJsonBody(request, context);
             }
-            else
-            {
-                // TODO: Arthur, handle error
-                throw new Exception("Unsupported content type");
-            }
+
+            // TODO: Arthur, handle error
+            throw new Exception("Unsupported content type");
         }
 
         private IDictionary<string, object> GetActionParamsFromQueryString(HttpRequest request)
