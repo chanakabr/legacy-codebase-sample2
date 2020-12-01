6e612bf73eb59a7e740fabaeaabc5dff05986fd2
SunnySimantov <sunny.simantov@kaltura.com>
2020-12-01 10:09:32 +0200

    add kafka health check (#341)
    * add kafka health check
    
    * remove duplicate enum of health check
    
    * fix kafka health check
    
    * like this
    
    * rewrite kafka publisher

---
+++ Core/HealthCheck/KafkaHealthCheck.cs
@@ -0,0 +1,28 @@
+﻿using EventBus.Kafka;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheck
+{
+    class KafkaHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var publisher = (KafkaPublisher)KafkaPublisher.GetFromTcmConfiguration();
+            bool isHealthy = publisher.HealthCheck();
+
+            if (isHealthy)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("Kafka is healthy"));
+            }
+            else
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy("Kafka is unhealthy"));
+            }
+        }
+    }
+}
