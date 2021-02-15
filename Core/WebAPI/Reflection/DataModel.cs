f73decd1f00b0dfd77ef4d6fe7f0c360b714c982
AnatGr <anat.grossberg@kaltura.com>
2021-02-15 11:09:53 +0200

    611 with 604 (#489)
    * BEO-9292 Implement user self delete prevention in case userId isn't specified.
    
    * BEO-9292 Code refactoring due to comments from code review.
    
    * BEO-9292 Remove unnecessary error type.
    
    * usercache
    
    * 5100 beo 9216 announcement add now (#399)
    
    * WIP BEO-9216 - set epoch utc now when has no start time
    
    * WIP BEO-9216
    
    * wip BEO-9398 (#411)
    
    Co-authored-by: irayanai <iray@tvinci.com>
    
    * 601 beo 9395 subscriptions renewing error (#417)
    
    * WIP BEO-9395
    
    * WIP BEO-9395 - Empty
    
    * fix - BEO-9428
    
    Co-authored-by: irayanai <iray@tvinci.com>
    
    * BEO-8643 Add explanations for properties with "inherited" behavior.
    
    * BEO-9319 IngestV2 and recordings (#424)
    
    Motivation:
    - update recordings when assets added/deleted/updated via IngestV2
    
    Modifications:
    - add added/deleted/updated EPGs to BulkUpload on first step(BulkUploadTransformationHandler)
    - calculate really updated/deleted EPGs in IngestFinalizer
    - and call ConditionalAccess.Module.IngestRecording in order to recalculate recordings
    
    * 601 beo 9346 couponlist new (#428)
    
    * BEO-9346 CLONE 5.4.6- Coupon.list not implemented
    
    * No change. Just for checkin
    
    * BEO-9346 CLONE 5.4.6- Coupon.list not implemented
    
    * changed deletedir
    
    * changed deletedir
    
    * BEO-9536 [Beeline RU]API /api_v3/service/household/action/get started… (#466)
    
    * BEO-9536 [Beeline RU]API /api_v3/service/household/action/get started to present in its response new field "roleId": 0
    
    * BEO-9536 [Beeline RU]API /api_v3/service/household/action/get started to present in its response new field "roleId": 0
    
    * BEO-9536 [Beeline RU]API /api_v3/service/household/action/get started to present in its response new field "roleId": 0
    
    * fix ws clean
    
    Co-authored-by: shiransky <Shiran.Melamed@kaltura.com>
    
    * 604 beo 9559 search aggregations (#484)
    
    * fix - BEO-9559
    
    * fix - BEO-9559
    
    Co-authored-by: irayanai <iray@tvinci.com>
    
    Co-authored-by: Artsiom Rusak <artsiom.rusak@kaltura.com>
    Co-authored-by: irayanai <iray@tvinci.com>
    Co-authored-by: Matan Oved <53995576+matanovkaltura@users.noreply.github.com>
    Co-authored-by: irayanai <ira.yanai@kaltura.com>
    Co-authored-by: vstatkevich <Vladimir.Statkevich@kaltura.com>
    Co-authored-by: amitde69 <amitde7896@gmail.com>
    Co-authored-by: shiransky <Shiran.Melamed@kaltura.com>

--- Core/WebAPI/Reflection/DataModel.cs
+++ Core/WebAPI/Reflection/DataModel.cs
@@ -8561,10 +8561,6 @@ namespace WebAPI.Reflection
                             RolesManager.ValidateActionPermitted("coupon", "get", false);
                             return CouponController.Get((string) methodParams[0]);
                             
-                        case "list":
-                            RolesManager.ValidateActionPermitted("coupon", "list", false);
-                            return CouponController.List((KalturaCouponFilter) methodParams[0]);
-                            
                     }
                     break;
                     
@@ -14195,14 +14191,6 @@ namespace WebAPI.Reflection
                             ret.Add("code", new MethodParam(){
                                 NewName = newParamName,
                                 Type = typeof(string),
-                            });
-                            return ret;
-                            
-                        case "list":
-                            ret.Add("filter", new MethodParam(){
-                                NewName = newParamName,
-                                IsKalturaObject = true,
-                                Type = typeof(KalturaCouponFilter),
                             });
                             return ret;
                             
