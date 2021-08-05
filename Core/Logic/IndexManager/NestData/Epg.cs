e1bec2da1fcb4e95227a2a934d6e9f8f54f90e6d
Gil Galditzky <gil.galditzky@Kaltura.com>
2021-08-05 16:51:34 +0300

    remove all unnecessary fields from index

--- Core/Logic/IndexManager/NestData/Epg.cs
+++ Core/Logic/IndexManager/NestData/Epg.cs
@@ -24,18 +24,15 @@ namespace ApiObjects.Nest
         
         [PropertyName("epg_identifier")]
         public string EpgIdentifier { get; set; }
+        
+        [PropertyName("external_id")]
+        public string ExternalId { get; set; }
 
         [PropertyName("is_active")]
         public bool IsActive { get; set; }
 
-        [PropertyName("status")]
-        public int Status { get; set; }
-
         [PropertyName("group_id")]
         public int GroupID { get; set; }
-
-        [PropertyName("parent_group_id")]
-        public int ParentGroupID { get; set; }
 
         [PropertyName("channel_id")]
         public int ChannelID { get; set; }
@@ -58,29 +55,14 @@ namespace ApiObjects.Nest
         [Date(Name = "end_date" )]
         public DateTime EndDate { get; set; }
 
-        [Date(Name = "end_date" )]
+        [Date(Name = "search_end_date" )]
         public DateTime SearchEndDate { get; set; }
         
-        [PropertyName("co_guid")]
-        public string CoGuid { get; set; }
-
-        [PropertyName("pic_url")]
-        public string PicUrl { get; set; }
-
-        [PropertyName("pic_id")]
-        public int PicID { get; set; }
-
+        [Date(Name = "cache_date" )]
+        public DateTime CacheDate { get; set; }
+        
         [PropertyName("linear_media_id")]
-        public long LinearMediaId { get; set; }
-
-        [PropertyName("basic")]
-        public EpgBasicData BasicData { get; protected set; }
-
-        [PropertyName("stats")]
-        public Stats Statistics { get; protected set; }
-
-        [PropertyName("extra_data")]
-        public EpgExtraData ExtraData { get; set; }
+        public long? LinearMediaId { get; set; }
 
         [PropertyName("metas")]
         public Dictionary<string,Dictionary<string, List<string>>> Metas { get; set; }
@@ -97,21 +79,12 @@ namespace ApiObjects.Nest
         [PropertyName("date_routing")]
         public string DateRouting { get; set; }
 
-        [PropertyName("pictures")]
-        public List<EpgPicture> pictures { get; set; }
-
         [PropertyName("enable_cdvr")]
-        public int EnableCDVR { get; set; }
+        public int? EnableCDVR { get; set; }
 
         [PropertyName("enable_catch_up")]
-        public int EnableCatchUp { get; set; }
-
-        [PropertyName("enable_start_over")]
-        public int EnableStartOver { get; set; }
-
-        [PropertyName("enable_trick_play")]
-        public int EnableTrickPlay { get; set; }
-
+        public int? EnableCatchUp { get; set; }
+        
         [PropertyName ("crid")]
         public string Crid { get; set; }
 
@@ -120,13 +93,9 @@ namespace ApiObjects.Nest
         
         [PropertyName ("is_auto_fill")]
         public bool IsAutoFill { get; set; }
-
-        [PropertyName ("is_ingest_v2")]
-        public bool IsIngestV2 { get; set; }
-
+        
         [PropertyName ("suppressed")]
         public string Suppressed { get; set; }
-        
         
         [PropertyName ("recording_id")]
         public long? RecordingId { get; set; }
@@ -151,23 +120,33 @@ namespace ApiObjects.Nest
             long? recordingId, long? expiryUnixTimeStamp)
         {
             EpgID = epgCb.EpgID;
-            EpgIdentifier = epgCb.EpgIdentifier;
+            GroupID = isOpc ? epgCb.ParentGroupID : epgCb.GroupID;
+            ChannelID = epgCb.ChannelID;
             IsActive = epgCb.IsActive;
-            Status = epgCb.Status;
+            StartDate = epgCb.StartDate;
+            EndDate = epgCb.EndDate;
+            CacheDate = DateTime.UtcNow;
             CreateDate = epgCb.CreateDate;
             UpdateDate = epgCb.UpdateDate;
-            PicUrl = epgCb.PicUrl;
-            PicID = epgCb.PicID;
-            GroupID = isOpc ? epgCb.ParentGroupID : epgCb.GroupID;
-            ParentGroupID = epgCb.ParentGroupID;
-            ChannelID = epgCb.ChannelID;
-            EndDate = epgCb.EndDate;
             SearchEndDate = epgCb.SearchEndDate;
-            CoGuid = epgCb.CoGuid;
-            BasicData = epgCb.BasicData;
-            Statistics = epgCb.Statistics;
-            ExtraData = epgCb.ExtraData;
-            StartDate = epgCb.StartDate;
+            Crid = epgCb.Crid;
+            EpgIdentifier = epgCb.EpgIdentifier;
+            ExternalId = epgCb.EpgIdentifier;
+            DocumentId = epgCb.DocumentId;
+            IsAutoFill = epgCb.IsAutoFill;
+            EnableCDVR = epgCb.EnableCDVR;
+            EnableCatchUp = epgCb.EnableCatchUp;
+            Suppressed = epgCb.Suppressed;
+
+            if (epgCb.LinearMediaId > 0)
+            {
+                LinearMediaId = epgCb.LinearMediaId;
+            }
+
+            if (withRouting)
+            {
+                DateRouting = epgCb.StartDate.ToUniversalTime().ToString(esDateOnlyFormat);
+            }
 
             var metasDict = new Dictionary<string, Dictionary<string, List<string>>>();
             var langCode = epgCb.Language;
@@ -175,43 +154,33 @@ namespace ApiObjects.Nest
             var metas = new Dictionary<string, List<string>>();
             foreach (var epgCbMeta in epgCb.Metas)
             {
-                metas[epgCbMeta.Key] = 
+                metas[epgCbMeta.Key] =
                     epgCbMeta.Value.Select(x => ESUtils.ReplaceDocumentReservedCharacters(x, false)).ToList();
             }
-            metasDict.Add(langCode,new Dictionary<string, List<string>>(metas));
+
+            metasDict.Add(langCode, new Dictionary<string, List<string>>(metas));
             Metas = metasDict; //lang
-            
+
             var tagsDict = new Dictionary<string, Dictionary<string, List<string>>>();
             var tags = new Dictionary<string, List<string>>();
             foreach (var tag in epgCb.Tags)
             {
-                tags[tag.Key]=tag.Value.Select(x=> ESUtils.ReplaceDocumentReservedCharacters(x, false)).ToList();
+                tags[tag.Key] = tag.Value.Select(x => ESUtils.ReplaceDocumentReservedCharacters(x, false)).ToList();
             }
-            tagsDict.Add(langCode,new Dictionary<string, List<string>>(tags));
+
+            tagsDict.Add(langCode, new Dictionary<string, List<string>>(tags));
             Tags = tagsDict; //lang
 
             var nameDict = new Dictionary<string, string>();
-            nameDict.Add(langCode, ESUtils.ReplaceDocumentReservedCharacters(epgCb.Name,false));
+            nameDict.Add(langCode, ESUtils.ReplaceDocumentReservedCharacters(epgCb.Name, false));
             Name = nameDict; //lang
-            
+
             var descriptionDict = new Dictionary<string, string>();
-            descriptionDict.Add(langCode, ESUtils.ReplaceDocumentReservedCharacters(epgCb.Description,false));
+            descriptionDict.Add(langCode, ESUtils.ReplaceDocumentReservedCharacters(epgCb.Description, false));
             Description = descriptionDict; //lang
-            
+
             Language = langCode;
             LanguageId = languageId;
-            pictures = epgCb.pictures;
-            EnableCDVR = epgCb.EnableCDVR;
-            EnableCatchUp = epgCb.EnableCatchUp;
-            EnableStartOver = epgCb.EnableStartOver;
-            EnableTrickPlay = epgCb.EnableTrickPlay;
-            Crid = epgCb.Crid;
-            IsIngestV2 = epgCb.IsIngestV2;
-            
-            if (withRouting)
-            {
-                DateRouting = epgCb.StartDate.ToUniversalTime().ToString(esDateOnlyFormat);
-            }
 
             if (recordingId.HasValue)
             {
