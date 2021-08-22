8ebad89187a45178b172584b4af0ffd1926abed4
SunnySimantov <sunny.simantov@kaltura.com>
2021-08-22 14:47:03 +0300

    nu

--- Core/ApiLogic.Tests/ConfigurationMocks/ApplicationConfigurationMocks.cs
+++ Core/ApiLogic.Tests/ConfigurationMocks/ApplicationConfigurationMocks.cs
@@ -1,7 +1,5 @@
 ﻿using ConfigurationManager;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ApiLogic.Tests.ConfigurationMocks
 {
@@ -13,16 +11,16 @@ namespace ApiLogic.Tests.ConfigurationMocks
 
             //if running from local env not on tests env
             //update to localhost
-            var isJenkins = System.Environment.GetEnvironmentVariable("IS_ON_JENKINS")?.ToLower() == "true";
+            var isJenkins = Environment.GetEnvironmentVariable("IS_ON_JENKINS")?.ToLower() == "true";
 
-            //9201 is the port of es2 on local tests for now
             if (isJenkins)
             {
-                SetActualValue(URL_V2, "http://elastic02:9201");
+                SetActualValue(URL_V2, "http://elastic02:9200");
                 SetActualValue(URL_V7_13, "http://elastic07:9200");
             }
             else
             {
+                //9201 is the port of es2 on local tests for now
                 SetActualValue(URL_V2, "http://localhost:9201");
                 SetActualValue(URL_V7_13, "http://localhost:9200");
             }
