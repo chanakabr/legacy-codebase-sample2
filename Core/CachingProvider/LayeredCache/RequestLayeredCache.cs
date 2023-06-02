af22f8b01d5445d3bf9666a0da12fd4e50d9f0f9
vstatkevich <Vladimir.Statkevich@kaltura.com>
2023-06-02 13:27:57 +0300

    Fix layered cache exceptions (#1980)
    Motivation:
    - NullReferenceException and InvalidOperationException (detailed stacktrace below)
    
    Modifications:
    - check for null
    - replace `HashSet` with `ConcurrentDictionary`
    
    ```
    Failed to get key db_procedures_routing from LayeredCache, layeredCacheConfigName ProceduresRouting, MethodName InitializeDbProceduresRouting and funcParameters No_Func_Parameters
    System.NullReferenceException: Object reference not set to an instance of an object.
       at System.Collections.Generic.HashSet`1.AddIfNotPresent(T value)
       at System.Collections.Generic.HashSet`1.UnionWith(IEnumerable`1 other)
       at CachingProvider.LayeredCache.LayeredCache.InsertResultsToCurrentRequest[T](Dictionary`2 results, Dictionary`2 invalidationKeysToKeys) in /src/Core/CachingProvider/LayeredCache/LayeredCache.cs:line 634
       at CachingProvider.LayeredCache.LayeredCache.Get[T](String key, T& genericParameter, Func`2 fillObjectMethod, Dictionary`2 funcParameters, Int32 groupId, String layeredCacheConfigName, List`1 inValidationKeys, Boolean shouldUseAutoNameTypeHandling) in /src/Core/CachingProvider/LayeredCache/LayeredCache.cs:line 160
    ```
    ```
    Failed to get key db_procedures_routing from LayeredCache, layeredCacheConfigName ProceduresRouting, MethodName InitializeDbProceduresRouting and funcParameters No_Func_Parameters
    System.InvalidOperationException: Operations that change non-concurrent collections must have exclusive access. A concurrent update was performed on this collection and corrupted its state. The collection's state is no longer correct.
       at System.Collections.Generic.HashSet`1.AddIfNotPresent(T value)
       at System.Collections.Generic.HashSet`1.UnionWith(IEnumerable`1 other)
       at CachingProvider.LayeredCache.LayeredCache.InsertResultsToCurrentRequest[T](Dictionary`2 results, Dictionary`2 invalidationKeysToKeys) in /src/Core/CachingProvider/LayeredCache/LayeredCache.cs:line 627
       at CachingProvider.LayeredCache.LayeredCache.Get[T](String key, T& genericParameter, Func`2 fillObjectMethod, Dictionary`2 funcParameters, Int32 groupId, String layeredCacheConfigName, List`1 inValidationKeys, Boolean shouldUseAutoNameTypeHandling) in /src/Core/CachingProvider/LayeredCache/LayeredCache.cs:line 163
    ```

--- Core/CachingProvider/LayeredCache/RequestLayeredCache.cs
+++ Core/CachingProvider/LayeredCache/RequestLayeredCache.cs
@@ -8,13 +8,13 @@ namespace CachingProvider.LayeredCache
     public class RequestLayeredCache 
     {
         public ConcurrentDictionary<string, object> cachedObjects;
-        public ConcurrentDictionary<string, HashSet<string>> invalidationKeysToKeys;
+        public ConcurrentDictionary<string, ConcurrentDictionary<string, byte>> invalidationKeysToKeys;
         public ConcurrentDictionary<string, long> invalidationKeysValues;
 
         public RequestLayeredCache()
         {
             cachedObjects = new ConcurrentDictionary<string, object>();
-            invalidationKeysToKeys = new ConcurrentDictionary<string, HashSet<string>>();
+            invalidationKeysToKeys = new ConcurrentDictionary<string, ConcurrentDictionary<string, byte>>();
             invalidationKeysValues = new ConcurrentDictionary<string, long>();
         }
     }
