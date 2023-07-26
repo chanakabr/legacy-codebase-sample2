90f3f622e9ed7a625512285c77a97d8aec53b3f2
vstatkevich <Vladimir.Statkevich@kaltura.com>
2023-07-26 12:23:07 +0300

    BEO-14258 Unify delete device by removal policy with usual delete (#2039)
    Motivation: rabbitmq/kafka messages should be sent when device is deleted
    Modifications:
    - extracted private method `RemoveDevice`
    - and used it in delete device by removal policy
    
    Also, functionality changed a bit:
    - set status=2(instead of 0) for `domains_devices` record
    -  `devices` table record is updated to status=2, active=2 (previously no updates)

--- Core/DAL/DomainDAL.cs
+++ Core/DAL/DomainDAL.cs
@@ -1937,19 +1937,6 @@ namespace DAL
             return devicesChange;
         }
 
-        public static int SetDevicesDomainUnActive(int domainID, List<int> devicesID, int? status)
-        {
-            List<string> devicesChange = new List<string>();
-            StoredProcedure sp = new StoredProcedure("SetDevicesDomainUnActive");
-            sp.SetConnectionKey("USERS_CONNECTION_STRING");
-            sp.AddParameter("@domainID", domainID);
-            sp.AddIDListParameter<int>("@devicesID", devicesID, "Id");
-            if (status.HasValue)
-                sp.AddParameter("@status", status.Value);
-            
-            return sp.ExecuteReturnValue<int>();
-        }
-        
         public static List<string> GetDevicesDomainByDowngradePolicy(int nDeviceToDelete, int domainId, List<int> devicesId, DowngradePolicy downgradePolicy)
         {
             List<string> devicesToBeModified = new List<string>();
